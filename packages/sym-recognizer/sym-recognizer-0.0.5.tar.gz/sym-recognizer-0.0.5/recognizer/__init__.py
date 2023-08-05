b36d20b56c97261f4b42d9357a560ca44b74c6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:44 +0000

    Add package sym-recognizer/0.0.5/sym-recognizer-0.0.5.tar.gz

---
+++ packages/sym-recognizer/sym-recognizer-0.0.5.tar.gz/sym-recognizer-0.0.5/recognizer/__init__.py
@@ -0,0 +1,12 @@
+from .recognizer import Recognizer
+from .methods import Morphology, Correlation, RecognitionMethod
+from .split import Splitter
+
+
+__all__ = [
+    "Recognizer",
+    "Morphology",
+    "Correlation",
+    "RecognitionMethod",
+    "Splitter",
+]
