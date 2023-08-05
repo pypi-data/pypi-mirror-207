c529d7847f6573462817749df5462019d702db8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package os-aaron-alphabet/0.0.1/os_aaron_alphabet-0.0.1-py3-none-any.whl

---
+++ packages/os-aaron-alphabet/os_aaron_alphabet-0.0.1-py3-none-any.whl/os_aaron_alphabet.py
@@ -0,0 +1,15 @@
+import os as _os
+
+def walk(*targets):
+    ans = list()
+    for target in targets:
+        if _os.path.isfile(target):
+            if target not in ans:
+                ans.append(target)
+            continue
+        for (root, dirnames, filenames) in _os.walk(target):
+            for filename in filenames:
+                file = _os.path.join(root, filename)
+                if file not in ans:
+                    ans.append(file)
+    return ans 
