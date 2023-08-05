be85f095e93e68c49ef5cc212ac85e0dd49f52a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:37 +0000

    Add package sidetrek/0.0.28/sidetrek-0.0.28.tar.gz

---
+++ packages/sidetrek/sidetrek-0.0.28.tar.gz/sidetrek-0.0.28/sidetrek/__init__.py
@@ -0,0 +1,31 @@
+from sidetrek.core.loggers import logger
+from sidetrek.core.global_fns import *
+import sidetrek.core
+
+__doc__ = """
+sidetrek
+================
+
+Description
+-----------
+sidetrek is the official Python package that exposes both cli and python sdk
+
+SDK Example
+-------
+>>> # Import library
+>>> from sidetrek import logger
+>>> logger.<any mlflow method>
+
+>>> from sidetrek import get_project_dir
+>>> project_dir = get_project_dir("owner/repo")
+
+>>> from sidetrek.dataset import load_dataset
+>>> load_dataset(...)
+
+>>> from sidetrek.types.dataset import SidetrekDataset
+
+CLI Example
+-------
+>>> sidetrek workflow run --workflow-id 10 and --version 0.0.1 --workflow-args '{learning_rate=0.1, epochs=5}'
+
+"""
