c13484636d1b79955ba5196d41e334cdca5cea6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:13 +0000

    Add package argparse-aaron-alphabet/0.0.2/argparse_aaron_alphabet-0.0.2-py3-none-any.whl

---
+++ packages/argparse-aaron-alphabet/argparse_aaron_alphabet-0.0.2-py3-none-any.whl/argparse_aaron_alphabet.py
@@ -0,0 +1,9 @@
+import argparse
+
+def parser(*args, **kwargs):
+    return argparse.ArgumentParser(
+        *args,
+        **kwargs,
+        fromfile_prefix_chars="@",
+        allow_abbrev=False,
+    )
