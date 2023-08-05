54724fed92b1b2329c88d901f4518f537b58f714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:49 +0000

    Add package PiML/0.5.0.post1/PiML-0.5.0.post1-cp37-none-win_amd64.whl

---
+++ packages/PiML/PiML-0.5.0.post1-cp37-none-win_amd64.whl/piml/models/reludnn/__init__.py
@@ -0,0 +1,6 @@
+from .aletheia import UnwrapperRegressor, UnwrapperClassifier
+from .reludnn import ReluDNNClassifier, ReluDNNRegressor
+
+
+__all__ = ['UnwrapperRegressor', 'UnwrapperClassifier',
+           'ReluDNNRegressor', 'ReluDNNClassifier']
---
+++ packages/PiML/PiML-0.5.0.post1-cp37-none-win_amd64.whl/piml/models/xgb2/__init__.py
@@ -0,0 +1,5 @@
+from .xgboost import XGB2Regressor, XGB2Classifier
+from .xgb_unwrapper import XGBUnwrapperRegressor, XGBUnwrapperClassifier
+
+__all__ = ["XGB2Regressor", "XGB2Classifier",
+           "XGBUnwrapperRegressor", "XGBUnwrapperClassifier"]
