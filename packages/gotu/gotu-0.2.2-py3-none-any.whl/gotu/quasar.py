a07a5f1a870f43bea5d91d47c7fbf969def025a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:02 +0000

    Add package gotu/0.2.2/gotu-0.2.2-py3-none-any.whl

---
+++ packages/gotu/gotu-0.2.2-py3-none-any.whl/gotu/quasar.py
@@ -0,0 +1,29 @@
+"""=======================================================================
+ What would light emerging at the event horizon of a quasar look like?
+=======================================================================
+
+For now that is the goal for this module, what should a quasar look like?
+
+With all fingers crossed for the `JWST`_, I think we are about to be
+surprised at how ubiquitous quasars are.
+
+Geometry
+========
+
+Oscillations, tidal ejection.  
+
+In a place where the passage of time is close to zero, but not actually, zero.
+
+
+JWST
+====
+
+The model for quasars outlined in `gotu`_ requires just two parameters.
+
+The central mass and the temperature and density of matter around the
+Eddington sphere of the quasar's black hole.
+
+There is a huge range of possible redshifts, depending on these two
+parameters.  
+
+"""
