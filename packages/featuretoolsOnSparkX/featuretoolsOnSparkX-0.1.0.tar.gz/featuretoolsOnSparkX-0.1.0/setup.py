f6a105f662ebfea214073724a876475d98c28878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:16 +0000

    Add package featuretoolsOnSparkX/0.1.0/featuretoolsOnSparkX-0.1.0.tar.gz

---
+++ packages/featuretoolsOnSparkX/featuretoolsOnSparkX-0.1.0.tar.gz/featuretoolsOnSparkX-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+dirname = path.abspath(path.dirname(__file__))
+with open(path.join(dirname, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='featuretoolsOnSparkX',
+    version='0.1.0',
+    packages=find_packages(),
+    description='a bugfix version for raw repo',
+    license='MIT',
+    url='https://github.com/tzbo/featuretoolsOnSpark',
+    author='giantcroc',
+    author_email='ringer0910@gmail.com',
+    classifiers=[
+         'Development Status :: 2 - Pre-Alpha',
+         'Intended Audience :: Developers',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.5',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=open('requirements.txt').readlines(),
+    python_requires='>=2.7, <4',
+    keywords='feature engineering data science machine learning',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
