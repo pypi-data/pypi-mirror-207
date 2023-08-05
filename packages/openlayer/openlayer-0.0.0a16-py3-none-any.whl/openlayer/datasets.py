24dcf755239a14913d62d8f7b9be65e5a01c2ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:03 +0000

    Add package openlayer/0.0.0a16/openlayer-0.0.0a16-py3-none-any.whl

---
+++ packages/openlayer/openlayer-0.0.0a16-py3-none-any.whl/openlayer/datasets.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+
+class DatasetType(Enum):
+    """The different dataset types that are supported by Openlayer.
+
+    Used by the ``dataset_type`` argument of the :meth:`openlayer.OpenlayerClient.add_dataset` and
+    :meth:`openlayer.OpenlayerClient.add_dataframe` methods."""
+
+    #: For validation sets.
+    Validation = "validation"
+    #: For training sets.
+    Training = "training"
+
+
+class Dataset:
+    """An object containing information about a dataset on the Openlayer platform."""
+
+    def __init__(self, json):
+        self._json = json
+        self.id = json["id"]
+
+    def __getattr__(self, name):
+        if name in self._json:
+            return self._json[name]
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute {name}")
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __str__(self):
+        return f"Dataset(id={self.id})"
+
+    def __repr__(self):
+        return f"Dataset({self._json})"
+
+    def to_dict(self):
+        """Returns object properties as a dict.
+
+        Returns
+        -------
+        Dict with object properties.
+        """
+        return self._json
