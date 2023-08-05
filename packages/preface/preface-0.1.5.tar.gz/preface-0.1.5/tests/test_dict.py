f04caa7f0441e79662babd2cf1bfa7f560f007e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:24 +0000

    Add package preface/0.1.5/preface-0.1.5.tar.gz

---
+++ packages/preface/preface-0.1.5.tar.gz/preface-0.1.5/tests/test_dict.py
@@ -0,0 +1,61 @@
+import preface
+
+
+def test_get_field_without_nested() -> None:
+    key = "a"
+    value = 1
+    dct = {key: value}
+
+    assert preface.dict.get(dct, key) == value
+
+
+def test_get_field_nested() -> None:
+    value = 1
+    dct = {"a": {"b": value}}
+
+    assert preface.dict.get(dct, "a.b") == value
+
+
+def test_set_dict_without_nested_dict() -> None:
+    dct = {}
+
+    key = "a.b.c"
+
+    value = 1
+
+    expected = {"a": {"b": {"c": 1}}}
+    preface.dict.set(dct, key, value)
+
+    assert dct == expected
+
+
+def test_merge_dict_without_nested() -> None:
+    a = {"a": 1}
+    b = {"b": 2}
+
+    expected = {"a": 1, "b": 2}
+    actual = preface.dict.merge(a, b)
+
+    assert actual == expected
+
+
+def test_merge_dict_with_nested():
+    a = {"a": {"b": 1}}
+    b = {"b": 2, "a": {"c": 3}}
+
+    expected = {"a": {"b": 1, "c": 3}, "b": 2}
+    actual = preface.dict.merge(a, b)
+
+    assert actual == expected
+
+
+def test_pop_dict_with_nested_dict() -> None:
+    dct = {"a": {"b": 1}}
+
+    expected_value = 1
+    expected_dct = {"a": {}}
+
+    actual_value = preface.dict.pop(dct, "a.b")
+
+    assert actual_value == expected_value
+    assert dct == expected_dct
---
+++ packages/preface/preface-0.1.5.tar.gz/preface-0.1.5/tests/test_preface.py
@@ -0,0 +1,52 @@
+import hypothesis
+from hypothesis import strategies
+
+from preface import argsort, flattened, get, grouped, indexed
+
+integers = strategies.integers()
+
+
+@hypothesis.given(strategies.sets(strategies.integers(), min_size=1))
+def test_get(s):
+    assert get(s) in s
+
+
+@hypothesis.given(strategies.lists(strategies.integers()))
+def test_argsort(lst):
+    assert list(sorted(lst)) == [lst[i] for i in argsort(lst)]
+
+
+@hypothesis.given(strategies.lists(strategies.lists(strategies.integers())))
+def test_flattened(seq):
+    flat = flattened(seq)
+    assert len(flat) == sum(len(subseq) for subseq in seq)
+    for subseq in seq:
+        for item in subseq:
+            assert item in flat
+
+
+@hypothesis.given(
+    strategies.lists(strategies.integers(min_value=-100, max_value=100)),
+    strategies.integers(min_value=1, max_value=5),
+)
+def test_grouped(seq, size):
+    for window in grouped(seq, size=size):
+        assert len(window) == size
+
+
+@hypothesis.strategies.composite
+def list_and_indices(draw, elements=integers):
+    lst = draw(strategies.lists(elements, min_size=1))
+    indices = draw(
+        strategies.lists(
+            strategies.integers(min_value=0, max_value=len(lst) - 1), min_size=1
+        )
+    )
+    return (lst, indices)
+
+
+@hypothesis.given(list_and_indices())
+def test_indexed(args):
+    seq, indices = args
+    for elem in indexed(seq, indices):
+        assert elem in seq
