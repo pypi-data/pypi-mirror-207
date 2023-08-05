2d6b5a5aea80536ea9aeb2fcd2ef182e07b69c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:10 +0000

    Add package typed-stream/0.32.2/typed_stream-0.32.2.tar.gz

---
+++ packages/typed-stream/typed_stream-0.32.2.tar.gz/typed_stream-0.32.2/typed_stream/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This script contains the version of this package.
+
+Update this to create a new release.
+To get the current version run this script.
+"""
+
+VERSION = "0.32.2"
+
+if __name__ == "__main__":  # pragma: no cover
+    print(VERSION)
