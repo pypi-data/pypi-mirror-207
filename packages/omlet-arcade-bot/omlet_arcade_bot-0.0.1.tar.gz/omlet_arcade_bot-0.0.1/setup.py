b6aa9d5be538e71267c861649487482747fea9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:05 +0000

    Add package omlet-arcade-bot/0.0.1/omlet_arcade_bot-0.0.1.tar.gz

---
+++ packages/omlet-arcade-bot/omlet_arcade_bot-0.0.1.tar.gz/omlet_arcade_bot-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='omlet_arcade_bot',
+    version='0.0.1',
+    description='Модуль использующий, апи Omlet Arcade с 2-мя классами Auth,и Bot.',
+    long_description=open('README.md').read(),     long_description_content_type='text/markdown',
+    py_modules=['omlet_arcade_app'],
+)
+
