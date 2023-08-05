36401ed5a8b24f48e6092fbf66a1183f065029e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:56 +0000

    Add package portman/1.0.0/portman-1.0.0.tar.gz

---
+++ packages/portman/portman-1.0.0.tar.gz/portman-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='portman',
+    version='1.0.0',
+    author='Ayoub Almontaser',
+    author_email='',
+    description="""A for recording the screen during tests.""",
+    packages=find_packages(),
+    install_requires=[
+        'socket',
+    ],
+)
+
