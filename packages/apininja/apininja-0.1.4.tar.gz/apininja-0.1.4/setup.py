c784e949daf4affd981f16d6c728fcb9e0d839fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:38 +0000

    Add package apininja/0.1.4/apininja-0.1.4.tar.gz

---
+++ packages/apininja/apininja-0.1.4.tar.gz/apininja-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='apininja',
+    version='0.1.4',
+    author='Alex Academia',
+    author_email='alexius.sayco.academia@gmail.com',
+    description='An api module generator.',
+    long_description='An api module generator.',
+    url='https://github.com/alexiusacademia/apininja',
+    py_modules=['apininja'],
+    entry_points={
+        'console_scripts': [
+            'apininja = apininja:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='api generator'
+)
