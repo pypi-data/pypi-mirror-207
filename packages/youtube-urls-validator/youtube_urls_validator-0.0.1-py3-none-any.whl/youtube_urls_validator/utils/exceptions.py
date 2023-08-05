2f2c1761c72adcd8e619ed6bbf41b96a24e28c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:32 +0000

    Add package youtube-urls-validator/0.0.1/youtube_urls_validator-0.0.1-py3-none-any.whl

---
+++ packages/youtube-urls-validator/youtube_urls_validator-0.0.1-py3-none-any.whl/youtube_urls_validator/utils/exceptions.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class BaseValidationError(Exception):
+    pass
+
+
+class HostNotInPossibleHostsError(BaseValidationError):
+    def __init__(self, url: str, possible_hosts: List[str]):
+        self.url = url
+        self.possible_hosts = possible_hosts
+
+    def __str__(self):
+        return f"URL's ({self.url}) host not in possible hosts: {', '.join(self._get_possible_hosts_in_quotes())}"
+
+    def _get_possible_hosts_in_quotes(self) -> List[str]:
+        return [f'"{prefix}"' for prefix in self.possible_hosts]
+
+
+class InvalidSchemeError(BaseValidationError):
+    def __init__(self, url: str):
+        self.url = url
+
+    def __str__(self):
+        return f'Invalid scheme for URL {self.url}'
