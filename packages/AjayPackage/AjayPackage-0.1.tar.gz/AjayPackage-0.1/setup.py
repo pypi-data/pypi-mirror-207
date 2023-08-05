d1e614919bdc4966a46e4b25ab8ae1eae9d099be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:57 +0000

    Add package AjayPackage/0.1/AjayPackage-0.1.tar.gz

---
+++ packages/AjayPackage/AjayPackage-0.1.tar.gz/AjayPackage-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='AjayPackage',
+version='0.1',
+description='Testing installation of Package',
+url='',
+author='Ajay Patil',
+author_email='ajaypatil.nmu@gmail.com',
+license='MIT',
+packages=['AjayPackage'],
+zip_safe=False)
