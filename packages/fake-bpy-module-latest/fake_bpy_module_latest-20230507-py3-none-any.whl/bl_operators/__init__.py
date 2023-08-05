aa171f53fabe533402247092737da072401e7d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:41 +0000

    Add package fake-bpy-module-latest/20230507/fake_bpy_module_latest-20230507-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20230507-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import typing
+from . import vertexpaint_dirt
+from . import clip
+from . import constraint
+from . import image
+from . import sequencer
+from . import freestyle
+from . import geometry_nodes
+from . import wm
+from . import anim
+from . import object_quick_effects
+from . import uvcalc_transform
+from . import bmesh
+from . import add_mesh_torus
+from . import object_align
+from . import view3d
+from . import object
+from . import mesh
+from . import screen_play_rendered_anim
+from . import spreadsheet
+from . import assets
+from . import uvcalc_follow_active
+from . import object_randomize_transform
+from . import userpref
+from . import presets
+from . import uvcalc_lightmap
+from . import rigidbody
+from . import console
+from . import node
+from . import file
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20230507-py3-none-any.whl/gpu/__init__.py
@@ -0,0 +1,12 @@
+import sys
+import typing
+from . import texture
+from . import shader
+from . import state
+from . import matrix
+from . import types
+from . import select
+from . import capabilities
+from . import platform
+
+GenericType = typing.TypeVar("GenericType")
