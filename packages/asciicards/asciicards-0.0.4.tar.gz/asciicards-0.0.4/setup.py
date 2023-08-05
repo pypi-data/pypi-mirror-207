e89170b2a2766cb235ecf857abc1e64a117c2286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:19 +0000

    Add package asciicards/0.0.4/asciicards-0.0.4.tar.gz

---
+++ packages/asciicards/asciicards-0.0.4.tar.gz/asciicards-0.0.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+long_desc = open("README.md").read()
+setuptools.setup(
+    name="asciicards",
+    version="0.0.4", # eg:1.0.0
+    author="PaperJam",
+    author_email="archood2@gmail.com",
+    license="GNU General Public License v3.0",
+    description="Ascii Card Library",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArchooD2/asciicards",
+    packages = [],
+    py_modules=['asciicards'],
+    # project_urls is optional
+    python_requires=">=3.6",
+)
