69a204cf94b53a979756b2cc2d70ae8260b74a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:36 +0000

    Add package terminut/0.0.0.3/terminut-0.0.0.3.tar.gz

---
+++ packages/terminut/terminut-0.0.0.3.tar.gz/terminut-0.0.0.3/terminut/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'terminut'
+__author__ = 'vast#1337'
+__version__ = '0.0.1a'
+
+CURRENT_VERSION = '0.0.2'
+
+
+if __version__ < CURRENT_VERSION:
+  print("Version Out-of-Date. Please upgrade by using: \"python.exe -m pip install -U terminut\"")
