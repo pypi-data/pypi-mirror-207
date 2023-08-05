6c69960858b08e7f7da0a4ff330bd70454f13b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:18 +0000

    Add package cercis/0.1.3/cercis-0.1.3-py3-none-any.whl

---
+++ packages/cercis/cercis-0.1.3-py3-none-any.whl/cercis/const.py
@@ -0,0 +1,9 @@
+DEFAULT_LINE_LENGTH = 79
+DEFAULT_EXCLUDES = r"/(\.direnv|\.eggs|\.git|\.hg|\.mypy_cache|\.nox|\.tox|\.venv|venv|\.svn|\.ipynb_checkpoints|_build|buck-out|build|dist|__pypackages__)/"  # noqa: B950
+DEFAULT_INCLUDES = r"(\.pyi?|\.ipynb)$"
+STDIN_PLACEHOLDER = "__BLACK_STDIN_FILENAME__"
+DEFAULT_FUNCTION_DEFINITION_EXTRA_INDENT = True
+DEFAULT_SINGLE_QUOTE = True
+DEFAULT_WRAP_LINE_WITH_LONG_STRING = False
+DEFAULT_COLLAPSE_NESTED_BRACKETS = True
+DEFAULT_WRAP_PRAGMA_COMMENTS = False
