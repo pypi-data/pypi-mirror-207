d33b67ee89ecaed66231d673cc07dba7876d9acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:38 +0000

    Add package nyckel/0.0.9/nyckel-0.0.9.tar.gz

---
+++ packages/nyckel/nyckel-0.0.9.tar.gz/nyckel-0.0.9/src/nyckel/__init__.py
@@ -0,0 +1,6 @@
+from .functions.text_classification_function import (
+    TextClassificationFunction,  # noqa: F401
+    TextClassificationSample,  # noqa: F401
+    ClassificationPrediction,  # noqa: F401
+)
+from .auth import OAuth2Renewer  # noqa: F401
