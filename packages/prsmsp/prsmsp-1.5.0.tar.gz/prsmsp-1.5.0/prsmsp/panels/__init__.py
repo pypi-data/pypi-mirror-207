d3f6ddff3597d5e149f33bfda0b1a4012a5e5edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:57 +0000

    Add package prsmsp/1.5.0/prsmsp-1.5.0.tar.gz

---
+++ packages/prsmsp/prsmsp-1.5.0.tar.gz/prsmsp-1.5.0/prsmsp/panels/__init__.py
@@ -0,0 +1,21 @@
+from .kavenegar import Kavenegar
+from .melipayamak import MeliPayamak
+from .smsdotir import SmsDotIr
+from .webonesms import WebOneSms
+from .mediana import Mediana
+from .ghasedaksms import GhasedakSms
+from .farazsms import FarazSms
+from .niksms import NikSms
+from .smsone import SmsOne
+
+__all__ = [
+    "Kavenegar",
+    "SmsDotIr",
+    "WebOneSms",
+    "MeliPayamak",
+    "Mediana",
+    "GhasedakSms",
+    "FarazSms",
+    "NikSms",
+    "SmsOne",
+]
