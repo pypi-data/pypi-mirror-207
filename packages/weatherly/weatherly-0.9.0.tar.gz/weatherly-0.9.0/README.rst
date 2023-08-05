cc260da4d4f5ded90ab47ed67979b75bb6e6a628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:51 +0000

    Add package weatherly/0.9.0/weatherly-0.9.0.tar.gz

---
+++ packages/weatherly/weatherly-0.9.0.tar.gz/weatherly-0.9.0/README.rst
@@ -0,0 +1,65 @@
+⛅ weatherly
+====================
+.. image:: https://img.shields.io/github/license/konradsic/weatherly?color=blue&style=for-the-badge
+    :target: https://github.com/konradsic/weatherly/blob/master/LICENSE
+    :alt: Weatherly license
+.. image:: https://img.shields.io/pypi/v/weatherly?color=blue&style=for-the-badge
+    :target: https://pypi.python.org/project/weatherly
+    :alt: Weatherly version on PyPI
+.. image:: https://img.shields.io/pypi/pyversions/weatherly?color=blue&style=for-the-badge
+    :target: https://pypi.python.org/project/weatherly
+    :alt: Supported Python versions
+.. image:: https://img.shields.io/pypi/status/weatherly?style=for-the-badge
+    :target: https://pypi.python.org/project/weatherly
+    :alt: Project status
+.. image:: https://img.shields.io/github/actions/workflow/status/konradsic/weatherly/build.yml?style=for-the-badge
+    :target: https://github.com/konradsic/weatherly
+    :alt: Build status
+
+Weatherly is a simple package that retrieves weather data from WeatherAPI.com. It provides an easy to use interface to access current and historical weather data for a specific location.
+
+Features
+---------------
+* Easy to use,
+* Intuitive design,
+* Can provide current weather data aswell as forecast, historical data, future predictions and even more!
+* Modern and typed Python package,
+* Support for languages (provided by WeatherAPI)
+
+Installing
+------------
+To install weatherly on your computer Python 3.10 or higher is required. If your python version meets the requirements run:
+
+.. code:: shell
+    
+    # Windows
+    py -3 -m pip install -U weatherly
+
+    # MacOS / Linux
+    python3 -m pip install -U weatherly
+
+Congratulations! Now weatherly is ready to use on your machine.
+
+Code example
+---------------------
+
+.. code:: python
+
+    import weatherly
+    
+    client = weatherly.Client(api_key="your WeatherAPI key")
+    # you can set language to all requests, or pass it manually
+    client.set_language("fr")     # lang code
+    client.set_language("German") # language full name
+
+    # getting weather info
+    current_weather = client.get_current_weather(query="London")
+
+    # getting forecast info
+    forecast = client.get_forecast_data(query="Paris", days=3)
+
+    # historical data
+    history = client.get_historical_data(query="48.8567,2.3508", date="2010-01-01") # query could also be latitude,longitude
+
+    # marine data
+    marine = client.get_marine_data(query="Madrid")
---
+++ packages/weatherly/weatherly-0.9.0.tar.gz/weatherly-0.9.0/weatherly/utils.py
@@ -0,0 +1,97 @@
+"""
+MIT License
+
+Copyright (c) 2023 Konrad (@konradsic)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from typing import (
+    Dict,
+    Any,
+    overload
+)
+from .enums import Languages
+
+gray = "#535353"
+white = "#ffffff"
+
+def parse_kwargs_to_urlargs(kwargs: Dict[str, Any]) -> str:
+    """
+    Parses keyword arguments (kwargs) to format fitting URLs
+    Example: ``{"some_key": "some value", "yes": "no"}`` -> ``?some_key=some_value&yes=no``
+    
+    Parameters
+    ----------
+    kwargs: Dict[:class:`str`, Any]
+        A dictionary of keyword arguments to be parsed.
+        
+    Returns
+    -------
+    args_string: :class:`str`
+        A string - formatted keyword arguments
+    """
+    if not kwargs: return ""
+    
+    args_string = ""
+    first = True
+    
+    for k,v in kwargs.items():
+        args_string += f"{'?' if first else '&'}{k}={v}"
+        first = False
+    return args_string
+
+@overload
+def find_language(lang: str, asobj: bool=False) -> None:
+    ...
+    
+@overload
+def find_language(lang: str, asobj: bool=False) -> Languages:
+    ...
+
+def find_language(lang: str, asobj: bool=False) -> str:
+    """
+    Used to find language code from available languages.
+
+    Paremeters
+    ----------
+    lang: :class:`str`
+        A language you want to search for
+
+    Returns
+    -------
+    Union[:class:`str`, :class:`Languages`, None]
+        Language code, could be ``None``, could be a :class:`Languages` enum.
+    """
+
+    if isinstance(lang, Languages):
+        try:
+            if asobj: return lang
+            return lang.value
+        except: 
+            return None
+
+    lang = lang.lower()
+
+    for language in Languages:
+        if language.name.lower() == lang or language.value == lang:
+            if asobj: return language
+            return language.value
+        
+    return None
