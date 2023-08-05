5925ae60a80185ba2b02b8f1d1e47a01ec12d09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:17 +0000

    Add package bibverse/1.0/bibverse-1.0-py3-none-any.whl

---
+++ packages/bibverse/bibverse-1.0-py3-none-any.whl/verse/verse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+from bs4 import BeautifulSoup
+
+
+text = requests.get("https://www.verseoftheday.com/").text
+soup = BeautifulSoup(text, "html.parser")
+result = soup.find_all("div", "bilingual-left")[0].get_text()
+print(result)
