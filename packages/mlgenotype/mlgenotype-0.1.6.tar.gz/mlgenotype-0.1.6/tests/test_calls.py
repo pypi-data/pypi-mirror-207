0939f25f016ae56a80a00ab62044b59bfc2c7d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:21 +0000

    Add package mlgenotype/0.1.6/mlgenotype-0.1.6.tar.gz

---
+++ packages/mlgenotype/mlgenotype-0.1.6.tar.gz/mlgenotype-0.1.6/tests/test_calls.py
@@ -0,0 +1,7 @@
+import pytest
+import subprocess
+import sys
+from mlgenotype.src.mlgenotype import read_data_file, separate_test_data, rfgrid, best_rf_model_and_score
+
+def test_predict():
+    assert 1 == 1
