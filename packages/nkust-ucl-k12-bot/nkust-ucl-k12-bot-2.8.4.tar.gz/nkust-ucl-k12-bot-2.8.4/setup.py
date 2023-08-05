06df362ffe7c1021e59c312611a212cc765ec319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:03 +0000

    Add package nkust-ucl-k12-bot/2.8.4/nkust-ucl-k12-bot-2.8.4.tar.gz

---
+++ packages/nkust-ucl-k12-bot/nkust-ucl-k12-bot-2.8.4.tar.gz/nkust-ucl-k12-bot-2.8.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nkust-ucl-k12-bot",
+    version="2.8.4",
+    author="Ethan Cheng",
+    author_email="asdewq45445@gmail.com",
+    description="一個用於k12的bot包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinbow99/k12-telegram",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
