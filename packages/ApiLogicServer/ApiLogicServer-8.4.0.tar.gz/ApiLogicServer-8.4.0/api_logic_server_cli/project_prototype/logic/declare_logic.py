83dc2368db4e2e347dcfcdf937c77b6a2608ed1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package ApiLogicServer/8.4.0/ApiLogicServer-8.4.0.tar.gz

---
+++ packages/ApiLogicServer/ApiLogicServer-8.4.0.tar.gz/ApiLogicServer-8.4.0/api_logic_server_cli/project_prototype/logic/declare_logic.py
@@ -0,0 +1,21 @@
+import datetime
+from decimal import Decimal
+from logic_bank.exec_row_logic.logic_row import LogicRow
+from logic_bank.extensions.rule_extensions import RuleExtension
+from logic_bank.logic_bank import Rule
+from database import models
+import logging
+
+app_logger = logging.getLogger(__name__)
+
+def declare_logic():
+    ''' Declarative multi-table derivations and constraints, extensible with Python. 
+
+    Brief background: see readme_declare_logic.md
+    
+    Use code completion (Rule.) to declare rules here:
+    '''
+
+
+    app_logger.debug("..logic/declare_logic.py (logic == rules + code)")
+
