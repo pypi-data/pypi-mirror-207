a399f74eeefb6581f061e73b84b48601a1fc260e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package ukmon-meteortools/2023.5.8/ukmon_meteortools-2023.5.8-py3-none-any.whl

---
+++ packages/ukmon-meteortools/ukmon_meteortools-2023.5.8-py3-none-any.whl/ukmon_meteortools/rmsutils/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018-2023 Mark McIntyre
+# flake8: noqa
+
+"""
+The functions in this module require RMS and/or WMPL to be in the PythonPath.  
+
+Require RMS, and must be run from the source/RMS folder:  
+    multiDayRadiant  
+    multiTrackStack  
+    analyseUFOwithRMS  
+
+Require WMPL:  
+    multiEventGroundMap  
+    plotCAMSOrbits  
+    plotRMSOrbits  
+    getTrajPickle  
+
+"""
+
+from .multiDayRadiant import multiDayRadiant
+from .multiTrackStack import multiTrackStack
+from .analyseUFOwithRMS import analyseUFOwithRMS
+from .multiEventGroundMap import multiEventGroundMap
+from .plotCAMSOrbits import plotCAMSOrbits
+from .plotRMSOrbits import plotRMSOrbits
+from .trajPickle import getTrajPickle, trajectoryKML
