0efc73cd705292c91408b57692a9d06bea3dea03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:08 +0000

    Add package rcmpy/1.3.0/rcmpy-1.3.0.tar.gz

---
+++ packages/rcmpy/rcmpy-1.3.0.tar.gz/rcmpy-1.3.0/rcmpy/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=df9ec3d9eec74bca1749625545b15667
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A configuration-file management system."
+PKG_NAME = "rcmpy"
+VERSION = "1.3.0"
