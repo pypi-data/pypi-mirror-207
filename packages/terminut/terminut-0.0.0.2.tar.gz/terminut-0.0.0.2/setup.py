2f949b25b46d0b6a30c4be42d0cac0131552e8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package terminut/0.0.0.2/terminut-0.0.0.2.tar.gz

---
+++ packages/terminut/terminut-0.0.0.2.tar.gz/terminut-0.0.0.2/setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+projname = "terminut"
+
+setup(name=f"{projname}",
+      version="0.0.0.2",
+      description="Terminut // vast#1337",
+      packages=find_packages(exclude=['tests']),
+      author="vast#1337",
+      url=f"http://pypi.python.org/pypi/({projname})",
+      author_email="vastcord@proton.me",
+      license="MIT",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Topic :: Scientific/Engineering",
+          "Topic :: Scientific/Engineering :: Information Analysis",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "Topic :: Scientific/Engineering :: Visualization",
+          "Topic :: Software Development :: Libraries",
+          "Topic :: Utilities",
+      ],
+
+      python_requires="~=3.7",
+
+      #install_requires=[
+      #    "config4py>=0.1.0"
+      #]
+)
