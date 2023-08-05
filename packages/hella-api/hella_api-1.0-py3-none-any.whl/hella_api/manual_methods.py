1e6ad7b616f24f1a8bd246cc7d6054954733a918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:57 +0000

    Add package hella-api/1.0/hella_api-1.0-py3-none-any.whl

---
+++ packages/hella-api/hella_api-1.0-py3-none-any.whl/hella_api/manual_methods.py
@@ -0,0 +1,24 @@
+from .hella_api import HellaApi
+
+class Handler():
+	
+	def __init__(self, token: str):
+		self.token = token
+		
+	def getStickers(self, **kwargs):
+		getStickers = HellaApi(self.token).methods(method='getStickers', **kwargs)
+		return getStickers
+	
+	def getGroups(self, **kwargs):
+		getGroups = HellaApi(self.token).methods(method='getGroups', **kwargs)
+		return getGroups
+		
+	def GenerationTTS(self, **kwargs):
+		GenerationTTS = HellaApi(self.token).methods(method='GenerationTTS', **kwargs)
+		return GenerationTTS
+	
+	def GenerationQuotes(self, **keargs):
+		GenerationQuotes = HellaApi(self.token).methods(method='GenerationQuotes', **kwargs)
+		return GenerationQuotes
+	
+		
