c4f9ada895a06adbebd9ac79d230b9f16a35f5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package cnki-html2json/0.1.6/cnki_html2json-0.1.6.tar.gz

---
+++ packages/cnki-html2json/cnki_html2json-0.1.6.tar.gz/cnki_html2json-0.1.6/tests/test_html2json.py
@@ -0,0 +1,29 @@
+from cnki_html2json import html2json
+
+def test_fulltext():
+    with open('tests/fulltext.html','r') as f:
+        content = f.read()
+
+    text_structure = html2json.ExtractContent(content).extract('structure')
+    assert text_structure['body_text']['1 引言']['text'][:14] == '近年来，以Chat GPT(' # type: ignore
+    assert text_structure['body_text']['1 引言']['reference_index'][0]==1 # type: ignore
+
+    text_plain = html2json.ExtractContent(content).extract('plain')
+    assert text_plain['body_text'][:14] == '近年来，以Chat GPT(' # type: ignore
+
+    text_raw = html2json.ExtractContent(content).extract('raw')
+    assert '[1]' in text_raw['body_text']['1 引言']['text'] # type: ignore
+
+def test_fulltext_crawl():
+    with open('tests/fulltext_crawl.html','r') as f:
+        content = f.read()
+
+    text_structure = html2json.ExtractContent(content).extract('structure')
+    assert text_structure['body_text']['1 引言']['text'][:14] == '近年来，以Chat GPT(' # type: ignore
+    assert text_structure['body_text']['1 引言']['reference_index'][0]==1 # type: ignore
+
+    text_plain = html2json.ExtractContent(content).extract('plain')
+    assert text_plain['body_text'][:14] == '近年来，以Chat GPT(' # type: ignore
+
+    text_raw = html2json.ExtractContent(content).extract('raw')
+    assert '[1]' in text_raw['body_text']['1 引言']['text'] # type: ignore
