79e9286125f80fa7a6dbf485cfc2272d62093569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:04 +0000

    Add package frontengine-dev/0.0.7/frontengine_dev-0.0.7.tar.gz

---
+++ packages/frontengine-dev/frontengine_dev-0.0.7.tar.gz/frontengine_dev-0.0.7/frontengine/show/text/draw_text.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+
+from PySide6.QtCore import Qt, QRect
+from PySide6.QtGui import QPainter, QFontDatabase
+from PySide6.QtWidgets import QWidget
+
+
+class TextWidget(QWidget):
+
+    def __init__(self, text: str, font_size: int = 100, opacity: float = 0.2):
+        super().__init__()
+        self.setWindowFlag(
+            Qt.WindowType.WindowTransparentForInput |
+            Qt.WindowType.FramelessWindowHint |
+            Qt.WindowType.WindowStaysOnTopHint
+        )
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.text = text
+        self.font_size = font_size
+        self.opacity = opacity
+        self.draw_font = QFontDatabase.font(self.font().family(), "", self.font_size)
+        self.setWindowTitle("Text")
+        # Set Icon
+        self.icon_path = Path(os.getcwd() + "/je_driver_icon.ico")
+        self.icon = QIcon(str(self.icon_path))
+        if self.icon.isNull() is False:
+            self.setWindowIcon(self.icon)
+            self.system_icon = QSystemTrayIcon()
+            self.system_icon.setIcon(self.icon)
+
+    def paintEvent(self, event) -> None:
+        painter = QPainter(self)
+        painter.setFont(
+            self.draw_font
+        )
+        painter.setPen(Qt.GlobalColor.black)
+        painter.setOpacity(self.opacity)
+        painter.drawText(
+            QRect(self.x(), self.y(), self.width(), self.height()),
+            Qt.AlignmentFlag.AlignCenter,
+            self.text
+        )
+        painter.restore()
