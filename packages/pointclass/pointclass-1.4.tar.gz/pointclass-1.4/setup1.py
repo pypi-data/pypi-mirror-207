20daa6f9bc2b7d5fc699f1967d6e9ff492b6be61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:29 +0000

    Add package pointclass/1.4/pointclass-1.4.tar.gz

---
+++ packages/pointclass/pointclass-1.4.tar.gz/pointclass-1.4/setup1.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(name="pointclass",version="1.4",description="this is mathematics perps lybrary",
+      long_description="point class",
+      author="yshirsath466@gmail.com",
+      packages=['pointclass'],install_requires=[])
