99296341584a9e0fde94cd1985742585d12a895a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:44 +0000

    Add package tsmetric/0.1.2/tsmetric-0.1.2.tar.gz

---
+++ packages/tsmetric/tsmetric-0.1.2.tar.gz/tsmetric-0.1.2/tsmetric/length.py
@@ -0,0 +1,8 @@
+from rich import print
+def m2f(m) :
+	return m*3.2808
+def f2m(f) :
+	return f/3.2808
+
+def blueprint(mess):
+        print('this is a [blue] blue[/blue] display')
---
+++ packages/tsmetric/tsmetric-0.1.2.tar.gz/tsmetric-0.1.2/tsmetric/temp.py
@@ -0,0 +1,4 @@
+def f2c(f):
+	return(f*9/5+32)
+def c2f(c):
+	return(c-32)*5/9
