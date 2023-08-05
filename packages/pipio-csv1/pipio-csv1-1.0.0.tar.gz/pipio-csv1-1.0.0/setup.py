88534e225685e5c7e54e71889df809ce41113fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:43 +0000

    Add package pipio-csv1/1.0.0/pipio-csv1-1.0.0.tar.gz

---
+++ packages/pipio-csv1/pipio-csv1-1.0.0.tar.gz/pipio-csv1-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pipio-csv1',
+    version='1.0.0',
+    packages=['pipio-csv1'],
+    install_requires=[],
+    url='https://github.com/majajanito/pipio-csv1',
+    license='MIT',
+    author='Majajanieto',
+    author_email='majajanitor@blacked.gay',
+    description='A + pipio-csv1 package.'
+)
