29851ccb77f2664d1dd1608fa207735acd8613c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:05 +0000

    Add package gvsbuild/2023.5.0/gvsbuild-2023.5.0.tar.gz

---
+++ packages/gvsbuild/gvsbuild-2023.5.0.tar.gz/gvsbuild-2023.5.0/gvsbuild/projects/adwaita_icon_theme.py
@@ -0,0 +1,57 @@
+#  Copyright (C) 2016 - Yevgen Muntyan
+#  Copyright (C) 2016 - Ignacio Casal Quinteiro
+#  Copyright (C) 2016 - Arnavion
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from gvsbuild.utils.base_expanders import Tarball
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class AdwaitaIconTheme(Tarball, Project):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "adwaita-icon-theme",
+            version="43",
+            repository="https://gitlab.gnome.org/GNOME/adwaita-icon-theme",
+            archive_url="https://download.gnome.org/sources/adwaita-icon-theme/{version}/adwaita-icon-theme-{version}.tar.xz",
+            hash="2e3ac77d32a6aa5554155df37e8f0a0dd54fc5a65fd721e88d505f970da32ec6",
+            dependencies=[
+                "librsvg",
+            ],
+        )
+
+    def build(self):
+        # Create the destination dir, before the build
+        os.makedirs(
+            os.path.join(self.builder.gtk_dir, "share", "icons", "Adwaita"),
+            exist_ok=True,
+        )
+
+        self.push_location(r".\win32")
+        self.exec_vs(
+            r'nmake /nologo /f adwaita-msvc.mak CFG=%(configuration)s PYTHON="%(python_dir)s\python.exe" PREFIX="%(gtk_dir)s"',
+            add_path=os.path.join(self.builder.opts.msys_dir, "usr", "bin"),
+        )
+        self.exec_vs(
+            r'nmake /nologo /f adwaita-msvc.mak install CFG=%(configuration)s PYTHON="%(python_dir)s\python.exe" PREFIX="%(gtk_dir)s"',
+            add_path=os.path.join(self.builder.opts.msys_dir, "usr", "bin"),
+        )
+        self.pop_location()
+
+        self.install(r".\COPYING_CCBYSA3 share\doc\adwaita-icon-theme")
---
+++ packages/gvsbuild/gvsbuild-2023.5.0.tar.gz/gvsbuild-2023.5.0/gvsbuild/projects/portaudio.py
@@ -0,0 +1,44 @@
+#  Copyright (C) 2016 - Yevgen Muntyan
+#  Copyright (C) 2016 - Ignacio Casal Quinteiro
+#  Copyright (C) 2016 - Arnavion
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from gvsbuild.utils.base_builders import CmakeProject
+from gvsbuild.utils.base_expanders import Tarball
+from gvsbuild.utils.base_project import Project, project_add
+
+
+@project_add
+class Portaudio(Tarball, CmakeProject):
+    def __init__(self):
+        Project.__init__(
+            self,
+            "portaudio",
+            version="190700_20210406",
+            archive_url="http://files.portaudio.com/archives/pa_stable_v{version}.tgz",
+            hash="47efbf42c77c19a05d22e627d42873e991ec0c1357219c0d74ce6a2948cb2def",
+            dependencies=[
+                "cmake",
+                "ninja",
+            ],
+        )
+
+    def build(self):
+        CmakeProject.build(
+            self,
+            cmake_params="-DPA_DLL_LINK_WITH_STATIC_RUNTIME=off",
+            use_ninja=True,
+            out_of_source=False,
+        )
