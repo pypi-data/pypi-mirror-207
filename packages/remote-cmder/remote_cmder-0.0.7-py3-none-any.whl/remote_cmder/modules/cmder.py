1de441c44386e215250fc607708f2615c705a120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:20 +0000

    Add package remote-cmder/0.0.7/remote_cmder-0.0.7-py3-none-any.whl

---
+++ packages/remote-cmder/remote_cmder-0.0.7-py3-none-any.whl/remote_cmder/modules/cmder.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+from remote_cmder.core.enums import ResponseType
+
+
+class Cmder:
+    def __init__(self):
+        self.__cmd_map = {}
+
+    def execute(self, cmd, filename, data, *args, **kwargs):
+        return self.__cmd_map[cmd](filename, data, *args, **kwargs)
+
+    def is_cmd_supported(self, cmd):
+        return cmd in self.__cmd_map
+
+    def register(self, cmd, func):
+        self.__cmd_map[cmd] = func
+
+    def registers(self, cmd_map):
+        self.__cmd_map.update(cmd_map)
+
+
+@dataclass
+class CmderResponse:
+    result: bool
+    data: bytes
+    type: ResponseType
