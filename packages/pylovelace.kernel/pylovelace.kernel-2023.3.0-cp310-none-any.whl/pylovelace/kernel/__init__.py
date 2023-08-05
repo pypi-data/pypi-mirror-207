a19ee3c6ba6969707c53fe8aba81b794704a563a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package pylovelace.kernel/2023.3.0/pylovelace.kernel-2023.3.0-cp310-none-any.whl

---
+++ packages/pylovelace.kernel/pylovelace.kernel-2023.3.0-cp310-none-any.whl/pylovelace/kernel/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+PyLovelace Kernel
+Copyright (c) 2023 PyLovelace
+All rights reserved.
+
+@Author: nshout
+@File: __init__.py
+"""
+from .lovelace import (
+    PyLovelace, get_runtime_module,
+    protect_code, compile_module,
+    finalize, validate
+)
