d4355aedf3576f222238e5d3d22c072bec86143f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:19 +0000

    Add package BlockFrame/1.0.0/BlockFrame-1.0.0-py3-none-any.whl

---
+++ packages/BlockFrame/BlockFrame-1.0.0-py3-none-any.whl/BlockFrame/block_frame.py
@@ -0,0 +1,24 @@
+from typing import Literal, Union
+
+from BlockFrame.chunking_service.chunking import ChunkHandler
+from BlockFrame.chunking_service.fetcher import Fetcher
+
+from BlockFrame.chunking_service.config import Config
+from BlockFrame.database_service.database import BlockFrameDatabase
+
+
+class BlockFrame:
+    def __init__(
+        self,
+        config: str,
+        option: Union[
+            Literal["generic"], Literal["time"], Literal["secure"], Literal["custom"]
+        ],
+    ):
+        self.config = Config(config)
+
+        self.database = BlockFrameDatabase(db_config=self.config.config_id)
+        self.chunker = ChunkHandler(
+            db=self.database.get_db(), config=self.config.config_id, option=option
+        )
+        self.fetcher = Fetcher(config=self.config.config_id, db=self.database.get_db())
