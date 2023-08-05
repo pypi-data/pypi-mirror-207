8df5c06f0df3d19fcd28882e56251561f68d6226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:33 +0000

    Add package ini-klass/0.0.4/ini_klass-0.0.4-py3-none-any.whl

---
+++ packages/ini-klass/ini_klass-0.0.4-py3-none-any.whl/test/simple.py
@@ -0,0 +1,28 @@
+from ini import IniConfig
+from pathlib import Path
+from unittest import TestCase
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        path = Path(__file__).parent / 'simple.ini'
+
+        self.config = IniConfig.read(str(path), empty_to_none=False)
+
+    def test_numeric(self):
+        self.assertEqual(self.config.example.id, 12)
+
+    def test_string(self):
+        self.assertEqual(self.config.example.name, 'John')
+
+    def test_boolean(self):
+        self.assertEqual(self.config.example.status, True)
+
+    def test_float(self):
+        self.assertEqual(self.config.example.salary, 100.5)
+
+    def test_list(self):
+        self.assertEqual(self.config.example.group, ['car', 'book', 'phone'])
+
+    def test_empty_list(self):
+        self.assertEqual(self.config.example.detail, ['user', 'name'])
