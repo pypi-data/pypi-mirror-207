bbe406704263f346a6d6f8e2b665b4f947831e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:47 +0000

    Add package frontengine-dev/0.0.10/frontengine_dev-0.0.10.tar.gz

---
+++ packages/frontengine-dev/frontengine_dev-0.0.10.tar.gz/frontengine_dev-0.0.10/frontengine/show/web/webview.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QIcon
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
+
+class WebWidget(QWebEngineView):
+
+    def __init__(self, url: str, opacity: float = 0.2):
+        super().__init__()
+        self.setWindowFlag(
+            Qt.WindowType.WindowTransparentForInput |
+            Qt.WindowType.FramelessWindowHint |
+            Qt.WindowType.WindowStaysOnTopHint
+        )
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.load(url)
+        self.setWindowOpacity(opacity)
+        # Set Icon
+        self.icon_path = Path(os.getcwd() + "/je_driver_icon.ico")
+        if self.icon_path.exists() and self.icon_path.is_file():
+            self.setWindowIcon(QIcon(str(self.icon_path)))
