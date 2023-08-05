41f875a50e3f02707b76b271904e3b87b3f241c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package xia-scw-mongodb/0.0.3/xia_scw_mongodb-0.0.3-cp39-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-scw-mongodb/xia_scw_mongodb-0.0.3-cp39-none-macosx_11_0_x86_64.whl/xia_scw_mongodb/__init__.py
@@ -0,0 +1,10 @@
+from xia_scw_mongodb.engine import ScwMongoAdmParam, ScwMongoAdmClient, ScwMongoAdmEngine
+from xia_scw_mongodb.mongo import ScwMongodb, ScwMongoInstance
+
+
+__all__ = [
+    "ScwMongoAdmParam", "ScwMongoAdmClient", "ScwMongoAdmEngine",
+    "ScwMongodb", "ScwMongoInstance"
+]
+
+__version__ = "0.0.3"
