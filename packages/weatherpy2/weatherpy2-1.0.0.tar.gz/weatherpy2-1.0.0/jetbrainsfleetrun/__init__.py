6156c54187dc22bd423a0e34d6f4ad32cbcd13c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:24 +0000

    Add package weatherpy2/1.0.0/weatherpy2-1.0.0.tar.gz

---
+++ packages/weatherpy2/weatherpy2-1.0.0.tar.gz/weatherpy2-1.0.0/jetbrainsfleetrun/__init__.py
@@ -0,0 +1,1 @@
+from jetbrainsfleetrun.jetbrainsfleetrun import find_filename, run
---
+++ packages/weatherpy2/weatherpy2-1.0.0.tar.gz/weatherpy2-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name='weatherpy2',
+    packages=['jetbrainsfleetrun'],
+    version="1.0.0",
+    liscense="MIT",
+    description="Easy Python file running in JetBrains Fleet",
+    author="GamerXZEN",
+    author_email="codspecialops@gmail.com",
+    url="https://github.com/GamerXZEN/jetbrainsfleetrun",
+    keywords=["weather", "forecast", "python",],
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
