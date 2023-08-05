e7482ccb2c90b988e4201f96d243f14e427df83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:49 +0000

    Add package spotON_sdk/0.0.1.3/spotON_sdk-0.0.1.3.tar.gz

---
+++ packages/spotON_sdk/spotON_sdk-0.0.1.3.tar.gz/spotON_sdk-0.0.1.3/spotON_sdk/__init__.py
@@ -0,0 +1,13 @@
+"""
+The spotON_sdk package provides tools for interacting with the spotON API.
+
+This package includes modules for authenticating with the API, sending requests,
+and processing responses. It also includes utility functions for working with
+the data returned by the API.
+"""
+
+
+from .logic.Config import *
+
+__version__ = "0.0.1.3"
+__all__ = [name for name in dir() if not name.startswith('_')]
