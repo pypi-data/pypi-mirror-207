b68de3258d158fde4a94f209821ac3b5bb7218d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:39 +0000

    Add package hindilemmatizer/0.2.0/hindilemmatizer-0.2.0.tar.gz

---
+++ packages/hindilemmatizer/hindilemmatizer-0.2.0.tar.gz/hindilemmatizer-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hindilemmatizer',
+    version='0.2.0',
+    description='A lemmatization package for hindi language, by students of MNIT Jaipur',
+    author='Nitish Bhardwaj',
+    packages=['hindilemmatizer'],
+    install_requires=[
+    ],
+    include_package_data=True,
+)
