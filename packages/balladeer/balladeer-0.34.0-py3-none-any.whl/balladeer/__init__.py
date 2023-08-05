3b3a6b9aa53e42dd61d71a6932b39217643b8ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:28 +0000

    Add package balladeer/0.34.0/balladeer-0.34.0-py3-none-any.whl

---
+++ packages/balladeer/balladeer-0.34.0-py3-none-any.whl/balladeer/__init__.py
@@ -0,0 +1,28 @@
+import datetime
+import importlib.metadata
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+
+try:
+    __version__ = importlib.metadata.version("balladeer")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = datetime.date.today().strftime("%Y.%m.%d") + "+local_repository"
+
+from balladeer.classic.drama import Drama
+from balladeer.classic.gesture import Gesture
+from balladeer.classic.gesture import Hand
+from balladeer.classic.gesture import Head
+from balladeer.classic.speech import Article
+from balladeer.classic.speech import Name
+from balladeer.classic.speech import Phrase
+from balladeer.classic.speech import Pronoun
+from balladeer.classic.speech import Verb
+from balladeer.classic.story import Story
+from balladeer.classic.types import Fruition
+from balladeer.classic.types import Grouping
+from balladeer.classic.types import Named
+from balladeer.classic.types import World
