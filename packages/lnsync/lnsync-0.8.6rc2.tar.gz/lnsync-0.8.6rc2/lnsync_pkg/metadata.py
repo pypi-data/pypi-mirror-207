67e0c542d7e91d61304c55f684479cec4a7601bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:50 +0000

    Add package lnsync/0.8.6rc2/lnsync-0.8.6rc2.tar.gz

---
+++ packages/lnsync/lnsync-0.8.6rc2.tar.gz/lnsync-0.8.6rc2/lnsync_pkg/metadata.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+lnsync project metadata.
+"""
+
+# pylint: disable-all
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'lnsync'
+version = '0.8.6rc2'
+description = "Sync local dirs by content, with rename detection and " \
+              "hard link support, plus fast fdupes, and more."
+summary = description
+project = 'lnsync dir sync tool'
+project_no_spaces = project.replace(' ', '')
+url = 'https://github.com/mrsimoes/lnsync'
+download_url = 'https://github.com/mrsimoes/lnsync/archive/v%s.tar.gz' % version
+keywords = 'sync rsync fast rename backup link hardlink fdupes'
+authors = ['Miguel Simoes']
+authors_string = ', '.join(authors)
+emails = ['miguelrsimoes@yahoo.com']
+license = 'GNU General Public License v3'
+copyright = '(C) 2018-2021 ' + authors_string
