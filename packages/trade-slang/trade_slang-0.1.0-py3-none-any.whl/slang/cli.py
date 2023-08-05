bc973cae975b98b96b3c1ab31cc971602c87f5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:21 +0000

    Add package trade-slang/0.1.0/trade_slang-0.1.0-py3-none-any.whl

---
+++ packages/trade-slang/trade_slang-0.1.0-py3-none-any.whl/slang/cli.py
@@ -0,0 +1,42 @@
+import requests
+import rich
+import rich_click as click
+from rich.panel import Panel
+
+url = "https://tinkoff.dan.tatar"
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('word')
+def term(word):
+    """Get term definition"""
+    response = requests.get(url + "/term/" + word)
+    click.echo(response.json())
+
+
+@cli.command()
+@click.argument('text')
+def detect_slang(text):
+    """Detect slang in text"""
+    payload = {"text": text}
+    response = requests.post(url + "/detect_slang", json=payload)
+    words = text.split()
+    highlight_words_indexes = map(lambda x: x.split("_")[0], response.json()["result"]["highlight"].keys())
+    for word_index in highlight_words_indexes:
+        words[int(word_index)] = f"[yellow]{words[int(word_index)]}[/yellow]"
+
+    text = " ".join(words)
+    console = rich.get_console()
+    console.print(Panel(text, title="Биржевой Сленг"))
+
+
+@cli.command()
+def ping():
+    """Ping the server"""
+    response = requests.get(url + "/")
+    click.echo(response.json())
