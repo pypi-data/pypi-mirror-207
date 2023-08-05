c6e6b298a15bb89e8fe7070f8423251b5a309deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:28 +0000

    Add package jaxip/0.5.6/jaxip-0.5.6-py2.py3-none-any.whl

---
+++ packages/jaxip/jaxip-0.5.6-py2.py3-none-any.whl/jaxip/potentials/__init__.py
@@ -0,0 +1,7 @@
+from jaxip.potentials.atomic_potential import AtomicPotential
+from jaxip.potentials.nnp.potential import NeuralNetworkPotential
+
+__all__ = [
+    "NeuralNetworkPotential",
+    "AtomicPotential",
+]
---
+++ packages/jaxip/jaxip-0.5.6-py2.py3-none-any.whl/jaxip/potentials/nnp/__init__.py
@@ -0,0 +1,7 @@
+from jaxip.potentials.nnp.potential import NeuralNetworkPotential
+from jaxip.potentials.nnp.settings import PotentialSettings
+
+__all__ = [
+    "NeuralNetworkPotential",
+    "PotentialSettings",
+]
