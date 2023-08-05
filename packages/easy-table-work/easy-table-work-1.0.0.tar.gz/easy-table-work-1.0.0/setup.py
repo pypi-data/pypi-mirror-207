a3627de0f4cede6ffcbb023121f251949a667a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:08 +0000

    Add package easy-table-work/1.0.0/easy-table-work-1.0.0.tar.gz

---
+++ packages/easy-table-work/easy-table-work-1.0.0.tar.gz/easy-table-work-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easy-table-work",
+    version="1.0.0",
+    description="Easy work with table json",
+    author="AlekiTimur",
+    author_email="aleki.levoe@gmail.com",
+    packages=find_packages(include=['*']),
+)
