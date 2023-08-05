5699f020557eb45222c7c5d0b63919a14bf8048d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:23 +0000

    Add package huma-signals/0.1.1/huma_signals-0.1.1-py3-none-any.whl

---
+++ packages/huma-signals/huma_signals-0.1.1-py3-none-any.whl/huma_signals/commons/chains.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import enum
+
+
+class Chain(enum.Enum):
+    ETHEREUM = "ETHEREUM"
+    GOERLI = "GOERLI"
+    POLYGON = "POLYGON"
+    MUMBAI = "MUMBAI"
+
+    @staticmethod
+    def from_chain_name(chain_name: str) -> Chain:
+        if chain_name.lower() in ("ethereum", "mainnet", "eth", "homestead"):
+            return Chain.ETHEREUM
+        if chain_name.lower() in ("goerli"):
+            return Chain.GOERLI
+        if chain_name.lower() in ("polygon", "matic"):
+            return Chain.POLYGON
+        if chain_name.lower() in ("mumbai"):
+            return Chain.MUMBAI
+        raise ValueError(f"Unsupported chain: {chain_name}")
+
+    def chain_name(self) -> str:
+        return self.name.lower()
+
+    def is_testnet(self) -> bool:
+        return self.chain_name() in ("goerli", "mumbai")
