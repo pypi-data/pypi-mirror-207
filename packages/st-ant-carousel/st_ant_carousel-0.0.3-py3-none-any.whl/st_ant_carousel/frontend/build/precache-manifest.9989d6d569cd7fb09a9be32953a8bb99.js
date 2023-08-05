2c01327522e90a1ec83184b64980ab6eb60161ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:15 +0000

    Add package st-ant-carousel/0.0.3/st_ant_carousel-0.0.3-py3-none-any.whl

---
+++ packages/st-ant-carousel/st_ant_carousel-0.0.3-py3-none-any.whl/st_ant_carousel/frontend/build/precache-manifest.9989d6d569cd7fb09a9be32953a8bb99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d19dacf394f359a0f076d93e98a462",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a857eefef605b712b7da",
+    "url": "./static/js/2.6c1beff1.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.6c1beff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c206a7140945c2ce0dc7",
+    "url": "./static/js/main.6acda223.chunk.js"
+  },
+  {
+    "revision": "389c2abfab6a66f5a473",
+    "url": "./static/js/runtime-main.502c8693.js"
+  }
+]);
