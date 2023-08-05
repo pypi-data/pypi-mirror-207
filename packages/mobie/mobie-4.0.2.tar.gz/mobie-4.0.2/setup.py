4e1a2facd4e95864d45cf8f1862040dbf3949b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:28 +0000

    Add package mobie/4.0.2/mobie-4.0.2.tar.gz

---
+++ packages/mobie/mobie-4.0.2.tar.gz/mobie-4.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+install_requires = [
+    "jgo>=0.4.0",
+]
+
+entry_points = {
+    "console_scripts": [
+        "mobie=mobie:main",
+    ]
+}
+
+version = "4.0.2"
+
+classifiers = [
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python :: 3 :: Only",
+]
+
+package_urls = {
+    "Documentation": "https://mobie.github.io/",
+}
+
+setup(
+    name="mobie",
+    version=version,
+    license_files=("LICENSE.txt",),
+    author="Christian Tischer",
+    author_email="christian.tischer@embl.de",
+    maintainer="Christian Tischer",
+    maintainer_email="christian.tischer@embl.de",
+    description="Viewer for big multi-modal image data",
+    url="https://github.com/mobie/mobie-viewer-fiji",
+    package_urls=package_urls,
+    packages=["mobie"],
+    entry_points=entry_points,
+    install_requires=install_requires,
+    classifiers=classifiers,
+)
