222e28d46df1cc4830f59e38430adcdce9f2c631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:47 +0000

    Add package tpcollections/0.0.2/tpcollections-0.0.2.tar.gz

---
+++ packages/tpcollections/tpcollections-0.0.2.tar.gz/tpcollections-0.0.2/src/tpcollections/__init__.py
@@ -0,0 +1,10 @@
+from ._db import Database, Mode, Connection
+from ._mapping import OrderedMapping, Mapping
+
+__all__ = (
+    'Connection',
+    'Database',
+    'Mapping',
+    'OrderedMapping',
+    'Mode',
+)
