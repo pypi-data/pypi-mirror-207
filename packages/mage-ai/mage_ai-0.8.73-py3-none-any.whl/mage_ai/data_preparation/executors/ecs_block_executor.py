8840694afde430c779162b88a00aea426c6e170a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:21 +0000

    Add package mage-ai/0.8.73/mage_ai-0.8.73-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.8.73-py3-none-any.whl/mage_ai/data_preparation/executors/ecs_block_executor.py
@@ -0,0 +1,33 @@
+from mage_ai.data_preparation.executors.block_executor import BlockExecutor
+from mage_ai.services.aws.ecs import ecs
+from mage_ai.shared.hash import merge_dict
+from requests import get
+from typing import Dict
+
+
+class EcsBlockExecutor(BlockExecutor):
+    def __init__(self, pipeline, block_uuid: str, execution_partition: str = None):
+        super().__init__(pipeline, block_uuid, execution_partition=execution_partition)
+        self.executor_config = self.pipeline.repo_config.ecs_config or dict()
+        if self.block.executor_config is not None:
+            self.executor_config = merge_dict(self.executor_config, self.block.executor_config)
+
+    def execute(
+        self,
+        block_run_id: int = None,
+        global_vars: Dict = None,
+        **kwargs,
+    ) -> None:
+        cmd = f'mage run {self.pipeline.repo_config.repo_name} {self.pipeline.uuid}'
+        options = [
+            f'--block-uuid {self.block_uuid}',
+            '--executor-type local_python',
+        ]
+        if self.execution_partition is not None:
+            options.append(f'--execution-partition {self.execution_partition}')
+        if block_run_id is not None:
+            ip = get('https://api.ipify.org').content.decode('utf8')
+            callback_url = f'http://{ip}:6789/api/block_runs/{block_run_id}'
+            options.append(f'--callback-url {callback_url}')
+        options_str = ' '.join(options)
+        ecs.run_task(f'{cmd} {options_str}', ecs_config=self.executor_config)
---
+++ packages/mage-ai/mage_ai-0.8.73-py3-none-any.whl/mage_ai/tests/orchestration/notification/constants.py
@@ -0,0 +1,51 @@
+EMAIL_NOTIFICATION_CONFIG = dict(
+    alert_on=[
+        'trigger_failure',
+        'trigger_success',
+    ],
+    email_config=dict(
+        smtp_host='test_host',
+        smtp_mail_from='test_from@abc.com',
+        smtp_user='test_user',
+        smtp_password='test_password',
+        to_emails=['test_to@xyz.com'],
+    )
+)
+
+SLACK_NOTIFICATION_CONFIG = dict(
+    alert_on=[
+        'trigger_failure',
+        'trigger_success',
+    ],
+    slack_config=dict(
+        webhook_url='test_webhook_url',
+    )
+)
+
+GOOGLE_CHAT_NOTIFICATION_CONFIG = dict(
+    alert_on=[
+        'trigger_failure',
+        'trigger_success',
+    ],
+    google_chat_config=dict(
+        webhook_url='test_webhook_url',
+    )
+)
+
+TEAMS_NOTIFICATION_CONFIG = dict(
+    alert_on=[
+        'trigger_failure',
+        'trigger_success',
+    ],
+    teams_config=dict(
+        webhook_url='test_webhook_url',
+    )
+)
+
+
+TEAMS_NOTIFICATION_CONFIG_NO_ALERT_ON = dict(
+    alert_on=[],
+    teams_config=dict(
+        webhook_url='test_webhook_url',
+    )
+)
