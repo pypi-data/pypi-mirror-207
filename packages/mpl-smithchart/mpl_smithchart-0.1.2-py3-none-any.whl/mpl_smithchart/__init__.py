4bf935f7796acbd5f0d61110a1804f1c46725c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:42 +0000

    Add package mpl-smithchart/0.1.2/mpl_smithchart-0.1.2-py3-none-any.whl

---
+++ packages/mpl-smithchart/mpl_smithchart-0.1.2-py3-none-any.whl/mpl_smithchart/__init__.py
@@ -0,0 +1,11 @@
+import matplotlib
+from matplotlib.projections import register_projection
+
+from .smithaxes import SmithAxes
+
+# check version requierment
+if matplotlib.__version__ < '1.2':
+    raise ImportError("pySmithPlot requires at least matplotlib version 1.2")
+
+# add smith projection to available projections
+register_projection(SmithAxes)
