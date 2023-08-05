0cede6e42f87c048ac65423e32398644e22ccf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:28 +0000

    Add package openrl/0.0.8/openrl-0.0.8.tar.gz

---
+++ packages/openrl/openrl-0.0.8.tar.gz/openrl-0.0.8/openrl/runners/common/__init__.py
@@ -0,0 +1,8 @@
+from openrl.runners.common.chat_agent import Chat6BAgent, ChatAgent
+from openrl.runners.common.ppo_agent import PPOAgent
+
+__all__ = [
+    "PPOAgent",
+    "ChatAgent",
+    "Chat6BAgent",
+]
