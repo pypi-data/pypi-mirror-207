1fe7c8f5192e7d04447632bed9e2d1a30ef6ec14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:37 +0000

    Add package nonebot-plugin-csgo-case-simulator/0.1.8/nonebot-plugin-csgo-case-simulator-0.1.8.tar.gz

---
+++ packages/nonebot-plugin-csgo-case-simulator/nonebot-plugin-csgo-case-simulator-0.1.8.tar.gz/nonebot-plugin-csgo-case-simulator-0.1.8/nonebot-plugin-csgo-case-simulator/skins.py
@@ -0,0 +1,58 @@
+import json
+import random
+from os.path import dirname
+
+
+JSON_DIR = dirname(__file__) + "/json"
+
+
+class Skins:
+    def __init__(self):
+        self.skins = []
+
+    async def get_skins_json(self):
+        # async with httpx.AsyncClient() as client:
+        #     return await client.get(self.skins_api)
+        with open(f"{JSON_DIR}/skins.json", 'rb') as f:
+            data = f.read()
+            return json.loads(data)
+
+    def get_skins(self, name: str) -> dict:
+        # print(name)
+        for skin in self.skins:
+            if skin["name"] == name:
+                wear_rating = round(
+                    random.uniform(skin["min_float"], skin["max_float"]), 5
+                )
+                return {
+                    "id": skin["id"],
+                    "name": skin["name"],
+                    "image": skin["image"],
+                    "rarity": skin["rarity"],
+                    "wear_rating": wear_rating,
+                }
+        # return self.check_if_wrong_name(name)
+
+    # temp fix
+    # def check_if_wrong_name(self, name: str):
+    #     if name.find("M4A1 消音型") != -1:
+    #         skin_name = name.replace("M4A1 消音型", "M4A4")
+    #         for skin in self.skins:
+    #             if skin["name"] == skin_name:
+    #                 wear_rating = round(
+    #                     random.uniform(skin["min_float"], skin["max_float"]), 5
+    #                 )
+    #                 return {
+    #                     "id": skin["id"],
+    #                     "name": name,
+    #                     "image": skin["image"],
+    #                     "rarity": skin["rarity"],
+    #                     "wear_rating": wear_rating,
+    #                 }
+
+    def search_skin(self, skin_name: str) -> dict:
+        found_skin_list = []
+        for skin in self.skins:
+            if skin["pattern"].find(skin_name.lower()) != -1:
+                found_skin_list.append(skin)
+        return found_skin_list
