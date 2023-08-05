89baf74459b6084fcfbc63d3e4fa29e387262c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:21 +0000

    Add package corecrud/0.1.2/corecrud-0.1.2-py3-none-any.whl

---
+++ packages/corecrud/corecrud-0.1.2-py3-none-any.whl/corecrud/cursors/__init__.py
@@ -0,0 +1,9 @@
+from corecrud.cursors.abc import ABCCursor
+from corecrud.cursors.mapping import Mappings
+from corecrud.cursors.scalars import Scalars
+
+__all__ = (
+    "ABCCursor",
+    "Mappings",
+    "Scalars",
+)
