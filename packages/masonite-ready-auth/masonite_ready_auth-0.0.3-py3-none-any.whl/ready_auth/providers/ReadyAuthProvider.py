46e47f5c7d546d4b0a1d0cdd44c753548dca99c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:50 +0000

    Add package masonite-ready-auth/0.0.3/masonite_ready_auth-0.0.3-py3-none-any.whl

---
+++ packages/masonite-ready-auth/masonite_ready_auth-0.0.3-py3-none-any.whl/ready_auth/providers/ReadyAuthProvider.py
@@ -0,0 +1,34 @@
+"""A ReadyAuthProvider Service Provider."""
+
+from masonite.packages import PackageProvider
+
+
+class ReadyAuthProvider(PackageProvider):
+
+    def configure(self):
+        """Register objects into the Service Container."""
+        (
+            self
+                .root("ready_auth")
+                .name("ready_auth")
+                .config("config/ready_auth.py", publish=True)
+                .migrations(
+                    "database/migrations/2023_05_06_171516_drop_conflicting_tables.py", 
+                    "database/migrations/2023_05_06_171713_create_ip_addresses_table.py", 
+                    "database/migrations/2023_05_06_171721_create_user_agents_table.py", 
+                    "database/migrations/2023_05_06_171726_create_auth_tokens_table.py", 
+                    "database/migrations/2023_05_06_171731_create_user_tokens_table.py", 
+                    "database/migrations/2023_05_06_171737_create_users_table.py", 
+                    "database/migrations/2023_05_06_171804_create_user_sessions_table.py", 
+                    "database/migrations/2023_05_06_171821_create_access_locations_table.py", 
+                    "database/migrations/2023_05_06_171859_create_personal_access_tokens_table.py"
+                )
+
+        )
+
+    def register(self):
+        super().register()
+
+    def boot(self):
+        """Boots services required by the container."""
+        pass
