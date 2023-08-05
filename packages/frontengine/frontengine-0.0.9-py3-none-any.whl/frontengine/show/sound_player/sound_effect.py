6903e924154480889c3efc0b00e7f7d6fe0f22c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:57 +0000

    Add package frontengine/0.0.9/frontengine-0.0.9-py3-none-any.whl

---
+++ packages/frontengine/frontengine-0.0.9-py3-none-any.whl/frontengine/show/sound_player/sound_effect.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+from PySide6.QtCore import Qt, QUrl
+from PySide6.QtGui import QIcon
+from PySide6.QtMultimedia import QSoundEffect
+from PySide6.QtWidgets import QWidget, QMessageBox
+
+
+class SoundEffectWidget(QWidget):
+
+    def __init__(self, sound_path: str, volume: int = 100):
+        super().__init__()
+        self.setWindowFlag(
+            Qt.WindowType.WindowTransparentForInput |
+            Qt.WindowType.FramelessWindowHint |
+            Qt.WindowType.WindowStaysOnTopHint
+        )
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.sound_player = QSoundEffect()
+        self.sound_path = Path(sound_path)
+        if self.sound_path.exists() and self.sound_path.is_file():
+            self.sound_player.setSource(QUrl.fromLocalFile(str(self.sound_path)))
+            self.sound_player.setVolume(volume)
+            # -2 means loop forever
+            self.sound_player.setLoopCount(-2)
+            self.sound_player.play()
+        else:
+            message_box = QMessageBox(self)
+            message_box.setText("Sound file error")
+            message_box.show()
+        # Window setting
+        self.setWindowTitle("Sound Wave")
+        # Set Icon
+        self.icon_path = Path(os.getcwd() + "/je_driver_icon.ico")
+        if self.icon_path.exists() and self.icon_path.is_file():
+            self.setWindowIcon(QIcon(str(self.icon_path)))
+
+    def close(self):
+        super().close()
+        self.sound_player.stop()
+
+    def closeEvent(self, event):
+        super().closeEvent(event)
+        self.sound_player.stop()
