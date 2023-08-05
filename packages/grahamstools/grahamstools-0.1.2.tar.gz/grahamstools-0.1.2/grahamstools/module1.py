dff61f98e82066fdf08fa99c90a602528f11be13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:44 +0000

    Add package grahamstools/0.1.2/grahamstools-0.1.2.tar.gz

---
+++ packages/grahamstools/grahamstools-0.1.2.tar.gz/grahamstools-0.1.2/grahamstools/module1.py
@@ -0,0 +1,63 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pystray import Icon as icon, Menu as menu, MenuItem as item
+from PIL import Image
+import threading
+import ctypes
+import time
+
+# Function to show the window
+def show_window(icon, item):
+    icon.stop()
+
+# Function to quit the program
+def quit_program(icon, item):
+    icon.stop()
+    
+    # exit() only exits the current thread, so we have to kill all the other threads too.
+    # exit()
+    import psutil
+    import os
+
+    parent_pid = os.getpid()
+    parent = psutil.Process(parent_pid)
+    children = parent.children(recursive=True)
+    for child in children:
+        child.kill()
+    parent.kill()
+
+# Function to set the volume of Discord
+def set_program_volume(volume, process_name):
+    global discord_volume
+    
+    # Set the global volume variable to the specified value
+    discord_volume = volume
+    
+    # Get all audio sessions
+    sessions = AudioUtilities.GetAllSessions()
+    for session in sessions:
+        # Check if this is the Discord session
+        if session.Process and session.Process.name() == process_name:
+            # Set the volume of Discord
+            session.SimpleAudioVolume.SetMasterVolume(volume, None)
+
+def system_tray_icon(tooltip, image_path):
+    # Create an image for the system tray icon
+    image = Image.open(image_path)
+    # Create a system tray icon with the specified image and menu
+    global tray_icon
+    tray_icon = icon(tooltip, image, tooltip, menu)
+
+def create_function_in_thread(function):
+    # Create a thread for the system tray icon
+    global tray_thread
+    tray_thread = threading.Thread(target=function)
+
+def run_thread(thread):
+    # Start the thread for the system tray icon
+    thread.start()
+
+def run_program_in_background():
+    # Make it run in the background
+    ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
---
+++ packages/grahamstools/grahamstools-0.1.2.tar.gz/grahamstools-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='grahamstools',
+    version='0.1.2',
+    author='GrahamboJangles',
+    author_email='lafebregraham@gmail.com',
+    description='Tools and resources for Python developers',
+    url = "https://github.com/GrahamboJangles/GrahamsTools",
+    download_url = "https://github.com/GrahamboJangles/GrahamsTools/archive/refs/tags/0.1.0.tar.gz",
+    packages=find_packages(),
+    # This is for required dependencies
+    install_requires=[],
+    # This is for command line commands
+    # entry_points={
+    #     'console_scripts': [
+    #         'your_command=your_package_name.command_line:main'
+    #     ]
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning', 
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    python_requires='>=3.6',
+)
