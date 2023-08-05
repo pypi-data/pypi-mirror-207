71d09cb1a3b4d9e7b97afb0bb47f1765a9ee3fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:03 +0000

    Add package quantsight/0.1.10/quantsight-0.1.10.tar.gz

---
+++ packages/quantsight/quantsight-0.1.10.tar.gz/quantsight-0.1.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="quantsight",
+    version="0.1.10",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "langchain",
+        "data-cache",
+        "openai",
+        "tabulate"
+    ],
+    python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
