d2a3361d2768017d2d05074e723a786b64c5aa14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:18 +0000

    Add package pyth-sysdistv1/0.1.0/pyth-sysdistv1-0.1.0.tar.gz

---
+++ packages/pyth-sysdistv1/pyth-sysdistv1-0.1.0.tar.gz/pyth-sysdistv1-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyth-sysdistv1',
+    version='0.1.0',
+    packages=['pyth-sysdistv1'],
+    install_requires=[],
+    url='https://github.com/your_username/pyth-sysdistv1',
+    license='MIT',
+    author='Your Name',
+    author_email='your_email@example.com',
+    description='A hello world package'
+)
