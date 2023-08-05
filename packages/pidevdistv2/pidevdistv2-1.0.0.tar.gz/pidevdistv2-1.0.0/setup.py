1aeeccd89a73ab1ba25874b10bf1982ff6c000aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:44 +0000

    Add package pidevdistv2/1.0.0/pidevdistv2-1.0.0.tar.gz

---
+++ packages/pidevdistv2/pidevdistv2-1.0.0.tar.gz/pidevdistv2-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pidevdistv2',
+    version='1.0.0',
+    packages=['pidevdistv2'],
+    install_requires=[],
+    url='https://github.com/majajanito/pidevdistv2',
+    license='MIT',
+    author='Majajanieto',
+    author_email='majajanitor@blacked.gay',
+    description='A multi purpose package mainly for bots.'
+)
