4dc3bd080ed9dfc2173eeeceada3859028d07d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:36 +0000

    Add package pyvts/0.3.1/pyvts-0.3.1.tar.gz

---
+++ packages/pyvts/pyvts-0.3.1.tar.gz/pyvts-0.3.1/pyvts/config.py
@@ -0,0 +1,9 @@
+""" default configuration of pyvts """
+vts_api = {"version": "1.0", "name": "VTubeStudioPublicAPI", "port": 8001}
+
+plugin_default = {
+    "plugin_name": "pyvts",
+    "developer": "genteki",
+    "icon": None,
+    "authentication_token_path": "./pyvts_token.txt",
+}
