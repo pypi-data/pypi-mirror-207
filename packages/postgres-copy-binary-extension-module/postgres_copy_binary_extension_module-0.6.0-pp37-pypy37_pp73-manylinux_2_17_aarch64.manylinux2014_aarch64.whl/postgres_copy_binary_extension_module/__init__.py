4d9c7137de26ee69792354185a44378c99989e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package postgres-copy-binary-extension-module/0.6.0/postgres_copy_binary_extension_module-0.6.0-pp37-pypy37_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/postgres-copy-binary-extension-module/postgres_copy_binary_extension_module-0.6.0-pp37-pypy37_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/postgres_copy_binary_extension_module/__init__.py
@@ -0,0 +1,5 @@
+from .postgres_copy_binary_extension_module import *
+
+__doc__ = postgres_copy_binary_extension_module.__doc__
+if hasattr(postgres_copy_binary_extension_module, "__all__"):
+    __all__ = postgres_copy_binary_extension_module.__all__
