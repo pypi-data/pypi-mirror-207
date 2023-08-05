5dfb17b30f9682065e232456b222948087fbb55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package chafa.py/1.1.2/chafa.py-1.1.2-cp311-cp311-macosx_12_0_arm64.whl

---
+++ packages/chafa.py/chafa.py-1.1.2-cp311-cp311-macosx_12_0_arm64.whl/chafa/__init__.py
@@ -0,0 +1,20 @@
+from .chafa import PixelMode
+from .chafa import DitherMode
+from .chafa import CanvasMode
+from .chafa import PixelType
+from .chafa import SymbolTags
+from .chafa import Optimizations
+from .chafa import TermSeq
+from .chafa import ColorSpace
+from .chafa import ColorExtractor
+
+from .chafa import SymbolMap
+from .chafa import ReadOnlySymbolMap
+from .chafa import Canvas
+from .chafa import CanvasConfig
+from .chafa import ReadOnlyCanvasConfig
+from .chafa import TermDb
+from .chafa import TermInfo
+from .chafa import Canvas
+
+from .chafa import get_device_attributes
