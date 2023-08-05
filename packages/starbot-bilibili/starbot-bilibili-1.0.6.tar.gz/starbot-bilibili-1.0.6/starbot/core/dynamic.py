811052ece452a5656e7b42d5de0bbd9203e16cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:07 +0000

    Add package starbot-bilibili/1.0.6/starbot-bilibili-1.0.6.tar.gz

---
+++ packages/starbot-bilibili/starbot-bilibili-1.0.6.tar.gz/starbot-bilibili-1.0.6/starbot/core/dynamic.py
@@ -0,0 +1,71 @@
+import asyncio
+import time
+
+from aiohttp import ClientOSError, ServerDisconnectedError
+from loguru import logger
+
+from .datasource import DataSource
+from ..exception import ResponseCodeException, DataSourceException, NetworkException
+from ..utils import config
+from ..utils.network import request
+from ..utils.utils import get_credential
+
+
+async def dynamic_spider(datasource: DataSource):
+    logger.success("动态推送模块已启动")
+
+    dynamic_url = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/dynamic_new?type_list=268435455"
+    credential = get_credential()
+    dynamic_interval = config.get("DYNAMIC_INTERVAL")
+
+    if dynamic_interval < 10:
+        logger.warning("当前动态推送抓取频率设置过小, 可能会造成动态抓取 API 访问被暂时封禁, 推荐将其设置为 10 以上的数值")
+
+    # url = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id=773981999188672531"
+    # detail = (await request("GET", url))["card"]
+    # up = datasource.get_up(5834)
+    # up.dispatch("DYNAMIC_UPDATE", detail)
+
+    while True:
+        await asyncio.sleep(dynamic_interval)
+
+        latest_dynamic = {}
+        try:
+            latest_dynamic = await request("GET", dynamic_url, credential=credential)
+        except ResponseCodeException as ex:
+            if ex.code == -6:
+                continue
+            logger.error(f"动态推送任务抓取最新动态异常, HTTP 错误码: {ex.code} ({ex.msg})")
+        except NetworkException:
+            continue
+        except ClientOSError:
+            continue
+        except ServerDisconnectedError:
+            continue
+        except Exception as ex:
+            logger.exception("动态推送任务抓取最新动态异常", ex)
+            continue
+
+        if "new_num" not in latest_dynamic:
+            continue
+
+        new_num = latest_dynamic["new_num"]
+        if new_num > 0:
+            logger.debug(f"检测到新动态个数: {new_num}")
+        for i in range(new_num):
+            try:
+                detail = latest_dynamic["cards"][i]
+            except IndexError:
+                break
+
+            if int(time.time()) - detail["desc"]["timestamp"] > dynamic_interval:
+                break
+
+            try:
+                up = datasource.get_up(detail["desc"]["uid"])
+            except DataSourceException:
+                # TODO: 删除
+                # continue
+                up = datasource.get_up(5834)
+
+            asyncio.create_task(up.dynamic_update(detail))
