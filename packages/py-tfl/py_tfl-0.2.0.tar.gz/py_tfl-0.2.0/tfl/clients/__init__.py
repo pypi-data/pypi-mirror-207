3f97ae6a3595eaee0e70b629899003297e86071f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:01 +0000

    Add package py-tfl/0.2.0/py_tfl-0.2.0.tar.gz

---
+++ packages/py-tfl/py_tfl-0.2.0.tar.gz/py_tfl-0.2.0/tfl/clients/__init__.py
@@ -0,0 +1,6 @@
+"""The clients module contains the clients for the Transport for London API."""
+
+from tfl.clients._auth import Auth
+from tfl.clients._client import TFLClient
+from tfl.clients._accident_stats_client import AccidentStatsClient
+from tfl.clients._lift_disruptions_client import LiftDisruptionsV2Client
