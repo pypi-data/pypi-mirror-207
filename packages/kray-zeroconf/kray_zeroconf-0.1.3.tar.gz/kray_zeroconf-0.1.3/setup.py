9fffacc40e09e524f94e178a5b3ff821582e89e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:48 +0000

    Add package kray-zeroconf/0.1.3/kray_zeroconf-0.1.3.tar.gz

---
+++ packages/kray-zeroconf/kray_zeroconf-0.1.3.tar.gz/kray_zeroconf-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(name='kray-zeroconf',
+      version='0.1.3',
+      description='Zeroconf auto networking configuration tool for Kray Works DevOps',
+      url='https://gitlab-ua.kray.technology/lanceoflife/kray-zeroconf',
+      author='Dmytro Surdu',
+      author_email='dmytro.surdu@kray.technology',
+      scripts=['zeroconf'],
+      install_requires=[
+          "setuptools>=61.0","zeroconf == 0.24.4","pyyaml","netifaces","psutil",
+      ],
+      packages=find_packages(),
+      zip_safe=False)
