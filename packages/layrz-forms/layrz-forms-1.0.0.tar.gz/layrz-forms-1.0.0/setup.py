9bcc1b3f8e740cf1b4adbbec28ba3b56aa79717d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:41 +0000

    Add package layrz-forms/1.0.0/layrz-forms-1.0.0.tar.gz

---
+++ packages/layrz-forms/layrz-forms-1.0.0.tar.gz/layrz-forms-1.0.0/setup.py
@@ -0,0 +1,19 @@
+""" Setup """
+import setuptools
+
+setuptools.setup(
+  name="layrz-forms",
+  version="1.0.0",
+  author="Golden M",
+  author_email="software@goldenmcorp.com",
+  description="Layrz forms and tools for Python",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+  ],
+  python_requires=">=3.8",
+)
