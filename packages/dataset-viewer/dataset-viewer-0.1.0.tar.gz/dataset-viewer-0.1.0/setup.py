73bdf6caf4f0db628ebc39d07ef8db45544a9d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:16 +0000

    Add package dataset-viewer/0.1.0/dataset-viewer-0.1.0.tar.gz

---
+++ packages/dataset-viewer/dataset-viewer-0.1.0.tar.gz/dataset-viewer-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='dataset-viewer',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
