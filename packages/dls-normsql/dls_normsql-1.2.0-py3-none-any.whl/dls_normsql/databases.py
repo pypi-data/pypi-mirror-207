01a924b522f8308273547a522ca1b9a1a423a5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package dls-normsql/1.2.0/dls_normsql-1.2.0-py3-none-any.whl

---
+++ packages/dls-normsql/dls_normsql-1.2.0-py3-none-any.whl/dls_normsql/databases.py
@@ -0,0 +1,46 @@
+# Use standard logging in this module.
+import logging
+
+# Utilities.
+from dls_utilpack.require import require
+
+# Types.
+from dls_normsql.constants import ClassTypes
+
+# Exceptions.
+from dls_normsql.exceptions import NotFound
+
+logger = logging.getLogger(__name__)
+
+
+class Databases:
+    """
+    List of available databases.
+    """
+
+    # ----------------------------------------------------------------------------------------
+    def build_object(self, specification):
+        """"""
+
+        class_type = require("database specification", specification, "type")
+        database_class = self.lookup_class(class_type)
+
+        try:
+            database_object = database_class(specification)
+        except Exception as exception:
+            raise RuntimeError(
+                "unable to build database object for type %s" % (database_class)
+            ) from exception
+
+        return database_object
+
+    # ----------------------------------------------------------------------------------------
+    def lookup_class(self, class_type):
+        """"""
+
+        if class_type == ClassTypes.AIOSQLITE:
+            from dls_normsql.aiosqlite import Aiosqlite
+
+            return Aiosqlite
+
+        raise NotFound("unable to get database class for type %s" % (class_type))
