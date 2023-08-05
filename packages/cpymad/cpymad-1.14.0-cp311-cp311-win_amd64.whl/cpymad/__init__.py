a165d05db82847e1a00769623ec224021e9abf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:56 +0000

    Add package cpymad/1.14.0/cpymad-1.14.0-cp311-cp311-win_amd64.whl

---
+++ packages/cpymad/cpymad-1.14.0-cp311-cp311-win_amd64.whl/cpymad/__init__.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+import sys
+import warnings
+
+
+__title__ = 'cpymad'
+__version__ = '1.14.0'
+
+__summary__ = 'Cython binding to MAD-X'
+__uri__ = 'https://github.com/hibtc/cpymad'
+
+__credits__ = """
+Current cpymad maintainer:
+
+    - Thomas Gläßle <t_glaessle@gmx.de>
+
+Initial pymad creators:
+
+    - Yngve Inntjore Levinsen <Yngve.Inntjore.Levinsen@cern.ch>
+    - Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
+"""
+
+
+if sys.version_info < (3, 7):
+    _unsupported_version = (
+        "Support for python 3.6 and below will be removed in a future release!\n"
+        "If you need continued support for an older version, let us know at:\n"
+        "  https://github.com/hibtc/cpymad/issues")
+    warnings.warn(_unsupported_version, DeprecationWarning)
+
+
+def get_copyright_notice() -> str:
+    from importlib_resources import read_text
+    return read_text('cpymad.COPYING', 'cpymad.rst', encoding='utf-8')
