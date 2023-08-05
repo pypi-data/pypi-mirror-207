b9df7d9dbe2b9b37c8c0719fa1ab5025fc8ec60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:42 +0000

    Add package fake-bpy-module-latest/20230507/fake-bpy-module-latest-20230507.tar.gz

---
+++ packages/fake-bpy-module-latest/fake-bpy-module-latest-20230507.tar.gz/fake-bpy-module-latest-20230507/freestyle/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import types
+from . import utils
+from . import functions
+from . import shaders
+from . import predicates
+from . import chainingiterators
+
+GenericType = typing.TypeVar("GenericType")
