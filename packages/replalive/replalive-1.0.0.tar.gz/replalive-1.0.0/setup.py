2da3986e6cafc0e51d70a0d45696690a77ac4597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:56 +0000

    Add package replalive/1.0.0/replalive-1.0.0.tar.gz

---
+++ packages/replalive/replalive-1.0.0.tar.gz/replalive-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='replalive',
+    version='1.0.0',
+    description='Keep your replit project online 24/7!',
+    author='Hologramsteve#1900 on discord',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ]
+)
