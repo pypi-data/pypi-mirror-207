cf96a410bd0bca55818bd9962794e967c04a9628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:18 +0000

    Add package hyperlight/0.0.5/hyperlight-0.0.5-py3-none-any.whl

---
+++ packages/hyperlight/hyperlight-0.0.5-py3-none-any.whl/hyperlight/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Jose Javier Gonzalez Ortiz"
+__version__ = '0.0.5'
+
+from .convert import hypernetize, hypernetize_single
+from .find import *
+from .hypernet import *
+from .nn import *
