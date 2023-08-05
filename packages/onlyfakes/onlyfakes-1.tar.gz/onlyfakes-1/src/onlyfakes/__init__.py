1aa4d5477277c9ada75d7177eb9f0fd76e17e230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package onlyfakes/1/onlyfakes-1.tar.gz

---
+++ packages/onlyfakes/onlyfakes-1.tar.gz/onlyfakes-1/src/onlyfakes/__init__.py
@@ -0,0 +1,14 @@
+'''
+An API wrapper for the onlyfakes website.
+
+see https://github.com/Egsagon/onlyfakes-api
+'''
+
+__author__ = 'Egsagon'
+__license__ = 'MIT'
+
+from onlyfakes.core import API as Client, Seed, Image, Prompt
+
+from onlyfakes.consts import Engine, Preset
+
+# EOF
