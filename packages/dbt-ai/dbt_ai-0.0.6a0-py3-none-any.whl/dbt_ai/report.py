7fa66f2ab3a807d5dcc7d07ec7ab2ac578f2cf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:02 +0000

    Add package dbt-ai/0.0.6a0/dbt_ai-0.0.6a0-py3-none-any.whl

---
+++ packages/dbt-ai/dbt_ai-0.0.6a0-py3-none-any.whl/dbt_ai/report.py
@@ -0,0 +1,20 @@
+from jinja2 import Environment, PackageLoader
+import webbrowser 
+import markdown2
+
+
+def markdown_filter(value):
+    return markdown2.markdown(value, extras=["fenced-code-blocks"])
+
+
+def generate_html_report(models, output_path, missing_metadata: list[str]):
+    env = Environment(loader=PackageLoader('dbt_ai', 'templates'))
+    env.filters['markdown'] = markdown_filter
+    template = env.get_template("report_template.html")
+
+    rendered_report = template.render(models=models, missing_metadata=missing_metadata)
+    with open(output_path, "w") as f:
+        f.write(rendered_report)
+
+    # Open the report in a new browser tab
+    webbrowser.open(output_path)
