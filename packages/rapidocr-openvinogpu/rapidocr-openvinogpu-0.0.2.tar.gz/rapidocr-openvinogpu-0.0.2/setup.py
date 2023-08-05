e316addaf5f7425227b56ffbb25a9c3085a06f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:24 +0000

    Add package rapidocr-openvinogpu/0.0.2/rapidocr-openvinogpu-0.0.2.tar.gz

---
+++ packages/rapidocr-openvinogpu/rapidocr-openvinogpu-0.0.2.tar.gz/rapidocr-openvinogpu-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import sys
+from pathlib import Path
+
+import setuptools
+
+
+def get_readme():
+    root_dir = Path(__file__).resolve().parent
+    readme_path = str(root_dir / 'README.md')
+    print(readme_path)
+    with open(readme_path, 'r', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
+
+
+MODULE_NAME = 'rapidocr_openvinogpu'
+
+
+VERSION_NUM = "0.0.2"#obtainer.version_add_one(latest_version)
+
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=VERSION_NUM,
+    platforms="Any",
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    package_dir={'': MODULE_NAME},
+    packages=setuptools.find_namespace_packages(where=MODULE_NAME),
+    package_data={'': ['*.onnx', '*.yaml', '*.txt']},
+    python_requires='>=3.6,<=3.11',
+    entry_points={
+        'console_scripts': [f'{MODULE_NAME}={MODULE_NAME}.rapid_ocr_api'],
+    }
+)
