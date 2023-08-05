b44ab7b41095dedff8f0595caf09933ee99a29c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:23 +0000

    Add package Jlab/1.1.47/Jlab-1.1.47.tar.gz

---
+++ packages/Jlab/Jlab-1.1.47.tar.gz/Jlab-1.1.47/Jlab/watcher.py
@@ -0,0 +1,30 @@
+import os
+import signal
+import sys
+
+
+class Watcher:
+    def __init__(self):
+        self.child = os.fork()
+        if self.child == 0:
+            return
+        else:
+            self.watch()
+
+    def watch(self):
+        try:
+            os.wait()
+        except KeyboardInterrupt:
+            self.kill()
+        sys.exit()
+
+    def kill(self):
+        try:
+            print("kill")
+            os.kill(self.child, signal.SIGKILL)
+        except OSError:
+            pass
+
+
+if __name__ == "__main__":
+    Watcher()
