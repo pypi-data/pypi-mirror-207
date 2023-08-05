e857346a639d9d1ca4255582beb5b86fbc32e651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:49 +0000

    Add package grahamstools/0.1.3/grahamstools-0.1.3.tar.gz

---
+++ packages/grahamstools/grahamstools-0.1.3.tar.gz/grahamstools-0.1.3/grahamstools/__init__.py
@@ -0,0 +1,8 @@
+# The .__init__.py file is used to import all the modules in the package
+# the . is used to indicate that the module is in the same directory as the __init__.py file
+from .module1 import *
+# from .module2 import *
+
+# You could also explicity import the modules
+# from .module1 import function1, MyClass1
+# from .module2 import function2, MyClass2
