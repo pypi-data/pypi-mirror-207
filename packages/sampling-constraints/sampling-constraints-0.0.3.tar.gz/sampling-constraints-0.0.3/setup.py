68039d730b0e982c314b74ce8f4d47b1f66cca07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:56 +0000

    Add package sampling-constraints/0.0.3/sampling-constraints-0.0.3.tar.gz

---
+++ packages/sampling-constraints/sampling-constraints-0.0.3.tar.gz/sampling-constraints-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sampling-constraints',
+    version='0.0.3',
+    url='https://github.com/IsaacRe/Syntactically-Constrained-Sampling',
+    description='Library of incremental parsers used to force syntax constraints on next-token predictions during language model generation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    packages=find_packages("scs", include=["scs", "scs.*"]),
+    package_dir={"": "scs"},
+)
