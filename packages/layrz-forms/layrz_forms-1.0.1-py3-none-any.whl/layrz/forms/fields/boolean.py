3c519073388e8a99c8a5f8b88c8a8c6447950baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:01 +0000

    Add package layrz-forms/1.0.1/layrz_forms-1.0.1-py3-none-any.whl

---
+++ packages/layrz-forms/layrz_forms-1.0.1-py3-none-any.whl/layrz/forms/fields/boolean.py
@@ -0,0 +1,41 @@
+""" Boolean field """
+from .base import Field
+from layrz.converters import convert_to_camel
+
+
+class BooleanField(Field):
+  """
+  IdField class for validation
+  ---
+  Attributes
+    required: bool
+      Indicates if the field is required or not
+  """
+
+  def __init__(self, required=False):
+    super(BooleanField, self).__init__(required=required)
+
+  def validate(self, key, value, errors, cleaned_data):
+    """
+    Validate the field with the following rules:
+    - Should be a bool
+    ---
+    Arguments
+      key: str
+        Key of the field
+      value: any
+        Value to validate
+      errors: dict
+        Dict of errors
+      cleaned_data: dict
+        Dict of cleaned data
+    """
+
+    super(BooleanField, self).validate(key=key, value=value, errors=errors, cleaned_data=cleaned_data)
+
+    if not isinstance(value, bool):
+      self._append_error(
+        key=key,
+        errors=errors,
+        to_add={'code': 'invalid'},
+      )
