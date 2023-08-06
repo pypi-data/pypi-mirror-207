3525f1e18641fa7e9049f3142dfca31082635217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:35 +0000

    Add package SNPfilter/0.0.2/SNPfilter-0.0.2-py3-none-any.whl

---
+++ packages/SNPfilter/SNPfilter-0.0.2-py3-none-any.whl/snpfilter/versions.py
@@ -0,0 +1,20 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.0.2",
+        "features": [
+            "1. update README.md",
+            "2. add change site in output file",
+            "3. debug",
+        ],
+    },
+    {
+        "number": "0.0.1",
+        "features": [
+            "1. build base framework",
+        ],
+    },
+]
