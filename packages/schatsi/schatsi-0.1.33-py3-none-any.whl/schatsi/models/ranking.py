5cd8c92e838507b2caf2cc8c77805d534bd07c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:21 +0000

    Add package schatsi/0.1.33/schatsi-0.1.33-py3-none-any.whl

---
+++ packages/schatsi/schatsi-0.1.33-py3-none-any.whl/schatsi/models/ranking.py
@@ -0,0 +1,17 @@
+from typing import Union
+from pydantic import BaseModel
+
+
+class Ranking(BaseModel):
+    """Model that represents the ranking of a file.
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    filename: str
+    term: str
+    sum_functional_terms: int
+    sum_terms: int
+    cluster: Union[None, str]
+    rank: float
