e94f4543c5abe37574e5d935c29096893700d307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:05 +0000

    Add package tester-flask/1.2/tester_flask-1.2.tar.gz

---
+++ packages/tester-flask/tester_flask-1.2.tar.gz/tester_flask-1.2/README.md
@@ -0,0 +1,66 @@
+# Class for autotests flask python apps
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/vb64/test.helper.flask/pep257.yml?label=Pep257&style=plastic&branch=master)](https://github.com/vb64/test.helper.flask/actions?query=workflow%3Apep257)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/vb64/test.helper.flask/py2.yml?label=Python%202.7&style=plastic&branch=master)](https://github.com/vb64/test.helper.flask/actions?query=workflow%3Apy2)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/vb64/test.helper.flask/py3.yml?label=Python%203.7-3.11&style=plastic&branch=master)](https://github.com/vb64/test.helper.flask/actions?query=workflow%3Apy3)
+[![Codacy Badge](https://app.codacy.com/project/badge/Coverage/7b91398bad9f4f3db99b727f3b225d6b)](https://app.codacy.com/gh/vb64/test.helper.flask/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_coverage)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/7b91398bad9f4f3db99b727f3b225d6b)](https://app.codacy.com/gh/vb64/test.helper.flask/dashboard?utm_source=gh&utm_medium=referral&utm_content=&utm_campaign=Badge_grade)
+
+## Install
+```bash
+pip install tester_flask
+```
+
+## Usage in tests
+
+```python
+from flask import Flask
+from tester_flask import TestFlask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    return "Flask OK"
+
+
+@app.route('/redirect', methods=['POST'])
+def redirect_page():
+    return redirect(url_for('main_page'))
+
+
+class TestFlaskApp(TestFlask):
+
+    def setUp(self):
+        super(TestFlaskApp, self).setUp()
+        TestFlask.set_up(self, app)
+
+    def test_app(self):
+        assert self.simple_view('main_page').status_code == 200
+        assert self.param_post('main_page', {'hello': 1}, {'one': 1}).status_code == 200
+
+        response = self.simple_post('redirect_page', {'one': 1}, follow=False)
+        assert self.final_url(response) == self.get_url('main_page')
+
+```
+
+## Development
+
+```bash
+git clone git@github.com:vb64/test.helper.flask.git
+cd test.helper.flask
+```
+
+With Python 3
+
+```bash
+make setup PYTHON_BIN="/path/to/python3
+make tests
+```
+
+With Python 2
+
+```bash
+make setup2 PYTHON_BIN="/path/to/python2
+make tests2
+```
---
+++ packages/tester-flask/tester_flask-1.2.tar.gz/tester_flask-1.2/tester_flask/__init__.py
@@ -0,0 +1,91 @@
+"""Test helper for Flask app.
+
+http://flask.pocoo.org/docs/1.0/testing/
+https://cloud.google.com/appengine/docs/standard/python/getting-started/python-standard-env
+"""
+import unittest
+try:  # pragma: no cover
+    from urllib import urlencode
+    from urlparse import urlparse
+except ImportError:  # pragma: no cover
+    from urllib.parse import urlparse, urlencode  # pylint: disable=ungrouped-imports
+
+
+class RequestFactory:  # pylint: disable=too-few-public-methods
+    """Generator of the requests."""
+
+    def __init__(self, app):
+        """Save Flask app instance."""
+        self.app = app
+
+    def post(self, url, data='', content_type='application/octet-stream'):
+        """Post requests tests."""
+        return self.app.test_request_context(url, data=data, method='POST', content_type=content_type)
+
+
+class TestFlask(unittest.TestCase):
+    """Flask apps tester."""
+
+    app = None
+    client = None
+    factory = None
+
+    def set_up(self, app):
+        """Set defaults for Flask testing."""
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+        self.factory = RequestFactory(self.app)
+
+    def get_url(self, view_name, **kwargs):
+        """Generate url for view in app test context."""
+        with self.app.test_request_context():
+            from flask import url_for
+            url = url_for(view_name, **kwargs)
+
+        return url
+
+    def guest_view(self, url, return_code=200, follow_redirects=False):
+        """Make generic get request."""
+        response = self.client.get(url, follow_redirects=follow_redirects)
+
+        assert response.status_code == return_code, "guest_view {}: {} -> {}".format(
+          return_code, url, response.status_code
+        )
+        return response
+
+    def simple_view(self, view_name, return_code=200, follow=False, get_param=None):
+        """Make simple get request."""
+        url = self.get_url(view_name)
+        if get_param:
+            url = url + "?" + urlencode(get_param)
+        return self.guest_view(url, return_code=return_code, follow_redirects=follow)
+
+    def param_view(
+      self, view_name, params, return_code=200, follow=False, get_param=None
+    ):
+        """Get request with parameters."""
+        url = self.get_url(view_name, **params)
+        if get_param:
+            url = url + "?" + urlencode(get_param)
+        return self.guest_view(url, return_code=return_code, follow_redirects=follow)
+
+    def simple_post(self, view_name, post_dict, follow=True, content_type=None):
+        """Make simple post request."""
+        url = self.get_url(view_name)
+        return self.client.post(url, data=post_dict, follow_redirects=follow, content_type=content_type)
+
+    def param_post(
+      self, view_name, params, post_dict, follow=True, get_param=None, content_type=None
+    ):  # pylint: disable=too-many-arguments
+        """Post request with parameters."""
+        url = self.get_url(view_name, **params)
+        if get_param:
+            url = url + "?" + urlencode(get_param)
+        return self.client.post(url, data=post_dict, follow_redirects=follow, content_type=content_type)
+
+    @staticmethod
+    def final_url(response):
+        """Return final url after redirect."""
+        assert response.status_code == 302
+        return urlparse(response.location).path
