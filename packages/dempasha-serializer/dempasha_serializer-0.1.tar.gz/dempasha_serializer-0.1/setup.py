4d8de8cbde454fb2cbdf445dec3883f81ad285be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:07 +0000

    Add package dempasha-serializer/0.1/dempasha_serializer-0.1.tar.gz

---
+++ packages/dempasha-serializer/dempasha_serializer-0.1.tar.gz/dempasha_serializer-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dempasha_serializer',
+    version='0.1',
+    packages=['tests', 'object_serializer', 'object_serializer.pre_serializer',
+              'object_serializer.built_in_serializers'],
+    url='',
+    license='',
+    author='pavel',
+    author_email='',
+    description=''
+)
