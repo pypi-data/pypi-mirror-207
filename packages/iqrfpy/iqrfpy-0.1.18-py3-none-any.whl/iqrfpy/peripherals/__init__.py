fdb8e8b8d0dc42dc895d8cf216d517a93e624377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:59 +0000

    Add package iqrfpy/0.1.18/iqrfpy-0.1.18-py3-none-any.whl

---
+++ packages/iqrfpy/iqrfpy-0.1.18-py3-none-any.whl/iqrfpy/peripherals/__init__.py
@@ -0,0 +1,9 @@
+"""
+Peripherals submodule containing specific peripherals and their messages.
+"""
+
+from . import coordinator
+from . import os
+from . import eeprom
+from . import ledr
+from . import ledg
