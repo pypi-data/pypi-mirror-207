1af2f9952519e2bbd072ab459bc7b11fa31c44e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:48 +0000

    Add package matviz/0.0.8/matviz-0.0.8.tar.gz

---
+++ packages/matviz/matviz-0.0.8.tar.gz/matviz-0.0.8/test/test_2.py
@@ -0,0 +1,15 @@
+'''
+This file is to make your working environment pretty similar to the matlab working environment, with lots of useful
+functions imported so you don't need to worry about typing 'plt.' beforehand. It also imports several custom functions
+'''
+
+from matviz.histogram_utils import nhist, ndhist
+import pandas
+
+
+def test_test():
+    print("correct one ran")
+
+
+
+    assert True
