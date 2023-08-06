7041b275df5697877a40c50c94c5fa74cd03cd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:07 +0000

    Add package lazi/1.1.0.post13/lazi-1.1.0.post13.tar.gz

---
+++ packages/lazi/lazi-1.1.0.post13.tar.gz/lazi-1.1.0.post13/lazi/conf/conf.py
@@ -0,0 +1,23 @@
+from lazi.conf.base import *  # noqa
+
+
+def __load():
+    import sys
+    from pkgutil import iter_modules
+    from importlib import import_module
+    import lazi.conf as namespace
+
+    dic = sys.modules[__name__].__dict__
+    keys = set(key for key in dic.keys() if not key.startswith("_") and key.isupper())
+
+    for module_info in (mi for mi in iter_modules(namespace.__path__, namespace.__name__ + ".") if mi.name != __name__):
+        module = import_module(module_info.name)
+
+        for key in (key for key in keys if hasattr(module, key)):
+            dic[key] = getattr(module, key)  # TODO (As Needed): Check type and merge accordingly.
+
+    for key in keys:
+        setattr(namespace, key, dic[key])
+
+
+__load()
