7e7f28317635c9218317545f1675c446321adbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:22 +0000

    Add package qbandas/1.2.1/qbandas-1.2.1.tar.gz

---
+++ packages/qbandas/qbandas-1.2.1.tar.gz/qbandas-1.2.1/qbandas/__init__.py
@@ -0,0 +1,9 @@
+'''
+.. include:: ../README.md
+'''
+
+from .profiles import (_del_profile, _get_headers, _get_profile_path,
+                       is_valid_profile, list_profiles, set_profile)
+from .records import fetch_records, upload_records
+from .schemas import (_read_schema, _write_schema, add_schema_args,
+                      fetch_schema, list_schemas, set_schema_args)
