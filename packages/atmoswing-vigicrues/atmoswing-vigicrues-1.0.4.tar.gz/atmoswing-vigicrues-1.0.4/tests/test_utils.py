1cb8efa3d1877a5f34cec5ee68d369b6ccfde7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:01 +0000

    Add package atmoswing-vigicrues/1.0.4/atmoswing-vigicrues-1.0.4.tar.gz

---
+++ packages/atmoswing-vigicrues/atmoswing-vigicrues-1.0.4.tar.gz/atmoswing-vigicrues-1.0.4/tests/test_utils.py
@@ -0,0 +1,23 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+
+import atmoswing_vigicrues as asv
+
+
+def test_check_file_exists_fails():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        with pytest.raises(asv.Error):
+            asv.check_file_exists(tmp_dir)
+
+
+def test_check_file_exists_succeeds():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        tmp_file = tmp_dir + '/file.txt'
+        Path(tmp_file).touch()
+        asv.check_file_exists(tmp_file)
+
+
+def test_build_cumulative_frequency():
+    f = asv.utils.build_cumulative_frequency(100)
