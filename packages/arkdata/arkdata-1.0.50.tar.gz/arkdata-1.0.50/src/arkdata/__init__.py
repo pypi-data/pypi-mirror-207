3d879c703fe5a706ceb03b025215fb517c877154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:58 +0000

    Add package arkdata/1.0.50/arkdata-1.0.50.tar.gz

---
+++ packages/arkdata/arkdata-1.0.50.tar.gz/arkdata-1.0.50/src/arkdata/__init__.py
@@ -0,0 +1,33 @@
+from . import models
+from .import blueprints
+
+
+def create_all():
+    blueprints.create_all_blueprints()
+    models.create_all_models()
+
+
+def clear_all():
+    blueprints.clear_all_blueprints()
+    models.clear_all_models()
+
+
+def drop_all():
+    blueprints.drop_all_blueprints()
+    models.drop_all_models()
+
+
+def seed_all():
+    blueprints.seed_all_blueprints()
+    models.seed_all_models()
+
+
+def reset():
+    drop_all()
+    print("Dropped All Tables")
+    create_all()
+    print("Created All Tables")
+    seed_all()
+
+
+__all__ = ['models', 'blueprints']
