bc0ce8d46b5decb6e8faad3a31911650c42e33c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:26 +0000

    Add package accelbyte-py-sdk/0.40.0/accelbyte_py_sdk-0.40.0.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.40.0.tar.gz/accelbyte_py_sdk-0.40.0/accelbyte_py_sdk/api/dsmc/operations/server/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Dsm Controller Service."""
+
+__version__ = "6.2.7"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .deregister_local_server import DeregisterLocalServer
+from .get_server_session import GetServerSession
+from .register_local_server import RegisterLocalServer
+from .register_server import RegisterServer
+from .shutdown_server import ShutdownServer
