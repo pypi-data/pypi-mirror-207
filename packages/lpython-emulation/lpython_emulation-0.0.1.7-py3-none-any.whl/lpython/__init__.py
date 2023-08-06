0682afed27cc74ee97414e59c97e97d0ec13f55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:02 +0000

    Add package lpython-emulation/0.0.1.7/lpython_emulation-0.0.1.7-py3-none-any.whl

---
+++ packages/lpython-emulation/lpython_emulation-0.0.1.7-py3-none-any.whl/lpython/__init__.py
@@ -0,0 +1,1 @@
+from .lpython import i8, i16, i32, i64, f32, f64, c32, c64, CPtr, overload, ccall, TypeVar, pointer, c_p_pointer, Pointer, p_c_pointer, vectorize, inline, Union, static, with_goto, packed, Const, sizeof, ccallable, dataclass
