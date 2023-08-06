38602a3126714e90cdf7c933daea8da4a72088bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:50 +0000

    Add package llm-foundry/0.1.0/llm_foundry-0.1.0-py3-none-any.whl

---
+++ packages/llm-foundry/llm_foundry-0.1.0-py3-none-any.whl/llmfoundry/models/hf/hf_causal_lm.py
@@ -0,0 +1,95 @@
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Implements a Hugging Causal LM wrapped inside a :class:`.ComposerModel`."""
+
+from typing import Union
+
+from composer.metrics.nlp import (InContextLearningLMAccuracy,
+                                  InContextLearningLMExpectedCalibrationError,
+                                  InContextLearningMCExpectedCalibrationError,
+                                  InContextLearningMultipleChoiceAccuracy,
+                                  InContextLearningQAAccuracy,
+                                  LanguageCrossEntropy, LanguagePerplexity)
+from omegaconf import DictConfig
+from transformers import (AutoConfig, AutoModelForCausalLM, PreTrainedTokenizer,
+                          PreTrainedTokenizerFast)
+
+from llmfoundry.models.hf.model_wrapper import HuggingFaceModelWithZLoss
+from llmfoundry.models.utils import init_empty_weights
+
+__all__ = ['ComposerHFCausalLM']
+
+Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
+
+
+class ComposerHFCausalLM(HuggingFaceModelWithZLoss):
+    """Configures a :class:`.HuggingFaceModel` around a Causal LM.
+
+    Args:
+        cfg (DictConfig): An omegaconf dictionary used to configure the model:
+            cfg.pretrained_model_name_or_path (str): The name of or local path to
+                the HF Causal LM (e.g., `gpt2` to instantiate a GPT2LMHeadModel).
+            cfg.config_overrides (dict, optional): An optional dictionary of keyword
+                arguments that override the default configuration associated with
+                cfg.pretrained_model_name_or_path.
+            cfg.pretrained (bool): Whether to instantiate the model with pre-trained
+                weights coming from cfg.pretrained_model_name_or_path. If ``True``,
+                cfg.config_overrides must be compatible with the pre-trained weights.
+            cfg.init_device ('cpu' | 'meta'): Which device, 'cpu' or 'meta', to
+                initialize the model on. Currently, `meta` is only supported when
+                cfg.pretrained is ``False``. Default: ``'cpu'``.
+        tokenizer (PreTrainedTokenizer): The tokenizer that the model will use.
+    """
+
+    def __init__(self, om_model_config: DictConfig, tokenizer: Tokenizer):
+        config = AutoConfig.from_pretrained(
+            om_model_config.pretrained_model_name_or_path,
+            trust_remote_code=om_model_config.get('trust_remote_code', True),
+            use_auth_token=om_model_config.get('use_auth_token', False),
+            **om_model_config.get('config_overrides', {}))
+
+        train_metrics = [
+            LanguageCrossEntropy(len(tokenizer)),
+            LanguagePerplexity(len(tokenizer)),
+        ]
+        eval_metrics = [
+            LanguageCrossEntropy(len(tokenizer)),
+            LanguagePerplexity(len(tokenizer)),
+            InContextLearningLMAccuracy(),
+            InContextLearningMultipleChoiceAccuracy(),
+            InContextLearningQAAccuracy(),
+            InContextLearningLMExpectedCalibrationError(),
+            InContextLearningMCExpectedCalibrationError()
+        ]
+
+        init_device = om_model_config.get('init_device', 'cpu')
+        if init_device == 'cpu':
+            if om_model_config.pretrained:
+                model = AutoModelForCausalLM.from_pretrained(
+                    om_model_config.pretrained_model_name_or_path,
+                    trust_remote_code=om_model_config.get(
+                        'trust_remote_code', True),
+                    use_auth_token=om_model_config.get('use_auth_token', False),
+                    config=config)
+            else:
+                model = AutoModelForCausalLM.from_config(config)
+        elif init_device == 'meta':
+            if om_model_config.pretrained:
+                raise ValueError(
+                    'Setting cfg.pretrained=True is not supported when init_device="meta".'
+                )
+            with init_empty_weights(include_buffers=False):
+                model = AutoModelForCausalLM.from_config(config)
+        else:
+            raise ValueError(
+                f'init_device="{init_device}" must be either "cpu" or "meta".')
+
+        composer_model = super().__init__(model=model,
+                                          tokenizer=tokenizer,
+                                          metrics=train_metrics,
+                                          eval_metrics=eval_metrics,
+                                          z_loss=om_model_config.get(
+                                              'z_loss', 0.0))
+
+        return composer_model
---
+++ packages/llm-foundry/llm_foundry-0.1.0-py3-none-any.whl/llmfoundry/models/hf/hf_t5.py
@@ -0,0 +1,104 @@
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Implements a Hugging Face T5 wrapped inside a :class:`.ComposerModel`."""
+
+from __future__ import annotations
+
+from typing import Union
+
+from composer.metrics.nlp import LanguageCrossEntropy, MaskedAccuracy
+from omegaconf import DictConfig
+from transformers import (AutoConfig, PreTrainedTokenizer,
+                          PreTrainedTokenizerFast, T5ForConditionalGeneration)
+
+from llmfoundry.models.hf.model_wrapper import HuggingFaceModelWithZLoss
+from llmfoundry.models.utils import (adapt_tokenizer_for_denoising,
+                                     init_empty_weights)
+
+__all__ = ['ComposerHFT5']
+
+Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
+
+# HuggingFace hardcodes the ignore index to -100
+_HF_IGNORE_INDEX = -100
+
+
+class ComposerHFT5(HuggingFaceModelWithZLoss):
+    """Configures a :class:`.HuggingFaceModel` around a T5.
+
+    Note: This function uses `transformers.T5ForConditionalGenration`. Future releases
+        will expand support to more general classes of HF Encoder-Decoder models.
+
+    Args:
+        cfg (DictConfig): An omegaconf dictionary used to configure the model:
+            cfg.pretrained_model_name_or_path (str): The name of or local path to
+                the HF model (e.g., `t5-base` to instantiate a T5 using the base config).
+            cfg.config_overrides (dict, optional): An optional dictionary of keyword
+                arguments that override the default configuration associated with
+                cfg.pretrained_model_name_or_path. Default: ``{}``.
+            cfg.pretrained (bool): Whether to instantiate the model with pre-trained
+                weights coming from cfg.pretrained_model_name_or_path. If ``True``,
+                cfg.config_overrides must be compatible with the pre-trained weights.
+            cfg.init_device ('cpu' | 'meta'): Which device, 'cpu' or 'meta', to
+                initialize the model on. Currently, `meta` is only supported when
+                cfg.pretrained is ``False``. Default: ``'cpu'``.
+            cfg.z_loss (float, optional): The coefficient of the z-loss. If >0.0, this
+                the z-loss will be multiplied by this value before being added to the
+                standard loss term. Default: ``0.0``.
+            cfg.adapt_vocab_for_denoising (bool, optional):  Whether to adapt the vocab
+                of the model/tokenizer to include sentinel tokens that are used in denoising
+                tasks like Span Corruption. If you intend to load from an existing Composer
+                checkpoint that was trained on such a task, set this to ``True`` to ensure
+                that the model vocab size matches your checkpoint's vocab size when loading
+                the weights. Default: ``False``.
+        tokenizer (PreTrainedTokenizer): The tokenizer that the model will use.
+    """
+
+    def __init__(self, om_model_config: DictConfig, tokenizer: Tokenizer):
+        config = AutoConfig.from_pretrained(
+            om_model_config.pretrained_model_name_or_path,
+            **om_model_config.get('config_overrides', {}))
+
+        if not config.is_encoder_decoder:
+            raise ValueError(f'Model type "hf_t5" currently only supports T5 models ' +\
+                             f'using configs where `is_encoder_decoder` is ``True``.')
+
+        # Set up the tokenizer (add tokens for denoising sentinels if needed)
+        if om_model_config.get('adapt_vocab_for_denoising', False):
+            adapt_tokenizer_for_denoising(tokenizer)
+
+        vocab_size = len(tokenizer)
+
+        init_device = om_model_config.get('init_device', 'cpu')
+        if init_device == 'cpu':
+            if om_model_config.pretrained:
+                model = T5ForConditionalGeneration.from_pretrained(
+                    om_model_config.pretrained_model_name_or_path,
+                    config=config)
+            else:
+                model = T5ForConditionalGeneration(config)
+        elif init_device == 'meta':
+            if om_model_config.pretrained:
+                raise ValueError(
+                    'Setting cfg.pretrained=True is not supported when init_device="meta".'
+                )
+            with init_empty_weights(include_buffers=False):
+                model = T5ForConditionalGeneration(config)
+        else:
+            raise ValueError(
+                f'init_device="{init_device}" must be either "cpu" or "meta".')
+
+        metrics = [
+            LanguageCrossEntropy(vocab_size=vocab_size,
+                                 ignore_index=_HF_IGNORE_INDEX),
+            MaskedAccuracy(ignore_index=_HF_IGNORE_INDEX)
+        ]
+
+        composer_model = super().__init__(model=model,
+                                          tokenizer=tokenizer,
+                                          metrics=metrics,
+                                          z_loss=om_model_config.get(
+                                              'z_loss', 0.0))
+
+        return composer_model
