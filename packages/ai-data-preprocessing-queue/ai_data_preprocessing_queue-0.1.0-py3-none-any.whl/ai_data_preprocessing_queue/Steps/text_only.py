0a77f420b33c64c981d27c4bfaa626d2fce98279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package ai-data-preprocessing-queue/0.1.0/ai_data_preprocessing_queue-0.1.0-py3-none-any.whl

---
+++ packages/ai-data-preprocessing-queue/ai_data_preprocessing_queue-0.1.0-py3-none-any.whl/ai_data_preprocessing_queue/Steps/text_only.py
@@ -0,0 +1,8 @@
+import re
+from typing import Any, Dict, Optional
+
+
+def step(item: Any, itemState: Dict[str, Any], globalState: Optional[Dict[str, Any]], preprocessorData: str) -> Any:
+    item = re.sub(r"[^\w\s]", " ", item)
+    item = re.sub(r"""\d""", " ", item)
+    return item
