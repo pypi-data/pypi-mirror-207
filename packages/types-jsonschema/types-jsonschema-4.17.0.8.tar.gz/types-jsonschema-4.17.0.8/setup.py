1e5d190ff5c69c3ad344bf0dc5f440b75cdd590d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:55 +0000

    Add package types-jsonschema/4.17.0.8/types-jsonschema-4.17.0.8.tar.gz

---
+++ packages/types-jsonschema/types-jsonschema-4.17.0.8.tar.gz/types-jsonschema-4.17.0.8/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-jsonschema"
+description = "Typing stubs for jsonschema"
+long_description = '''
+## Typing stubs for jsonschema
+
+This is a PEP 561 type stub package for the `jsonschema` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`jsonschema`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/jsonschema. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `d1bfd08b4bc843227d097decfd99d70272a1f804`.
+'''.lstrip()
+
+setup(name=name,
+      version="4.17.0.8",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/jsonschema.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['jsonschema-stubs'],
+      package_data={'jsonschema-stubs': ['__init__.pyi', '_format.pyi', '_legacy_validators.pyi', '_types.pyi', '_utils.pyi', '_validators.pyi', 'cli.pyi', 'exceptions.pyi', 'protocols.pyi', 'validators.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
