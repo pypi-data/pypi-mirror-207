6126a6253ac3f147ae16b3d5059fc3169f30d6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:09 +0000

    Add package CalSciPy/0.2.7/CalSciPy-0.2.7.tar.gz

---
+++ packages/CalSciPy/CalSciPy-0.2.7.tar.gz/CalSciPy-0.2.7/src/CalSciPy/__init__.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "bruker",
+    "coloring",
+    "event_processing",
+    "imaging_processing",
+    "interactive_visuals",
+    "_io_tools.py",
+    "reorganization",
+    "static_visuals",
+    "trace_processing",
+    "validators",
+    "version"
+]
+
+from .version import author, name, maintainer, version
+
+author = author
+name = name
+maintainer = maintainer
+version = version
