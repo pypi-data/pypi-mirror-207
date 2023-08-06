368feff262b039e401a7a4d6107c7a553d0e77d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:29 +0000

    Add package intake-dataframe-catalog/0.0.7/intake_dataframe_catalog-0.0.7-py3-none-any.whl

---
+++ packages/intake-dataframe-catalog/intake_dataframe_catalog-0.0.7-py3-none-any.whl/intake_dataframe_catalog/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import intake
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
