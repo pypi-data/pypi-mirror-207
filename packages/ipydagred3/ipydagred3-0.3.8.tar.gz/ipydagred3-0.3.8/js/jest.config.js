446c3e266739afa2add5ac8255219bd46faaa3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:10 +0000

    Add package ipydagred3/0.3.8/ipydagred3-0.3.8.tar.gz

---
+++ packages/ipydagred3/ipydagred3-0.3.8.tar.gz/ipydagred3-0.3.8/js/jest.config.js
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2021, the ipydagred3 authors.
+ *
+ * This file is part of the ipydagred3 library, distributed under the terms of
+ * the Apache License 2.0.  The full license can be found in the LICENSE file.
+ *
+ */
+const esModules = ["@jupyterlab", "@jupyter-widgets", "lib0", "y-protocols", "dagre-d3-es", "d3", "internmap", "delaunator", "robust-predicates", "lodash-es"].join("|");
+
+module.exports = {
+  moduleDirectories: ["node_modules", "src", "tests"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/tests/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/fileMock.js",
+  },
+  reporters: [ "default", "jest-junit" ],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css",
+  },
+  transformIgnorePatterns: [`/node_modules/(?!(${esModules}))`],
+};
