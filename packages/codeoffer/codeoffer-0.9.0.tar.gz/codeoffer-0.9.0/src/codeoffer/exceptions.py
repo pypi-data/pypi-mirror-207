db49dc10c3227293906c4562d8a21d14d16b184d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:52 +0000

    Add package codeoffer/0.9.0/codeoffer-0.9.0.tar.gz

---
+++ packages/codeoffer/codeoffer-0.9.0.tar.gz/codeoffer-0.9.0/src/codeoffer/exceptions.py
@@ -0,0 +1,38 @@
+# AssetNotFoundException
+class AssetNotFoundException(Exception):
+    pass
+
+
+# BadRequestException
+class BadRequestException(Exception):
+    pass
+
+
+# ConflictException
+class ConflictException(Exception):
+    pass
+
+
+# ForbiddenException
+class ForbiddenException(Exception):
+    pass
+
+
+# InternalServerException
+class InternalServerException(Exception):
+    pass
+
+
+# NotFoundException
+class NotFoundException(Exception):
+    pass
+
+
+# TimeoutException
+class TimeoutException(Exception):
+    pass
+
+
+# UnauthorizedException
+class UnauthorizedException(Exception):
+    pass
