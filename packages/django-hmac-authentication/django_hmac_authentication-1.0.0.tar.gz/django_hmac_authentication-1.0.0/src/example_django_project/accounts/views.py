dbd980b9f088ac15cda1bda36b9fc276e6f9caf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:36 +0000

    Add package django-hmac-authentication/1.0.0/django_hmac_authentication-1.0.0.tar.gz

---
+++ packages/django-hmac-authentication/django_hmac_authentication-1.0.0.tar.gz/django_hmac_authentication-1.0.0/src/example_django_project/accounts/views.py
@@ -0,0 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class AnAuthenticatedView(APIView):
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
+
+    def post(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
