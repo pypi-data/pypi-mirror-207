e54cbb2cccff0b896bf17977b0de3f9e3082914e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:55 +0000

    Add package bioat/0.1.5.4/bioat-0.1.5.4-py3-none-any.whl

---
+++ packages/bioat/bioat-0.1.5.4-py3-none-any.whl/bioat/__init__.py
@@ -0,0 +1,16 @@
+"""Doc of bioat.
+
+"""
+from __future__ import annotations, absolute_import
+from bioat.about import about
+from bioat.bam import Bam
+from bioat.fastx import Fastx
+from bioat.hic import HiC
+from bioat.logger import set_logging_level
+from bioat.mgi import Mgi
+from bioat.system import System
+from bioat.table import Table
+from bioat.targeted_deep_sequencing import TargetedDeepSequencing
+from bioat.version import (__version__, __author__, __doc_format__)
+
+__name__ = "bioat"
---
+++ packages/bioat/bioat-0.1.5.4-py3-none-any.whl/bioat/cli.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+import fire
+from bioat import about, Bam, Fastx, HiC, Mgi, System, Table, TargetedDeepSequencing, __version__
+
+
+class Cli(object):
+    """Cli interface of python package <bioat>
+
+    - <bioat> is a commandline tool and a python package, which is short for
+        Bioinformatic Analysis Tools.
+    - <bioat> has many subcommand to deal with different bio-format:
+        BED, BAM, FASTA, FASTQ, VCF, et al.
+    """
+
+    def __init__(self):
+        self.bam = Bam()
+        # self.fastx = Fastx()
+        self.hic = HiC()
+        self.mgi = Mgi()
+        # self.system = System()
+        self.table = Table()
+        self.targeted_deep_sequencing = TargetedDeepSequencing()
+
+    @classmethod
+    def about(self):
+        """Print information about <bioat>.
+        """
+        return about
+
+    def list(self):
+        """Print commands and subcommands.
+        """
+        out = ""
+        for att in dir(self):
+            if not att.startswith('_'):
+                out += f'{att}\n'
+
+                for sub_att in dir(getattr(self, att)):
+                    if not sub_att.startswith('_'):
+                        out += f'  ├── {sub_att}\n'
+        return out
+    @classmethod
+    def version(self):
+        """Print version information.
+        """
+        return __version__
+
+
+def main() -> int:
+    calculator = Cli()
+    fire.Fire(calculator, name='bioat')
+
+
+if __name__ == '__main__':
+    main()
