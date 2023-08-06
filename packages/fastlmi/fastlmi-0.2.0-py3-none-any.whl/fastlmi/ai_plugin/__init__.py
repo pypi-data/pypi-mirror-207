9551359841ee620656485c5b11bf460ed5ae1ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:27 +0000

    Add package fastlmi/0.2.0/fastlmi-0.2.0-py3-none-any.whl

---
+++ packages/fastlmi/fastlmi-0.2.0-py3-none-any.whl/fastlmi/ai_plugin/__init__.py
@@ -0,0 +1,9 @@
+from .models import (
+    ManifestNoAuth,
+    ManifestOAuthAuth,
+    ManifestServiceHttpAuth,
+    ManifestUserHttpAuth,
+    PluginManifest,
+    ApiSpec,
+    BaseManifestAuth,
+)
