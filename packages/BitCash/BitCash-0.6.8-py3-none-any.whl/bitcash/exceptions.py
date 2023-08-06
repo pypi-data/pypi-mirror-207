1de23e735498aeb869df03afe6720cbc40244416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:21 +0000

    Add package BitCash/0.6.8/BitCash-0.6.8-py3-none-any.whl

---
+++ packages/BitCash/BitCash-0.6.8-py3-none-any.whl/bitcash/exceptions.py
@@ -0,0 +1,14 @@
+class InsufficientFunds(Exception):
+    pass
+
+
+class InvalidAddress(Exception):
+    pass
+
+
+class InvalidNetwork(Exception):
+    pass
+
+
+class InvalidEndpointURLProvided(Exception):
+    pass
