1c3f2338b70ad2419948909ecd2b56617fbdb0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package unisci/1.4.7/unisci-1.4.7.tar.gz

---
+++ packages/unisci/unisci-1.4.7.tar.gz/unisci-1.4.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+setup(
+    name='unisci',
+    version='1.4.7',
+    author='Vivaan Singhvi',
+    author_email='singhvi.vivaan@gmail.com',
+    description='Units Conversions, and Science Package',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    include_package_data=True,
+    project_urls={
+        "Documentation": "https://unisci.readthedocs.io/en/latest/"
+    },
+    install_requires=requirements
+)
