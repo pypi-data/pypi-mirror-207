4c724d6e650ac55f81c4db32340bedbe7079e500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:54 +0000

    Add package utilmy/0.1.16836407/utilmy-0.1.16836407-py3-none-any.whl

---
+++ packages/utilmy/utilmy-0.1.16836407-py3-none-any.whl/utilmy/tabular/causal/util_causal.py
@@ -0,0 +1,23 @@
+""""
+
+https://pypi.org/project/confounds/
+
+
+https://github.com/darya-chyzhyk/confound_prediction
+
+
+
+
+
+
+Bayesian Network Learning:
+
+
+https://erdogant.github.io/bnlearn/pages/html/UseCases.html
+
+https://causalnex.readthedocs.io/en/latest/
+
+
+
+
+"""
