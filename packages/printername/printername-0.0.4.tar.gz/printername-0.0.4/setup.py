3eed3a284214b40f6001701f47ed5604f741ff10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:39 +0000

    Add package printername/0.0.4/printername-0.0.4.tar.gz

---
+++ packages/printername/printername-0.0.4.tar.gz/printername-0.0.4/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="printername",
+    version="0.0.4",
+    author="Yitian",
+    author_email="123@456.com",
+    url="https://dev.azure.com/yliu9yl/pypi-test",
+    packages=[
+        "test"
+    ],
+    python_requires='>=3.6',
+)
