f35014803764198585c85e5cf5d008fcae77ae0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:32 +0000

    Add package mimeograph/0.4.3/mimeograph-0.4.3-py3-none-any.whl

---
+++ packages/mimeograph/mimeograph-0.4.3-py3-none-any.whl/mimeo/mimeo.py
@@ -0,0 +1,49 @@
+"""The Mimeo module.
+
+This module is a main module providing the most high level class
+in Mimeo:
+    * Mimeograph
+        A class responsible for the Mimeo processing.
+"""
+from __future__ import annotations
+
+import logging
+
+from mimeo.config.mimeo_config import MimeoConfig
+from mimeo.consumers import ConsumerFactory
+from mimeo.context import MimeoContextManager
+from mimeo.generators import GeneratorFactory
+
+logger = logging.getLogger(__name__)
+
+
+class Mimeograph:
+    """A class responsible for the Mimeo processing.
+
+    Based on the Mimeo Configuration it instantiates generator and
+    consumer to produce desired data.
+
+    Methods
+    -------
+    process()
+        Process the Mimeo Configuration (generate data and consume).
+    """
+
+    def __init__(
+            self,
+            mimeo_config: MimeoConfig,
+    ):
+        self._mimeo_config = mimeo_config
+        self._generator = GeneratorFactory.get_generator(self._mimeo_config)
+        self._consumer = ConsumerFactory.get_consumer(self._mimeo_config)
+
+    def process(
+            self,
+    ):
+        """Process the Mimeo Configuration (generate data and consume)."""
+        logger.info("Starting data generation")
+        with MimeoContextManager(self._mimeo_config):
+            for data in self._generator.generate(self._mimeo_config.templates):
+                data_str = self._generator.stringify(data)
+                self._consumer.consume(data_str)
+        logger.info("Data has been processed")
