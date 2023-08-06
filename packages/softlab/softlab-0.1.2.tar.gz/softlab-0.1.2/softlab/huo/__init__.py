267ab4ba6793a77d0894fac9f66439953059b4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:30 +0000

    Add package softlab/0.1.2/softlab-0.1.2.tar.gz

---
+++ packages/softlab/softlab-0.1.2.tar.gz/softlab-0.1.2/softlab/huo/__init__.py
@@ -0,0 +1,30 @@
+"""
+Submodule to organize dynamic processes
+
+
+- ``Action``: wrapper of action committed into scheduler
+- ``Scheduler``: abstract interface of scheduler, can not be instantiated
+                 directly
+- ``get_scheduler``: function to get instance of implemented scheduler
+- ``Process``: interface of any working process
+- ``CompositeProcess``, ``SeriesProcess``, ``ParallelProcess``,
+  ``SwitchProcess``, ``SweepProcess``: special implementations of ``Process``
+  to achieve compositions of subprocesses
+"""
+
+from softlab.huo.scheduler import (
+    Action,
+    Scheduler,
+)
+from softlab.huo.impl_scheduler import get_scheduler
+
+from softlab.huo.process import (
+    Process,
+    SimpleProcess,
+    CompositeProcess,
+    SeriesProcess,
+    ParallelProcess,
+    SwitchProcess,
+    SweepProcess,
+    run_process,
+)
