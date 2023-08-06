9084565fb13b6971a446f43df04a52ba84c5276e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:33 +0000

    Add package userfs/1.0.1/userfs-1.0.1-py3-none-any.whl

---
+++ packages/userfs/userfs-1.0.1-py3-none-any.whl/userfs/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=59cd3b0da37fbf4617d901d693ab06fc
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A system-bootstrapping automation and introspection tool."
+PKG_NAME = "userfs"
+VERSION = "1.0.1"
