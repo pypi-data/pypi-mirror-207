0846875b5a93f5d3fd9d98691e053ad2d6ed4386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:27 +0000

    Add package django-accounts-api/0.2.1a0/django_accounts_api-0.2.1a0-py3-none-any.whl

---
+++ packages/django-accounts-api/django_accounts_api-0.2.1a0-py3-none-any.whl/django_accounts_api/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import (
+    Login,
+    Logout,
+    PasswordChange,
+    PasswordReset,
+    PasswordResetConfirmView,
+    PasswordResetConfirm,
+    PasswordResetComplete,
+    login_check,
+    manifest
+)
+
+app_name = 'django_accounts_api'
+urlpatterns = [
+    path('', manifest, name='manifest'),
+    path('check', login_check, name='login_check'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', Logout.as_view(), name='logout'),
+    path('password_change', PasswordChange.as_view(), name='password_change'),
+    path('password_reset', PasswordReset.as_view(), name='password_reset'),
+    path(
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path('reset/done/', PasswordResetComplete.as_view(), name='password_reset_complete'),
+]
