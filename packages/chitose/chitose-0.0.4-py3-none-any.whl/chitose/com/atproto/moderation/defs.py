36f4ca62a0d7bae1a04b8d6f7e04004db894fbd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:03 +0000

    Add package chitose/0.0.4/chitose-0.0.4-py3-none-any.whl

---
+++ packages/chitose/chitose-0.0.4-py3-none-any.whl/chitose/com/atproto/moderation/defs.py
@@ -0,0 +1,18 @@
+# GENERATED CODE - DO NOT MODIFY
+""""""
+from __future__ import annotations
+import enum
+
+class ReasonType(enum.Enum):
+    REASON_SPAM = 'com.atproto.moderation.defs#reasonSpam'
+    REASON_VIOLATION = 'com.atproto.moderation.defs#reasonViolation'
+    REASON_MISLEADING = 'com.atproto.moderation.defs#reasonMisleading'
+    REASON_SEXUAL = 'com.atproto.moderation.defs#reasonSexual'
+    REASON_RUDE = 'com.atproto.moderation.defs#reasonRude'
+    REASON_OTHER = 'com.atproto.moderation.defs#reasonOther'
+REASON_SPAM = 'com.atproto.moderation.defs#reasonSpam'
+REASON_VIOLATION = 'com.atproto.moderation.defs#reasonViolation'
+REASON_MISLEADING = 'com.atproto.moderation.defs#reasonMisleading'
+REASON_SEXUAL = 'com.atproto.moderation.defs#reasonSexual'
+REASON_RUDE = 'com.atproto.moderation.defs#reasonRude'
+REASON_OTHER = 'com.atproto.moderation.defs#reasonOther'
