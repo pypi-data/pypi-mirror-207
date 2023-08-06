1a064825f168151184316662db7e6a7a8c9d54e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:19 +0000

    Add package html-msg/1.0.0/html_msg-1.0.0-py3-none-any.whl

---
+++ packages/html-msg/html_msg-1.0.0-py3-none-any.whl/html_msg/__init__.py
@@ -0,0 +1,10 @@
+from html_message import HTMLMessage
+from html_table import HTMLTable
+
+# Version of the html_msg package 
+__version__= "1.0.0"
+
+__all__ = [
+    'HTMLMessage',
+    'HTMLTable'
+]
