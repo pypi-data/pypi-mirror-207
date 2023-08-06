8f9829d9aae22c560a25afa7e89871be72959fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:00 +0000

    Add package serdeserf/0.1/serdeserf-0.1.tar.gz

---
+++ packages/serdeserf/serdeserf-0.1.tar.gz/serdeserf-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="serdeserf",
+    version="0.1",
+    description="don't use it, even if you have to",
+    packages=["serdeserf"],
+    author_email="kirkat6@gmail.com",
+    zip_safe=False,
+)
