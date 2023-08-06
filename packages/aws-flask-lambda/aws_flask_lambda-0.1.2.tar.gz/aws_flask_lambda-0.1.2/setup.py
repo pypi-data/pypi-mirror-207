0e35e40a0c2aff8b5163c5e265d19b3f3d222c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:15 +0000

    Add package aws-flask-lambda/0.1.2/aws_flask_lambda-0.1.2.tar.gz

---
+++ packages/aws-flask-lambda/aws_flask_lambda-0.1.2.tar.gz/aws_flask_lambda-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aws_flask_lambda',
+    version='0.1.2',
+    description='Library to run applications on AWS Lambda Function using Flask',
+    author='Seven Clouds Technologies',
+    author_email='admin@seventechnologies.cloud',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=["aws_flask_lambda"],
+    package_dir={"aws_flask_lambda": "aws_flask_lambda"},
+    include_package_data=True,
+    install_requires=[
+        'Flask==2.0.2',
+        'werkzeug==2.0.2'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
