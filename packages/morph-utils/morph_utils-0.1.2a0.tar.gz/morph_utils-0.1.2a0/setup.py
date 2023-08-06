2a0e219833cc679b6e17ea3572eee3094c43c71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:17 +0000

    Add package morph-utils/0.1.2a0/morph_utils-0.1.2a0.tar.gz

---
+++ packages/morph-utils/morph_utils-0.1.2a0.tar.gz/morph_utils-0.1.2a0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+with open(readme_path, "r") as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+setup(
+    name = 'morph_utils',
+    version = '0.1.2a',
+    description = "Functions for common and not so common morphology operations",
+    long_description=readme,
+    author = "Matthew Mallory",
+    author_email = "matt.mallory@alleninstitute.org",
+    url = '',
+    packages = find_packages(),
+    install_requires = required,
+    include_package_data=True,
+    setup_requires=['pytest-runner'],
+)
