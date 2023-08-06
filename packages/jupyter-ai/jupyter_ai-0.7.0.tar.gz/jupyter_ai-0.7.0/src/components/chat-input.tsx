0f3f19c1f3d93d61901c658276b554c34d907c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:41 +0000

    Add package jupyter-ai/0.7.0/jupyter_ai-0.7.0.tar.gz

---
+++ packages/jupyter-ai/jupyter_ai-0.7.0.tar.gz/jupyter_ai-0.7.0/src/components/chat-input.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import {
+  Box,
+  SxProps,
+  TextField,
+  Theme,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  IconButton,
+  InputAdornment
+} from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+
+type ChatInputProps = {
+  value: string;
+  onChange: (newValue: string) => unknown;
+  onSend: () => unknown;
+  hasSelection: boolean;
+  includeSelection: boolean;
+  toggleIncludeSelection: () => unknown;
+  replaceSelection: boolean;
+  toggleReplaceSelection: () => unknown;
+  helperText: JSX.Element
+  sx?: SxProps<Theme>;
+};
+
+export function ChatInput(props: ChatInputProps): JSX.Element {
+  
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter' && event.shiftKey) {
+      props.onSend();
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+  return (
+    <Box sx={props.sx}>
+      <Box sx={{ display: 'flex'}}>
+        <TextField
+          value={props.value}
+          onChange={e => props.onChange(e.target.value)}
+          fullWidth
+          variant="outlined"
+          multiline
+          onKeyDown={handleKeyDown}
+          InputProps={{
+            endAdornment: (
+               <InputAdornment position="end">
+                  <IconButton size="small" color="primary" onClick={props.onSend} disabled={!props.value.trim().length}>
+                     <SendIcon />
+                  </IconButton>
+               </InputAdornment>
+            )
+         }}
+         FormHelperTextProps={{
+          sx: {marginLeft: 'auto', marginRight: 0}
+         }}
+         helperText={props.value.length > 2 ? props.helperText : ' '}
+        />
+      </Box>
+      {props.hasSelection && (
+        <FormGroup sx={{ display: 'flex', flexDirection: 'row' }}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={props.includeSelection}
+                onChange={props.toggleIncludeSelection}
+              />
+            }
+            label="Include selection"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={props.replaceSelection}
+                onChange={props.toggleReplaceSelection}
+              />
+            }
+            label="Replace selection"
+          />
+        </FormGroup>
+      )}
+    </Box>
+  );
+}
