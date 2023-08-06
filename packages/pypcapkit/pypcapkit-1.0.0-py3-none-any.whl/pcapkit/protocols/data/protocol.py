b7c50837866dcef3b31429189ad0c88a6787346c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:18 +0000

    Add package pypcapkit/1.0.0/pypcapkit-1.0.0-py3-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.0.0-py3-none-any.whl/pcapkit/protocols/data/protocol.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""data models for root protocol"""
+
+from typing import TYPE_CHECKING
+
+from pcapkit.protocols.data.data import Data
+
+__all__ = [
+    'Packet',
+]
+
+
+class Packet(Data):
+    """Header and payload data."""
+
+    #: packet header
+    header: 'bytes'
+    #: packet payload
+    payload: 'bytes'
+
+    if TYPE_CHECKING:
+        def __init__(self, header: 'bytes', payload: 'bytes') -> 'None': ...  # pylint: disable=unused-argument,super-init-not-called,multiple-statements
