744f3188934e9202a9cde2c0b118a3ea848aeef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:02 +0000

    Add package wheecode/0.5.0/wheecode-0.5.0.tar.gz

---
+++ packages/wheecode/wheecode-0.5.0.tar.gz/wheecode-0.5.0/setup.py
@@ -0,0 +1,45 @@
+from os import path
+from setuptools import setup, find_packages
+
+INFO = {'name': 'wheecode',
+        'version': '0.5.0',
+        'description': 'Simple GCode generator',
+        'author': 'Wheeeeeeeee!!',
+        'author_email': '0xloem@gmail.com',
+        }
+
+here = path.abspath(path.dirname(__file__))
+
+'''gather install package requirements'''
+with open(path.join(here, 'requirements.txt')) as requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    requirements = [line for line in requirements_file.read().splitlines()
+                    if not line.startswith('#')]
+    
+requirements = [r for r in requirements if not r.startswith('git+')]
+
+'''gather development requirements'''
+with open(path.join(here, 'requirements.dev.txt')) as dev_requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    dev_requirements = [line for line in dev_requirements_file.read().splitlines()
+                    if not line.startswith('#')]
+    
+dev_requirements = [r for r in dev_requirements if not r.startswith('git+')]
+
+setup(
+    name=INFO['name'],
+    version=INFO['version'],
+    description=INFO['description'],
+    author=INFO['author'],
+    author_email=INFO['author_email'],
+    packages=find_packages(),
+    url='https://github.com/xloem/wheecode',
+    download_url='https://github.com/xloem/wheecode/tarball/main',
+    keywords=['gcode', '3dprinting', 'cnc', 'reprap', 'additive'],
+    zip_safe=False,
+    package_data = {
+        '': ['*.txt', '*.md'],
+    },
+    install_requires=requirements,
+    tests_require=dev_requirements,
+)
