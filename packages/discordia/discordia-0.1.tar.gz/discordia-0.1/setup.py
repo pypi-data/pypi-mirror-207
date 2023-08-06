74e4ba93c614cfc5940479552cefeff748764cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:16 +0000

    Add package discordia/0.1/discordia-0.1.tar.gz

---
+++ packages/discordia/discordia-0.1.tar.gz/discordia-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='discordia',
+    version='0.1',
+    description='A Discord bot API wrapper',
+    packages=['discordia'],
+    install_requires=[
+        'requests',
+    ],
+)
