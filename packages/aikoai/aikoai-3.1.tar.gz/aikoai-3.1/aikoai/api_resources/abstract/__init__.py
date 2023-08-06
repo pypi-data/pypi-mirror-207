3fc85668e0f964d8bb1882c5abeae45fe6dcc1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:55 +0000

    Add package aikoai/3.1/aikoai-3.1.tar.gz

---
+++ packages/aikoai/aikoai-3.1.tar.gz/aikoai-3.1/aikoai/api_resources/abstract/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+from aikoai.api_resources.abstract.api_resource import APIResource
+from aikoai.api_resources.abstract.createable_api_resource import CreateableAPIResource
+from aikoai.api_resources.abstract.deletable_api_resource import DeletableAPIResource
+from aikoai.api_resources.abstract.listable_api_resource import ListableAPIResource
+from aikoai.api_resources.abstract.nested_resource_class_methods import (
+    nested_resource_class_methods,
+)
+from aikoai.api_resources.abstract.updateable_api_resource import UpdateableAPIResource
