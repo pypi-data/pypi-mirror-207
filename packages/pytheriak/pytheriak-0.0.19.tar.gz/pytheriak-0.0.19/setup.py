cf47534fc135b97068c926ddac8959a43079dda1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package pytheriak/0.0.19/pytheriak-0.0.19.tar.gz

---
+++ packages/pytheriak/pytheriak-0.0.19.tar.gz/pytheriak-0.0.19/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+setup(
+    name="pytheriak",
+    version="0.0.19",
+    description="Wrappers to call and read_out theriak from python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["pytheriak.wrapper", "pytheriak.hdfwriter"],
+    package_dir={"": "src"},
+    python_requires=">=3.10",
+    install_requires=["numpy >= 1.23", ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering"]
+)
