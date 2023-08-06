a206d1b3c1bbd1a0380408438e15b8761ec61865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:21:25 +0000

    Add package pihole5-list-tool/0.6.2/pihole5-list-tool-0.6.2.tar.gz

---
+++ packages/pihole5-list-tool/pihole5-list-tool-0.6.2.tar.gz/pihole5-list-tool-0.6.2/ph5lt/banner.py
@@ -0,0 +1,19 @@
+""" silly ansi banner"""
+
+from colors import color
+
+from ph5lt import utils
+
+__version__ = "0.6.2"
+
+
+def display():
+    """display the banner"""
+    print(color("    ┌──────────────────────────────────────────┐", fg="#b61042"))
+    print(
+        color("    │       ", fg="#b61042")
+        + color(f"π-hole 5 list tool  v{__version__}", "#FFF")
+        + color("         │", fg="#b61042")
+    )
+    print(color("    └──────────────────────────────────────────┘", fg="#b61042"))
+    utils.info("    https://github.com/jessedp/pihole5-list-tool\n")
