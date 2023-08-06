aed3c9e3f917f954e1cb3df633192eeab0e51413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:10 +0000

    Add package griptape-tools/0.10.1/griptape_tools-0.10.1-py3-none-any.whl

---
+++ packages/griptape-tools/griptape_tools-0.10.1-py3-none-any.whl/griptape/tools/sql_client/tool.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from attr import define, field
+from griptape.artifacts import BaseArtifact, TextArtifact, ErrorArtifact
+from griptape.core import BaseTool
+from griptape.core.decorators import activity
+from schema import Schema, Literal
+
+
+@define
+class SqlClient(BaseTool):
+    engine_url: Optional[str] = field(default=None, kw_only=True, metadata={"env": "ENGINE_URL"})
+    engine_name: str = field(kw_only=True)
+
+    @property
+    def schema_template_args(self) -> dict:
+        return {
+            "engine": self.engine_name
+        }
+
+    @activity(config={
+        "name": "query",
+        "description": "Can be used to execute SQL queries in {{ engine }}",
+        "schema": Schema({
+            Literal(
+                "query",
+                description="SQL query to execute. For example, SELECT, CREATE, INSERT, DROP, DELETE, etc."
+            ): str
+        })
+    })
+    def query(self, params: dict) -> BaseArtifact:
+        from sqlalchemy import create_engine, text
+
+        query = params["values"]["query"]
+        engine = create_engine(self.env_value("ENGINE_URL"))
+
+        try:
+            with engine.connect() as con:
+                results = con.execute(text(query))
+
+                if results.returns_rows:
+                    return TextArtifact(str([row for row in results]).strip('[]'))
+                else:
+                    return TextArtifact("query successfully executed")
+
+        except Exception as e:
+            return ErrorArtifact(f"error executing SQL: {e}")
