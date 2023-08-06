7fe0029a21720beca01ff91a0973078422be65dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:33 +0000

    Add package grizzly-loadtester/2.6.5/grizzly_loadtester-2.6.5-py3-none-any.whl

---
+++ packages/grizzly-loadtester/grizzly_loadtester-2.6.5-py3-none-any.whl/grizzly/users/restapi.py
@@ -0,0 +1,295 @@
+# pylint: disable=line-too-long
+'''Communicates with HTTP and HTTPS, with built-in support for Azure authenticated endpoints.
+
+## Request methods
+
+Supports the following request methods:
+
+* get
+* put
+* post
+
+## Format
+
+Format of `host` is the following:
+
+``` plain
+http[s]://<hostname>
+```
+
+## Examples
+
+Example on how to use it in a scenario:
+
+``` gherkin
+Given a user of type "RestApi" load testing "https://api.example.com"
+Then post request "test/request.j2.json" to endpoint "/api/test"
+Then get request from endpoint "/api/test"
+```
+
+To change how often the token should be refreshed, default is 3000 seconds:
+``` gherkin
+And set context variable "auth.refresh_time" to "3500"
+```
+
+### Authentication
+
+#### Client secret
+
+``` gherkin
+Given a user of type "RestApi" load testing "https://api.example.com"
+And set context variable "auth.provider" to "<provider>"
+And set context variable "auth.client.id" to "<client id>"
+And set context variable "auth.client.secret" to "<client secret>"
+And set context variable "auth.client.resource" to "<resource url/guid>"
+```
+
+#### Username and password
+
+`auth.user.redirect_uri` needs to correspond to the endpoint that the client secret is registrered for.
+
+``` gherkin
+Given a user of type "RestApi" load testing "https://api.example.com"
+And set context variable "auth.provider" to "<provider>"
+And set context variable "auth.client.id" to "<client id>"
+And set context variable "auth.user.username" to "alice@example.onmicrosoft.com"
+And set context variable "auth.user.password" to "HemL1gaArn3!"
+And set context variable "auth.user.redirect_uri" to "/app-registrered-redirect-uri"
+```
+
+#### Multipart/form-data
+
+RestApi supports posting of multipart/form-data content-type, and in that case additional arguments needs to be passed with the request:
+
+* `multipart_form_data_name` _str_ - the name of the input form
+
+* `multipart_form_data_filename` _str_ - the filename
+
+Example:
+
+``` gherkin
+Then post request "path/my_template.j2.xml" with name "FormPost" to endpoint "example.url.com | content_type=multipart/form-data, multipart_form_data_filename=my_filename, multipart_form_data_name=form_name"
+```
+'''  # noqa: E501
+import json
+
+from typing import Dict, Optional, Any, Tuple, Union, cast
+from time import time
+from abc import ABCMeta
+
+from locust.contrib.fasthttp import FastHttpSession
+
+from grizzly_extras.transformer import TransformerContentType
+
+from grizzly.types import GrizzlyResponse, RequestType, RequestMethod, GrizzlyResponseContextManager
+from grizzly.types.locust import Environment, StopUser
+from grizzly.utils import merge_dicts, safe_del
+from grizzly.tasks import RequestTask
+from grizzly.clients import ResponseEventSession
+from grizzly.auth import GrizzlyHttpAuthClient, AAD, refresh_token
+from locust.user.users import UserMeta
+
+from .base import RequestLogger, ResponseHandler, GrizzlyUser, HttpRequests, AsyncRequests
+from . import logger
+
+from urllib3 import disable_warnings as urllib3_disable_warnings
+urllib3_disable_warnings()
+
+
+class RestApiUserMeta(UserMeta, ABCMeta):
+    pass
+
+
+class RestApiUser(ResponseHandler, RequestLogger, GrizzlyUser, HttpRequests, AsyncRequests, GrizzlyHttpAuthClient, metaclass=RestApiUserMeta):  # type: ignore[misc]
+    session_started: Optional[float]
+    headers: Dict[str, str]
+    environment: Environment
+
+    _context = {
+        'verify_certificates': True,
+        'auth': {
+            'refresh_time': 3000,
+            'provider': None,
+            'client': {
+                'id': None,
+                'secret': None,
+                'resource': None,
+            },
+            'user': {
+                'username': None,
+                'password': None,
+                'redirect_uri': None,
+                'initialize_uri': None,
+            },
+        },
+        'metadata': None,
+    }
+
+    def __init__(self, environment: Environment, *args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> None:
+        super().__init__(environment, *args, **kwargs)
+
+        self.headers: Dict[str, str] = {
+            'Content-Type': 'application/json',
+            'x-grizzly-user': self.__class__.__name__,
+        }
+
+        self.session_started = None
+        self._context = merge_dicts(
+            super().context(),
+            # this is needed since we create a new class with this class as sub class, context will be messed up otherwise
+            # in other words, don't use RestApiUser._context. This should only be used in classes which are direct created
+            # in grizzly
+            self.__class__._context,
+        )
+
+        metadata = self._context.get('metadata', None)
+        if metadata is not None:
+            metadata = cast(Dict[str, str], metadata)
+            self.headers.update(metadata)
+
+        self.parent = None
+        self.cookies = {}
+
+    def on_start(self) -> None:
+        super().on_start()
+
+        self.session_started = time()
+
+    def get_error_message(self, response: GrizzlyResponseContextManager) -> str:
+        if response.text is None:
+            return f'unknown response {type(response)}'
+
+        if len(response.text) < 1:
+            if response.status_code == 401:
+                message = 'unauthorized'
+            elif response.status_code == 403:
+                message = 'forbidden'
+            elif response.status_code == 404:
+                message = 'not found'
+            else:
+                message = 'unknown'
+        else:
+            try:
+                payload = json.loads(response.text)
+
+                # special handling for dynamics error messages
+                if 'Message' in payload:
+                    message = payload['Message'].split('\\n', 1)[0].replace('\n', ' ')
+                elif 'error_description' in payload:
+                    message = payload['error_description'].split('\r\n')[0]
+                else:
+                    message = response.text
+            except json.decoder.JSONDecodeError:
+                message = response.text
+
+        return message
+
+    def async_request(self, request: RequestTask) -> GrizzlyResponse:
+        client = FastHttpSession(
+            environment=self.environment,
+            base_url=self.host,
+            user=self,
+            insecure=not self._context.get('verify_certificates', True),
+            max_retries=1,
+            connection_timeout=60.0,
+            network_timeout=60.0,
+        )
+
+        return self._request(client, request)
+
+    def request(self, request: RequestTask) -> GrizzlyResponse:
+        return self._request(self.client, request)
+
+    @refresh_token(AAD)
+    def _request(self, client: Union[FastHttpSession, ResponseEventSession], request: RequestTask) -> GrizzlyResponse:
+        if request.method not in [RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST]:
+            raise NotImplementedError(f'{request.method.name} is not implemented for {self.__class__.__name__}')
+
+        if request.response.content_type == TransformerContentType.UNDEFINED:
+            request.response.content_type = TransformerContentType.JSON
+        elif request.response.content_type == TransformerContentType.XML:
+            self.headers['Content-Type'] = 'application/xml'
+        elif request.response.content_type == TransformerContentType.MULTIPART_FORM_DATA:
+            if 'Content-Type' in self.headers:
+                del self.headers['Content-Type']
+
+        request_name, endpoint, payload, arguments, metadata = self.render(request)
+
+        if metadata is not None:
+            self.headers.update(metadata)
+
+        parameters: Dict[str, Any] = {'headers': self.headers}
+
+        url = f'{self.host}{endpoint}'
+
+        # only render endpoint once, so needs to be done here
+        if isinstance(client, ResponseEventSession):
+            parameters.update({
+                'request': request,
+                'verify': self._context.get('verify_certificates', True),
+            })
+
+        name = f'{request.scenario.identifier} {request_name}'
+
+        if payload is not None:
+            if request.response.content_type == TransformerContentType.JSON:
+                try:
+                    parameters['json'] = json.loads(payload)
+                except json.decoder.JSONDecodeError as exception:
+                    # so that locust treats it as a failure
+                    self.environment.events.request.fire(
+                        request_type=RequestType.from_method(request.method),
+                        name=name,
+                        response_time=0,
+                        response_length=0,
+                        context=self._context,
+                        exception=exception,
+                    )
+                    logger.error(f'{url}: failed to decode: {payload=}')
+
+                    # this is a fundemental error, so we'll always stop the user
+                    raise StopUser()
+            elif request.response.content_type == TransformerContentType.MULTIPART_FORM_DATA and arguments:
+                parameters['files'] = {arguments['multipart_form_data_name']: (arguments['multipart_form_data_filename'], payload)}
+            else:
+                parameters['data'] = bytes(payload, 'UTF-8')
+
+        with client.request(
+            method=request.method.name,
+            name=name,
+            url=url,
+            catch_response=True,
+            cookies=self.cookies,
+            **parameters,
+        ) as response:
+            if not isinstance(client, ResponseEventSession):
+                # monkey patch in request body... not available otherwise
+                setattr(response, 'request_body', payload)
+
+                self.response_event.fire(
+                    name=name,
+                    request=request,
+                    context=response,
+                    user=self,
+                )
+
+            if response._manual_result is None:
+                if response.status_code in request.response.status_codes:
+                    response.success()
+                else:
+                    message = self.get_error_message(response)
+                    response.failure(f'{response.status_code} not in {request.response.status_codes}: {message}')
+
+            if response._manual_result is not True and request.scenario.failure_exception is not None:
+                raise request.scenario.failure_exception()
+
+            headers = dict(response.headers.items()) if response.headers not in [None, {}] else None
+
+            return headers, response.text
+
+    def add_context(self, context: Dict[str, Any]) -> None:
+        # something change in auth context, we need to re-authenticate
+        if context.get('auth', {}).get('user', {}).get('username', None) is not None:
+            safe_del(self.headers, 'Authorization')
+
+        super().add_context(context)
---
+++ packages/grizzly-loadtester/grizzly_loadtester-2.6.5-py3-none-any.whl/grizzly/users/sftp.py
@@ -0,0 +1,188 @@
+'''Communicates with Secure File Transport Protocol.
+
+!!! attention
+    Both local and remote files will be overwritten if they already exists. Downloaded files will be stored in `requests/download`.
+
+## Request methods
+
+Supports the following request methods:
+
+* put
+* get
+
+## Format
+
+Format of `host` is the following:
+
+``` plain
+sftp://<host>[:<port>]
+```
+
+## Examples
+
+Example of how to use it in a scenario:
+
+``` gherkin
+Given a user of type "Sftp" load testing "sftp://sftp.example.com"
+And set context variable "auth.username" to "bob"
+And set context variable "auth.password" to "great-scott-42-file-bar"
+Then put request "test/blob.file" to endpoint "/pub/blobs"
+Then get request from endpoint "/pub/blobs/blob.file"
+```
+'''
+from typing import Any, Dict, Tuple, Optional, Union
+from urllib.parse import urlparse
+from time import perf_counter as time
+from os import path, environ, mkdir
+
+from paramiko import SFTPClient
+
+from grizzly.types import RequestMethod, GrizzlyResponse, RequestType
+from grizzly.types.locust import Environment, StopUser
+from grizzly.utils import merge_dicts
+from grizzly.clients import SftpClientSession
+from grizzly.tasks import RequestTask
+
+from .base import GrizzlyUser, FileRequests, ResponseHandler, RequestLogger
+
+
+class SftpUser(ResponseHandler, RequestLogger, GrizzlyUser, FileRequests):
+    _context: Dict[str, Any] = {
+        'auth': {
+            'username': None,
+            'password': None,
+            'key_file': None,
+        }
+    }
+
+    _auth_context: Dict[str, Any]
+
+    _context_root: str
+    _payload_root: str
+
+    host: str
+    port: int
+
+    sftp_client: SftpClientSession
+    session: SFTPClient
+
+    def __init__(self, environment: Environment, *args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> None:
+        super().__init__(environment, *args, **kwargs)
+
+        self._context = merge_dicts(super().context(), self.__class__._context)
+
+        self._payload_root = path.join(environ.get('GRIZZLY_CONTEXT_ROOT', '.'), 'requests')
+        self._download_root = path.join(self._payload_root, 'download')
+
+        if not path.exists(self._download_root):
+            mkdir(self._download_root)
+
+        parsed = urlparse(self.host or '')
+
+        if parsed.scheme != 'sftp':
+            raise ValueError(f'{self.__class__.__name__}: "{parsed.scheme}" is not supported')
+
+        if parsed.username is not None or parsed.password is not None:
+            raise ValueError(f'{self.__class__.__name__}: username and password should be set via context variables "auth.username" and "auth.password"')
+
+        if len(parsed.path) > 0:
+            raise ValueError(f'{self.__class__.__name__}: only hostname and port should be included as host')
+
+        # should read key?
+        if self._context['auth']['key_file'] is not None:
+            raise NotImplementedError(f'{self.__class__.__name__}: key authentication is not implemented')
+
+        host = parsed.netloc
+        if ':' in host:
+            [host, _] = host.split(':', 1)
+
+        self.host = host
+        self.port = int(parsed.port) if parsed.port is not None else 22
+        username = self._context['auth']['username']
+        password = self._context['auth']['password']
+        key_file = self._context['auth']['key_file']
+
+        if username is None:
+            raise ValueError(f'{self.__class__.__name__}: "auth.username" context variable is not set')
+
+        if password is None and key_file is None:
+            raise ValueError(f'{self.__class__.__name__}: "auth.password" or "auth.key" context variable must be set')
+
+    def on_start(self) -> None:
+        super().on_start()
+        self.sftp_client = SftpClientSession(self.host, self.port)
+        self.session = self.sftp_client.session(**self._context['auth']).__enter__()
+
+    def on_stop(self) -> None:
+        self.session.__exit__(None, None, None)
+        super().on_stop()
+
+    def request(self, request: RequestTask) -> GrizzlyResponse:
+        request_name, endpoint, payload, _, _ = self.render(request)
+
+        name = f'{request.scenario.identifier} {request_name}'
+
+        exception: Optional[Exception] = None
+        headers: Dict[str, Union[str, int]] = {}
+        response_length = 0
+        start_time = time()
+
+        try:
+            def get_response_length(transferred: int, total: int) -> None:
+                nonlocal response_length
+                response_length = transferred
+
+            if request.method == RequestMethod.PUT:
+                if payload is None:
+                    raise ValueError(f'{self.__class__.__name__}: request "{name}" does not have a payload, incorrect method specified')
+
+                payload = path.realpath(path.join(self._payload_root, payload))
+                self.session.put(payload, endpoint, get_response_length)
+            elif request.method == RequestMethod.GET:
+                file_name = path.basename(endpoint)
+                # @TODO: if endpoint is a directory, should we download all files in there?
+                self.session.get(endpoint, path.join(self._download_root, file_name), get_response_length)
+            else:
+                raise NotImplementedError(f'{self.__class__.__name__} has not implemented {request.method.name}')
+        except Exception as e:
+            exception = e
+        finally:
+            response_time = int((time() - start_time) * 1000)
+            headers = {
+                'method': request.method.name.lower(),
+                'time': response_time,
+                'host': self.host or '',
+                'path': endpoint,
+            }
+
+            try:
+                self.response_event.fire(
+                    name=name,
+                    request=request,
+                    context=(
+                        headers,
+                        payload,
+                    ),
+                    user=self,
+                    exception=exception,
+                )
+            except Exception as e:
+                if exception is None:
+                    exception = e
+
+            self.environment.events.request.fire(
+                request_type=RequestType.from_method(request.method),
+                name=name,
+                response_time=response_time,
+                response_length=response_length,
+                context=self._context,
+                exception=exception,
+            )
+
+            if exception is not None:
+                if isinstance(exception, NotImplementedError):
+                    raise StopUser()
+                elif request.scenario.failure_exception is not None:
+                    raise request.scenario.failure_exception()
+
+            return None, payload
