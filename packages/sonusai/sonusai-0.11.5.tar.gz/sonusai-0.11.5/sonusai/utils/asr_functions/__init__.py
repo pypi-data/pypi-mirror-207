1df31a670478b48318050d21043465d973727ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:34 +0000

    Add package sonusai/0.11.5/sonusai-0.11.5.tar.gz

---
+++ packages/sonusai/sonusai-0.11.5.tar.gz/sonusai-0.11.5/sonusai/utils/asr_functions/__init__.py
@@ -0,0 +1,3 @@
+from sonusai.utils.asr_functions.deepgram import deepgram
+from sonusai.utils.asr_functions.google import google
+from sonusai.utils.asr_functions.whisper import whisper
---
+++ packages/sonusai/sonusai-0.11.5.tar.gz/sonusai-0.11.5/sonusai/utils/asr_functions/data.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from typing import Any
+
+from sonusai.mixture.types import AudioT
+
+
+@dataclass(frozen=True)
+class Data:
+    audio: AudioT
+    whisper_model: Any = None
+    whisper_model_name: str = None
