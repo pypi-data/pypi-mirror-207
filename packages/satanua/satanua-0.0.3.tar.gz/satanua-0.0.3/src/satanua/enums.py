9d255a50a56244b5a86dbd21e4e859305199af03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:14 +0000

    Add package satanua/0.0.3/satanua-0.0.3.tar.gz

---
+++ packages/satanua/satanua-0.0.3.tar.gz/satanua-0.0.3/src/satanua/enums.py
@@ -0,0 +1,11 @@
+class DecoderModes:
+    AND_OR = "AND"
+    OR_AND = "OR"
+
+class LogicGateTypes:
+    AND = "Logic AND"
+    OR = "Logic OR"
+    NOT = "Logic NOT"
+    XOR = "Logic XOR"
+    NOR = "Logic NOR"
+    NAND = "Logic NAND"
