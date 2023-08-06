6788e6dc99fb39c55f1c702037fe385feef6d35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:43 +0000

    Add package prunerr/1.1.11b0/prunerr-1.1.11b0.tar.gz

---
+++ packages/prunerr/prunerr-1.1.11b0.tar.gz/prunerr-1.1.11b0/src/prunerr/tests/test_runner.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: 2023 Ross Patterson <me@rpatterson.net>
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Tests covering the `prunerr.runner` module.
+"""
+
+import os
+import pathlib
+
+from unittest import mock
+
+import prunerr.runner
+
+from .. import tests
+
+HOME = pathlib.Path(__file__).parent / "home" / "empty"
+ENV = dict(tests.PrunerrTestCase.ENV, HOME=str(HOME))
+
+
+@mock.patch.dict(os.environ, ENV)
+class PrunerrRunnerTests(tests.PrunerrTestCase):
+    """
+    Tests covering the `prunerr.runner` module.
+    """
+
+    HOME = HOME
+    CONFIG = HOME / ".config" / "prunerr.yml"
+    ENV = ENV
+
+    RESPONSES_DIR = tests.PrunerrTestCase.RESPONSES_DIR.parent / "download-client-only"
+
+    def test_runner_empty_config(self):
+        """
+        The runner succeeds with a configuration file as close to empty as supported.
+        """
+        runner = prunerr.runner.PrunerrRunner(self.CONFIG)
+        runner.update()
+        self.assertIsNone(
+            runner.exec_(),
+            "Wrong `exec` result from empty config file",
+        )
+
+    def test_runner_empty_operations(self):
+        """
+        The runner succeeds when only a download client is defined.
+        """
+        runner = prunerr.runner.PrunerrRunner(
+            self.HOME.parent / "download-client-only" / ".config" / "prunerr.yml",
+        )
+        default_request_mocks = self.mock_responses()
+        runner.update()
+        self.assertIsNone(
+            runner.exec_(),
+            "Wrong `exec` result from empty config file",
+        )
+        self.assert_request_mocks(default_request_mocks)
---
+++ packages/prunerr/prunerr-1.1.11b0.tar.gz/prunerr-1.1.11b0/src/prunerr/utils.py
@@ -0,0 +1,53 @@
+# SPDX-FileCopyrightText: 2023 Ross Patterson <me@rpatterson.net>
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Utility functions or other shared constants and values.
+
+Particularly useful to avoid circular imports.
+"""
+
+import os
+import logging
+
+try:
+    # BBB: Python <3.10 compat
+    import pathlib3x as pathlib  # pylint: disable=unused-import
+except ImportError:  # pragma: no cover
+    import pathlib  # type: ignore # pylint: disable=unused-import
+
+try:
+    from functools import (  # type: ignore # pylint: disable=unused-import
+        cached_property,
+    )
+except ImportError:  # pragma: no cover
+    # BBB: Python <3.8 compatibility
+    from backports.cached_property import cached_property  # type: ignore
+
+TRUE_STRS = {"1", "true", "yes", "on"}
+DEBUG = (  # noqa: F841
+    "DEBUG" in os.environ and os.environ["DEBUG"].strip().lower() in TRUE_STRS
+)
+POST_MORTEM = (  # noqa: F841
+    "POST_MORTEM" in os.environ
+    and os.environ["POST_MORTEM"].strip().lower() in TRUE_STRS
+)
+
+
+class DaemonOnceFilter(logging.Filter):  # pylint: disable=too-few-public-methods
+    """
+    Log a given message only once per daemon session, the first loop.
+    """
+
+    def filter(self, record):
+        """
+        Check the record extra attributes to see if the runner has already looped once.
+        """
+        runner = getattr(record, "runner", None)
+        if runner is not None:
+            return not runner.quiet
+        return True
+
+
+daemon_once_filter = DaemonOnceFilter()
