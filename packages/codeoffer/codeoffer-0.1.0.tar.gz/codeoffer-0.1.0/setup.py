ea57b0c273bc593cdfb032573a68fdc441337723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:44 +0000

    Add package codeoffer/0.1.0/codeoffer-0.1.0.tar.gz

---
+++ packages/codeoffer/codeoffer-0.1.0.tar.gz/codeoffer-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codeoffer',
+    version='0.1.0',
+    description='Python library that simplifies access to the Public CodeOffer API',
+    author='CodeOffer',
+    author_email='contact@codeoffer.net',
+    packages=find_packages(),
+    url='https://github.com/codeoffer/v1-python-library',
+    project_urls={
+        'Documentation': 'https://github.com/codeoffer/v1-python-library',
+        'Source': 'https://github.com/codeoffer/v1-python-library'
+    },
+)
