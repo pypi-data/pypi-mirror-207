8b975e88d4d1f24b576f6a4ae547a6ab0751b3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:35 +0000

    Add package dogfood-logger/0.1.1/dogfood-logger-0.1.1.tar.gz

---
+++ packages/dogfood-logger/dogfood-logger-0.1.1.tar.gz/dogfood-logger-0.1.1/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dogfood-logger',
+    version='0.1.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Logger for Grafana',
+    long_description=open('README.txt').read(),
+    install_requires=['numpy'],
+    url='https://github.com/dogfoodhq/dogfood-logger',
+    author='Matt Wong',
+    author_email='nycmattw@gmail.com'
+)
