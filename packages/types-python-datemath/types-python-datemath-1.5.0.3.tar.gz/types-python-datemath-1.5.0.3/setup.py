b25c5d579cd36f651f7e9f655935b90bf312c1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:02 +0000

    Add package types-python-datemath/1.5.0.3/types-python-datemath-1.5.0.3.tar.gz

---
+++ packages/types-python-datemath/types-python-datemath-1.5.0.3.tar.gz/types-python-datemath-1.5.0.3/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-python-datemath"
+description = "Typing stubs for python-datemath"
+long_description = '''
+## Typing stubs for python-datemath
+
+This is a PEP 561 type stub package for the `python-datemath` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`python-datemath`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/python-datemath. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `d1bfd08b4bc843227d097decfd99d70272a1f804`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.5.0.3",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/python-datemath.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['arrow>=1.0.1'],
+      packages=['datemath-stubs'],
+      package_data={'datemath-stubs': ['__init__.pyi', 'helpers.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
