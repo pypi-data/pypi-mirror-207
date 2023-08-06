79165a93d32540ea1bac911b72d1bc0391338fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:09 +0000

    Add package gleaner/0.0.2/gleaner-0.0.2-py3-none-any.whl

---
+++ packages/gleaner/gleaner-0.0.2-py3-none-any.whl/gleaner/gleaner.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+import pandas as pd
+
+from gleaner.explorer import Explorer, TrainResult
+from gleaner.generator import Generator
+from gleaner.model import GleanerDashboard
+from gleaner.config import GleanerConfig
+from gleaner.oracle import Oracle
+from gleaner.utill import display_function
+
+
+class Gleaner:
+    oracle: Oracle
+    generator: Generator
+    explorer: Explorer
+    config: GleanerConfig
+    preferences: list[str]
+
+    def __init__(self, df: pd.DataFrame) -> None:
+        self.config = GleanerConfig(df)
+        self.oracle = Oracle(df)
+        self.generator = Generator(df, self.config)
+        self.explorer = Explorer(df, self.config)
+
+    def train_display(self, preferences: list[str]) -> None:
+        self.preferences = preferences
+        train_results: list[TrainResult] = []
+        for epoch in range(self.config.n_epoch):
+            train_result = self.explorer._train(self.generator, self.oracle, preferences)
+            train_results.append(train_result)
+            display_function(epoch, train_results)
+
+    def train(self, preferences: list[str]) -> list[TrainResult]:
+        self.preferences = preferences
+        return self.explorer.train(self.generator, self.oracle, preferences)
+
+    def infer(self) -> GleanerDashboard:
+        return self.explorer.infer(self.generator, self.oracle, self.preferences)
---
+++ packages/gleaner/gleaner-0.0.2-py3-none-any.whl/gleaner/oracle/scores/__init__.py
@@ -0,0 +1,8 @@
+from .coverage import get_coverage_from_nodes
+from .interestingness import (
+    hashmap,
+    get_interestingness,
+)
+from .specificity import get_specificity_from_nodes
+from .diversity import get_diversity_from_nodes
+from .conciseness import get_conciseness_from_nodes
