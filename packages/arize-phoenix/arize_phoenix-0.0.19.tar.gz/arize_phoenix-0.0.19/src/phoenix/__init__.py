330186cbec801e54f2a9aead1664a4440a9ae278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:40 +0000

    Add package arize-phoenix/0.0.19/arize_phoenix-0.0.19.tar.gz

---
+++ packages/arize-phoenix/arize_phoenix-0.0.19.tar.gz/arize_phoenix-0.0.19/src/phoenix/__init__.py
@@ -0,0 +1,38 @@
+from .datasets import (
+    Dataset,
+    EmbeddingColumnNames,
+    ExampleDatasets,
+    Schema,
+    load_example,
+)
+from .session import Session, active_session, close_app, launch_app
+
+__version__ = "0.0.19"
+
+# module level doc-string
+__doc__ = """
+arize-phoenix - ML Observability in a notebook
+=====================================================================
+**phoenix** is a Python package that provides MLOps insights at
+lightning speed with zero-config observability for model drift, performance, and
+data quality.
+
+Main Features
+-------------
+Here are just a few of the things that phoenix does well:
+  - Compare two sets of model inferences against one another
+  - Identify problematic embeddings cohorts using UMAP and clustering
+  - Explore model performance, drift, and data quality metrics
+"""
+
+__all__ = [
+    "Dataset",
+    "EmbeddingColumnNames",
+    "Schema",
+    "load_example",
+    "ExampleDatasets",
+    "active_session",
+    "close_app",
+    "launch_app",
+    "Session",
+]
