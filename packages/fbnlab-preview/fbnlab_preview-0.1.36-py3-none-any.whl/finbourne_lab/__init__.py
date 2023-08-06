34662b71c4568bb5f931d3c444c3b59d24813b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:16 +0000

    Add package fbnlab-preview/0.1.36/fbnlab_preview-0.1.36-py3-none-any.whl

---
+++ packages/fbnlab-preview/fbnlab_preview-0.1.36-py3-none-any.whl/finbourne_lab/__init__.py
@@ -0,0 +1,6 @@
+from finbourne_lab.common.convener import Convener
+from finbourne_lab.common.shopper import Shopper
+from finbourne_lab.common.recorder import FileRecorder, DriveRecorder
+
+from finbourne_lab.analysis.linear import LinearModel
+from finbourne_lab.analysis.distribution import Distribution
