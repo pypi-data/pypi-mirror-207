c67e0161b24ed3c84f5a77c6f4b3541ec147447d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:32 +0000

    Add package chromedriverhandlar/0.1/chromedriverhandlar-0.1.tar.gz

---
+++ packages/chromedriverhandlar/chromedriverhandlar-0.1.tar.gz/chromedriverhandlar-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='chromedriverhandlar',
+    version='0.1',
+    description='A simple Python library that check, update or download chrome driver',
+    author='Joshi Hrithik V',
+    author_email='hrithikjoshi987@gmail.com',
+    license = "BSD",
+    url='',
+    packages=[],
+    install_requires=['requests==2.28.1',]
+)
