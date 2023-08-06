b479dbb4d7a754e8c32d75cf0f1f0fe8bc703bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:33 +0000

    Add package serdeserd/0.1/serdeserd-0.1.tar.gz

---
+++ packages/serdeserd/serdeserd-0.1.tar.gz/serdeserd-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="serdeserd",
+    version="0.1",
+    description="don't use it, even if you have to",
+    packages=["serdeserd"],
+    author_email="kirkat6@gmail.com",
+    zip_safe=False,
+)
