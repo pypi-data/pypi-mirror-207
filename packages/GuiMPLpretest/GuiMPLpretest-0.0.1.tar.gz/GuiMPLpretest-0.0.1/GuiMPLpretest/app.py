701bef0623c32e0d6804af484c663381b79c103c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:43 +0000

    Add package GuiMPLpretest/0.0.1/GuiMPLpretest-0.0.1.tar.gz

---
+++ packages/GuiMPLpretest/GuiMPLpretest-0.0.1.tar.gz/GuiMPLpretest-0.0.1/GuiMPLpretest/app.py
@@ -0,0 +1,21 @@
+from matplotlib.figure import Figure
+import pickle
+
+from controller import Controller
+
+
+def from_fig(fig: Figure = None):
+    contro = Controller(fig=fig)
+    contro.run()
+
+
+def export_fig(fig: Figure, file_name: str):
+    try:
+        pickle.dump(fig, open(file_name, "wb"))
+    except:
+        print("Erreur lors de l'enregistrement de la figure par pickle")
+
+
+def from_file(file_name: str = None):
+    contro = Controller(fig_file_name=file_name)
+    contro.run()
