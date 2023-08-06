5f5b5eda3dac339883e862dccbbec2bef9a4e134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:28 +0000

    Add package obrpy/0.0.7/obrpy-0.0.7.tar.gz

---
+++ packages/obrpy/obrpy-0.0.7.tar.gz/obrpy-0.0.7/test/test.py
@@ -0,0 +1,32 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from obrpy import obrpy
+
+obrpy_obj = obrpy('C:/Users/temis/0_CCMSS/CCMSS/1_Software/FOS/OBRpy/obrpy-base/test/folder_test')
+
+Y0 = obrpy_obj.OBRfiles['2'].Data
+Y1 = obrpy_obj.OBRfiles['B11'].Data
+F =  obrpy_obj.OBRfiles['2'].f
+Z =  obrpy_obj.OBRfiles['2'].z
+
+import numpy as np
+Y0 = np.array(Y0)
+Y0 = Y0[:,0:10000]
+
+Y1 = np.array(Y1)
+Y1 = Y1[:,0:10000]
+
+Z = np.array(Z)
+Z = Z[0:10000]
+
+out = obrpy_obj.global_analysis_GPU(Y0, Y1, Z, F, local_function=obrpy_obj.Signal.spectral_shift_GPU, point=1)
+
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(Z,out)
+plt.grid()
+plt.show()
