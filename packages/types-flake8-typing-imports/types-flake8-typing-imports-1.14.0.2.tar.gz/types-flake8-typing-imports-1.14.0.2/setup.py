afa468dd3d7e916d0df6f48ee5a830ed5999e14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:51 +0000

    Add package types-flake8-typing-imports/1.14.0.2/types-flake8-typing-imports-1.14.0.2.tar.gz

---
+++ packages/types-flake8-typing-imports/types-flake8-typing-imports-1.14.0.2.tar.gz/types-flake8-typing-imports-1.14.0.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-flake8-typing-imports"
+description = "Typing stubs for flake8-typing-imports"
+long_description = '''
+## Typing stubs for flake8-typing-imports
+
+This is a PEP 561 type stub package for the `flake8-typing-imports` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`flake8-typing-imports`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/flake8-typing-imports. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `d1bfd08b4bc843227d097decfd99d70272a1f804`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.14.0.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/flake8-typing-imports.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['flake8_typing_imports-stubs'],
+      package_data={'flake8_typing_imports-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
