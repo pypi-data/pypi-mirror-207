bf67274363be4e2131ad129622b7446d199a10ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:14 +0000

    Add package tensorflow-rocm/2.11.1.550/tensorflow_rocm-2.11.1.550-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.11.1.550-cp39-cp39-manylinux2014_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/rocrand_mrg31k3p_precomputed.h
@@ -0,0 +1,26 @@
+/*
+    Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+*/
+
+#ifndef ROCM_SYMLINK_ROCRAND_MRG31K3P_PRECOMPUTED_H
+#define ROCM_SYMLINK_ROCRAND_MRG31K3P_PRECOMPUTED_H
+
+#if defined(ROCM_NO_WRAPPER_HEADER_WARNING) || defined(ROCM_SYMLINK_GAVE_WARNING)
+/* include file */
+#include "rocrand/rocrand_mrg31k3p_precomputed.h"
+#else
+/* give warning */
+#if defined(_MSC_VER)
+#pragma message(": warning:This file is deprecated. Use the header file from /opt/rocm-5.5.0/include/rocrand/rocrand_mrg31k3p_precomputed.h by using #include <rocrand/rocrand_mrg31k3p_precomputed.h>")
+#elif defined(__GNUC__)
+#warning "This file is deprecated. Use the header file from /opt/rocm-5.5.0/include/rocrand/rocrand_mrg31k3p_precomputed.h by using #include <rocrand/rocrand_mrg31k3p_precomputed.h>"
+#endif
+/* include file */
+#define ROCM_SYMLINK_GAVE_WARNING
+#include "rocrand/rocrand_mrg31k3p_precomputed.h"
+#undef ROCM_SYMLINK_GAVE_WARNING
+#endif /* defined(ROCM_NO_WRAPPER_HEADER_WARNING) || defined(ROCM_SYMLINK_GAVE_WARNING) */
+
+#endif /* ROCM_SYMLINK_ROCRAND_MRG31K3P_PRECOMPUTED_H */
+
+
