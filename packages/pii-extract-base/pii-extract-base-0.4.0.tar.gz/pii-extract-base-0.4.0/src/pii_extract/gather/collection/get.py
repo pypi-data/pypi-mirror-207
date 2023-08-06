8816c039bc9edd2408b8ed1086c712fc17803bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:04 +0000

    Add package pii-extract-base/0.4.0/pii-extract-base-0.4.0.tar.gz

---
+++ packages/pii-extract-base/pii-extract-base-0.4.0.tar.gz/pii-extract-base-0.4.0/src/pii_extract/gather/collection/get.py
@@ -0,0 +1,45 @@
+"""
+Gather and return the collection of tasks from all available sources
+"""
+
+from typing import Dict
+
+from pii_data.helper.logger import PiiLogger
+
+from ...defs import FMT_CONFIG_TASKS
+from .sources import PluginTaskCollector, JsonTaskCollector
+from .task_collection import PiiTaskCollection
+
+
+LOGGER = None
+
+def get_task_collection(config: Dict = None, load_plugins: bool = True,
+                        debug: bool = False) -> PiiTaskCollection:
+    """
+    Create a task collection object & collect all available tasks
+     :param config: a configuration object
+     :param load_plugins: load tasks from available plugins
+     :param debug:
+    """
+    global LOGGER
+    if LOGGER is None:
+        LOGGER = PiiLogger(__name__, debug)
+    LOGGER("get_task_collection")
+
+    piic = PiiTaskCollection(debug=debug)
+
+    # Add task descriptors from installed plugins
+    if load_plugins:
+        LOGGER("load plugin tasks")
+        c = PluginTaskCollector(config=config, debug=debug)
+        piic.add_collector(c)
+
+    # Add task descriptors from JSON configs
+    task_cfg = config.get(FMT_CONFIG_TASKS) if config else None
+    if task_cfg:
+        LOGGER("load JSON tasks")
+        c = JsonTaskCollector(debug=debug)
+        c.add_tasks(task_cfg)
+        piic.add_collector(c)
+
+    return piic
