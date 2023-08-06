913783a8a19cdae6e76f2326b19880e5c69ff9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:56 +0000

    Add package nesterpack/1.3.2/nesterpack-1.3.2.tar.gz

---
+++ packages/nesterpack/nesterpack-1.3.2.tar.gz/nesterpack-1.3.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterpack',
+    version  = '1.3.2',
+    py_modules = ['nesterpack'],
+    author = 'starken21',
+    author_email = 'starkenamine@proton.me',
+    url = 'https://github.com/users/nagajas/projects/1',
+    description= 'A simple printer of nested lists',
+)
