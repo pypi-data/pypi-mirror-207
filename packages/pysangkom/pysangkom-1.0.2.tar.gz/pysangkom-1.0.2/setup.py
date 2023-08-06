fa3cff0b35e4d95bb3ba70e63ebd739c5531a921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:36 +0000

    Add package pysangkom/1.0.2/pysangkom-1.0.2.tar.gz

---
+++ packages/pysangkom/pysangkom-1.0.2.tar.gz/pysangkom-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysangkom',
+    version='1.0.2',
+    packages=find_packages(),
+    author='m_keen',
+    author_email='vxfqkn8wtx@privaterelay.appleid.com',
+    description='This library is pysangkom, Please becareful',
+    install_requires=[
+        'numpy>=1.24.0',
+        'requests>=2.30.0'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
