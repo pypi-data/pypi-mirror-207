c55601a9533c1b86bba852e72779f5b49ea6b3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:00 +0000

    Add package audioviz/0.1.8.dev1/audioviz-0.1.8.dev1.tar.gz

---
+++ packages/audioviz/audioviz-0.1.8.dev1.tar.gz/audioviz-0.1.8.dev1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='audioviz',
+    packages=['audioviz'],
+    version='0.1.8.dev1',
+    
+    description='An user-friendly music information retrieval tools interfacing with Google Colab',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    
+    url='https://github.com/TrangDuLam/audioviz',
+    author='ayTrang',
+    author_email='andrew.chuang@gapp.nthu.edu.tw',
+    license='MIT',
+    zip_safe=False,
+    keywords=['Music Information Retrieval', "Academia Sinica", "NTHU"],
+
+    install_requires = ["numpy", 
+                        "matplotlib",
+                        "pandas",
+                        "scipy", 
+                        "librosa", 
+                        "libfmp", 
+                        "plotly",
+                        "soundfile",
+                        ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'Topic :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    
+    python_requires='>=3.9'
+
+)
