28bf6477ff23bde251b765c41af2a48a5c54d3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:36 +0000

    Add package quantumcarrot/1.0.0/quantumcarrot-1.0.0.tar.gz

---
+++ packages/quantumcarrot/quantumcarrot-1.0.0.tar.gz/quantumcarrot-1.0.0/README.md
@@ -0,0 +1,4 @@
+#######
+import quatumcarrot
+quatumcarrot.curse()
+#######
---
+++ packages/quantumcarrot/quantumcarrot-1.0.0.tar.gz/quantumcarrot-1.0.0/quantumcarrot/quantumcarrot.py
@@ -0,0 +1,5 @@
+import webbrowser
+def curse():
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    print("quantum carrot in action")
+curse()
