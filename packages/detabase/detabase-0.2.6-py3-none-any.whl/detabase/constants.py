73accf38b5babaf8466cc6d96f265c1c0f80b8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:13 +0000

    Add package detabase/0.2.6/detabase-0.2.6-py3-none-any.whl

---
+++ packages/detabase/detabase-0.2.6-py3-none-any.whl/detabase/constants.py
@@ -0,0 +1,65 @@
+__all__ = ['Undefined', 'ModelMap', 'ContextVarMap', 'ModelDataMap', 'GenericType', 'EMPTY_TAGS', 'FORM_FIELDS', 'TAGS',
+           'INPUT_TYPES', 'Number', 'YEAR_PATTERN', 'DAY_PATTERN', 'MONTH_PATTERN', 'ISO_DATE_PATTERN',
+           'ISO_DATETIME_PATTERN', 'LiteralFormField', 'LiteralInputType', 'BRAZIL_STATES_PATTERN', 'BRAZIL_STATES'
+           ]
+
+import re
+from typing import TypeVar, Literal
+from collections import ChainMap
+from decimal import Decimal
+
+Undefined = object()
+
+ModelMap = ChainMap()
+
+ContextVarMap = ChainMap()
+
+ModelDataMap = ChainMap()
+
+GenericType = TypeVar('GenericType')
+
+Number = TypeVar('Number', float, int, Decimal)
+
+EMPTY_TAGS: list[str] = ['input', 'img', 'meta', 'link', 'br', 'hr']
+STRUCTURE: list[str] = ['html', 'head', 'body', 'title']
+SEMANTIC: list[str] = ['header', 'footer', 'main', 'aside', 'nav']
+FORM_FIELDS: list[str] = ['input', 'select', 'textarea']
+
+LiteralFormField = Literal['input', 'select', 'textarea']
+
+TAGS: list[str] = ['a', 'address', 'area', 'abbr', 'article', 'aside', 'audio', 'body',
+		'b', 'engine', 'bdi', 'bdo', 'blockquote', 'br', 'button', 'canvas', 'caption', 'cite', 'code', 'col',
+		'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'div', 'dialog', 'dl', 'dt', 'em', 'embed',
+		'fieldset', 'figcaption', 'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header',
+		'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map',
+		'mark', 'meta', 'meter', 'nav', 'nonscript', 'object', 'ol', 'optgroup', 'output', 'p', 'param', 'picture',
+		'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source',
+		'span', 'strong', 'style', 'sub', 'summary', 'sup', 'svg', 'table', 'tbody', 'td', 'template', 'textarea',
+		'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'u', 'ul', 'var', 'video', 'wbr', 'option']
+
+INPUT_TYPES: list[str] = ['button', 'checkbox', 'color', 'date', 'datetime-local', 'email', 'file', 'hidden', 'image',
+                          'month', 'number', 'password', 'radio', 'range', 'reset', 'search', 'submit', 'tel', 'text',
+                          'time', 'url', 'week']
+
+LiteralInputType = Literal['button', 'checkbox', 'color', 'date', 'datetime-local', 'email', 'file', 'hidden', 'image',
+                          'month', 'number', 'password', 'radio', 'range', 'reset', 'search', 'submit', 'tel', 'text',
+                          'time', 'url', 'week']
+
+
+YEAR_PATTERN = re.compile(r'\b(19|20)\d{2}\b')
+
+DAY_PATTERN = re.compile(r'\b([0-2]\d|3[0-1])\b')
+
+MONTH_PATTERN = re.compile(r'\b(0\d|1[0-2])\b')
+
+ISO_DATE_PATTERN = re.compile(r'(19|20)(\d{2})(-)(01|02|03|04|05|06|07|08|09|10|11|12)(-)([0-3]\d)')
+
+ISO_DATETIME_PATTERN = re.compile(r'(19|20)(\d{2})(-)(01|02|03|04|05|06|07|08|09|10|11|12)(-)([0-3]\d)(T)([0-1]\d|2[0-3])(:)([0-5]\d)(?::[0-5]\d)?')
+
+BRAZIL_STATES = {"AC", "AL", "AP", "AM", "BA", "CE", "DF", "GO", "ES", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE",
+                 "PI", "RJ", "RN", "RS", "RO", "RR", "SP", "SC", "SE", "TO"}
+
+BRAZIL_STATES_PATTERN = re.compile(r'{}'.format('|'.join(BRAZIL_STATES)))
+
+if __name__ == '__main__':
+    print(BRAZIL_STATES_PATTERN.pattern)
---
+++ packages/detabase/detabase-0.2.6-py3-none-any.whl/detabase/descriptor.py
@@ -0,0 +1,303 @@
+from __future__ import annotations
+
+__all__ = ['Descriptor', 'HiddenField', 'DetaModelField', 'DateField', 'DateTimeField', 'TitleField', 'EnumField',
+           'CheckBoxField', 'AutoField', 'StringField', 'RegexField', 'IntField', 'FloatField', 'NumberField',
+           'TextAreaField', 'PasswordField', 'BytesField', 'ModelKeyField']
+
+import datetime
+from functools import partial
+from dataclasses import MISSING
+from typing import ClassVar, Optional, Union, Any, Callable, Literal, get_type_hints
+from abc import ABC
+
+from smartjs.functions import *
+from smartjs.base import *
+from smartjs.elements import *
+
+from detabase.constants import *
+from detabase.protocols import *
+from detabase.casting import *
+from detabase.types import *
+from detabase.context import *
+
+
+class Descriptor(ABC):
+    HTML_TAG: ClassVar[Optional[LiteralFormField]] = None
+    INPUT_TYPE: ClassVar[Optional[LiteralInputType]] = None
+    PARSER: ClassVar[Parser] = cast
+    FINAL_TYPE: ClassVar[Optional[type]] = None
+    HTMX: ClassVar[bool] = False
+    DATALIST: ClassVar[bool] = False
+    OPTIONS: ClassVar[bool] = False
+    IS_ENUM: ClassVar[bool] = False
+    
+    def __init__(self, *args, **kwargs):
+        self.args = [item for item in args if not item in [None, '']]
+        self.default = kwargs.pop('default', MISSING if self.is_required else None)
+        self.default_factory: Callable = kwargs.pop('default_factory', MISSING)
+        self.label: Optional[str] = kwargs.pop('label', None)
+        self.fieldset: Optional[str] = kwargs.pop('fieldset', None)
+        self.group: Optional[str] = kwargs.pop('group', None)
+        self.parser: Callable = kwargs.pop('parser', type(self).PARSER)
+        self.min: Optional[Number] = kwargs.pop('min', None)
+        self.max: Optional[Number] = kwargs.pop('max', None)
+        self.step: Optional[Number] = kwargs.pop('step', None)
+        self.max_length: Optional[int] = kwargs.pop('max_length', None)
+        self.min_length: Optional[int] = kwargs.pop('min_length', None)
+
+        if self.default_factory is not MISSING:
+            self.default = None
+        self.kwargs = kwargs
+    
+    def __set_name__(self, owner, name):
+        self.public_name = name
+        self.private_name = f'_{name}'
+        self.owner = owner
+    
+    def __set__(self, instance, value):
+        if value is None:
+            if self.default_factory is not MISSING:
+                value = self.default_factory()
+        setattr(instance, self.private_name, self.validate(value))
+        
+    class ValidationError(BaseException):
+        pass
+    
+    def __get__(self, instance, owner=None):
+        if instance is None:
+            return self.default
+        return getattr(instance, self.private_name)
+    
+    @property
+    def type_hint(self):
+        return TypeHint(get_type_hints(self.owner)[self.public_name])
+    
+    @property
+    def model_type(self):
+        return self.type_hint.expected_type
+        
+    def validate(self, value):
+        value = self.parser(value, self.FINAL_TYPE or self.type_hint.expected_type)
+        if self.type_hint.check_type(value):
+            return value
+        else:
+            raise self.ValidationError(f'{type(self.owner).__name__}.{self.public_name} exige um tipo '
+                                       f'{self.type_hint.expected_type}, que não coresponde ao encontrado para {value} '
+                                       f'({type(value)}).')
+    
+    @property
+    def is_required(self):
+        return 'required' in self.args
+    
+    @property
+    def html_tag(self):
+        return None if not self.HTML_TAG else Tag(self.HTML_TAG)
+    
+    @property
+    def input_type(self):
+        return None if not self.INPUT_TYPE else InputType(self.INPUT_TYPE)
+    
+    @property
+    def required(self):
+        return 'required' if 'required' in self.args else None
+    
+    @property
+    def multiple(self):
+        return 'multiple' if 'multiple' in self.args else None
+    
+    @property
+    def label_text(self):
+        return self.label or self.public_name
+    
+    def htmx_config(self, instance=None):
+        return ''
+    
+    def datalist(self):
+        if self.DATALIST:
+            return f'{self.model_type.item_name()}-list"'
+        return ''
+    
+    def form_field_args(self, instance=None):
+        return Arg(self.required), Arg(self.multiple), Kwarg('type', self.input_type), Kwarg('name', self.public_name), \
+            Kwarg('id', self.public_name), Kwarg('list', self.datalist()) if self.datalist() else '', \
+            Arg(self.htmx_config(instance)) if self.HTMX else '', \
+            Klass(self.html_tag.bootstrap if not self.input_type else self.input_type.bootstrap), \
+            Kwarg('placeholder', self.public_name)
+    
+    def form_field_element(self, instance=None):
+        if self.HTMX:
+            return BaseElement(*self.form_field_args(instance), Arg(self.htmx_config(instance)),
+                               Tag(self.html_tag), self.form_field_value(instance))
+        return BaseElement(*self.form_field_args(instance), Tag(self.html_tag), self.form_field_value(instance))
+    
+    def form_field_value(self, instance=None):
+        value = self.__get__(instance)
+        if self.INPUT_TYPE:
+            if value not in [None, '', MISSING]:
+                if self.INPUT_TYPE == 'checkbox':
+                    return Arg('checked') if value == True or value == 'on' else ''
+                return Kwarg('value', str(value))
+            return ''
+        elif self.IS_ENUM:
+            return Text(self.model_type.options(value))
+
+    @property
+    def label_class(self):
+        if self.INPUT_TYPE:
+            return self.input_type.label_bootstrap
+        return self.html_tag.label_bootstrap
+
+    def label_element(self):
+        id = Kwarg('id', f'{self.public_name}__label')
+        if self.INPUT_TYPE == 'hidden':
+                return ''
+        return BaseElement(id, Tag('label'), Klass(self.label_class),
+                                   Kwarg('for', self.public_name), Text(self.label_text))
+  
+    @property
+    def form_field_container_class(self):
+        if self.INPUT_TYPE == 'checkbox':
+            return Klass('form-check mb-2 me-3')
+        elif self.INPUT_TYPE == 'range':
+            return Klass('form-control mb-2')
+        else:
+            return Klass('form-floating mb-2')
+        
+        
+    def form_field(self, instance=None):
+        if self.HTML_TAG:
+            id = Kwarg('id', f'{self.public_name}__container')
+            if self.INPUT_TYPE in ['checkbox', 'range']:
+                return BaseElement(id, Tag('div'), self.form_field_container_class,
+                                   elements=[self.label_element(), self.form_field_element(instance)])
+            return BaseElement(id, Tag('div'), self.form_field_container_class,
+                               elements=[self.form_field_element(instance), self.label_element()])
+        return ''
+
+
+    
+class AutoField(Descriptor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __set__(self, instance, value):
+        setattr(instance, self.private_name, getattr(instance, f'{self.public_name}_getter'))
+    
+    
+class StringField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'text'
+    
+
+class RegexField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'text'
+    
+    
+class TitleField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'text'
+    PARSER = lambda x, y: remove_extra_whitespaces(str(str(cast(x, y)))).title()
+
+
+class HiddenField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'hidden'
+    
+    
+class NumberField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'number'
+    PARSER = lambda x, v: parse_to_number(x)
+    
+    
+    @property
+    def form_field_number_args(self):
+        args = list()
+        if self.min:
+            args.append(Kwarg('min', self.min))
+        if self.max:
+            args.append(Kwarg('max', self.max))
+        if self.step:
+            args.append(Kwarg('step', self.step))
+        return args
+    
+    def form_field_args(self, instance=None):
+        return *super().form_field_args(instance=instance), *self.form_field_number_args
+
+    
+class IntField(NumberField):
+    FINAL_TYPE = int
+    PARSER = cast
+
+    
+class FloatField(NumberField):
+    FINAL_TYPE = float
+    PARSER = cast
+
+    
+class CheckBoxField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'checkbox'
+    
+    
+class EnumField(Descriptor):
+    HTML_TAG = 'select'
+    IS_ENUM = True
+    
+    
+class DateField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'date'
+    FINAL_TYPE = datetime.date
+    
+    
+class DateTimeField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'datetime-local'
+    FINAL_TYPE = datetime.datetime
+    
+
+class TextAreaField(Descriptor):
+    HTML_TAG = 'textarea'
+    FINAL_TYPE = str
+    
+    
+class DetaModelField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'text'
+    FINAL_TYPE = str
+    # HTMX = True
+    DATALIST = True
+    
+    def __set__(self, instance, value):
+        super().__set__(instance, value)
+        if value:
+            setattr(instance, self.model_type.item_name(), self.model_type.create(**get_from_context(self.model_type.table(), value)))
+        else:
+            setattr(instance, self.model_type.item_name(), None)
+
+    def htmx_config(self, instance=None):
+        def data_value():
+            value = self.__get__(instance)
+            if value not in [None, '']:
+                return f'data-value="{value}"'
+            return ''
+        return data_value()
+        # return f'hx-get="/options/{self.model_type.item_name()}" hx-trigger="load" hx-target="#{self.model_type.item_name()}-list" {data_value()}"'
+
+
+class ModelKeyField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'text'
+    HTMX = True
+
+
+class BytesField(Descriptor):
+    HTML_TAG = 'input'
+    INPUT_TYPE = 'text'
+
+
+class PasswordField(BytesField):
+    INPUT_TYPE = 'password'
+
