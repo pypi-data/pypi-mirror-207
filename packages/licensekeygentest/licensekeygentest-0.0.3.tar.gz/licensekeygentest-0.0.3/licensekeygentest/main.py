769ccd8ceb38f1e93dd0a1c9302de931d31a14ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:30 +0000

    Add package licensekeygentest/0.0.3/licensekeygentest-0.0.3.tar.gz

---
+++ packages/licensekeygentest/licensekeygentest-0.0.3.tar.gz/licensekeygentest-0.0.3/licensekeygentest/main.py
@@ -0,0 +1,67 @@
+import machineid
+import requests
+import json
+import os
+
+class library():
+  def __init__(self, license_key):
+    machine_fingerprint = machineid.hashed_id('example-app')
+    validation = requests.post(
+      "https://api.keygen.sh/v1/accounts/{}/licenses/actions/validate-key".format(os.environ['KEYGEN_ACCOUNT_ID']),
+      headers={
+        "Content-Type": "application/vnd.api+json",
+        "Accept": "application/vnd.api+json"
+      },
+      data=json.dumps({
+        "meta": {
+          "scope": { "fingerprint": machine_fingerprint },
+          "key": license_key
+        }
+      })
+    ).json()
+    # print(validation)
+    if "errors" in validation:
+      errs = validation["errors"]
+      raise Exception("license validation failed: {}".format(
+        map(lambda e: "{} - {}".format(e["title"], e["detail"]).lower(), errs)
+      ))
+
+    if validation["meta"]["valid"]:
+      print("license has already been activated on this machine")
+    
+    validation_code = validation["meta"]["code"]
+    activation_is_required = validation_code == 'FINGERPRINT_SCOPE_MISMATCH' or \
+                             validation_code == 'NO_MACHINES' or \
+                             validation_code == 'NO_MACHINE'
+    print(validation_code)
+    if not activation_is_required:
+      raise Exception("license {}".format(validation["meta"]["detail"]))
+
+    activation = requests.post(
+      "https://api.keygen.sh/v1/accounts/{}/machines".format(os.environ['KEYGEN_ACCOUNT_ID']),
+      headers={
+        "Authorization": "License {}".format(license_key),
+        "Content-Type": "application/vnd.api+json",
+        "Accept": "application/vnd.api+json"
+      },
+      data=json.dumps({
+        "data": {
+          "type": "machines",
+          "attributes": {
+            "fingerprint": machine_fingerprint
+          },
+          "relationships": {
+            "license": {
+              "data": { "type": "licenses", "id": validation["data"]["id"] }
+            }
+          }
+        }
+      })
+    ).json()
+
+    if "errors" in activation:
+      errs = activation["errors"]
+      raise Exception("license activation failed: {}".format(
+        ','.join(map(lambda e: "{} - {}".format(e["title"], e["detail"]).lower(), errs))
+      ))
+
