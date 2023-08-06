cefe704b92e1e8c932d5d910e70a44e3c9df5271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:13 +0000

    Add package tybase/0.0.4/tybase-0.0.4.tar.gz

---
+++ packages/tybase/tybase-0.0.4.tar.gz/tybase-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tybase',
+    version='0.0.4',
+    include_package_data=True,
+    description='新增lc.eg1,用于协助llm生成prompt',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',  # 版本描述
+    author='Tuya',
+    author_email='353335447@qq.com',
+    url='https://github.com/yourusername/your_package',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        'requests',
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
