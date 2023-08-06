c2c6c760118836a2ae19ba11192c115495363b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:15 +0000

    Add package cveutils/0.4/cveutils-0.4.tar.gz

---
+++ packages/cveutils/cveutils-0.4.tar.gz/cveutils-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name='cveutils',
+    version='0.4',
+    description='This library provides functionality for fetching and parsing information about Common Vulnerabilities and Exposures (CVE) using the NIST National Vulnerability Database (NVD) RESTful API.',
+    author='Vinicius Pereira (big0us)',
+    author_email='vini@cius.xyz',
+    package_dir={'': 'src'},
+    packages=['cveutils'],
+    install_requires=[
+        'requests',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
