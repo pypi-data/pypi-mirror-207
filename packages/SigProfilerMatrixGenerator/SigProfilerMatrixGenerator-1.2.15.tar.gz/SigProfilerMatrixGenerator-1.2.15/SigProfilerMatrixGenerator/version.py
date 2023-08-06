64ced8802fd6821d9fac4e0798653ec72db5eb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:40 +0000

    Add package SigProfilerMatrixGenerator/1.2.15/SigProfilerMatrixGenerator-1.2.15.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.15.tar.gz/SigProfilerMatrixGenerator-1.2.15/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.15'
+version = '1.2.15'
+Update = 'v1.2.15: Add object for installation directory, unit tests, and README formatting.'
+
+	
