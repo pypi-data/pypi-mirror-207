65d813e09c7298eccf41908dafd470a869fe3a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package hget/1.0.6/hget-1.0.6-py2.py3-none-any.whl

---
+++ packages/hget/hget-1.0.6-py2.py3-none-any.whl/hget/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+import os
+import pdb
+import sys
+
+from .src import *
+
+
+def _main(args):
+    log = loger()
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+    outfile = args.output
+    if not outfile and args.dir:
+        outfile = os.path.join(args.dir, os.path.basename(args.url))
+    hget(args.url, outfile, args.num, quiet=args.quiet,
+         tcp_conn=args.connections, timeout=args.timeout,
+         access_key=args.access_key, secrets_key=args.secrets_key,
+         max_speed=args.max_speed, proxy=args.proxy)
+
+
+def main():
+    args = parseArg()
+    if args.use_reloader:
+        autoreloader(_main, args)
+    else:
+        _main(args)
+
+
+if __name__ == "__main__":
+    main()
