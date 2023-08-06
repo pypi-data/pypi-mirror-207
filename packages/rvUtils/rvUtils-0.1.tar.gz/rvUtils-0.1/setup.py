5048e80b7720c16313d7f16c84c07172dcaac105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:11 +0000

    Add package rvUtils/0.1/rvUtils-0.1.tar.gz

---
+++ packages/rvUtils/rvUtils-0.1.tar.gz/rvUtils-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rvUtils',
+    version='0.1',
+    packages=find_packages(),
+    author='Raj Vihari Vudattu',
+    author_email='vihari.raj@gmail.com',
+    description='Common Utils Libs',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
