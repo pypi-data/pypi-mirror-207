ae5af5470392b0caaa6bf9b82e15bc7d88cdafa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:11 +0000

    Add package pyboil/0.5.8/pyboil-0.5.8.tar.gz

---
+++ packages/pyboil/pyboil-0.5.8.tar.gz/pyboil-0.5.8/pyboil/pyboil.py
@@ -0,0 +1,24 @@
+from pyboil.parser import parser
+import shutil, os, sys
+
+def launch():
+    args = parser.parse_args()
+
+    final_path = (os.pardir / "template")
+    shutil.copytree(final_path, '.')
+
+    with open('./setup.py') as f:
+        setup = f.read()
+
+    setup.replace('{NAME}', args['NAME'])
+    setup.replace('{VERSION}', args['VERSION'])
+    setup.replace('{DESCRIPTION}', args['DESCRIPTION'])
+    setup.replace('{AUTHOR}', args['AUTHOR'])
+    setup.replace('{EMAIL}', args['EMAIL'])
+    setup.replace('{URL}', args['URL'])
+
+    with open('./setup.py', 'w') as f:
+        f.write(setup)
+
+if __name__ == "__main__":
+    launch()
