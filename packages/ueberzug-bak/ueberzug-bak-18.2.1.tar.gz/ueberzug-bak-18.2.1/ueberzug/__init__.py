07a1612f96fd0509299ec8e2e78c9d7f5c2c06dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:50 +0000

    Add package ueberzug-bak/18.2.1/ueberzug-bak-18.2.1.tar.gz

---
+++ packages/ueberzug-bak/ueberzug-bak-18.2.1.tar.gz/ueberzug-bak-18.2.1/ueberzug/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '18.2.1'
+__license__ = 'GPLv3'
+__description__ ='ueberzug is a command line util which allows to display images in combination with X11'
+__url_repository__ = 'https://github.com/vesche/ueberzug-bak'
+__url_bug_reports__ = 'https://github.com/vesche/ueberzug-bak/issues'
+__url_project__ = __url_repository__
+__author__ = 'Unknown'
