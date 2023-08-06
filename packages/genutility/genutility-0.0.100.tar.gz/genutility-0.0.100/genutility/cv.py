9eae525286b7d10a5870d70689dac1ce32eb6f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:27 +0000

    Add package genutility/0.0.100/genutility-0.0.100.tar.gz

---
+++ packages/genutility/genutility-0.0.100.tar.gz/genutility-0.0.100/genutility/cv.py
@@ -0,0 +1,43 @@
+import logging
+from typing import TYPE_CHECKING, Iterator, Union
+
+import cv2
+import numpy as np
+
+if TYPE_CHECKING:
+    import wx  # noqa: F401
+
+logger = logging.getLogger(__name__)
+
+
+def wx_to_cv_image(wximage: "wx.Image") -> np.ndarray:
+    buf = wximage.GetDataBuffer()
+    arr = np.asarray(buf, dtype=np.uint8)
+
+    return np.reshape(arr, (wximage.Height, wximage.Width, 3))[:, :, ::-1]  # RGB => BGR
+
+
+def grayscale(cvimg: np.ndarray) -> np.ndarray:
+    return cv2.cvtColor(cvimg, cv2.COLOR_RGB2GRAY)
+
+
+def iter_video(input: Union[str, int] = 0, show: bool = False) -> Iterator[np.ndarray]:
+    cap = cv2.VideoCapture(input)
+    logger.debug("Reading video using %s backend", cap.getBackendName())
+
+    try:
+        while True:
+            retval, image = cap.read()
+            if retval:
+                yield image
+            else:
+                break
+
+            if show:
+                cv2.imshow("iter_video", image)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    return
+    finally:
+        cap.release()
+        if show:
+            cv2.destroyAllWindows()
