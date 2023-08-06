a79108438a31efb5a528611d8482f879fbc235cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:47 +0000

    Add package pytranslations/0.0.1/pytranslations-0.0.1.tar.gz

---
+++ packages/pytranslations/pytranslations-0.0.1.tar.gz/pytranslations-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='pytranslations',
+    packages=[],
+    version='0.0.1',
+    description='Lightweight library for handling translations',
+    author='Wym',
+    license='MIT',
+)
