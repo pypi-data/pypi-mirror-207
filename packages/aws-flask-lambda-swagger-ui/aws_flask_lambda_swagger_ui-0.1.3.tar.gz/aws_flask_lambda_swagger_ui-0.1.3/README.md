5e56cfda3814ae2d39017fe9a725a28478ea07fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package aws-flask-lambda-swagger-ui/0.1.3/aws_flask_lambda_swagger_ui-0.1.3.tar.gz

---
+++ packages/aws-flask-lambda-swagger-ui/aws_flask_lambda_swagger_ui-0.1.3.tar.gz/aws_flask_lambda_swagger_ui-0.1.3/README.md
@@ -0,0 +1,37 @@
+# aws_flask_lambda_swagger_ui
+
+New version includes the templates folder
+
+## Installation
+
+Install the package using pip:
+
+```sh
+pip install aws-flask-lambda-swagger-ui
+```
+
+## Usage
+
+```python
+from flask import Flask
+from aws_flask_lambda_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+
+SWAGGER_URL = '/aws/test/docs' 
+API_URL = 'https://petstore.swagger.io/v2/swagger.json' 
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  
+    API_URL,
+    config={  
+        'app_name': "AWS Lambda application"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+```
---
+++ packages/aws-flask-lambda-swagger-ui/aws_flask_lambda_swagger_ui-0.1.3.tar.gz/aws_flask_lambda_swagger_ui-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aws_flask_lambda_swagger_ui',
+    version='0.1.3',
+    description='Adding swagger ui on your AWS Lambda Function using a Flask blueprint',
+    author='Seven Clouds Technologies',
+    author_email='admin@seventechnologies.cloud',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+packages=["aws_flask_lambda_swagger_ui", "aws_flask_lambda_swagger_ui.templates"],
+    package_dir={"aws_flask_lambda_swagger_ui": "aws_flask_lambda_swagger_ui"},
+    include_package_data=True,
+    install_requires=[
+        'Flask==2.0.2'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
