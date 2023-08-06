c93cb6c03a6b635bfff773f7e84252a8fb8d1b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:37 +0000

    Add package easyapi-django/0.0.21/easyapi_django-0.0.21.tar.gz

---
+++ packages/easyapi-django/easyapi_django-0.0.21.tar.gz/easyapi_django-0.0.21/easyapi/__init__.py
@@ -0,0 +1,5 @@
+from easyapi.base import BaseResource
+from easyapi.exception import HTTPException
+from easyapi.middleware import ExceptionMiddleware
+from easyapi.routes import get_routes
+from easyapi.tenant.db_router import DBRouter
