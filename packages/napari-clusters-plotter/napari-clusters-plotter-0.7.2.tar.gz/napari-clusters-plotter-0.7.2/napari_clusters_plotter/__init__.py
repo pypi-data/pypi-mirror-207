83e5f80ce53fae9e67a225d13095e93d2d7ab655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:12 +0000

    Add package napari-clusters-plotter/0.7.2/napari-clusters-plotter-0.7.2.tar.gz

---
+++ packages/napari-clusters-plotter/napari-clusters-plotter-0.7.2.tar.gz/napari-clusters-plotter-0.7.2/napari_clusters_plotter/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.7.2"
+
+
+from ._dock_widget import (
+    ClusteringWidget,
+    DimensionalityReductionWidget,
+    PlotterWidget,
+    napari_experimental_provide_dock_widget,
+)
