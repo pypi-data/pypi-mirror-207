f71b5acd73145daf3ce7673b01d042ff7e868b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:32:39 +0000

    Add package package-name-example/0.1/Package_Name_Example-0.1.tar.gz

---
+++ packages/package-name-example/Package_Name_Example-0.1.tar.gz/Package_Name_Example-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Package_Name_Example',
+    version='0.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    packages=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
