71cec1ee7d83b00025c863fe624f99addd174498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:04 +0000

    Add package lamineml/0.0.40/lamineml-0.0.40.tar.gz

---
+++ packages/lamineml/lamineml-0.0.40.tar.gz/lamineml-0.0.40/src/lamine/tools.py
@@ -0,0 +1,17 @@
+import time
+from jnius import autoclass
+import platform
+context=autoclass("android.content.Context")
+from  plyer.platforms.android import activity
+Context = autoclass('android.content.Context')
+from kivy.app import App
+from jnius import autoclass ,cast
+def Secure: 
+    x=cast ("android.app.KeyguardManager",activity.getSystemService(Context.KEYGUARD_SERVICE)  )
+    h=x.isKeyguardSecure()
+    if(h):
+        return True
+    else:
+        return False
+    
+            
