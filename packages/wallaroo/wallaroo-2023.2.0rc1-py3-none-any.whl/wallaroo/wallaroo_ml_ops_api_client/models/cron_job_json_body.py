187d1cd4cef6eb02c483edb80c3e410ce5880be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:23:53 +0000

    Add package wallaroo/2023.2.0rc1/wallaroo-2023.2.0rc1-py3-none-any.whl

---
+++ packages/wallaroo/wallaroo-2023.2.0rc1-py3-none-any.whl/wallaroo/wallaroo_ml_ops_api_client/models/cron_job_json_body.py
@@ -0,0 +1,108 @@
+from typing import TYPE_CHECKING, Any, Dict, List, Type, TypeVar, Union
+
+import attr
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+  from ..models.cron_job_json_body_json import CronJobJsonBodyJson
+
+
+
+
+
+T = TypeVar("T", bound="CronJobJsonBody")
+
+
+@attr.s(auto_attribs=True)
+class CronJobJsonBody:
+    """ 
+        Attributes:
+            json (CronJobJsonBodyJson):
+            name (str):
+            orch_id (str):
+            schedule (str):
+            workspace_id (int):
+            timeout (Union[Unset, None, int]):
+     """
+
+    json: 'CronJobJsonBodyJson'
+    name: str
+    orch_id: str
+    schedule: str
+    workspace_id: int
+    timeout: Union[Unset, None, int] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+
+    def to_dict(self) -> Dict[str, Any]:
+        json = self.json.to_dict()
+
+        name = self.name
+        orch_id = self.orch_id
+        schedule = self.schedule
+        workspace_id = self.workspace_id
+        timeout = self.timeout
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update({
+            "json": json,
+            "name": name,
+            "orch_id": orch_id,
+            "schedule": schedule,
+            "workspace_id": workspace_id,
+        })
+        if timeout is not UNSET:
+            field_dict["timeout"] = timeout
+
+        return field_dict
+
+
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        from ..models.cron_job_json_body_json import CronJobJsonBodyJson
+        d = src_dict.copy()
+        json = CronJobJsonBodyJson.from_dict(d.pop("json"))
+
+
+
+
+        name = d.pop("name")
+
+        orch_id = d.pop("orch_id")
+
+        schedule = d.pop("schedule")
+
+        workspace_id = d.pop("workspace_id")
+
+        timeout = d.pop("timeout", UNSET)
+
+        cron_job_json_body = cls(
+            json=json,
+            name=name,
+            orch_id=orch_id,
+            schedule=schedule,
+            workspace_id=workspace_id,
+            timeout=timeout,
+        )
+
+        cron_job_json_body.additional_properties = d
+        return cron_job_json_body
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
