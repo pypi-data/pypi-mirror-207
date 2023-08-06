4f19e288e96c939d04b7fc42cc9dd01376ac2094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:27 +0000

    Add package cg/29.8.3/cg-29.8.3.tar.gz

---
+++ packages/cg/cg-29.8.3.tar.gz/cg-29.8.3/cg/constants/demultiplexing.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+
+import click
+from cg.utils.enums import StrEnum
+
+
+class BclConverter(StrEnum):
+    """Define the BCL converter."""
+
+    DRAGEN: str = "dragen"
+    BCL2FASTQ: str = "bcl2fastq"
+
+
+class FlowCellType(StrEnum):
+    """Define the flow cell type."""
+
+    NOVASEQ: str = "novaseq"
+    HISEQ: str = "hiseq"
+
+
+UNKNOWN_REAGENT_KIT_VERSION: str = "unknown"
+
+SAMPLE_SHEET_HEADERS = {
+    "bcl2fastq": [
+        "FCID",
+        "Lane",
+        "SampleID",
+        "SampleRef",
+        "index",
+        "index2",
+        "SampleName",
+        "Control",
+        "Recipe",
+        "Operator",
+        "Project",
+    ],
+    "dragen": [
+        "FCID",
+        "Lane",
+        "Sample_ID",
+        "SampleRef",
+        "index",
+        "index2",
+        "SampleName",
+        "Control",
+        "Recipe",
+        "Operator",
+        "Sample_Project",
+    ],
+}
+
+SAMPLE_SHEET_DATA_HEADER = "[Data]"
+
+SAMPLE_SHEET_SETTINGS_HEADER = "[Settings]"
+
+SAMPLE_SHEET_SETTING_BARCODE_MISMATCH_INDEX1 = "BarcodeMismatchesIndex1,0"
+
+SAMPLE_SHEET_SETTING_BARCODE_MISMATCH_INDEX2 = "BarcodeMismatchesIndex2,0"
+
+OPTION_BCL_CONVERTER = click.option(
+    "-b",
+    "--bcl-converter",
+    type=click.Choice(["bcl2fastq", "dragen"]),
+    default="bcl2fastq",
+    help="Specify bcl conversion software. Choose between bcl2fastq and dragen. Default is "
+    "bcl2fastq.",
+)
+
+FASTQ_FILE_SUFFIXES = [".fastq", ".gz"]
+
+DEMUX_STATS_PATH = {
+    "bcl2fastq": {
+        "demultiplexing_stats": Path("Stats", "DemultiplexingStats.xml"),
+        "conversion_stats": Path("Stats", "ConversionStats.xml"),
+        "runinfo": None,
+    },
+    "dragen": {
+        "demultiplexing_stats": Path("Reports", "Demultiplex_Stats.csv"),
+        "conversion_stats": Path("Reports", "Demultiplex_Stats.csv"),
+        "adapter_metrics_stats": Path("Reports", "Adapter_Metrics.csv"),
+        "runinfo": Path("Reports", "RunInfo.xml"),
+        "quality_metrics": Path("Reports", "Quality_Metrics.csv"),
+    },
+}
+
+DRAGEN_PASSED_FILTER_PCT = 100.00000
+
+
+class DemultiplexingDirsAndFiles(StrEnum):
+    """Demultiplexing related directories and files."""
+
+    COPY_COMPLETE: str = "CopyComplete.txt"
+    DELIVERY: str = "delivery.txt"
+    DEMUX_STARTED: str = "demuxstarted.txt"
+    DEMUX_COMPLETE: str = "demuxcomplete.txt"
+    Hiseq_X_COPY_COMPLETE: str = "copycomplete.txt"
+    HiseqX_TILE_DIR: str = "l1t11"
+    RTACOMPLETE: str = "RTAComplete.txt"
+    RUN_PARAMETERS: str = "RunParameters.xml"
+    SAMPLE_SHEET_FILE_NAME: str = "SampleSheet.csv"
+    UNALIGNED_DIR_NAME: str = "Unaligned"
---
+++ packages/cg/cg-29.8.3.tar.gz/cg-29.8.3/tests/cli/demultiplex/test_validate_sample_sheet.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+from click.testing import CliRunner, Result
+
+from cg.apps.demultiplex.sample_sheet.validate import get_sample_sheet_from_file
+from cg.cli.demultiplex.sample_sheet import validate_sample_sheet
+
+
+def test_validate_non_existing_sample_sheet(cli_runner: CliRunner, sample_sheet_context: dict):
+    # GIVEN a cli runner
+    # GIVEN a sample sheet file that does not exist
+    sample_sheet: Path = Path("a_sample_sheet.csv")
+    assert sample_sheet.exists() is False
+
+    # WHEN validating the sample sheet
+    result = cli_runner.invoke(validate_sample_sheet, [str(sample_sheet)], obj=sample_sheet_context)
+
+    # THEN assert that it exits with a non-zero exit code
+    assert result.exit_code != 0
+    # THEN assert the correct information was communicated
+    assert f'Path "{sample_sheet.name}" does not exist' in result.output
+
+
+def test_validate_sample_sheet_wrong_file_type(
+    cli_runner: CliRunner, sample_sheet_context: dict, novaseq_run_parameters: Path, caplog
+):
+    # GIVEN a existing file in the wrong file format
+    sample_sheet = novaseq_run_parameters
+    assert sample_sheet.exists()
+    assert sample_sheet.suffix != ".csv"
+
+    # WHEN validating the sample sheet
+    result: Result = cli_runner.invoke(
+        validate_sample_sheet, [str(sample_sheet)], obj=sample_sheet_context
+    )
+
+    # THEN assert it exits with a non zero exit code
+    assert result.exit_code != 0
+
+    # THEN assert the correct information was communicated
+    assert "seems to be in wrong format" in caplog.text
+
+
+def test_validate_correct_bcl2fastq_sample_sheet(
+    cli_runner: CliRunner, sample_sheet_context: dict, novaseq_bcl2fastq_sample_sheet_path: Path
+):
+    # GIVEN the path to a bcl2fastq sample sheet that exists
+    sample_sheet: Path = novaseq_bcl2fastq_sample_sheet_path
+    bcl_converter = "bcl2fastq"
+    assert sample_sheet.exists()
+    # GIVEN that the sample sheet is correct
+    get_sample_sheet_from_file(infile=sample_sheet, sheet_type="S2", bcl_converter=bcl_converter)
+
+    # WHEN validating the sample sheet
+    result: Result = cli_runner.invoke(
+        validate_sample_sheet, [str(sample_sheet)], obj=sample_sheet_context
+    )
+
+    # THEN assert that it exits without any problems
+    assert result.exit_code == 0
+
+
+def test_validate_correct_dragen_sample_sheet(
+    cli_runner: CliRunner, sample_sheet_context: dict, novaseq_dragen_sample_sheet_path: Path
+):
+    # GIVEN the path to a bcl2fastq sample sheet that exists
+    sample_sheet: Path = novaseq_dragen_sample_sheet_path
+    bcl_converter = "dragen"
+    assert sample_sheet.exists()
+    # GIVEN that the sample sheet is correct
+    get_sample_sheet_from_file(infile=sample_sheet, sheet_type="S2", bcl_converter=bcl_converter)
+
+    # WHEN validating the sample sheet
+    result: Result = cli_runner.invoke(
+        validate_sample_sheet, [str(sample_sheet), "-b", "dragen"], obj=sample_sheet_context
+    )
+
+    # THEN assert that it exits without any problems
+    assert result.exit_code == 0
