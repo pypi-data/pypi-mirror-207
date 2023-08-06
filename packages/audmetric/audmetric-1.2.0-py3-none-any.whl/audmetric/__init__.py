51b914e0001e181e20e73e01a69e7e1b3ea0c2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:02 +0000

    Add package audmetric/1.2.0/audmetric-1.2.0-py3-none-any.whl

---
+++ packages/audmetric/audmetric-1.2.0-py3-none-any.whl/audmetric/__init__.py
@@ -0,0 +1,36 @@
+from audmetric import utils
+from audmetric.core.api import (
+    accuracy,
+    concordance_cc,
+    confusion_matrix,
+    detection_error_tradeoff,
+    edit_distance,
+    equal_error_rate,
+    event_error_rate,
+    fscore_per_class,
+    linkability,
+    mean_absolute_error,
+    mean_squared_error,
+    pearson_cc,
+    precision_per_class,
+    recall_per_class,
+    unweighted_average_bias,
+    unweighted_average_fscore,
+    unweighted_average_recall,
+    unweighted_average_precision,
+    weighted_confusion_error,
+    word_error_rate
+)
+
+
+__all__ = []
+
+
+# Dynamically get the version of the installed module
+try:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:  # pragma: no cover
+    pkg_resources = None  # pragma: no cover
+finally:
+    del pkg_resources
