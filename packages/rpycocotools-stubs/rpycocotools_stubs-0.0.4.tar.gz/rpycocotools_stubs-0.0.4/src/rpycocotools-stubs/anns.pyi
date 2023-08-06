dbf04a3eb7d2335f1283ee2f0b9a974a93e6b09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package rpycocotools-stubs/0.0.4/rpycocotools_stubs-0.0.4.tar.gz

---
+++ packages/rpycocotools-stubs/rpycocotools_stubs-0.0.4.tar.gz/rpycocotools_stubs-0.0.4/src/rpycocotools-stubs/anns.pyi
@@ -0,0 +1,64 @@
+from typing import Generic, Iterator, TypeAlias, TypeVar
+
+from typing_extensions import Self
+
+_TSegmentation = TypeVar("_TSegmentation", Polygons, PolygonsRS, RLE, COCO_RLE)
+
+class Annotation(Generic[_TSegmentation]):
+    id: int
+    image_id: int
+    category_id: int
+    segmentation: _TSegmentation
+    area: float
+    bbox: BBox
+    iscrowd: int
+    def __init__(
+            self: Self,
+            id: int,
+            image_id: int,
+            category_id: int,
+            segmentation: Polygons | PolygonsRS | RLE | COCO_RLE,
+            area: float,
+            bbox: BBox,
+            iscrowd: int,
+    ) -> None: ...
+
+AnnotationAny: TypeAlias = Annotation[Polygons] | Annotation[PolygonsRS] | Annotation[RLE] | Annotation[COCO_RLE]
+
+class Category:
+    id: int
+    name: str
+    supercategory: str
+    def __init__(self: Self, id: int, name: str, supercategory: str) -> None: ...
+
+class BBox:
+    left: float
+    top: float
+    width: float
+    height: float
+    def __init__(self: Self, left: float, top: float, width: float, height: float) -> None: ...
+    def __iter__(self: Self) -> Iterator[float]: ...
+
+class Image:
+    id: int
+    width: int
+    height: int
+    file_name: str
+    def __init__(self: Self, id: int, width: int, height: int, file_name: str) -> None: ...
+
+Polygons: TypeAlias = list[list[float]]
+
+class PolygonsRS:
+    size: list[int]
+    counts: list[list[float]]
+    def __init__(self: Self, size: list[int], counts: list[list[float]]) -> None: ...
+
+class RLE:
+    size: list[int]
+    counts: list[int]
+    def __init__(self: Self, size: list[int], counts: list[int]) -> None: ...
+
+class COCO_RLE:  # noqa: N801
+    size: list[int]
+    counts: str
+    def __init__(self: Self, size: list[int], counts: str) -> None: ...
