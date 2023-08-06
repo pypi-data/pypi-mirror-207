a668f87f49ada82aff5c85fcfad775e9b155b521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:11 +0000

    Add package pydra-fsl/0.0.16/pydra_fsl-0.0.16.tar.gz

---
+++ packages/pydra-fsl/pydra_fsl-0.0.16.tar.gz/pydra_fsl-0.0.16/src/pydra/tasks/fsl/__init__.py
@@ -0,0 +1,36 @@
+"""Pydra tasks for FSL.
+
+>>> from pydra.tasks import fsl
+"""
+
+from .bet import BET, RobustFOV
+from .eddy import Eddy
+from .fast import FAST
+from .flirt import (
+    FLIRT,
+    ApplyXFM,
+    ConcatXFM,
+    ConvertXFM,
+    Img2ImgCoord,
+    Img2StdCoord,
+    InvertXFM,
+    Std2ImgCoord,
+)
+from .fnirt import FNIRT, ApplyWarp, ConvertWarp, FNIRTFileUtils, InvWarp
+from .fugue import FUGUE, FSLPrepareFieldmap, Prelude, SigLoss
+from .susan import SUSAN
+from .utils import (
+    FSLFFT,
+    FSLROI,
+    FSLChFileType,
+    FSLInfo,
+    FSLInterleave,
+    FSLMerge,
+    FSLReorient2Std,
+    FSLSelectVols,
+    FSLSlice,
+    FSLSmoothFill,
+    FSLSplit,
+    FSLSwapDim,
+    fslmaths,
+)
