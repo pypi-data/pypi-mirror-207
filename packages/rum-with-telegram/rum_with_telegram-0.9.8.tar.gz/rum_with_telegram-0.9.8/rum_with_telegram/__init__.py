4e1bae1d54286f3e0227f28db3805b257b60c58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:31 +0000

    Add package rum-with-telegram/0.9.8/rum_with_telegram-0.9.8.tar.gz

---
+++ packages/rum-with-telegram/rum_with_telegram-0.9.8.tar.gz/rum_with_telegram-0.9.8/rum_with_telegram/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+from rum_with_telegram.data_exchanger import DataExchanger
+
+__version__ = "0.9.8"
+__author__ = "liujuanjuan1984"
+
+logger = logging.getLogger(__name__)
+logger.info("Version %s", __version__)
