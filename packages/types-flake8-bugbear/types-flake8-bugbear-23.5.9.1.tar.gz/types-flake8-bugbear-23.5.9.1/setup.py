43025d3be223eb1283a7bacb743a7a3b8c0714d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:00 +0000

    Add package types-flake8-bugbear/23.5.9.1/types-flake8-bugbear-23.5.9.1.tar.gz

---
+++ packages/types-flake8-bugbear/types-flake8-bugbear-23.5.9.1.tar.gz/types-flake8-bugbear-23.5.9.1/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-flake8-bugbear"
+description = "Typing stubs for flake8-bugbear"
+long_description = '''
+## Typing stubs for flake8-bugbear
+
+This is a PEP 561 type stub package for the `flake8-bugbear` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`flake8-bugbear`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/flake8-bugbear. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `d1bfd08b4bc843227d097decfd99d70272a1f804`.
+'''.lstrip()
+
+setup(name=name,
+      version="23.5.9.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/flake8-bugbear.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['bugbear-stubs'],
+      package_data={'bugbear-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
