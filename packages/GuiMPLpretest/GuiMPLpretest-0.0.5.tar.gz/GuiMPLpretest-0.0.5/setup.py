8d88dcc4531a4dc08a5a934e7af48f2c8d06f17d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:11 +0000

    Add package GuiMPLpretest/0.0.5/GuiMPLpretest-0.0.5.tar.gz

---
+++ packages/GuiMPLpretest/GuiMPLpretest-0.0.5.tar.gz/GuiMPLpretest-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.5"
+DESCRIPTION = "Application pour modifier l'esthétique d'un graphique matplotlib"
+LONG_DESCRIPTION = "Projet Figure S2 MPCI ayant pour but le développement d'une interface graphique permettant d'intéragir ergonomiquement avec des figures matplotlib"
+
+# Setting up
+setup(
+    name="GuiMPLpretest",
+    version=VERSION,
+    author="Groupe Figure MPCI",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["matplotlib"],
+    keywords=["matplotlib", "graphique", "interface graphique", "python", "figures"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
