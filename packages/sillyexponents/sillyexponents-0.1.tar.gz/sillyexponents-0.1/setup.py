1dd4b75b1f3babe0401e1d2409ecebb5592226ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:21 +0000

    Add package sillyexponents/0.1/sillyexponents-0.1.tar.gz

---
+++ packages/sillyexponents/sillyexponents-0.1.tar.gz/sillyexponents-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='sillyexponents',
+      version='0.1',
+      author='sillymonke21',
+      author_email='sillymonke21@gmail.com',
+      description='A simple exponents package for Python.',
+      packages=find_packages())
