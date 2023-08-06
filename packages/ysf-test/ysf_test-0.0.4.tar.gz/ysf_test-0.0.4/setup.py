74685f61a0820f1c5c1d022cda095b468ce337d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:12 +0000

    Add package ysf-test/0.0.4/ysf_test-0.0.4.tar.gz

---
+++ packages/ysf-test/ysf_test-0.0.4.tar.gz/ysf_test-0.0.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+time: 2023/5/8 15:40
+file: setup.py
+author: yisifan
+email: yisifan@datagrand.com
+"""
+import setuptools
+
+setuptools.setup(
+    name="ysf_test",
+    version="0.0.4",
+    author="yisifan",
+    description="a small example package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
