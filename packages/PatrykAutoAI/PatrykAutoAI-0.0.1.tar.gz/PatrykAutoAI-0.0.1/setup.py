31a335075b9b06b4b8e236cd43ec6ec093c2cf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:47 +0000

    Add package PatrykAutoAI/0.0.1/PatrykAutoAI-0.0.1.tar.gz

---
+++ packages/PatrykAutoAI/PatrykAutoAI-0.0.1.tar.gz/PatrykAutoAI-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Auto_AI_patryk'
+LONG_DESCRIPTION = 'Package contain helping functions for creating ML models'
+
+setup(
+    name="PatrykAutoAI",
+    version=VERSION,
+    author="patryk",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'machine_learning', 'auto'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
