1dd17af73b6fb6ddf1828277acae2bd202f92591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:27 +0000

    Add package csv-to-dynamodb/0.2.6/csv_to_dynamodb-0.2.6.tar.gz

---
+++ packages/csv-to-dynamodb/csv_to_dynamodb-0.2.6.tar.gz/csv_to_dynamodb-0.2.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csv_to_dynamodb',
+    version='0.2.6',
+    author='Jason Miller',
+    author_email='hackr@duck.com',
+    description='A library for automatically creating DynamoDB tables from CSV files and, optionally, automatically populating those tables.',
+    long_description='A library for automatically creating DynamoDB tables from CSV files and, optionally, automatically populating those tables.',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'botocore',
+        'pandas'
+    ],
+)
