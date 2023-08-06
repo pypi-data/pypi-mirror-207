789b02509fd1d378ac09c146ef2c3e19400d31e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:34 +0000

    Add package minet/1.0.0a27/minet-1.0.0a27.tar.gz

---
+++ packages/minet/minet-1.0.0a27.tar.gz/minet-1.0.0a27/minet/cli/extract/extract.py
@@ -0,0 +1,218 @@
+# =============================================================================
+# Minet Extract Content CLI Action
+# =============================================================================
+#
+# Logic of the extract action.
+#
+from typing import Optional, List, Dict, Iterator, Any
+
+from os.path import isdir
+from trafilatura.core import bare_extraction
+from dataclasses import dataclass
+from itertools import count
+from casanova import TabularRecord, ThreadSafeEnricher
+
+from minet.multiprocessing import LazyPool
+from minet.serialization import serialize_error_as_slug
+from minet.fs import read_potentially_gzipped_path
+from minet.cli.utils import (
+    create_fetch_like_report_iterator,
+    with_enricher_and_loading_bar,
+    FetchReportLikeItem,
+)
+from minet.cli.console import console
+from minet.exceptions import TrafilaturaError
+from minet.cli.exceptions import FatalError
+
+
+@dataclass
+class ExtractWorkerPayload:
+    id: int
+    encoding: Optional[str]
+    path: Optional[str]
+    text: Optional[str]
+
+
+@dataclass
+class ExtractResult:
+    id: int
+    error: Optional[Exception] = None
+    meta: Optional[Dict[str, Any]] = None
+
+
+@dataclass
+class ExtractAddendum(TabularRecord):
+    extract_error: Optional[str] = None
+    canonical_url: Optional[str] = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    content: Optional[str] = None
+    comments: Optional[str] = None
+    author: Optional[str] = None
+    categories: Optional[List[str]] = None
+    tags: Optional[List[str]] = None
+    date: Optional[str] = None
+    sitename: Optional[str] = None
+
+
+def plural(meta: Dict[str, Any], key: str) -> List[str]:
+    l = meta.get(key, []) or []
+
+    items = []
+
+    if not l:
+        return items
+
+    for item in l:
+        if isinstance(item, dict):
+            item = item.get("name")
+
+            if item is None:
+                continue
+
+        if isinstance(item, (int, float)):
+            item = str(item)
+
+        if not isinstance(item, str):
+            continue
+
+        for subitem in item.split(","):
+            subitem = subitem.strip()
+
+            if subitem:
+                items.append(subitem)
+
+    return items
+
+
+def worker(payload: ExtractWorkerPayload) -> ExtractResult:
+    text = payload.text
+
+    result = ExtractResult(id=payload.id)
+
+    # Reading file
+    if text is None:
+        try:
+            text = read_potentially_gzipped_path(
+                payload.path, encoding=payload.encoding
+            )
+        except (FileNotFoundError, UnicodeDecodeError) as e:
+            result.error = e
+            return result
+
+    # Attempting extraction
+    try:
+        # https://trafilatura.readthedocs.io/en/latest/corefunctions.html
+        # TODO: discuss deduplication
+        # TODO: fallback options
+        result.meta = bare_extraction(text)
+    except Exception as e:
+        result.error = TrafilaturaError(reason=e)
+        return result
+
+    return result
+
+
+@with_enricher_and_loading_bar(
+    headers=ExtractAddendum,
+    enricher_type="threadsafe",
+    index_column="extract_original_index",
+    title="Extracting text",
+    unit="docs",
+    total_from_enricher=False,
+)
+def action(cli_args, enricher: ThreadSafeEnricher, loading_bar):
+    if cli_args.input_dir is not None and not isdir(cli_args.input_dir):
+        raise FatalError(
+            'Could not find the [cyan]-I/--input-dir "{}"[/cyan] directory!'.format(
+                cli_args.input_dir
+            )
+        )
+
+    if not cli_args.glob:
+        loading_bar.set_total(enricher.total)
+
+    items = create_fetch_like_report_iterator(cli_args, enricher)
+
+    worked_on: Dict[int, FetchReportLikeItem] = {}
+
+    def payloads() -> Iterator[ExtractWorkerPayload]:
+        current_id = count()
+
+        for item in items:
+
+            # Items we cannot process
+            if item.error is not None:
+                loading_bar.advance()
+                loading_bar.inc_stat(item.error, style="error")
+                enricher.writerow(item.index, item.row)
+                continue
+
+            item_id = next(current_id)
+
+            worked_on[item_id] = item
+
+            yield ExtractWorkerPayload(
+                id=item_id, encoding=item.encoding, path=item.path, text=item.text
+            )
+
+    pool = LazyPool(cli_args.processes)
+
+    loading_bar.set_title("Extracting text (p=%i)" % pool.processes)
+
+    warned_about_input_dir = False
+
+    with pool:
+        for result in pool.imap_unordered(
+            worker, payloads(), chunksize=cli_args.chunk_size
+        ):
+            with loading_bar.step():
+                assert isinstance(result, ExtractResult)
+
+                item = worked_on.pop(result.id)
+                row = item.row
+
+                if result.error is not None:
+                    error_code = serialize_error_as_slug(result.error)
+                    loading_bar.inc_stat(error_code, style="error")
+
+                    if not warned_about_input_dir and error_code == "file-not-found":
+                        warned_about_input_dir = True
+
+                        if cli_args.input_dir is None:
+                            console.warning(
+                                "did you forget to give [cyan]-I/--input-dir[/cyan]? "
+                            )
+                        else:
+                            console.warning(
+                                'are you sure [cyan]-I/--input-dir "{}"[/cyan] is the correct path?'.format(
+                                    cli_args.input_dir
+                                )
+                            )
+
+                    addendum = ExtractAddendum(extract_error=error_code)
+                    enricher.writerow(item.index, row, addendum)
+
+                    continue
+
+                meta = result.meta
+
+                if meta is None:
+                    loading_bar.inc_stat("no-trafilatura-result", style="error")
+                    console.warning("did you forget to use [cyan]-i/--input[/cyan]?")
+                    addendum = ExtractAddendum(extract_error="no-trafilatura-result")
+                else:
+                    addendum = ExtractAddendum(
+                        canonical_url=meta.get("url"),
+                        title=meta.get("title"),
+                        description=meta.get("description"),
+                        content=meta.get("text"),
+                        comments=meta.get("comments"),
+                        author=meta.get("author"),
+                        categories=plural(meta, "categories"),
+                        tags=plural(meta, "tags"),
+                        date=meta.get("date"),
+                        sitename=meta.get("sitename"),
+                    )
+
+                enricher.writerow(item.index, row, addendum)
---
+++ packages/minet/minet-1.0.0a27.tar.gz/minet-1.0.0a27/minet/executors.py
@@ -0,0 +1,428 @@
+# =============================================================================
+# Minet HTTP Multithreaded Executors
+# =============================================================================
+#
+# Exposing a specialized quenouille wrapper grabbing various urls from the
+# web in a multithreaded fashion.
+#
+from typing import (
+    Optional,
+    Iterator,
+    Callable,
+    TypeVar,
+    Generic,
+    Iterable,
+    Dict,
+    Union,
+    Any,
+)
+
+import urllib3
+import threading
+from threading import Event
+from quenouille import ThreadPoolExecutor
+from ural import get_domain_name, ensure_protocol
+from tenacity import RetryCallState
+
+from minet.exceptions import CancelledRequestError, HTTPCallbackError
+from minet.web import (
+    create_pool_manager,
+    create_request_retryer,
+    request,
+    resolve,
+    Response,
+    RedirectionStack,
+    AnyTimeout,
+    EXPECTED_WEB_ERRORS,
+)
+from minet.constants import (
+    DEFAULT_DOMAIN_PARALLELISM,
+    DEFAULT_IMAP_BUFFER_SIZE,
+    DEFAULT_THROTTLE,
+    DEFAULT_URLLIB3_TIMEOUT,
+    DEFAULT_FETCH_MAX_REDIRECTS,
+    DEFAULT_RESOLVE_MAX_REDIRECTS,
+)
+
+ItemType = TypeVar("ItemType")
+ResultType = TypeVar("ResultType")
+AddendumType = TypeVar("AddendumType")
+
+CANCELLED = object()
+
+
+class HTTPWorkerPayload(Generic[ItemType]):
+    __slots__ = ("item", "url", "__has_cached_domain", "__domain")
+
+    item: ItemType
+    url: Optional[str]
+
+    __has_cached_domain: bool
+    __domain: Optional[str]
+
+    def __init__(self, item: ItemType, url: Optional[str]):
+        self.item = item
+        self.url = url
+        self.__has_cached_domain = False
+        self.__domain = None
+
+    @property
+    def domain(self) -> Optional[str]:
+        if self.__has_cached_domain:
+            return self.__domain
+
+        if self.url is not None:
+            self.__domain = get_domain_name(self.url)
+
+        self.__has_cached_domain = True
+
+        return self.__domain
+
+
+ArgsCallbackType = Callable[[HTTPWorkerPayload[ItemType]], Dict]
+
+
+class RequestResult(Generic[ItemType, AddendumType]):
+    __slots__ = ("item", "url", "error", "response", "addendum")
+
+    item: ItemType
+    url: Optional[str]
+    error: Optional[Exception]
+    response: Optional[Response]
+    addendum: Optional[AddendumType]
+
+    def __init__(self, item: ItemType, url: Optional[str]):
+        self.item = item
+        self.url = url
+        self.error = None
+        self.response = None
+        self.addendum = None
+
+    def __repr__(self):
+        name = self.__class__.__name__
+
+        if not self.url:
+            return "<{name} null!>".format(name=name)
+
+        if not self.response:
+            return "<{name} url={url!r} pending!>".format(name=name, url=self.url)
+
+        if self.error:
+            return "<{name} url={url!r} error={error}>".format(
+                name=name, url=self.url, error=self.error.__class__.__name__
+            )
+
+        assert self.response is not None
+
+        return "<{name} url={url!r} status={status!r}>".format(
+            name=name, url=self.url, status=self.response.status
+        )
+
+
+class ResolveResult(Generic[ItemType, AddendumType]):
+    __slots__ = ("item", "url", "error", "stack", "addendum")
+
+    item: ItemType
+    url: Optional[str]
+    error: Optional[Exception]
+    stack: Optional[RedirectionStack]
+    addendum: Optional[AddendumType]
+
+    def __init__(self, item: ItemType, url: Optional[str]):
+        self.item = item
+        self.url = url
+        self.error = None
+        self.stack = None
+        self.addendum = None
+
+    def __repr__(self):
+        name = self.__class__.__name__
+
+        if not self.url:
+            return "<{name} null!>".format(name=name)
+
+        if self.error:
+            return "<{name} url={url!r} error={error}>".format(
+                name=name, url=self.url, error=self.error.__class__.__name__
+            )
+
+        assert self.stack is not None
+
+        return "<{name} url={url!r} status={status!r} redirects={redirects!r}>".format(
+            name=name,
+            url=self.url,
+            status=self.stack[-1].status,
+            redirects=len(self.stack),
+        )
+
+
+def key_by_domain_name(payload: HTTPWorkerPayload) -> Optional[str]:
+    return payload.domain
+
+
+def payloads_iter(
+    iterable: Iterable[ItemType],
+    key: Optional[Callable[[ItemType], Optional[str]]] = None,
+) -> Iterator[HTTPWorkerPayload[ItemType]]:
+    for item in iterable:
+        url = item if key is None else key(item)
+
+        if not url:
+            yield HTTPWorkerPayload(item=item, url=None)
+            continue
+
+        # Url cleanup
+        url = ensure_protocol(url.strip())  # type: ignore
+
+        yield HTTPWorkerPayload(item=item, url=url)
+
+
+class HTTPWorker(Generic[ItemType, AddendumType]):
+    def __init__(
+        self,
+        pool_manager: urllib3.PoolManager,
+        cancel_event: Event,
+        local_context: threading.local,
+        *,
+        resolving: bool = False,
+        get_args: Optional[ArgsCallbackType[ItemType]] = None,
+        max_redirects: int = DEFAULT_FETCH_MAX_REDIRECTS,
+        follow_refresh_header: bool = True,
+        follow_meta_refresh: bool = False,
+        follow_js_relocation: bool = False,
+        infer_redirection: bool = False,
+        canonicalize: bool = False,
+        callback: Optional[
+            Callable[[RequestResult[ItemType, AddendumType]], AddendumType]
+        ] = None,
+    ):
+        self.cancel_event = cancel_event
+        self.local_context = local_context
+        self.get_args = get_args
+        self.callback = callback
+
+        self.resolving = resolving
+        self.fn = request if not resolving else resolve
+        self.Result = RequestResult if not resolving else ResolveResult
+
+        self.default_kwargs = {
+            "pool_manager": pool_manager,
+            "max_redirects": max_redirects,
+            "cancel_event": cancel_event,
+            "follow_refresh_header": follow_refresh_header,
+            "follow_meta_refresh": follow_meta_refresh,
+            "follow_js_relocation": follow_js_relocation,
+            "infer_redirection": infer_redirection,
+            "canonicalize": canonicalize,
+        }
+
+    def __call__(
+        self, payload: HTTPWorkerPayload[ItemType]
+    ) -> Union[object, RequestResult[ItemType, AddendumType]]:
+        item, url = payload.item, payload.url
+
+        result = self.Result(item, url)
+
+        # Noop
+        if url is None:
+            return result
+
+        kwargs = {}
+
+        if self.cancel_event.is_set():
+            return CANCELLED
+
+        if self.get_args is not None:
+            # NOTE: given callback must be threadsafe
+            kwargs = self.get_args(payload)
+
+        if self.cancel_event.is_set():
+            return CANCELLED
+
+        try:
+            retryer = getattr(self.local_context, "retryer", None)
+            kwargs.update(self.default_kwargs)
+
+            if retryer is not None:
+                output = retryer(self.fn, url, **kwargs)
+            else:
+                output = self.fn(url, **kwargs)
+
+        except CancelledRequestError:
+            return CANCELLED
+
+        except EXPECTED_WEB_ERRORS as error:
+            result.error = error
+
+        else:
+            if self.resolving:
+                result.stack = output
+            else:
+                result.response = output
+
+            if self.callback is not None:
+                if self.cancel_event.is_set():
+                    return CANCELLED
+
+                try:
+                    if retryer is not None:
+                        result.addendum = retryer(self.callback, result)
+                    else:
+                        result.addendum = self.callback(result)
+                except Exception as reason:
+                    result.error = HTTPCallbackError(reason)
+
+        return result
+
+
+class HTTPThreadPoolExecutor(ThreadPoolExecutor):
+    def __init__(
+        self,
+        max_workers: Optional[int] = None,
+        wait: bool = True,
+        daemonic: bool = False,
+        insecure: bool = False,
+        timeout: AnyTimeout = DEFAULT_URLLIB3_TIMEOUT,
+        spoof_tls_ciphers: bool = False,
+        proxy: Optional[str] = None,
+        retry: bool = False,
+        retryer_kwargs: Optional[Dict[str, Any]] = None,
+        **kwargs,
+    ):
+        self.cancel_event = Event()
+        self.local_context = threading.local()
+
+        if retry:
+
+            def epilog(retry_state: RetryCallState) -> str:
+                return retry_state.args[0]
+
+            default_retryer_kwargs = {
+                "retry_on_timeout": False,
+                "cancel_event": self.cancel_event,
+                "max_attempts": 3,
+                "epilog": epilog,
+            }
+
+            default_retryer_kwargs.update(retryer_kwargs or {})
+
+            def init_local_context():
+                self.local_context.retryer = create_request_retryer(
+                    **default_retryer_kwargs
+                )
+
+            kwargs["initializer"] = init_local_context
+
+        super().__init__(
+            max_workers,
+            wait=wait,
+            daemonic=daemonic,
+            **kwargs,
+        )
+
+        # NOTE: 0 workers means a synchronous pool in quenouille,
+        # so we reserve at least one connection for the pool.
+        self.pool_manager = create_pool_manager(
+            threads=max(1, self.max_workers),
+            insecure=insecure,
+            timeout=timeout,
+            spoof_tls_ciphers=spoof_tls_ciphers,
+            proxy=proxy,
+        )
+
+    def cancel(self) -> None:
+        self.cancel_event.set()
+
+    def shutdown(self, wait=True) -> None:
+        self.cancel()
+        self.pool_manager.clear()
+        return super().shutdown(wait=wait)
+
+    def request(
+        self,
+        iterator: Iterable[ItemType],
+        *,
+        ordered: bool = False,
+        key: Optional[Callable[[ItemType], Optional[str]]] = None,
+        throttle: float = DEFAULT_THROTTLE,
+        request_args: Optional[ArgsCallbackType[ItemType]] = None,
+        buffer_size: int = DEFAULT_IMAP_BUFFER_SIZE,
+        domain_parallelism: int = DEFAULT_DOMAIN_PARALLELISM,
+        max_redirects: int = DEFAULT_FETCH_MAX_REDIRECTS,
+        callback: Optional[
+            Callable[[RequestResult[ItemType, AddendumType]], None]
+        ] = None,
+    ) -> Iterator[RequestResult[ItemType, AddendumType]]:
+
+        # TODO: validate
+        worker = HTTPWorker(
+            self.pool_manager,
+            self.cancel_event,
+            self.local_context,
+            get_args=request_args,
+            max_redirects=max_redirects,
+            callback=callback,
+        )
+
+        method = super().imap if ordered else super().imap_unordered
+
+        imap_unordered = method(
+            payloads_iter(iterator, key=key),
+            worker,
+            key=key_by_domain_name,
+            parallelism=domain_parallelism,
+            buffer_size=buffer_size,
+            throttle=throttle,
+        )
+
+        return (item for item in imap_unordered if item is not CANCELLED)  # type: ignore
+
+    def resolve(
+        self,
+        iterator: Iterable[ItemType],
+        *,
+        ordered: bool = False,
+        key: Optional[Callable[[ItemType], Optional[str]]] = None,
+        throttle: float = DEFAULT_THROTTLE,
+        resolve_args: Optional[ArgsCallbackType[ItemType]] = None,
+        buffer_size: int = DEFAULT_IMAP_BUFFER_SIZE,
+        domain_parallelism: int = DEFAULT_DOMAIN_PARALLELISM,
+        max_redirects: int = DEFAULT_RESOLVE_MAX_REDIRECTS,
+        follow_refresh_header: bool = True,
+        follow_meta_refresh: bool = False,
+        follow_js_relocation: bool = False,
+        infer_redirection: bool = False,
+        canonicalize: bool = False,
+        callback: Optional[
+            Callable[[ResolveResult[ItemType, AddendumType]], None]
+        ] = None,
+    ) -> Iterator[ResolveResult[ItemType, AddendumType]]:
+
+        # TODO: validate
+        worker = HTTPWorker(
+            self.pool_manager,
+            self.cancel_event,
+            self.local_context,
+            resolving=True,
+            get_args=resolve_args,
+            max_redirects=max_redirects,
+            follow_refresh_header=follow_refresh_header,
+            follow_meta_refresh=follow_meta_refresh,
+            follow_js_relocation=follow_js_relocation,
+            infer_redirection=infer_redirection,
+            canonicalize=canonicalize,
+            callback=callback,
+        )
+
+        method = super().imap if ordered else super().imap_unordered
+
+        imap_unordered = method(
+            payloads_iter(iterator, key=key),
+            worker,
+            key=key_by_domain_name,
+            parallelism=domain_parallelism,
+            buffer_size=buffer_size,
+            throttle=throttle,
+        )
+
+        return (item for item in imap_unordered if item is not CANCELLED)  # type: ignore
