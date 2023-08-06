9540d9ca9ab159a6842d2db8a4e95ed4bb6e0d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:20 +0000

    Add package pyboil/0.5.17/pyboil-0.5.17.tar.gz

---
+++ packages/pyboil/pyboil-0.5.17.tar.gz/pyboil-0.5.17/pyboil/pyboil.py
@@ -0,0 +1,25 @@
+from pyboil.parser import parser
+import shutil
+from os.path import dirname, abspath
+
+def launch():
+    args = parser.parse_args()
+
+    final_path = f"{dirname(abspath(__file__))}/template"
+    shutil.copytree(final_path, './', dirs_exist_ok=True)
+
+    with open('./setup.py') as f:
+        setup = f.read()
+
+    setup.replace('{NAME}', args['NAME'])
+    setup.replace('{VERSION}', args['VERSION'])
+    setup.replace('{DESCRIPTION}', args['DESCRIPTION'])
+    setup.replace('{AUTHOR}', args['AUTHOR'])
+    setup.replace('{EMAIL}', args['EMAIL'])
+    setup.replace('{URL}', args['URL'])
+
+    with open('./setup.py', 'w') as f:
+        f.write(setup)
+
+if __name__ == "__main__":
+    launch()
