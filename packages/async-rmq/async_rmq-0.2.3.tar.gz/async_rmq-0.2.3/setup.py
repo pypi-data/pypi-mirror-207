e93707be020d7c5b3fdf15abc214aa5a6d725f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:51 +0000

    Add package async-rmq/0.2.3/async_rmq-0.2.3.tar.gz

---
+++ packages/async-rmq/async_rmq-0.2.3.tar.gz/async_rmq-0.2.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='async_rmq',
+      version='0.2.3',
+      description='Module for working with asynchronous RabbitMQ library',
+      packages=['async_rmq'],
+      author_email='konstantizaharov@gmail.com',
+      zip_safe=False)
