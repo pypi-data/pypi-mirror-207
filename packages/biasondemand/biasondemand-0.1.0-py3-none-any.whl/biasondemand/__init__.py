ea3f7a81cf2c7ce78940c0dfbf49f11d82220e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:14 +0000

    Add package biasondemand/0.1.0/biasondemand-0.1.0-py3-none-any.whl

---
+++ packages/biasondemand/biasondemand-0.1.0-py3-none-any.whl/biasondemand/__init__.py
@@ -0,0 +1,13 @@
+"""
+biasondemand.
+
+biasondemand is a Python package that generates synthetic datasets with different types of bias.
+"""
+
+from .generate_dataset import generate_dataset
+from .generate_dataset import main as bias_on_demand_generate_dataset
+
+__version__ = "0.1.0"
+__author__ = 'Joachim Baumann'
+__email__ = 'joachimbaumann1@gmail.com'
+__credits__ = 'University of Zurich & Zurich University of Applied Sciences'
