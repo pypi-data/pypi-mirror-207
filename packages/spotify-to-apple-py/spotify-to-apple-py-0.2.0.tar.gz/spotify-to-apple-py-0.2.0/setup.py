6d4a2e493cc9f34e73cafc25de7684e2c54d924c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:30 +0000

    Add package spotify-to-apple-py/0.2.0/spotify-to-apple-py-0.2.0.tar.gz

---
+++ packages/spotify-to-apple-py/spotify-to-apple-py-0.2.0.tar.gz/spotify-to-apple-py-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+      name="spotify-to-apple-py",
+    url="https://github.com/Beza4598/spotify_apple_converter.git",
+    long_description="docs/index.rst",
+    long_description_content_type="text/markdown",
+    author="Beza Amsalu",
+    author_email="baa2159@columbia.edu",
+    version="0.2.0",
+)
