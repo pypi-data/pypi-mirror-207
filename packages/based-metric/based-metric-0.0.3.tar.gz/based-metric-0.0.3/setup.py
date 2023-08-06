52bd9dbf6cffc26796f3ebf0988dd3fddda75b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:05 +0000

    Add package based-metric/0.0.3/based-metric-0.0.3.tar.gz

---
+++ packages/based-metric/based-metric-0.0.3.tar.gz/based-metric-0.0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name="based-metric",
+    version="0.0.3",
+    url="",
+    author="Nick Alutis",
+    author_email="",
+    description="BASED metric for deblurring assesement",
+    packages=setuptools.find_packages(),
+    long_description="",
+    install_requires=["numpy",
+                    "lpips",
+                    "torchsummary",
+                    "opencv-contrib-python",
+                    "opencv-python",
+                    "pandas",
+                    "scikit-image",
+                    "scikit-learn",
+                    "matplotlib",
+                    "seaborn",
+                    "scipy",
+                    "pathos",
+                    "tqdm"
+                ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+    include_package_data=True,
+    package_data={'model': ['*.joblib']},
+)
