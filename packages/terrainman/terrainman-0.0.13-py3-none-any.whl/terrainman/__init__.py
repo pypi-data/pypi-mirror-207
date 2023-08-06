43f0f9bbbac9d35a2e7becc3dd93f9ce8297aa9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:03 +0000

    Add package terrainman/0.0.13/terrainman-0.0.13-py3-none-any.whl

---
+++ packages/terrainman/terrainman-0.0.13-py3-none-any.whl/terrainman/__init__.py
@@ -0,0 +1,18 @@
+import os
+import dotenv
+
+from .tile_io import *
+
+if os.path.exists('.env.shared'):
+    dotenv.load_dotenv('.env.shared')
+
+os.environ['TERRAIN_SRC'] = os.path.dirname(
+    os.path.abspath(inspect.getsourcefile(lambda: 0))
+)
+os.environ['TERRAIN_DATA'] = os.path.join(os.environ['TERRAIN_SRC'], 'data')
+_DATA_PRODUCT_TYPES = ["aerosol", "cloud", "terrain"]
+
+for product in _DATA_PRODUCT_TYPES:
+    product_data_path = os.path.join(os.environ['TERRAIN_DATA'], product)
+    if not os.path.exists(product_data_path):
+        os.mkdir(product_data_path)
