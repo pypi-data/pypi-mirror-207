d39d92c741f9983b8c5afb25dbb154a5c74f2bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:35 +0000

    Add package venakatest/0.1.32/venakatest-0.1.32-py3-none-any.whl

---
+++ packages/venakatest/venakatest-0.1.32-py3-none-any.whl/venakatest/test.py
@@ -0,0 +1,65 @@
+
+import binascii
+import traceback
+import warnings
+import sys
+import argparse
+import subprocess
+import json
+import re
+import logging
+import os
+import threading
+import logging.config
+import datetime
+python_executable = 'python.exe'
+batch_executable = 'cmd.exe'
+powershell_executable = 'powershell.exe'
+def testing():
+    return "ok"
+def exception_handler(type, value, tb):
+    logging.exception("Uncaught exception: {0} {1} - Line : {2} ".format(str(value), str(type),str(tb.tb_lineno)))
+logging.basicConfig(level=logging.DEBUG)
+def check():
+    if sys.executable.endswith(python_executable):
+        logging.exception("Running from python file")
+
+        print("Running from a python file")
+        # Additional batch-specific code here
+    elif sys.executable.endswith(batch_executable):
+        print("Running from batch")
+        logging.exception("Running from a batch file")
+        # Additional PowerShell-specific code here
+    elif sys.executable.endswith(powershell_executable):
+        logging.exception("Running from a powershell file")
+        print("Running directly from powershell")
+    # Code to handle other cases
+
+# Install exception handler
+sys.excepthook = exception_handler
+
+def tobe():
+    logging.info('Started syncing commits to {}'.format("testubg"))
+    check()
+    logging.info('Start commit: {}'.format("start"))
+
+
+parser = argparse.ArgumentParser(description='Sync a number of commits before a specific commit')
+
+parser.add_argument('--project', type=str, required=True,
+                    help='Enter project name')
+parser.add_argument('--token', type=str, required=True,
+                    help='The API key to communicate with API')
+
+args = parser.parse_args()
+
+project = args.project
+token = args.token
+
+debug = True
+
+
+
+
+if __name__ == '__main__':
+    tobe()
