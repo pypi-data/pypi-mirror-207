73fe073bdcfe9b7b06d30d0ec4cf55791340db27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:50 +0000

    Add package stacs/0.5.1/stacs-0.5.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/stacs/stacs-0.5.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/stacs/scan/output/__init__.py
@@ -0,0 +1,8 @@
+"""Defines outputs supported by STACS.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+from stacs.scan.output import markdown  # noqa: F401
+from stacs.scan.output import pretty  # noqa: F401
+from stacs.scan.output import sarif  # noqa: F401
