402034024b7e02df0791e131816fb1ecc5576b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:58 +0000

    Add package tbtool/0.1/tbtool-0.1.tar.gz

---
+++ packages/tbtool/tbtool-0.1.tar.gz/tbtool-0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt","r") as req:
+    required_packages = req.read().splitlines()
+
+setuptools.setup(
+    name="tbtool",                
+    version="0.1",                        
+    author="Bhanuja",  
+    author_email="abhanuja@presidio.com",
+    license= 'MIT',              
+    description="Data And Analytics Audit Tool",
+    long_description=long_description,     
+    long_description_content_type="text/markdown",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
+        'common': ['templates/*.html']
+    },
+    py_modules=['main'],
+    entry_points={
+        'console_scripts': [
+            'tbtool=main:generate_aws_audit_report'
+        ]
+    },
+    include_package_data=True,
+    python_requires='>=3.6',                
+    install_requires=required_packages                   
+)
