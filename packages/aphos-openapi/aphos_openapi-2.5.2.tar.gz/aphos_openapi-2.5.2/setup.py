1bbd9955f90568643768ea8a7ba11abfb7c4b1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:24 +0000

    Add package aphos-openapi/2.5.2/aphos_openapi-2.5.2.tar.gz

---
+++ packages/aphos-openapi/aphos_openapi-2.5.2.tar.gz/aphos_openapi-2.5.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aphos_openapi",
+    version="2.5.2",
+    author="Pavel Kinc",
+    description="APhoS Python library for data representation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    license_files=('LICENSE',),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'urllib3>=1.25.3,<2.0',
+        'python-dateutil',
+        'matplotlib',
+        'astropy<=5.2.2'
+    ]
+)
