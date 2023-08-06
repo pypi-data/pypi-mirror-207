331319994811702e1314f326820c475b9b33b65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:52 +0000

    Add package openrl/0.0.9/openrl-0.0.9.tar.gz

---
+++ packages/openrl/openrl-0.0.9.tar.gz/openrl-0.0.9/openrl/__init__.py
@@ -0,0 +1,15 @@
+__TITLE__ = "openrl"
+__VERSION__ = "v0.0.9"
+__DESCRIPTION__ = "Distributed Deep RL Framework"
+__AUTHOR__ = "OpenRL Contributors"
+__EMAIL__ = "huangshiyu@4paradigm.com"
+__version__ = __VERSION__
+
+import platform
+
+python_version_list = list(map(int, platform.python_version_tuple()))
+assert python_version_list >= [
+    3,
+    8,
+    0,
+], f"OpenRL requires Python 3.8 or newer, but your Python is {platform.python_version()}"
