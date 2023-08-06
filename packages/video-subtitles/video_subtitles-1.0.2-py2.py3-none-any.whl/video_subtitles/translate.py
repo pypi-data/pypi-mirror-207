1c30d1575b6c4b688dec5860168ee4561874d7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:59 +0000

    Add package video-subtitles/1.0.2/video_subtitles-1.0.2-py2.py3-none-any.whl

---
+++ packages/video-subtitles/video_subtitles-1.0.2-py2.py3-none-any.whl/video_subtitles/translate.py
@@ -0,0 +1,32 @@
+"""
+Handles srt translation and wrapping.
+"""
+
+# pylint: disable=import-error
+
+from srtranslator import SrtFile
+from srtranslator.translators.deepl_api import DeeplApi
+from srtranslator.translators.deepl_scrap import DeeplTranslator as FreeTranslator
+
+# from srtranslator.translators.translatepy import TranslatePy
+
+
+def srt_wrap(srt_file: str) -> None:
+    """Wrap lines in a srt file."""
+    srt = SrtFile(srt_file)
+    srt.wrap_lines()
+    srt.save(srt_file)
+
+
+def translate(
+    api_key: str | None, in_srt: str, out_srt: str, from_lang: str, to_lang: str
+) -> None:
+    """Translate a srt file."""
+    if api_key is None:
+        translator = FreeTranslator()
+    else:
+        translator = DeeplApi(api_key=api_key)
+    srt = SrtFile(in_srt)
+    srt.translate(translator, from_lang, to_lang)
+    srt.wrap_lines()
+    srt.save(out_srt)
