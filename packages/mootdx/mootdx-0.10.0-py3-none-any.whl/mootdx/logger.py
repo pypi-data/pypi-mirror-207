8df18e5d5238f7d7a7ff54fa9b3875070af28229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:41 +0000

    Add package mootdx/0.10.0/mootdx-0.10.0-py3-none-any.whl

---
+++ packages/mootdx/mootdx-0.10.0-py3-none-any.whl/mootdx/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+
+logger = logging.getLogger("mootdx")
+logger.addHandler(console)
+logger.setLevel(logging.INFO)
