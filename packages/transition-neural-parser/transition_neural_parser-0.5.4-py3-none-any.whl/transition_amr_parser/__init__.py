5e7d70469461bcce4691d7c15b5cba57e13957a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:58 +0000

    Add package transition-neural-parser/0.5.4/transition_neural_parser-0.5.4-py3-none-any.whl

---
+++ packages/transition-neural-parser/transition_neural_parser-0.5.4-py3-none-any.whl/transition_amr_parser/__init__.py
@@ -0,0 +1,25 @@
+import subprocess
+import warnings
+# check for installation of torch-scatter
+try:
+    import torch_scatter
+except:
+    warnings.warn("torch-scatter is either not installed or not properly installed; please check for the appropriate version", UserWarning)
+    raise Exception("please review README.d instructions on installing the appropriate version of torch-scatter")
+    # cmd = ["pip", "install", "torch-scatter", "-f", "https://data.pyg.org/whl/torch-1.13.1+cu117.html"]
+    # print("try downloading torch-scatter")
+    # subprocess.call(cmd)
+
+# set this to true to start the debugger on any exception
+DEBUG_MODE = False
+if DEBUG_MODE:
+    import sys
+    import ipdb
+    import traceback
+
+    def debughook(etype, value, tb):
+        traceback.print_exception(etype, value, tb)
+        print()
+        # post-mortem debugger
+        ipdb.pm()
+    sys.excepthook = debughook
