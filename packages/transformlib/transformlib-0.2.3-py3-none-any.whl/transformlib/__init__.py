52ef36e67a253b6f2ddd9b95eaee34f951701547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:19 +0000

    Add package transformlib/0.2.3/transformlib-0.2.3-py3-none-any.whl

---
+++ packages/transformlib/transformlib-0.2.3-py3-none-any.whl/transformlib/__init__.py
@@ -0,0 +1,16 @@
+"""Contains useful abstractions for organizing transformations of data in a modular way.
+"""
+from .node import Node, Output, Input
+from .transform import Transform, transform
+from .pipeline import Pipeline
+from . import config
+
+__all__ = [
+    'Node',
+    'Output', 'Input',
+    'Transform', 'transform',
+    'Pipeline',
+    'config',
+    '__version__',
+]
+__version__ = '0.2.3'
---
+++ packages/transformlib/transformlib-0.2.3-py3-none-any.whl/transformlib/config.py
@@ -0,0 +1,3 @@
+# The name of the environment variable containing the data directory.
+# All Node objects point to data relative to this directory.
+DATA_DIR = 'TRANSFORMLIB_DATA_DIR'
