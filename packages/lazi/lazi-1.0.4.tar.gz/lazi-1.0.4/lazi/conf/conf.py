5b72ddf15ea41e043d2cd08decb19dc596530e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:09 +0000

    Add package lazi/1.0.4/lazi-1.0.4.tar.gz

---
+++ packages/lazi/lazi-1.0.4.tar.gz/lazi-1.0.4/lazi/conf/conf.py
@@ -0,0 +1,32 @@
+"""Default configuration for Lazi.
+"""
+
+DEBUG_TRACING: bool = False          # Enable debug traces.
+
+SPECR_KEEP_EMPTY: bool = False      # Keep records without a spec.
+SPECR_KEEP_0HOOK: bool = False      # Keep records without a hook.
+SPECR_HOOK_STDLI: bool = False      # Hook stdlib modules.
+
+LOADER_AUTO_DEPS: bool = True       # Also load dependent imports when loading a module.
+
+
+def __load():
+    import sys
+    from pkgutil import iter_modules
+    from importlib import import_module
+    import lazi.conf as namespace
+
+    dic = sys.modules[__name__].__dict__
+    keys = set(key for key in dic.keys() if not key.startswith("_") and key.isupper())
+
+    for module_info in (mi for mi in iter_modules(namespace.__path__, namespace.__name__ + ".") if mi.name != __name__):
+        module = import_module(module_info.name)
+
+        for key in (key for key in keys if hasattr(module, key)):
+            dic[key] = getattr(module, key)  # TODO (As Needed): Check type and merge accordingly.
+
+    for key in keys:
+        setattr(namespace, key, dic[key])
+
+
+__load()
