b0b64eacb945a77e0c3e9cba96e3e8588fbd3cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:50 +0000

    Add package ml-starter/0.0.34/ml_starter-0.0.34-py3-none-any.whl

---
+++ packages/ml-starter/ml_starter-0.0.34-py3-none-any.whl/ml/scripts/resolve.py
@@ -0,0 +1,11 @@
+from omegaconf import DictConfig, OmegaConf
+
+
+def resolve_main(config: DictConfig) -> None:
+    """Resolves the command line to a config and prints it.
+
+    Args:
+        config: The configuration object.
+    """
+
+    print(OmegaConf.to_yaml(config))
