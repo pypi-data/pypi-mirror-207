c20ea408dc8e7751eb5f9a5f0ad4f6561c9d75bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:14 +0000

    Add package jetha-ai/0/jetha_ai-0.tar.gz

---
+++ packages/jetha-ai/jetha_ai-0.tar.gz/jetha_ai-0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0'
+DESCRIPTION = 'all solutions'
+LONG_DESCRIPTION = 'A package that allows to print all codes of ai lab.'
+
+# Setting up
+setup(
+    name="jetha_ai",
+    version=VERSION,
+    author="jetha",
+    author_email="jgada402@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
