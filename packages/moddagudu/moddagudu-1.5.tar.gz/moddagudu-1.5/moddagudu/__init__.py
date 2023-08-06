1a2a931f4953512bf86158df1cdbbfaee1ff9890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:14 +0000

    Add package moddagudu/1.5/moddagudu-1.5.tar.gz

---
+++ packages/moddagudu/moddagudu-1.5.tar.gz/moddagudu-1.5/moddagudu/__init__.py
@@ -0,0 +1,2 @@
+from moddagudu.mg import namaste
+from moddagudu.ep import compute_cumshot
---
+++ packages/moddagudu/moddagudu-1.5.tar.gz/moddagudu-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='moddagudu',
+    version='1.5',
+    description='Naa First package idhi...mast undi ga?',
+    packages=['moddagudu'],
+    install_requires = ['librosa', 'numpy']
+)
