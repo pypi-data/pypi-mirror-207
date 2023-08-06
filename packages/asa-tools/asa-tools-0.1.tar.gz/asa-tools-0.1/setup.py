de79680b9e13574667eb6dbd0d460d295e85566e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:47 +0000

    Add package asa-tools/0.1/asa-tools-0.1.tar.gz

---
+++ packages/asa-tools/asa-tools-0.1.tar.gz/asa-tools-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='asa-tools',
+    version='0.1',
+    py_modules=['ter'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        asa=turn2release:cli
+    ''',
+)
