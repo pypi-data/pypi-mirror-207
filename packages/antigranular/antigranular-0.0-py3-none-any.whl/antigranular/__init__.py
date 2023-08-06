0d4dec7f788856115d02507e9af4bbb0dbe887e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:11 +0000

    Add package antigranular/0.0/antigranular-0.0-py3-none-any.whl

---
+++ packages/antigranular/antigranular-0.0-py3-none-any.whl/antigranular/__init__.py
@@ -0,0 +1,6 @@
+"""
+Placeholder package for Antigranular Client.
+
+Antigranular is a community-driven, open-source platform that merges confidential computing and differential privacy. This creates a secure environment for handling unseen data with confidence.
+"""
+__version__ = "0.0.0"
