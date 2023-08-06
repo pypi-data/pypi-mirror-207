ea87b23d8c333e087dcea4824ac1ee402f1b5ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:15 +0000

    Add package servey/2.8.4/servey-2.8.4-py3-none-any.whl

---
+++ packages/servey/servey-2.8.4-py3-none-any.whl/servey/servey_aws/serverless/trigger_handler/web_trigger_handler.py
@@ -0,0 +1,34 @@
+from marshy.types import ExternalItemType
+
+from servey.action.action import Action
+from servey.trigger.trigger_abc import TriggerABC
+from servey.trigger.web_trigger import WebTrigger
+from servey.servey_aws.serverless.trigger_handler.trigger_handler_abc import (
+    TriggerHandlerABC,
+)
+
+
+class WebTriggerHandler(TriggerHandlerABC):
+    """
+    Handler which outputs serverless definitions for api gateway triggers.
+    """
+
+    path_pattern: str = "/actions/{action_name}"
+
+    def handle_trigger(
+        self,
+        action: Action,
+        trigger: TriggerABC,
+        lambda_definition: ExternalItemType,
+    ):
+        if not isinstance(trigger, WebTrigger):
+            return
+        events = lambda_definition.get("events")
+        if not events:
+            events = lambda_definition["events"] = []
+        path = trigger.path or self.path_pattern.format(
+            action_name=action.name.replace("_", "-")
+        )
+        events.append(
+            {"http": {"path": path, "method": trigger.method.value, "cors": True}}
+        )
