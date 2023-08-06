2e8a1ffe3bb81e0b8d9a85130c330562bf54a9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:19 +0000

    Add package PySciMath/1.2.1/PySciMath-1.2.1.tar.gz

---
+++ packages/PySciMath/PySciMath-1.2.1.tar.gz/PySciMath-1.2.1/PySciMath/__init__.py
@@ -0,0 +1,15 @@
+# PySciMath - Init
+
+''' This is the __init__.py file. '''
+
+# Constants
+pi = 3.14159
+euler = 2.71828
+goldenRatio = 1.61803
+planck = "The Planck's Constant is 6.626068 x 10⁻³⁴."
+avagadro = "The Avagadro's Constant is 6.0221515 x 10⁻²³."
+speedOfLight = "The speed of light is 299,792,458 m/s."
+gravitationalConstant = "The Gravitational Constant is 6.67300 x 10⁻¹¹."
+boltzmannConstant = "The Boltzmann's Constant is 1.380650 x 10²³."
+iota = "Iota, which is also referred to as 'i'. It's value is √-1."
+eulerIdentity = "The Euler's Identity is eⁱˣᴾⁱ + 1 = 0. 'e' is the Euler's Number and 'i' is √-1."
