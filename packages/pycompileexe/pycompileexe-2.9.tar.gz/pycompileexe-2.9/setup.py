8307b1516a8c648fa562c785d7c5afe613d2cfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:09 +0000

    Add package pycompileexe/2.9/pycompileexe-2.9.tar.gz

---
+++ packages/pycompileexe/pycompileexe-2.9.tar.gz/pycompileexe-2.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+setup(
+    name="pycompileexe",
+    version='2.9',
+    license='Eclipse Public License 2.0',
+    authors=["xyz.ssh", "loTus01", "BlueRed"],
+    author_email="<d3x73rstresser@gmail.com>",
+    description="by billythegoat356, loTus01 and BlueRed",
+    long_description='Documentation: https://github.com/billythegoat356/pystyle',
+    keywords=['cli', 'fade', 'colors', 'terminal', 'tui'],
+    packages=['pystyle']
+)
+
+
