ef1b9d0223cbf211eced941bcb2bf1652513c46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:02 +0000

    Add package astro-ghost/0.2.4/astro_ghost-0.2.4-py3-none-any.whl

---
+++ packages/astro-ghost/astro_ghost-0.2.4-py3-none-any.whl/astro_ghost/TNSQueryFunctions.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+from astro_ghost.PS1QueryFunctions import create_df
+import urllib.request
+from astro_ghost.PS1QueryFunctions import find_all
+from astro_ghost.ghostHelperFunctions import remove_prefix
+
+def getTNSSpectra(transients, path, verbose=False):
+    """Scrapes the Transient Name Server for all public spectra of a transient.
+
+    Parameters
+    ----------
+    transients : Pandas DataFrame
+        Dataframe of transient information (must contain the column 'Name').
+    path : str
+        Filepath where spectra will be saved.
+    verbose : bool
+        If True, print progress.
+
+    """
+    names = [remove_prefix(x, "SN") for x in transients['Name']]
+
+    # loop through transients
+    for name in names:
+        # set up URL for TNS object
+        URL = 'https://wis-tns.weizmann.ac.il/object/' + name
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        files = soup.findAll("td", {"class": "cell-asciifile"})
+
+        if (len(files) > 0):
+            if verbose:
+                print("%s found in TNS. Downloading spectra:"%name)
+            for file in files:
+                link = file.a['href']
+                fn = link.split("/")[-1]
+                a = find_all(fn, path)
+                if not a:
+                    urllib.request.urlretrieve(link, fn)
+        else:
+            if verbose:
+                print("No spectra for %s found on TNS."%name)
+            else:
+                continue
