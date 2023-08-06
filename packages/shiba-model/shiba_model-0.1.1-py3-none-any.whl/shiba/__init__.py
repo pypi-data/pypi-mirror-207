b486290cbaf7bccb41d93204816346db9c01783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:57 +0000

    Add package shiba-model/0.1.1/shiba_model-0.1.1-py3-none-any.whl

---
+++ packages/shiba-model/shiba_model-0.1.1-py3-none-any.whl/shiba/__init__.py
@@ -0,0 +1,3 @@
+from shiba.model import Shiba, ShibaForAutoregressiveLanguageModeling, ShibaForClassification, ShibaForSequenceLabeling, \
+    get_pretrained_from_hub
+from shiba.codepoint_tokenizer import CodepointTokenizer
