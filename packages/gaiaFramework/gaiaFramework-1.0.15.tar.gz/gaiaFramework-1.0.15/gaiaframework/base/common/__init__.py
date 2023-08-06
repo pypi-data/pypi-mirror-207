32c60e8be3ebb53f6679003f6d95523619912ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:27 +0000

    Add package gaiaFramework/1.0.15/gaiaFramework-1.0.15.tar.gz

---
+++ packages/gaiaFramework/gaiaFramework-1.0.15.tar.gz/gaiaFramework-1.0.15/gaiaframework/base/common/__init__.py
@@ -0,0 +1,4 @@
+from gaiaframework.base.common.component import ZIDS_Component
+from gaiaframework.base.common import functions
+from gaiaframework.base.common.object import ZIDS_Object
+from gaiaframework.base.common.regex_handler import RegexHandler
