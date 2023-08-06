de343c31a6511b5dcab09aa48fa4cf086754b3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:47 +0000

    Add package np-image-buffer/0.1.1/np_image_buffer-0.1.1-py2.py3-none-any.whl

---
+++ packages/np-image-buffer/np_image_buffer-0.1.1-py2.py3-none-any.whl/np_image_buffer/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for NumPy image circular buffer."""
+from np_image_buffer.np_image_buffer import ImageBuffer
+
+__author__ = """Jacopo Abramo"""
+__email__ = 'jacopo.abramo@gmail.com'
+__version__ = '0.1.0'
+
+__all__ = [
+    "ImageBuffer"
+]
+
+
