f034bd52dc5c5039ee155c58d18836054b07cbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:23 +0000

    Add package datanalyse/0.0.5/datanalyse-0.0.5.tar.gz

---
+++ packages/datanalyse/datanalyse-0.0.5.tar.gz/datanalyse-0.0.5/tests/test_dataelement.py
@@ -0,0 +1,13 @@
+import datanalyse.dataelement.dataelement as dta
+from datanalyse.dataelement import formate
+from pathlib import Path
+
+de = dta.Dataelement(pfad=Path("Ahlborn.xlsx"))
+print(de.name)
+
+print(de.elementtyp)
+
+print(de.daten)
+de.lese_daten_formatiert_ein(format_dict={"Ahlborn": formate.wincontrol_standard})
+print(de.daten)
+# D:\Projekte\datanalyse\tests\Ahlborn.xlsx
