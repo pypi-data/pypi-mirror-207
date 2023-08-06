8c5d6b9bd63f808f7ee3b8f7789680284e00ce24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:54 +0000

    Add package masonite-servicing/0.0.4/masonite-servicing-0.0.4.tar.gz

---
+++ packages/masonite-servicing/masonite-servicing-0.0.4.tar.gz/masonite-servicing-0.0.4/src/masonite_servicing/__init__.py
@@ -0,0 +1,42 @@
+# flake8: noqa F401
+from .providers.ServicingProvider import ServicingProvider
+from masonite.validation import Validator
+
+class ServiceResult:
+    def __init__(self, *args): 
+        self.status = args[0]
+        self.message = args[1]
+        self.data = args[2]
+
+def result(*args):
+   return ServiceResult(*args)
+
+def respond(request, validators, result):
+    errors = request.validate(*validators)
+
+    if errors: 
+        return {
+            "status"  : "not-ok", 
+            "message" : "VALIDATION_ERROR", 
+            "errors"  : errors.all()
+        }
+
+    return {
+        "status" : result.status, 
+        "message" : result.message, 
+        "data" : result.data
+    }
+
+def relay(result):
+    return result[1]
+
+def fetch(cb, *args): 
+    return cb(*args).data
+
+class SampleService:
+    def add(self, x, y): 
+        return result("ok", "added", x + y) 
+    
+def main():
+    sample_service = SampleService()
+    sample_service.add(3, 5)
