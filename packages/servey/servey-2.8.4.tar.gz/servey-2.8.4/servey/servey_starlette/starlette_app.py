114fc72e59d56fc82aea8bdf9259a91dbee2ab71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:15 +0000

    Add package servey/2.8.4/servey-2.8.4.tar.gz

---
+++ packages/servey/servey-2.8.4.tar.gz/servey-2.8.4/servey/servey_starlette/starlette_app.py
@@ -0,0 +1,17 @@
+import logging
+
+from marshy.factory.impl_marshaller_factory import get_impls
+from starlette.applications import Starlette
+
+from servey.servey_starlette.route_factory.route_factory_abc import RouteFactoryABC
+
+LOGGER = logging.getLogger(__name__)
+routes = []
+for route_factory in sorted(
+    list(get_impls(RouteFactoryABC)), key=lambda f: f.priority, reverse=True
+):
+    routes.extend(route_factory().create_routes())
+for route in routes:
+    LOGGER.debug("starlette_path:%s", route.path)
+
+app = Starlette(routes=routes)
