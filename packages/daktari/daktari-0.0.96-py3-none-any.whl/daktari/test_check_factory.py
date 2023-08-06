fa28bd9e843bdad998d02366e618d9b2a9d4f221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:31 +0000

    Add package daktari/0.0.96/daktari-0.0.96-py3-none-any.whl

---
+++ packages/daktari/daktari-0.0.96-py3-none-any.whl/daktari/test_check_factory.py
@@ -0,0 +1,23 @@
+from daktari.check import Check, CheckResult
+
+
+class DummyCheck(Check):
+    def __init__(self, name: str = "check.name", depends_on=None):
+        if depends_on is None:
+            depends_on = []
+        self.name = name
+        self.depends_on = depends_on
+
+    def check(self) -> CheckResult:
+        return self.passed("no check")
+
+
+class DummyCheck2(Check):
+    def __init__(self, name: str = "check.name", depends_on=None):
+        if depends_on is None:
+            depends_on = []
+        self.name = name
+        self.depends_on = depends_on
+
+    def check(self) -> CheckResult:
+        return self.passed("no check")
