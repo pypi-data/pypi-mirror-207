94d3393f393c4ce5274022cea951e029220c263f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:55 +0000

    Add package PySciMath/1.2.0/PySciMath-1.2.0.tar.gz

---
+++ packages/PySciMath/PySciMath-1.2.0.tar.gz/PySciMath-1.2.0/PySciMath/ComplexNumbers.py
@@ -0,0 +1,21 @@
+# PySciMath - ComplexNumbers
+
+''' This is the "ComplexNumbers" sub-module. '''
+
+# Imports
+import math
+import cmath
+
+# Functions - Algebra
+def addition(z1, z2): return z1 + z2
+def subtraction(z1, z2): return z1 - z2
+def multiplication(z1, z2): return z1 * z2
+def divide(z1, z2): return z1 / z2
+
+# Functions - Modulus, Conjugate, and Multiplicative Invers
+def modulus(z): return abs(z)
+def conjugate(z): return z.conjugate()
+def multiplicativeInverse(z): return conjugate(z) / (math.pow(modulus(z), 2))
+
+# Function - Polar Representation
+def polar(z): return cmath.polar(z)
