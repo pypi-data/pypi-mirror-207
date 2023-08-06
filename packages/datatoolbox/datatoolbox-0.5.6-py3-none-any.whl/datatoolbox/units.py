e01b2b2d14f767525e47ad7d80e0ae9f9fd9927b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:52 +0000

    Add package datatoolbox/0.5.6/datatoolbox-0.5.6-py3-none-any.whl

---
+++ packages/datatoolbox/datatoolbox-0.5.6-py3-none-any.whl/datatoolbox/units.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 09:38:11 2022
+
+@author: ageiges
+"""
+
+from . import core
+
+# unit registry
+ur = core.ur
+
+# conversion factor between two units
+conversionFactor = core.conversionFactor
+
+getUnit = core.getUnit
+getUnitWindows = core.getUnitWindows
