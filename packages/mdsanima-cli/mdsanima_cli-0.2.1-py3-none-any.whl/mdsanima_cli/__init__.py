1eb6b59e3750cf97fbc3105a55aee94acb5b6934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:28 +0000

    Add package mdsanima-cli/0.2.1/mdsanima_cli-0.2.1-py3-none-any.whl

---
+++ packages/mdsanima-cli/mdsanima_cli-0.2.1-py3-none-any.whl/mdsanima_cli/__init__.py
@@ -0,0 +1,64 @@
+# Copyright © 2023 Marcin Różewski MDSANIMA
+
+
+"""Initial main functionality for command-line tools."""
+
+
+from __future__ import annotations
+
+from mdsanima_cli.command.check import cli_check
+from mdsanima_cli.command.gifmaker import cli_gifmaker
+from mdsanima_cli.command.grid import cli_grid
+from mdsanima_cli.command.jpg import cli_jpg
+from mdsanima_cli.command.logo import cli_logo
+from mdsanima_cli.command.number import cli_number
+from mdsanima_cli.command.pixelart import cli_pixelart
+from mdsanima_cli.command.png import cli_png
+from mdsanima_cli.command.resize import cli_resize
+from mdsanima_cli.command.thumbnail import cli_thumbnail
+from mdsanima_cli.command.uuids import cli_uuid
+from mdsanima_cli.command.watermark import cli_watermark
+from mdsanima_cli.command.webp import cli_webp
+from mdsanima_cli.parser import create_parser
+from mdsanima_cli.utils.ascii import ascii_title
+
+
+def main_cli():
+    """The main command-line function responsible for sub-functions and allocation separation."""
+
+    # Argument parser and color print.
+    parser = create_parser()
+    args = parser.parse_args()
+
+    # Print nice asci text.
+    ascii_title("mdsanima cli")
+
+    try:
+        if args.command == "check":
+            cli_check()
+        if args.command == "uuid":
+            cli_uuid()
+        if args.command == "number":
+            cli_number()
+        if args.command == "logo":
+            cli_logo()
+        if args.command == "watermark":
+            cli_watermark()
+        if args.command == "jpg":
+            cli_jpg()
+        if args.command == "png":
+            cli_png()
+        if args.command == "webp":
+            cli_webp()
+        if args.command == "pixelart":
+            cli_pixelart()
+        if args.command == "grid":
+            cli_grid()
+        if args.command == "thumbnail":
+            cli_thumbnail()
+        if args.command == "gifmaker":
+            cli_gifmaker()
+        if args.command == "resize":
+            cli_resize()
+    except AttributeError:
+        parser.print_help()
---
+++ packages/mdsanima-cli/mdsanima_cli-0.2.1-py3-none-any.whl/mdsanima_cli/__main__.py
@@ -0,0 +1,13 @@
+# Copyright © 2023 Marcin Różewski MDSANIMA
+
+
+"""Main entry point for command-line tools."""
+
+
+from __future__ import annotations
+
+from mdsanima_cli import main_cli
+
+
+if __name__ == "__main__":
+    raise SystemExit(main_cli())
