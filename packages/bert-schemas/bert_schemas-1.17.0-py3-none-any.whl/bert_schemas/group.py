a640115c5b398404b52de9816d18a12d39d2dfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:48 +0000

    Add package bert-schemas/1.17.0/bert_schemas-1.17.0-py3-none-any.whl

---
+++ packages/bert-schemas/bert_schemas-1.17.0-py3-none-any.whl/bert_schemas/group.py
@@ -0,0 +1,16 @@
+from fastapi_utils.enums import StrEnum
+
+
+class GroupName(StrEnum):
+    GROUND = "GROUND"
+    EXPLORER = "EXPLORER"
+    INNOVATOR = "INNOVATOR"
+    ADMIN = "ADMIN"
+
+    def __str__(self):
+        return str(self.value)
+
+
+class RoleName(StrEnum):
+    SUPERUSER = "SUPERUSER"
+    ORG_ADMIN = "ORG_ADMIN"
