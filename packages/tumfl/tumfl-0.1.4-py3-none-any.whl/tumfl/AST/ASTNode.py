74e81984e37ae6f489b123c7e9f01a338e9e8549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:36 +0000

    Add package tumfl/0.1.4/tumfl-0.1.4-py3-none-any.whl

---
+++ packages/tumfl/tumfl-0.1.4-py3-none-any.whl/tumfl/AST/ASTNode.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from abc import ABC
+from typing import Any, Generator, Optional
+
+from tumfl.Token import Token
+from tumfl.utils import generic_str
+
+
+class ASTNode(ABC):
+    """Base class for all AST nodes"""
+
+    def __init__(self, token: Token, name: str) -> None:
+        self.name: str = name
+        self.token: Token = token
+        self.parent_class: Optional[ASTNode] = None
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, self.__class__):
+            return False
+        return all(
+            callable(getattr(self, i)) or getattr(self, i) == getattr(other, i)
+            for i in self.__dir()
+        )
+
+    def __repr__(self) -> str:
+        return generic_str(self, ["replace", "parent", "parent_class"])
+
+    def __dir(self) -> Generator[str, None, None]:
+        return (
+            i
+            for i in dir(self)
+            if not i.startswith("__")
+            # ignore "token" for comparison (and parent check)
+            and i not in ["replace", "parent", "parent_class", "var", "token"]
+        )
+
+    def parent(self, parent: Optional[ASTNode]) -> None:
+        self.parent_class = parent
+        for i in self.__dir():
+            node: Any = getattr(self, i)
+            if isinstance(node, ASTNode):
+                node.parent(self)
+
+    def replace_child(self, to_replace: ASTNode, replacement: ASTNode) -> None:
+        for i in self.__dir():
+            node: Any = getattr(self, i)
+            if node is to_replace:
+                setattr(self, i, replacement)
+                return
+            if isinstance(node, list):
+                for j, element in enumerate(node):
+                    if element is to_replace:
+                        node[j] = replacement
+                        return
