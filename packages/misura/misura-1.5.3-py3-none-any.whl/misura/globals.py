06c92f16e228d41b530cdc65550fb3b4e0f10744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:54 +0000

    Add package misura/1.5.3/misura-1.5.3-py3-none-any.whl

---
+++ packages/misura/misura-1.5.3-py3-none-any.whl/misura/globals.py
@@ -0,0 +1,28 @@
+# Globals.
+
+
+class style:
+    """
+    Styling global options for misura.
+    """
+
+    quantityHighlighting = True
+    quantityPlusMinus = " \u00b1 "
+
+
+class logic:
+    """
+    Logical global options for misura.
+    """
+
+    ignoreUncertainty = False
+
+
+class defined:
+    """
+    User defined units.
+    """
+
+    BASE_TABLE = {}
+    DERIVED_TABLE = {}
+    DERIVED_UNPACKING_TABLE = {}
