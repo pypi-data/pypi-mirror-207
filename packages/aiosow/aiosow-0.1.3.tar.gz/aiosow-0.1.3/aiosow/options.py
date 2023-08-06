0911f7427be16ae18dda850f89f6dd403d90ce6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:12 +0000

    Add package aiosow/0.1.3/aiosow-0.1.3.tar.gz

---
+++ packages/aiosow/aiosow-0.1.3.tar.gz/aiosow-0.1.3/aiosow/options.py
@@ -0,0 +1,36 @@
+"""
+When writing your own compositions you may want to add additionnal options to the
+command-line.
+
+You do it using the option function.
+
+**Example:**
+```
+option('keyword', default='foo', help='Keyword to use')
+```
+
+Any option defined is propagated into the memory, in fact the initial memory is
+a copy of the parsed arguments.
+"""
+
+OPTIONS = []
+
+
+def option(*args, **kwargs):  # pragma: no cover
+    """
+    Register an argparse option to be available using the command-line.
+
+    **args**:
+    - name: str -> the name of the option (will be used as f'--{name}')
+    - kwargs: dict -> the options used by [argparse](https://docs.python.org/3/library/argparse.html)
+    """
+    global OPTIONS
+    OPTIONS.append((args, kwargs))
+
+
+def options():  # pragma: no cover
+    global OPTIONS
+    return OPTIONS
+
+
+__all__ = ["option"]
