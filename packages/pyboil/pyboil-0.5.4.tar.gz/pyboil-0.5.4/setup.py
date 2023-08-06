31f143297dbb5e3d0343f220da29d356e9fb1a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:19 +0000

    Add package pyboil/0.5.4/pyboil-0.5.4.tar.gz

---
+++ packages/pyboil/pyboil-0.5.4.tar.gz/pyboil-0.5.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text()
+
+setup_info = dict(
+    name='pyboil',
+    python_requires=">=3.10",
+    version='0.5.4',
+    description='CLI script to initialize python packages',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Reed Schick",
+    author_email='rns350@nyu.edu',
+    url='https://github.com/rns350/pyinit',
+    packages=find_packages(
+        where='.', 
+        include=['pyboil*']
+    ),
+    include_package_data=True,
+    entry_points={
+            'console_scripts': [
+                'pyboil = pyboil:launch',
+            ]
+        },
+    install_requires=[],
+    setup_requires=[],
+    tests_require=['pytest']
+)
+
+setup(**setup_info)
