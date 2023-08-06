4fd47426eeb34b8cb51c47c96137a5605f549a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:26 +0000

    Add package ProjZ.py/2.4.5/ProjZ.py-2.4.5.tar.gz

---
+++ packages/ProjZ.py/ProjZ.py-2.4.5.tar.gz/ProjZ.py-2.4.5/projz/api/__init__.py
@@ -0,0 +1,4 @@
+from .request_manager import RequestManager
+import projz.api.util
+import projz.api.headers
+import projz.api.control
---
+++ packages/ProjZ.py/ProjZ.py-2.4.5.tar.gz/ProjZ.py-2.4.5/projz/model/parse/parseutils.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from dateutil.parser import isoparse
+from typing import Union
+from typing import Optional
+from dataclasses import Field
+from dataclasses import field
+from dataclasses_json import config
+
+
+def decode_time(encoded_time: Union[int, str, None]) -> Optional[datetime]:
+    if isinstance(encoded_time, int): return datetime.fromtimestamp(encoded_time)
+    elif isinstance(encoded_time, str): return isoparse(encoded_time)
+    else: return None
+
+
+def encode_time(time: datetime) -> int:
+    return int(time.timestamp() * 1000)
+
+
+def decode_wallet_amount(amount: str) -> int:
+    if amount == "0": return 0
+    return int(amount[:len(amount) - 18])
+
+
+def time_field() -> Field:
+    return field(metadata=config(decoder=decode_time, encoder=encode_time), default_factory=type(None))
+
+
+def extensions_field() -> Field:
+    return field(default_factory=dict)
+
+
+def wallet_amount_field() -> Field:
+    return field(metadata=config(decoder=decode_wallet_amount), default_factory=0)
