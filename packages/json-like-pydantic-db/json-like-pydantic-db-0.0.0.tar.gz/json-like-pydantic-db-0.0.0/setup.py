7e9ac259ca1acef4c6ee2207711e774f65f56ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:48 +0000

    Add package json-like-pydantic-db/0.0.0/json-like-pydantic-db-0.0.0.tar.gz

---
+++ packages/json-like-pydantic-db/json-like-pydantic-db-0.0.0.tar.gz/json-like-pydantic-db-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='json-like-pydantic-db',
+    version='',
+    packages=['serialization_db'],
+    url='',
+    license='',
+    author='dempasha',
+    author_email='',
+    description=''
+)
