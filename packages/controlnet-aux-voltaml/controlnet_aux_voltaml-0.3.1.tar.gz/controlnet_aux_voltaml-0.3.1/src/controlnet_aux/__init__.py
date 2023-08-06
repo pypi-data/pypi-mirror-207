ab2311ddfee1e3fac4d9bf3401c78cabc7904f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:00 +0000

    Add package controlnet-aux-voltaml/0.3.1/controlnet_aux_voltaml-0.3.1.tar.gz

---
+++ packages/controlnet-aux-voltaml/controlnet_aux_voltaml-0.3.1.tar.gz/controlnet_aux_voltaml-0.3.1/src/controlnet_aux/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.3.1"
+
+from .canny import CannyDetector
+from .hed import HEDdetector
+from .lineart import LineartDetector
+from .lineart_anime import LineartAnimeDetector
+from .mediapipe_face import MediapipeFaceDetector
+from .midas import MidasDetector
+from .mlsd import MLSDdetector
+from .normalbae import NormalBaeDetector
+from .open_pose import OpenposeDetector
+from .pidi import PidiNetDetector
+from .shuffle import ContentShuffleDetector
+from .zoe import ZoeDetector
