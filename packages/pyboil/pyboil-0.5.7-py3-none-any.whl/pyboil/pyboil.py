58ad80b5fc38c3cdfac6a97c8f5a9b5c6a2254e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:07 +0000

    Add package pyboil/0.5.7/pyboil-0.5.7-py3-none-any.whl

---
+++ packages/pyboil/pyboil-0.5.7-py3-none-any.whl/pyboil/pyboil.py
@@ -0,0 +1,25 @@
+from pyboil.parser import parser
+import shutil, os, sys
+
+def launch():
+    args = parser.parse_args()
+
+    current_dir = os.path.pardir(__file__)
+    final_path = (current_dir / "template")
+    shutil.copytree(final_path, '.')
+
+    with open('./setup.py') as f:
+        setup = f.read()
+
+    setup.replace('{NAME}', args['NAME'])
+    setup.replace('{VERSION}', args['VERSION'])
+    setup.replace('{DESCRIPTION}', args['DESCRIPTION'])
+    setup.replace('{AUTHOR}', args['AUTHOR'])
+    setup.replace('{EMAIL}', args['EMAIL'])
+    setup.replace('{URL}', args['URL'])
+
+    with open('./setup.py', 'w') as f:
+        f.write(setup)
+
+if __name__ == "__main__":
+    launch()
