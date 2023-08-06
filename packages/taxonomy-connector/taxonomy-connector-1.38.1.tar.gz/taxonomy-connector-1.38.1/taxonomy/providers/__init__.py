4484625163fad2c397fde6b6d37ed9fd7dd22644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:47 +0000

    Add package taxonomy-connector/1.38.1/taxonomy-connector-1.38.1.tar.gz

---
+++ packages/taxonomy-connector/taxonomy-connector-1.38.1.tar.gz/taxonomy-connector-1.38.1/taxonomy/providers/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Expose the abstract providers.
+"""
+
+from taxonomy.providers.course_metadata import CourseMetadataProvider
+from taxonomy.providers.program_metadata import ProgramMetadataProvider
+from taxonomy.providers.xblock_metadata import XBlockContent, XBlockMetadataProvider
---
+++ packages/taxonomy-connector/taxonomy-connector-1.38.1.tar.gz/taxonomy-connector-1.38.1/taxonomy/validators/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Expose all the validators.
+"""
+
+from taxonomy.validators.course_metadata import CourseMetadataProviderValidator
+from taxonomy.validators.program_metadata import ProgramMetadataProviderValidator
+from taxonomy.validators.xblock_metadata import XBlockMetadataProviderValidator
