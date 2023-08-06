9394420ff1d8525a897ad3aea7877f779f57b7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:05 +0000

    Add package HDRezka/3.0.0/HDRezka-3.0.0.tar.gz

---
+++ packages/HDRezka/HDRezka-3.0.0.tar.gz/HDRezka-3.0.0/hdrezka/__init__.py
@@ -0,0 +1,33 @@
+r"""
+# [HDRezka](https://rezka.ag/) site API.
+
+# Install
+
+`pip install HDRezka`
+
+# Example
+
+```python
+from hdrezka import Search
+
+player = Search('Breaking Bad').get_page(1)[0].player  # page 1 element 0; or just use Player(646)
+print(player.post.info, end='\n\n')
+
+translator_id = None  # default
+for name, id_ in player.post.translators.name_id.items():
+    if 'субтитры' in name.casefold(): translator_id = id_; break
+
+stream = player.get_stream(1, 1, translator_id)  # raise AJAXFail if invalid episode or translator
+video = stream.video
+print(video.last_url)  # best quality
+print(video[video.min].last_url.mp4, end='\n\n')
+
+subtitles = stream.subtitles
+print(subtitles.default.url)  # subtitles.ru.url or subtitles['Русский'].url
+```
+
+# [GitHub](https://github.com/NIKDISSV-Forever/HDRezka)
+"""
+from .api import *
+from .post import *
+from .stream import *
