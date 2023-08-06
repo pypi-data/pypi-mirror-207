9c565fad6bd0f75f2c7156fdaebd9cefce74defb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:03 +0000

    Add package osl-dynamics/1.2.3/osl-dynamics-1.2.3.tar.gz

---
+++ packages/osl-dynamics/osl-dynamics-1.2.3.tar.gz/osl-dynamics-1.2.3/osl_dynamics/data/__init__.py
@@ -0,0 +1,8 @@
+"""Classes and functions used to handle data.
+
+"""
+
+from osl_dynamics.data.base import Data
+from osl_dynamics.data.osl import OSL_HMM, HMM_MAR
+
+__all__ = ["Data", "OSL_HMM", "HMM_MAR"]
