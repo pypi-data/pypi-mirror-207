73fa5cfcc3eada8369635c3f0a50d82706a69173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:23 +0000

    Add package llmpool/0.1.4/llmpool-0.1.4-py3-none-any.whl

---
+++ packages/llmpool/llmpool-0.1.4-py3-none-any.whl/llmpool/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.4'
+
+from .local_model import LocalLLModel, LocalLoRALLModel
+from .model_pool import LLModelPool
+from .remote_model import TxtGenIfLLModel
+from .utils import instantiate_models
