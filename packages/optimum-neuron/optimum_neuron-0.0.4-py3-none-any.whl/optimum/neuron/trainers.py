264472dfd69a466b50aaad3800263d4d8ba553d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:34 +0000

    Add package optimum-neuron/0.0.4/optimum_neuron-0.0.4-py3-none-any.whl

---
+++ packages/optimum-neuron/optimum_neuron-0.0.4-py3-none-any.whl/optimum/neuron/trainers.py
@@ -0,0 +1,204 @@
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+"""Defines Trainer subclasses to perform training on AWS Trainium instances."""
+
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+from transformers import Seq2SeqTrainer, Trainer
+
+from ..utils import logging
+from .trainer_callback import NeuronCacheCallaback
+from .utils.argument_utils import validate_arg
+from .utils.cache_utils import get_neuron_cache_path
+from .utils.training_utils import (
+    FirstAndLastDataset,
+    is_model_officially_supported,
+    is_precompilation,
+    patch_model,
+    prepare_environment_for_neuron,
+)
+
+
+if TYPE_CHECKING:
+    from transformers import PreTrainedModel, TrainingArguments
+
+
+logger = logging.get_logger("transformers.trainer")
+
+KEEP_HF_HUB_PROGRESS_BARS = os.environ.get("KEEP_HF_HUB_PROGRESS_BARS")
+if KEEP_HF_HUB_PROGRESS_BARS is None:
+    os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
+
+# Used for torch.distributed.
+_ORIGINAL_NEURON_CACHE_PATH: Optional[Path] = None
+_TMP_NEURON_CACHE_DIR: Optional[TemporaryDirectory] = None
+
+if os.environ.get("TORCHELASTIC_RUN_ID"):
+    import torch_xla.distributed.xla_backend as xbn
+
+    if not isinstance(torch.distributed.group.WORLD, xbn.ProcessGroupXla):
+        _ORIGINAL_NEURON_CACHE_PATH = get_neuron_cache_path()
+        _TMP_NEURON_CACHE_DIR = NeuronCacheCallaback.create_temporary_neuron_cache(get_neuron_cache_path())
+        torch.distributed.init_process_group(backend="xla")
+        if not isinstance(torch.distributed.group.WORLD, xbn.ProcessGroupXla):
+            raise AssertionError("Failed to initialize torch.distributed process group using XLA backend.")
+
+
+class AugmentTrainerForTrainiumMixin:
+    def __init__(self, *args, **kwargs):
+        if not isinstance(self, Trainer):
+            raise TypeError(f"{self.__class__.__name__} can only be mixed with Trainer subclasses.")
+
+        training_args = kwargs.get("args", None)
+        if training_args is None and len(args) >= 2:
+            training_args = args[1]
+
+        if training_args is not None:
+            if training_args.bf16:
+                training_args.bf16 = False
+                os.environ["XLA_USE_BF16"] = "1"
+
+        self.validate_args(training_args)
+        if is_precompilation():
+            self.prepare_args_for_precompilation(training_args)
+
+        prepare_environment_for_neuron()
+        super().__init__(*args, **kwargs)
+
+        if self.args.local_rank <= 0:
+            logger.setLevel(logging.INFO)
+
+        if not is_precompilation():
+            callback = NeuronCacheCallaback(
+                tmp_neuron_cache=_TMP_NEURON_CACHE_DIR,
+                original_neuron_cache_path=_ORIGINAL_NEURON_CACHE_PATH,
+                only_do_fetching=self.args.local_rank > 0,
+            )
+            self.add_callback(callback)
+
+    def prepare_args_for_precompilation(self, args: "TrainingArguments"):
+        if args.num_train_epochs != 1:
+            logger.info("Setting the number of epochs for precompilation to 1.")
+            args.num_train_epochs = 1
+        if args.max_steps is not None:
+            logger.info("Disabling max_steps for precompilation.")
+            args.nax_steps = None
+        if args.do_eval is True:
+            logger.info("Disabling evaluation during precompilation as this is not well supported yet.")
+            args.do_eval = False
+        if args.do_predict is True:
+            logger.info("Disabling prediction during precompilation as this is not well supported yet.")
+            args.do_predict = False
+
+    def validate_args(self, args: "TrainingArguments"):
+        if isinstance(self, Seq2SeqTrainer):
+            validate_arg(
+                args,
+                "prediction_loss_only",
+                "prediction_loss_only=False is not supported for now because it requires generation.",
+                expected_value=True,
+            )
+
+    def _wrap_model(self, model, training=True, dataloader=None):
+        logger.info(
+            "Disabling DDP because it is currently not playing well with multiple workers training, for more "
+            "information please refer to https://awsdocs-neuron.readthedocs-hosted.com/en/latest/frameworks/torch/torch-neuronx/tutorials/training/finetune_hftrainer.html#multi-worker-training"
+        )
+        if not is_model_officially_supported(model):
+            logger.warning(
+                f"{model.__class__.__name__} is not officially supported by optimum-neuron. Training might not work as  "
+                "expected."
+            )
+        return super()._wrap_model(patch_model(model), training=training, dataloader=dataloader)
+
+    def get_train_dataloader(self) -> DataLoader:
+        if is_precompilation():
+            return DataLoader(
+                FirstAndLastDataset(
+                    super().get_train_dataloader(),
+                    gradient_accumulation_steps=self.args.gradient_accumulation_steps,
+                    world_size=self.args.world_size,
+                ),
+                batch_size=None,
+            )
+        return super().get_train_dataloader()
+
+    def get_eval_dataloader(self, eval_dataset: Optional[Dataset] = None) -> DataLoader:
+        if is_precompilation():
+            return DataLoader(
+                FirstAndLastDataset(
+                    super().get_eval_dataloader(eval_dataset=eval_dataset), world_size=self.args.world_size
+                ),
+                batch_size=None,
+            )
+        return super().get_eval_dataloader(eval_dataset=eval_dataset)
+
+    def get_test_dataloader(self, test_dataset: Dataset) -> DataLoader:
+        if is_precompilation():
+            return DataLoader(
+                FirstAndLastDataset(
+                    super().get_test_dataloader(test_dataset),
+                    world_size=self.args.world_size,
+                ),
+                batch_size=None,
+            )
+        return super().get_test_dataloader(test_dataset)
+
+    # TODO: make this cleaner.
+    def trigger_on_step_middle_for_neuron_cache_callback(self, model: "PreTrainedModel"):
+        for callback in self.callback_handler.callbacks:
+            if isinstance(callback, NeuronCacheCallaback):
+                # kwargs might not have everything expected (like metrics) but all we need is here.
+                kwargs = {
+                    "model": model,
+                    "tokenizer": self.tokenizer,
+                    "optimizer": self.optimizer,
+                    "lr_scheduler": self.lr_scheduler,
+                    "train_dataloader": self.callback_handler.train_dataloader,
+                    "eval_dataloader": self.callback_handler.eval_dataloader,
+                }
+                callback.on_step_middle(self.args, self.state, self.control, **kwargs)
+
+    def compute_loss(self, model, inputs, return_outputs: bool = False):
+        self.state.last_inputs = inputs
+        self.trigger_on_step_middle_for_neuron_cache_callback(model)
+        return super().compute_loss(model, inputs, return_outputs=return_outputs)
+
+    def prediction_step(
+        self,
+        model: torch.nn.Module,
+        inputs: Dict[str, Union[torch.Tensor, Any]],
+        prediction_loss_only: bool,
+        ignore_keys: Optional[List[str]] = None,
+    ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
+        self.state.last_inputs = inputs
+        self.trigger_on_step_middle_for_neuron_cache_callback(model)
+        return super().prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
+
+
+class TrainiumTrainer(AugmentTrainerForTrainiumMixin, Trainer):
+    """
+    Trainer that is suited for performing training on AWS Tranium instances.
+    """
+
+
+class Seq2SeqTrainiumTrainer(AugmentTrainerForTrainiumMixin, Seq2SeqTrainer):
+    """
+    Seq2SeqTrainer that is suited for performing training on AWS Tranium instances.
+    """
