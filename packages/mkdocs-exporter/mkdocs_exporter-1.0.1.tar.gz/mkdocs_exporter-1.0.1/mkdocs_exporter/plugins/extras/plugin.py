51351a50f159d400a241128095d1f57904d8df07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:41 +0000

    Add package mkdocs-exporter/1.0.1/mkdocs_exporter-1.0.1.tar.gz

---
+++ packages/mkdocs-exporter/mkdocs_exporter-1.0.1.tar.gz/mkdocs_exporter-1.0.1/mkdocs_exporter/plugins/extras/plugin.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from mkdocs.plugins import BasePlugin
+from mkdocs_exporter.page import Page
+from mkdocs_exporter.preprocessor import Preprocessor
+from mkdocs_exporter.plugins.extras.config import Config
+
+
+class Plugin(BasePlugin[Config]):
+  """The plugin."""
+
+
+  def on_post_page(self, html: str, page: Page, **kwargs) -> Optional[str]:
+    """Invoked after a page has been built."""
+
+    preprocessor = Preprocessor()
+
+    preprocessor.preprocess(html)
+
+    for button in self.config.buttons:
+      preprocessor.button(**{k: v(page) if callable(v) else v for k, v in button.items()})
+
+    return preprocessor.done()
