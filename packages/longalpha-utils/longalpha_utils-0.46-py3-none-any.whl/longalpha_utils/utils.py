0e56e357d1d6b0c9ef883e458dc63233c2a63e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:49 +0000

    Add package longalpha-utils/0.46/longalpha_utils-0.46-py3-none-any.whl

---
+++ packages/longalpha-utils/longalpha_utils-0.46-py3-none-any.whl/longalpha_utils/utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import datetime
+
+def max_pandas_display(pd: pd, max_row: int = 100) -> None:
+    """
+    set pandas print format to print all
+    Args:
+        pd: pandas object
+
+    Returns: None
+
+    """
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_rows", max_row)
+    pd.set_option("display.width", None)
+    pd.set_option("display.max_colwidth", None)
+    pd.set_option("display.expand_frame_repr", False)
+
+def validate_date_format(date_str, date_format = "%Y-%m-%d"):
+    try:
+        datetime.datetime.strptime(date_str, date_format)
+        return True
+    except ValueError:
+        return False
