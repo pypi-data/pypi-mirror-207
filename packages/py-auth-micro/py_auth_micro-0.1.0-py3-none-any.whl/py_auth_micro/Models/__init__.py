ea89068ba310f2e49b2bafb00823af3601f8d0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:57 +0000

    Add package py-auth-micro/0.1.0/py_auth_micro-0.1.0-py3-none-any.whl

---
+++ packages/py-auth-micro/py_auth_micro-0.1.0-py3-none-any.whl/py_auth_micro/Models/__init__.py
@@ -0,0 +1,11 @@
+"""This SubModule contains the Database Model Definitions.
+
+Since this package uses :code:`Tortoise ORM` they are needed!
+
+"""
+from ._group import Group
+from ._token import Token
+from ._user import User
+
+__models__ = [Token, User, Group]
+__all__ = ["Token", "User", "Group"]
