70073722a43f96a7f4995cbe2ac984ea1f99fdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:28 +0000

    Add package django-simple-stats/0.6.0/django-simple-stats-0.6.0.tar.gz

---
+++ packages/django-simple-stats/django-simple-stats-0.6.0.tar.gz/django-simple-stats-0.6.0/simple_stats/__init__.py
@@ -0,0 +1,10 @@
+from .stats import (
+    get_stats,
+    StatSet,
+    QueryAggregateStat,
+    QueryAggregateSingleStat,
+    QueryAggregateDateStat,
+    QueryAggregateBucketsStat,
+    ChoiceAggregateStat,
+    ChoiceAggregateNullStat,
+) # noqa
