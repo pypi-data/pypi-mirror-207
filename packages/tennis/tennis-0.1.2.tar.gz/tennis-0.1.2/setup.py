8dcb86f500d21b8a09a4b0e5cc0b5dee3d44e9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:56 +0000

    Add package tennis/0.1.2/tennis-0.1.2.tar.gz

---
+++ packages/tennis/tennis-0.1.2.tar.gz/tennis-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='tennis',
+    packages=find_packages(include=['tennis']),
+    version='0.1.2',
+    description='A library for tracking tennis matches and scoring',
+    author='bear102',
+    license='MIT',
+    install_requires=[],
+)
