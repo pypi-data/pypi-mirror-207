21d9304c953ede1cb10fa4bbc0bf25dd5611081b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:18 +0000

    Add package ProtWave-VAE/0.0.2/ProtWave_VAE-0.0.2.tar.gz

---
+++ packages/ProtWave-VAE/ProtWave_VAE-0.0.2.tar.gz/ProtWave_VAE-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="ProtWave_VAE",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here, e.g.:
+        "torch",
+        "numpy",
+    ],
+    extras_require={
+        # Add optional dependencies and their versions, e.g.:
+        "dev": ["pytest", "black", "isort"],
+    },
+    classifiers=[
+        # Add classifiers describing your package, e.g.:
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+
+    ],
+)
