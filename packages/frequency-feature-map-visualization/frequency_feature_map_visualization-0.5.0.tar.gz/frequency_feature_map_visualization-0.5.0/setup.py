cb3d98c14964702b85ec58c8343e643c58b50da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:06 +0000

    Add package frequency-feature-map-visualization/0.5.0/frequency_feature_map_visualization-0.5.0.tar.gz

---
+++ packages/frequency-feature-map-visualization/frequency_feature_map_visualization-0.5.0.tar.gz/frequency_feature_map_visualization-0.5.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="frequency_feature_map_visualization",
+    version="0.5.0",
+    description="This code is designed to visualize and save the feature maps of 3D and 2D models. The feature maps can be viewed in the image domain and frequency domain, and saved as .npy files.",
+    author="Guanghui FU",
+    author_email="aslanfu123@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "torch",
+        "matplotlib"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
