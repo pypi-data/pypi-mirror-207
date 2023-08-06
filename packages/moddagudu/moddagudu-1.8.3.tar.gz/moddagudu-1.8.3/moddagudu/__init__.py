15f92b427397f57f132cec8a09bd96bf559f3424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:56 +0000

    Add package moddagudu/1.8.3/moddagudu-1.8.3.tar.gz

---
+++ packages/moddagudu/moddagudu-1.8.3.tar.gz/moddagudu-1.8.3/moddagudu/__init__.py
@@ -0,0 +1,5 @@
+from moddagudu.mg import namaste
+from moddagudu.ep import compute_cumshot
+from moddagudu.sg import insert_cumshot
+from moddagudu.pn import delete_cumshot
+from moddagudu.gn import count_cumshot
---
+++ packages/moddagudu/moddagudu-1.8.3.tar.gz/moddagudu-1.8.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='moddagudu',
+    version='1.8.3',
+    description='Naa First package idhi...mast undi ga?',
+    packages=['moddagudu'],
+    install_requires = ['librosa', 'numpy', 'pinecone-client']
+)
