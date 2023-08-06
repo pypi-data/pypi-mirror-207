1be4e53c8feb7da0df1bb329c47ee008b0866d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:23 +0000

    Add package setupTestICP/0.1.1/setupTestICP-0.1.1.tar.gz

---
+++ packages/setupTestICP/setupTestICP-0.1.1.tar.gz/setupTestICP-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+
+
+
+
+
+
+
+
+setup(
+    author="AA",
+    
+    
+    name="setupTestICP",
+    # packages=find_packages(
+    #     where=".",
+    #     include=["setup_test", "setup_test.*"],
+    # ),
+    version="0.1.1",
+)
