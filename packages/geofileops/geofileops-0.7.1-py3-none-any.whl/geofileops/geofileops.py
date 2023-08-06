79669a762fd80d35591fca94eeba44c5e76339a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:39 +0000

    Add package geofileops/0.7.1/geofileops-0.7.1-py3-none-any.whl

---
+++ packages/geofileops/geofileops-0.7.1-py3-none-any.whl/geofileops/geofileops.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Only for backwards compatibility.
+"""
+
+import warnings
+
+warnings.warn(
+    "using from geofileops import geofileops is deprecated, "
+    "please just use import geofileops",
+    FutureWarning,
+)
+
+from .geoops import *  # noqa: F403, F401
