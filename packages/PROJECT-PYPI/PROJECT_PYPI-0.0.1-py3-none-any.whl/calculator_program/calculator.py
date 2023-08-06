02aa4b2630b423ba38ca4116c95bfeac2e0521b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:45 +0000

    Add package PROJECT-PYPI/0.0.1/PROJECT_PYPI-0.0.1-py3-none-any.whl

---
+++ packages/PROJECT-PYPI/PROJECT_PYPI-0.0.1-py3-none-any.whl/calculator_program/calculator.py
@@ -0,0 +1,27 @@
+def add_num(a,b):#function for addition
+    sum=a+b;
+    return sum; #return value
+num1=25  #variable declaration
+num2=55
+print("The sum is",add_num(num1,num2))#call the function
+
+def sub_num(a,b):#function for addition
+    sub=a-b;
+    return sub; #return value
+num1=25  #variable declaration
+num2=55
+print("The sub is",sub_num(num1,num2))#call the function
+
+def mul_num(a,b):#function for addition
+    mul=a*b;
+    return mul; #return value
+num1=25  #variable declaration
+num2=55
+print("The mul is",mul_num(num1,num2))#call the function
+
+def div_num(a,b):#function for addition
+    div=a/b;
+    return div; #return value
+num1=25  #variable declaration
+num2=55
+print("The div is",div_num(num1,num2))#call the function
