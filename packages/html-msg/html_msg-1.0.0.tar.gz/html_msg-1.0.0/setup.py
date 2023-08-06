56b2c942870397926b35f72e2bd45e19a338bd9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:19 +0000

    Add package html-msg/1.0.0/html_msg-1.0.0.tar.gz

---
+++ packages/html-msg/html_msg-1.0.0.tar.gz/html_msg-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+
+
+setup_args = dict(
+    name='html_msg',
+    version='1.0.0',
+    description='This tool allows to create HTML message via simple methods, \
+        even if you dont now HTML',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n',
+    license='MIT',
+    packages=find_packages(),
+    author='Sirakan Bagdasarian',
+    author_email='bsirak@bk.ru',
+    keywords=['HTML', 'Message'],
+    url='https://github.com/',
+    download_url='https://pypi.org/project/'
+)
+
+install_requires = [
+    'IPython',
+    'pandas'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
