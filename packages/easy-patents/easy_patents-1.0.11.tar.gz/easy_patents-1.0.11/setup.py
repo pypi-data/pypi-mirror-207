d8b41f422bbf0a908dbbafb5cf6cd8cc2906fd5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:32 +0000

    Add package easy-patents/1.0.11/easy_patents-1.0.11.tar.gz

---
+++ packages/easy-patents/easy_patents-1.0.11.tar.gz/easy_patents-1.0.11/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+     name="easy_patents",
+     version="1.0.11",
+     author="easypatents39",
+     author_email="easypatents39@gmail.com",
+     description="Utilities for JPO API",
+     url="https://qiita.com/easypatents39/",
+     packages=['easy_patents'],
+     include_package_data=True,
+     license="MIT",
+     install_requires=[
+         "requests",
+         "pandas",
+         "mojimoji",
+         "openpyxl",
+     ],
+ )
