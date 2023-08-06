67ddba5baeb5bfe938d4dc15261cb4959348cde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:25 +0000

    Add package openai-forward/0.1.8/openai_forward-0.1.8-py3-none-any.whl

---
+++ packages/openai-forward/openai_forward-0.1.8-py3-none-any.whl/openai_forward/app.py
@@ -0,0 +1,23 @@
+from .openai import Openai
+from .routers.openai_v1 import router as router_v1
+from sparrow.api import create_app
+import httpx
+
+app = create_app(title="openai_forward", version="1.0")
+openai = Openai()
+use_http2 = False
+
+
+@app.on_event('startup')
+async def startup_event():
+    app.state.client = httpx.AsyncClient(base_url=Openai.BASE_URL, http2=use_http2)
+
+
+@app.on_event('shutdown')
+async def shutdown_event():
+    await app.state.client.aclose()
+
+
+app.add_route(openai.ROUTE_PREFIX + '/{api_path:path}', openai.reverse_proxy,
+              methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH', 'TRACE'])
+app.include_router(router_v1)
