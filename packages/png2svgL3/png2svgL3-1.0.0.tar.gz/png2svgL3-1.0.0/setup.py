6be3416add6a745e223ef5d6747e5671925c9fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:28 +0000

    Add package png2svgL3/1.0.0/png2svgL3-1.0.0.tar.gz

---
+++ packages/png2svgL3/png2svgL3-1.0.0.tar.gz/png2svgL3-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='png2svgL3',
+    version='1.0.0',
+    description='A library to convert PNG images to SVG format',
+    author='Linlin Lu',
+    author_email='linlinlu13@gmail.com',
+    url='https://github.com/mialinlinlu/png2svgl3',
+    packages=['png2svgL3'],
+    install_requires=[
+        'Pillow'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
