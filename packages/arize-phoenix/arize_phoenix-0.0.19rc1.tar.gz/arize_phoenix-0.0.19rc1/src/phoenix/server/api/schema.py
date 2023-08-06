2f742f2a0cc2da9d8851e33a591fc9cc894ba9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:51 +0000

    Add package arize-phoenix/0.0.19rc1/arize_phoenix-0.0.19rc1.tar.gz

---
+++ packages/arize-phoenix/arize_phoenix-0.0.19rc1.tar.gz/arize_phoenix-0.0.19rc1/src/phoenix/server/api/schema.py
@@ -0,0 +1,36 @@
+import strawberry
+from strawberry.types import Info
+
+from .context import Context
+from .types.Dimension import to_gql_dimension
+from .types.EmbeddingDimension import to_gql_embedding_dimension
+from .types.ExportEventsMutation import ExportEventsMutation
+from .types.Model import Model
+from .types.node import GlobalID, Node, from_global_id
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def model(self) -> Model:
+        return Model()
+
+    @strawberry.field
+    def node(self, id: GlobalID, info: Info[Context, None]) -> Node:
+        type_name, node_id = from_global_id(str(id))
+        if type_name == "Dimension":
+            dimension = info.context.model.scalar_dimensions[node_id]
+            return to_gql_dimension(node_id, dimension)
+        elif type_name == "EmbeddingDimension":
+            embedding_dimension = info.context.model.embedding_dimensions[node_id]
+            return to_gql_embedding_dimension(node_id, embedding_dimension)
+
+        raise Exception(f"Unknown node type: {type}")
+
+
+@strawberry.type
+class Mutation(ExportEventsMutation):
+    ...
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
