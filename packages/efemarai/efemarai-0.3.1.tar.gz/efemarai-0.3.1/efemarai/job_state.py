d1e47575d86b6772cd02ea1fa2cef072ed2a8b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:08 +0000

    Add package efemarai/0.3.1/efemarai-0.3.1.tar.gz

---
+++ packages/efemarai/efemarai-0.3.1.tar.gz/efemarai-0.3.1/efemarai/job_state.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class JobState(Enum):
+    """
+    Possible Test Run, Baseline and Dataset States.
+    """
+
+    NotStarted = "NotStarted"
+    Starting = "Starting"
+    Evaluating = "Evaluating"
+    Testing = "Testing"
+    Loading = "Loading"
+    DataLoaded = "Data Loaded"
+    ExtractingAssets = "Extracting Assets"
+    CalculatingMetadata = "Calculating Metadata"
+    GeneratingReport = "Generating Report"
+    Finished = "Finished"
+    Failed = "Failed"
+    Stopped = "Stopped"
