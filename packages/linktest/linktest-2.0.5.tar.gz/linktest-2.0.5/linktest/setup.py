4628f19da1c6bc794bdf2cd306d3a693cf739e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package linktest/2.0.5/linktest-2.0.5.tar.gz

---
+++ packages/linktest/linktest-2.0.5.tar.gz/linktest-2.0.5/linktest/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='linktest',
+    version='2.0.5',
+    author='Wang Lin',
+    author_email='think_wl@163.com',
+    packages=['linktest'],
+    install_requires=[
+        "psutil",
+        "requests",
+        "curlify",
+        "selenium",
+        "selenium-wire",
+        "setuptools",
+        "urllib3",
+        "PyMySQL",
+        "jsoncomparison",
+        "chromedriver_autoinstaller"
+    ],
+    entry_points={
+        'console_scripts': [
+            'linktest = python3 run.py'
+        ]
+    }
+)
