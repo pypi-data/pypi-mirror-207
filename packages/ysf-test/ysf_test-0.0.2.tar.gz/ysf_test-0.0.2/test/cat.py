bd660d37636fe47be7142b74429c55dfefa8015c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:37 +0000

    Add package ysf-test/0.0.2/ysf_test-0.0.2.tar.gz

---
+++ packages/ysf-test/ysf_test-0.0.2.tar.gz/ysf_test-0.0.2/test/cat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+time: 2023/5/8 16:13
+file: cat.py
+author: yisifan
+email: yisifan@datagrand.com
+"""
+class Cat:
+    def __init__(self):
+        self.a = "喵喵喵"
+
+    def test(self):
+        print(self.a)
