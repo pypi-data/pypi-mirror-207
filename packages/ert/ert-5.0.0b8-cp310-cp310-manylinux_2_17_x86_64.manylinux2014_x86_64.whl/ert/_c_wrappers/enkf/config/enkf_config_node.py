150722ef65b59520a4a7659dd05b957289a23a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:25 +0000

    Add package ert/5.0.0b8/ert-5.0.0b8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-5.0.0b8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/_c_wrappers/enkf/config/enkf_config_node.py
@@ -0,0 +1,197 @@
+import logging
+import os
+from typing import TYPE_CHECKING, Dict, List, Tuple, Union
+
+from cwrap import BaseCClass
+from ecl.util.util import IntVector, StringList
+
+from ert._c_wrappers import ResPrototype
+from ert._c_wrappers.enkf.enums import ErtImplType
+
+from .ext_param_config import ExtParamConfig
+from .gen_data_config import GenDataConfig
+from .summary_config import SummaryConfig
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
+
+logger = logging.getLogger(__name__)
+
+
+class EnkfConfigNode(BaseCClass):
+    TYPE_NAME = "enkf_config_node"
+
+    _alloc = ResPrototype(
+        "enkf_config_node_obj enkf_config_node_alloc(ert_impl_type_enum, \
+                                                     char*, \
+                                                     void*)",
+        bind=False,
+    )
+    _alloc_gen_data_everest = ResPrototype(
+        "enkf_config_node_obj enkf_config_node_alloc_GEN_DATA_everest(char*, \
+                                                                      int_vector)",
+        bind=False,
+    )
+    _alloc_summary_node = ResPrototype(
+        "enkf_config_node_obj enkf_config_node_alloc_summary(char*)",
+        bind=False,
+    )
+    _get_ref = ResPrototype(
+        "void* enkf_config_node_get_ref(enkf_config_node)"
+    )  # todo: fix return type
+    _get_impl_type = ResPrototype(
+        "ert_impl_type_enum enkf_config_node_get_impl_type(enkf_config_node)"
+    )
+    _get_key = ResPrototype("char* enkf_config_node_get_key(enkf_config_node)")
+    _get_obs_keys = ResPrototype(
+        "stringlist_ref enkf_config_node_get_obs_keys(enkf_config_node)"
+    )
+    _free = ResPrototype("void enkf_config_node_free(enkf_config_node)")
+
+    _alloc_gen_data_full = ResPrototype(
+        "enkf_config_node_obj enkf_config_node_alloc_GEN_DATA_full(char*, \
+                                                                   gen_data_file_format_type, \
+                                                                   int_vector)",  # noqa
+        bind=False,
+    )
+
+    def __init__(self):
+        raise NotImplementedError("Class can not be instantiated directly!")
+
+    def getImplementationType(self) -> ErtImplType:
+        return self._get_impl_type()
+
+    def getPointerReference(self):
+        return self._get_ref()
+
+    def getDataModelConfig(self) -> GenDataConfig:
+        return GenDataConfig.createCReference(self._get_ref(), parent=self)
+
+    def getSummaryModelConfig(self) -> SummaryConfig:
+        return SummaryConfig.createCReference(self._get_ref(), parent=self)
+
+    def getObservationKeys(self) -> StringList:
+        return self._get_obs_keys().setParent(self)
+
+    @classmethod
+    def createSummaryConfigNode(cls, key: str) -> "EnkfConfigNode":
+        return cls._alloc_summary_node(key)
+
+    @classmethod
+    def create_ext_param(
+        cls,
+        key: str,
+        input_keys: Union[List[str], Dict[str, List[Tuple[str, str]]]],
+    ) -> ExtParamConfig:
+        config = ExtParamConfig(key, input_keys)
+        node = cls._alloc(
+            ErtImplType.EXT_PARAM,
+            key,
+            ExtParamConfig.from_param(config),
+        )
+        config.convertToCReference(node)  # config gets freed when node dies
+        return node
+
+    # This method only exposes the details relevant for Everest usage.
+    @classmethod
+    def create_gen_data(cls, key: str, report_steps: "Iterable[int]" = (0,)):
+        active_steps = IntVector()
+        for step in report_steps:
+            active_steps.append(step)
+
+        config_node = cls._alloc_gen_data_everest(key, active_steps)
+        return config_node
+
+    @staticmethod
+    def validate_gen_data_format(formatted_file: str = "") -> bool:
+        return formatted_file.count("%d") == 1
+
+    # GEN DATA FULL creation
+    @classmethod
+    def create_gen_data_full(
+        cls,
+        key,
+        result_file,
+        input_format,
+        report_steps,
+    ):
+        if os.path.isabs(result_file) or "%d" not in result_file:
+            msg = (
+                f"The RESULT_FILE:{result_file} setting for {key} is invalid - "
+                "must have an embedded %d - and be a relative path"
+            )
+            logger.error(msg)
+            return None
+        if not report_steps:
+            msg = (
+                "The GEN_DATA keywords must have a REPORT_STEPS:xxxx defined"
+                "Several report steps separated with ',' and ranges with '-'"
+                "can be listed"
+            )
+            logger.error(msg)
+            return None
+        active_steps = IntVector()
+        for step in report_steps:
+            active_steps.append(step)
+
+        config_node = cls._alloc_gen_data_full(
+            key,
+            input_format,
+            active_steps,
+        )
+
+        return config_node
+
+    def free(self):
+        self._free()
+
+    def __repr__(self):
+        return self._create_repr(
+            f"key = {self.getKey()}, "
+            f"implementation = {self.getImplementationType()}"
+        )
+
+    def getModelConfig(
+        self,
+    ) -> Union[GenDataConfig, SummaryConfig, ExtParamConfig]:
+        implementation_type = self.getImplementationType()
+
+        if implementation_type == ErtImplType.GEN_DATA:
+            return self.getDataModelConfig()
+        elif implementation_type == ErtImplType.SUMMARY:
+            return SummaryConfig.createCReference(
+                self.getPointerReference(), parent=self
+            )
+        elif implementation_type == ErtImplType.EXT_PARAM:
+            return ExtParamConfig.createCReference(
+                self.getPointerReference(), parent=self
+            )
+        else:
+            raise ValueError(
+                "[EnkfConfigNode::getModelConfig()] "
+                f"Unhandled implementation model type: {implementation_type:i}"
+            )
+
+    def getKey(self) -> str:
+        return self._get_key()
+
+    def __eq__(self, other) -> bool:
+        if any(
+            (
+                self.getImplementationType() != other.getImplementationType(),
+                self.getKey() != other.getKey(),
+            )
+        ):
+            return False
+
+        if any(
+            [
+                self.getImplementationType() == ErtImplType.GEN_DATA
+                and self.getDataModelConfig() != other.getDataModelConfig(),
+                self.getImplementationType() == ErtImplType.SUMMARY
+                and self.getSummaryModelConfig() != other.getSummaryModelConfig(),
+            ]
+        ):
+            return False
+
+        return True
---
+++ packages/ert/ert-5.0.0b8-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/_c_wrappers/job_queue/workflow_job.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import os
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import List, Optional, Type, Union
+
+from ert._c_wrappers.config import ConfigParser, ContentTypeEnum
+from ert._clib.job_kw import type_from_kw
+from ert.parsing import ConfigValidationError
+
+from .ert_plugin import ErtPlugin
+from .ert_script import ErtScript
+
+ContentTypes = Union[Type[int], Type[bool], Type[float], Type[str]]
+
+
+def _workflow_job_config_parser() -> ConfigParser:
+    parser = ConfigParser()
+    parser.add("MIN_ARG", value_type=ContentTypeEnum.CONFIG_INT).set_argc_minmax(1, 1)
+    parser.add("MAX_ARG", value_type=ContentTypeEnum.CONFIG_INT).set_argc_minmax(1, 1)
+    parser.add(
+        "EXECUTABLE", value_type=ContentTypeEnum.CONFIG_EXECUTABLE
+    ).set_argc_minmax(1, 1)
+    parser.add("SCRIPT", value_type=ContentTypeEnum.CONFIG_PATH).set_argc_minmax(1, 1)
+    parser.add("INTERNAL", value_type=ContentTypeEnum.CONFIG_BOOL).set_argc_minmax(1, 1)
+    item = parser.add("ARG_TYPE")
+    item.set_argc_minmax(2, 2)
+    item.iset_type(0, ContentTypeEnum.CONFIG_INT)
+    item.initSelection(1, ["STRING", "INT", "FLOAT", "BOOL"])
+    return parser
+
+
+_config_parser = _workflow_job_config_parser()
+
+
+class ErtScriptLoadFailure(ValueError):
+    pass
+
+
+@dataclass
+class WorkflowJob:
+    name: str
+    internal: bool
+    min_args: Optional[int]
+    max_args: Optional[int]
+    arg_types: List[ContentTypeEnum]
+    executable: Optional[str]
+    script: Optional[str]
+
+    def __post_init__(self):
+        self.ert_script: Optional[type] = None
+        if self.script is not None and self.internal:
+            try:
+                self.ert_script = ErtScript.loadScriptFromFile(
+                    self.script,
+                )  # type: ignore
+            # Bare Exception here as we have no control
+            # of exceptions in the loaded ErtScript
+            except Exception as err:  # noqa
+                raise ErtScriptLoadFailure(
+                    f"Failed to load {self.name}: {err}"
+                ) from err
+
+    @staticmethod
+    def _make_arg_types_list(
+        config_content, max_arg: Optional[int]
+    ) -> List[ContentTypeEnum]:
+        arg_types_dict = defaultdict(lambda: ContentTypeEnum.CONFIG_STRING)
+        if max_arg is not None:
+            arg_types_dict[max_arg - 1] = ContentTypeEnum.CONFIG_STRING
+        for arg in config_content["ARG_TYPE"]:
+            arg_types_dict[arg[0]] = ContentTypeEnum(type_from_kw(arg[1]))
+        if arg_types_dict:
+            return [
+                arg_types_dict[j]
+                for j in range(max(i for i in arg_types_dict.keys()) + 1)
+            ]
+        else:
+            return []
+
+    @classmethod
+    def fromFile(cls, config_file, name=None):
+        if os.path.isfile(config_file) and os.access(config_file, os.R_OK):
+            if name is None:
+                name = os.path.basename(config_file)
+
+            content = _config_parser.parse(config_file)
+
+            def optional_get(key):
+                return content.getValue(key) if content.hasKey(key) else None
+
+            max_arg = optional_get("MAX_ARG")
+
+            return cls(
+                name,
+                optional_get("INTERNAL"),
+                optional_get("MIN_ARG"),
+                max_arg,
+                cls._make_arg_types_list(content, max_arg),
+                optional_get("EXECUTABLE"),
+                optional_get("SCRIPT"),
+            )
+        else:
+            raise ConfigValidationError(f"Could not open config_file:{config_file!r}")
+
+    def isPlugin(self) -> bool:
+        if self.ert_script is not None:
+            return issubclass(self.ert_script, ErtPlugin)
+        return False
+
+    def argumentTypes(self) -> List["ContentTypes"]:
+        def content_to_type(c: Optional[ContentTypeEnum]):
+            if c == ContentTypeEnum.CONFIG_BOOL:
+                return bool
+            if c == ContentTypeEnum.CONFIG_FLOAT:
+                return float
+            if c == ContentTypeEnum.CONFIG_INT:
+                return int
+            if c == ContentTypeEnum.CONFIG_STRING:
+                return str
+            raise ValueError(f"Unknown job type {c} in {self}")
+
+        return list(map(content_to_type, self.arg_types))
