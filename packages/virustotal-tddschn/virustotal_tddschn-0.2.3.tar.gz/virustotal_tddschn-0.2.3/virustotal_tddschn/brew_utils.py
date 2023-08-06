82d39ae017e25a69ebe9205fe5c3d99ab6fc4cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:26 +0000

    Add package virustotal-tddschn/0.2.3/virustotal_tddschn-0.2.3.tar.gz

---
+++ packages/virustotal-tddschn/virustotal_tddschn-0.2.3.tar.gz/virustotal_tddschn-0.2.3/virustotal_tddschn/brew_utils.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+
+from .utils import get_arch
+from .types import BrewType, BrewTypeAndPaths
+import os, re
+from pathlib import Path
+from typing import Any, Literal
+
+
+def get_brew_tap_dir() -> Path:
+    def get_brew_tap_dir_with_subprocess():
+        import subprocess
+
+        p = subprocess.getoutput('brew --prefix') + '/Homebrew/Library/Taps'
+        return Path(p)
+
+    homebrew_repo_path = os.getenv('HOMEBREW_REPOSITORY')
+    if homebrew_repo_path is not None:
+        tap_dir_path = Path(homebrew_repo_path) / 'Library' / 'Taps'
+        if tap_dir_path.exists():
+            return tap_dir_path
+        return get_brew_tap_dir_with_subprocess()
+    else:
+        return get_brew_tap_dir_with_subprocess()
+
+
+def brew_get_type(formula_or_cask_content: str) -> Literal['f', 'c'] | None:
+    if re.match(r'class [a-zA-Z0-9]+ < Formula\n', formula_or_cask_content):
+        return 'f'
+    elif re.match(r'cask "[a-zA-Z0-0@._-]+" do\n', formula_or_cask_content):
+        return 'c'
+    else:
+        return None
+
+
+def brew_get_type_and_paths(brew_name: str) -> BrewTypeAndPaths:
+    brew_type: BrewType = {'f': 0, 'c': 0}
+    formula_paths: list[Path] = []
+    cask_paths: list[Path] = []
+    output_dict: BrewTypeAndPaths = {
+        'brew_type': brew_type,
+        'formula_paths': formula_paths,
+        'cask_paths': cask_paths,
+    }
+
+    brew_tap_dir = get_brew_tap_dir()
+    if match := re.match(
+        r'([a-zA-Z0-9@._-]+)/([a-zA-Z0-9@._-]+)/([a-zA-Z0-9@._-]+)$', brew_name
+    ):
+        brew_tap_dir = brew_tap_dir / match.group(1) / f'homebrew-{match.group(2)}'
+        brew_name = match.group(3)
+    matched_files = brew_tap_dir.rglob(f'**/{brew_name}.rb')
+    if matched_files:
+        for file in matched_files:
+            file_content = file.read_text()
+            file_type = brew_get_type(file_content)
+            if file_type == 'f':
+                brew_type['f'] = 1
+                formula_paths.append(file)
+            elif file_type == 'c':
+                brew_type['c'] = 1
+                cask_paths.append(file)
+    return output_dict
+
+
+def brew_get_file_path(brew_name: str, use_cask: bool = False) -> Path | None:
+    type_and_paths = brew_get_type_and_paths(brew_name)
+    if use_cask:
+        output = type_and_paths['cask_paths'][0]
+    else:
+        # test ['f'] first, if it's FC, f would be used
+        if type_and_paths['brew_type']['f']:
+            output = type_and_paths['formula_paths'][0]
+        elif type_and_paths['brew_type']['c']:
+            output = type_and_paths['cask_paths'][0]
+        else:
+            return None
+    if output:
+        return output
+
+
+def _get_checksum_from_formula_or_cask(
+    formula_or_cask_content: str, codename: str | None = None
+) -> str | dict:
+    if brew_type := brew_get_type(formula_or_cask_content):
+        # if brew_type == 'f':
+        #     pass
+        # elif brew_type == 'c':
+        #     if match := re.findall('^(?:  )+sha256 "([a-f0-9]{64})"$',
+        #                            file_content, re.MULTILINE):
+        #         return match[0]
+
+        # for multilang casks like firefox
+        if multilang_cask := re.search(
+            'default: true do\n(?:  )+sha256 "([a-f0-9]{64})"$',
+            formula_or_cask_content,
+            re.MULTILINE,
+        ):
+            return multilang_cask.group(1)
+
+        # for single lang formulas and casks
+        # - for formulas, only checks source sha256, not the bottle's
+        elif match := re.search(
+            '^(?:  )+sha256 "([a-f0-9]{64})"$', formula_or_cask_content, re.MULTILINE
+        ):
+            return match.group(1)
+        elif match := re.search(
+            "^(?:  )+sha256 arm: +\"(?P<arm>[a-f0-9]{64})\",\n(?: )+intel: +\"(?P<intel>[a-f0-9]{64})\"$",
+            formula_or_cask_content,
+            re.MULTILINE,
+        ):
+            return match.groupdict()
+        else:
+            raise Exception(f'Failed to find sha256 checksum.')
+    else:
+        raise Exception(f'Doesn\'t seem like a formula for cask file.')
+
+
+def get_checksum_from_brew_file(formula_or_cask_content: str) -> str:
+    checksum = _get_checksum_from_formula_or_cask(formula_or_cask_content)
+    if isinstance(checksum, dict):
+        return checksum[get_arch()]
+    return checksum
+
+
+def get_brew_cache_path(brew_name: str, use_cask: bool = False) -> str:
+    import subprocess, sys
+
+    brew_type = brew_get_type_and_paths(brew_name)['brew_type']
+    if use_cask:
+        cache_flag = '--cask'
+    elif sum(brew_type.values()) == 2:  # type: ignore
+        cache_flag = '--formula'
+    else:
+        cache_flag = ''
+
+    brew_cache_command = 'brew --cache ' + cache_flag + ' ' + brew_name
+    cache_path = subprocess.getoutput(brew_cache_command).strip()
+    if cache_path:
+        return cache_path
+    else:
+        sys.exit(f'Failed to get the cache path for {brew_name} .')
---
+++ packages/virustotal-tddschn/virustotal_tddschn-0.2.3.tar.gz/virustotal_tddschn-0.2.3/virustotal_tddschn/virustotal_sum_search.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+"""
+Author : Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
+Date   : 2021-05-12
+Purpose: Search file or Homebrew package's checksum on VitusTotal
+"""
+import argparse, re, sys
+import os
+from pathlib import Path
+from . import __version__
+from .hashing import sha256_checksum, sha256_checksum_from_fh
+from .config import browser_str_to_app_name_map
+from .utils import (
+    get_latest_downloaded_file,
+    browser_str_to_app_name_map,
+    open_url,
+    macos_get_version_codename,
+    get_arch,
+)
+from .brew_utils import (
+    brew_get_file_path,
+    get_brew_cache_path,
+    get_checksum_from_brew_file,
+)
+
+# cSpell:disable
+__app_name__ = 'vtpy'
+# cSpell:enable
+
+CREATE_ARG_PARSER_ARGS = (
+    __app_name__,
+    __version__,
+    'Search file or Homebrew package checksum on VirusTotal',
+)
+
+
+def create_arg_parser(
+    __app_name__: str,
+    __version__: str,
+    description: str,
+    add_help: bool = True,
+    brew_related_options_only: bool = False,
+):
+
+    parser = argparse.ArgumentParser(
+        prog=__app_name__,
+        add_help=add_help,
+        description=description,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    if brew_related_options_only:
+        parser.add_argument(
+            'brew',
+            metavar='BREW',
+            help='The formula or cask name to check',
+            type=str,
+        )
+
+    if not brew_related_options_only:
+        parser.add_argument(
+            '--hash', metavar='HASH', type=str, help='The hash to search'
+        )
+
+        parser.add_argument(
+            '-f',
+            '--file',
+            metavar='FILE',
+            type=argparse.FileType('rb'),
+            help='The file to hash and check',
+        )
+
+    parser.add_argument(
+        '-b',
+        '--browser',
+        help='Browser to open URLs',
+        metavar='browser',
+        type=str,
+        choices=list(browser_str_to_app_name_map),
+        # default='chrome',
+        default=None
+    )
+
+    parser.add_argument(
+        '-B', '--no-browser', help='Do not open URLs in a browser', action='store_true'
+    )
+
+    if not brew_related_options_only:
+        parser.add_argument(
+            '-ldl',
+            '--latest-download',
+            help='Use the latest downloaded file',
+            action='store_true',
+        )
+
+        parser.add_argument(
+            '-w',
+            '--brew',
+            metavar='BREW',
+            help='Use the checksum in Homebrew formula or cask file',
+            type=str,
+        )
+
+    # parser.add_argument(
+    # '-F', '--formula', help='Use formula if a cask exists with the same name', action='store_true', default=None)
+    parser.add_argument(
+        '-C', '--cask', help='Use cask', action='store_true', default=None
+    )
+
+    parser.add_argument(
+        '-c', '--brew-cache', metavar='BREW', help='Use brew downloaded cache'
+    )
+
+    if not brew_related_options_only:
+        parser.add_argument(
+            '-m', '--mac', metavar='APP', help='Path to app bundle', type=str
+        )
+
+    parser.add_argument(
+        '-F',
+        '--brew-file',
+        help='Use the checksum in the brew formula or cask file',
+        type=Path,
+        metavar='PATH',
+    )
+
+    parser.add_argument(
+        '-V', '--version', action='version', version=f'%(prog)s {__version__}'
+    )
+    return parser
+
+
+def parse_args(
+    parser: argparse.ArgumentParser, brew_related_options_only: bool = False
+) -> argparse.Namespace:
+
+    args = parser.parse_args()
+    if brew_name := args.brew:
+        if not re.match('[a-zA-Z@._-]+', brew_name):
+            if brew_related_options_only:
+                parser.error(f'Invalid brew formula / cask name: {brew_name}')
+            parser.error(f'Invalid --brew argument: {brew_name} .')
+    return args
+
+
+def main():
+    parser = create_arg_parser(*CREATE_ARG_PARSER_ARGS)
+    args = parse_args(parser)
+    if args.hash:
+        hash = args.hash
+        print_path_and_open_vt_link(None, hash, args)
+    else:
+        if args.file:
+            hash = sha256_checksum_from_fh(args.file)
+            file_path = args.file.name
+            print_path_and_open_vt_link(file_path, hash, args)
+        else:
+            if args.mac:
+                exec_name = Path(os.path.basename(args.mac)).with_suffix('')
+                file_path = os.path.join(args.mac, 'Contents/MacOS', exec_name)
+                hash = sha256_checksum(file_path)  # type: ignore
+                print_path_and_open_vt_link(file_path, hash, args)
+            elif args.brew_file:
+                hash = get_checksum_from_brew_file(args.brew_file.read_text())
+                print_path_and_open_vt_link(args.brew_file, hash, args)
+            elif args.brew:
+                brew_file_path = brew_get_file_path(
+                    brew_name=args.brew, use_cask=args.cask
+                )
+                if brew_file_path is None:
+                    raise FileNotFoundError(
+                        f'Cannot find brew formula or cask file for {args.brew} .'
+                    )
+                hash = get_checksum_from_brew_file(Path(brew_file_path).read_text())
+                file_path = brew_file_path
+                print_path_and_open_vt_link(file_path, hash, args)
+            elif brew_cache := args.brew_cache:
+                file_path = get_brew_cache_path(brew_cache, use_cask=args.cask)
+                hash = sha256_checksum(file_path)  # type: ignore
+                print_path_and_open_vt_link(file_path, hash, args)
+            elif args.latest_download:
+                file_path = get_latest_downloaded_file()
+                hash = sha256_checksum(file_path)
+                print_path_and_open_vt_link(file_path, hash, args)
+
+
+def print_path_and_open_vt_link(file_path, hash, args):
+    if file_path:
+        print(f'File path:       {file_path}')
+    if hash:
+        vt_url = 'https://www.virustotal.com/gui/search/' + hash
+        print(f'SHA256 checksum: {hash}')
+        print(f'VirusTotal URL:  {vt_url}')
+        if not args.no_browser:
+            open_url(url=vt_url, browser=args.browser)
+
+
+if __name__ == '__main__':
+    main()
