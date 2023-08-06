cd078c28caf89b1b33a304d6ffc706269accaa3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:55 +0000

    Add package draw-isabelle/0.0.1/draw-isabelle-0.0.1.tar.gz

---
+++ packages/draw-isabelle/draw-isabelle-0.0.1.tar.gz/draw-isabelle-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+required = [
+    "binarytree~=6.5.1",
+]
+
+# pylint: disable=use-dict-literal
+setup_args = dict(
+    name="draw-isabelle",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Søren Winkel Holm",
+    author_email="swholm@protonmail.com",
+    url="https://github.com/sorenmulli",
+    download_url="https://pypi.org/project/draw_isabelle",
+    install_requires=required,
+    description="Viz Isabelle ⟨l, a, r⟩ graphs",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    license="MIT",
+    entry_points={
+        "console_scripts": [
+            "draw-isabelle = draw_isabelle.__main__:run_from_cli",
+        ]
+    },
+)
+
+if __name__ == "__main__":
+    setup(**setup_args)
