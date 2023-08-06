c16aa35cb298f351d00a39c8df28d2db40014bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:12 +0000

    Add package sakkara/0.6.0/sakkara-0.6.0.tar.gz

---
+++ packages/sakkara/sakkara-0.6.0.tar.gz/sakkara-0.6.0/sakkara/model/composable/group.py
@@ -0,0 +1,103 @@
+from abc import ABC
+from typing import Tuple, Union, Any, Dict
+
+import pymc as pm
+from pytensor import tensor as pt
+import numpy as np
+from numpy import typing as npt
+
+from sakkara.model.fixed.data import DataComponent
+from sakkara.model.base import ModelComponent
+from sakkara.model.composable.base import Composable, T
+from sakkara.relation.groupset import GroupSet
+from sakkara.relation.representation import MinimalTensorRepresentation
+
+
+class GroupComponent(Composable[Tuple[str, ...], T], ABC):
+    """
+    Class for specifying components for each member of a group individually
+
+    :param group: Group of which the component is defined for.
+    :param membercomponents: Dictionary with key indicating member (corresponding to DataFrame value) and value its
+            corresponding value (ModelComponent or other)
+    :param name: Name of the corresponding variable to register in PyMC.
+    """
+
+    def __init__(self, group: Union[str, Tuple[str, ...]], name: str = None, membercomponents: Dict[Any, Any] = None):
+        super().__init__(name, group, dict())
+        if membercomponents is not None:
+            for k, v in membercomponents.items():
+                self.add(k, v)
+
+    def __getitem__(self, item: Any):
+        return self.subcomponents[item if isinstance(item, tuple) else (item,)]
+
+    def add(self, member: Any, component: Union[float, npt.NDArray, ModelComponent]) -> None:
+        """
+        Add component for a member to the GroupComponent
+
+        :param member: Key for the group member, corresponding to Dataframe value.
+        :param component: Value or component of the given member.
+        """
+        if not isinstance(component, ModelComponent):
+            component = DataComponent(component, 'global')
+        key = member if isinstance(member, tuple) else (member,)
+        self.subcomponents[key] = component
+
+    def prebuild(self, groupset: GroupSet) -> None:
+        self.build_components(groupset)
+
+    def build_representation(self, groupset: GroupSet):
+        self.base_representation = MinimalTensorRepresentation()
+        self.components_representation = MinimalTensorRepresentation(groupset['global'])
+
+        for g in self.group:
+            self.base_representation.add_group(groupset[g])
+
+        for component in self.subcomponents.values():
+            self.components_representation = MinimalTensorRepresentation(*component.representation.get_groups(),
+                                                                         *self.components_representation.get_groups())
+
+        self.representation = MinimalTensorRepresentation(*self.base_representation.get_groups(),
+                                                          *self.components_representation.get_groups())
+
+        if self.base_representation.get_groups() != self.representation.get_groups()[
+                                                    :len(self.base_representation.get_groups())]:
+            raise ValueError(
+                'The group value specified for this GroupComponent have children among the component groups')
+
+    def build_variable(self) -> None:
+        member_tuples = self.base_representation.get_member_tuples()
+
+        if not all(m in self.subcomponents for m in member_tuples):
+            raise ValueError('All member of group component must be specified.')
+
+        # Get the member array for each group, re-order with mapping
+        base_members = self.base_representation.get_members()
+        base_members_reordered = tuple(
+            map(lambda x: self.base_representation.map(x, self.representation), base_members))
+
+        # Create array for adding
+        member_tensor = np.empty(len(member_tuples), dtype=object)
+
+        for i, member_tuple in enumerate(member_tuples):
+            # Get the component
+            component = self.subcomponents[member_tuple]
+            # Get the variable, re-order to the representation and flatten
+            member_variable = component.representation.map(component.variable, self.representation).ravel()
+
+            # Mask all the entries of the group variable (with self.representation) corresponding to this member
+            mask = np.ones(np.prod(self.representation.get_shape()), dtype=bool)
+            for j, m in enumerate(member_tuple):
+                mask *= base_members_reordered[j].ravel() == m
+
+            member_tensor[i] = member_variable[mask]
+
+        # Stack the member-wise variables into the group variable
+        # NOTE! We assume that self.base_representation correspond to the first groups of self.representation
+        # This might not be the case if a child of a group in self.base_representation is included in
+        # self.components_representation (Hence, this is not supported)
+        full_tensor = pt.stack(member_tensor.tolist()).ravel().reshape(self.representation.get_shape())
+
+        # Create the group variable, wrapped with Deterministic
+        self.variable = pm.Deterministic(name=self.name, var=full_tensor, dims=self.dims())
---
+++ packages/sakkara/sakkara-0.6.0.tar.gz/sakkara-0.6.0/sakkara/model/composable/hierarchical/likelihood.py
@@ -0,0 +1,89 @@
+import warnings
+from abc import ABC
+from typing import Callable, Any, Dict, Union, Tuple
+
+import numpy as np
+
+from sakkara.model.fixed.data import DataComponent
+from sakkara.model.composable.group import GroupComponent
+from sakkara.model.composable.hierarchical.distribution import DistributionComponent
+
+
+class Likelihood(DistributionComponent, ABC):
+    """
+    Component for a likelihood distribution, e.g, with observed data.
+
+    :param generator: PyMC callable for distribution to use.
+    :param observed: Data to input as observed keyword in PyMC.
+    :param name: Name of the corresponding variable to register in PyMC.
+    :param group: Group of which the component is defined for.
+    :param nan_param_mask: Masked distribution parameters to use for rows with `Nan`, must be defined for each keyword argument entered. Required if there are `Nan` in observed.
+    :param nan_data_mask: Masked observed value to use for rows with `Nan`. Required if there are `Nan` in observed.
+    """
+
+    def __init__(self,
+                 generator: Callable,
+                 observed: DataComponent,
+                 name: str = 'likelihood',
+                 group: Union[str, Tuple[str, ...]] = 'obs',
+                 nan_param_mask: Dict[str, Any] = None,
+                 nan_data_mask: Any = None,
+                 **kwargs: Any):
+        if np.any(np.isnan(observed.values)):
+            if nan_param_mask is None or nan_data_mask:
+                raise ValueError(
+                    'Both nan_param_mask and nan_data_mask must be defined when there is Nan in the observed data.')
+
+            warnings.warn('Observables contains NaN values, these will be ignored in likelihood computation.',
+                          UserWarning)
+
+            components = {}
+
+            for k, v in kwargs.items():
+                if k not in nan_param_mask:
+                    raise ValueError('Mask values for all parameters must be defined in nan_param_mask')
+
+                var_mask = nan_param_mask[k] if isinstance(nan_param_mask, dict) else nan_param_mask
+
+                component = GroupComponent(group=group, name=k + '_masked')
+                mask_component = DataComponent(var_mask, 'global')
+                for m in np.argwhere(~np.isnan(observed.values)).flatten():
+                    component.add(m, v)
+                for m in np.argwhere(np.isnan(observed.values)).flatten():
+                    component.add(m, mask_component)
+                components[k] = component
+
+            non_nandata = np.array([o if not np.isnan(o) else nan_data_mask for o in observed.values])
+            components['observed'] = DataComponent(non_nandata, group, name='masked_' + observed.name)
+        else:
+            components = kwargs
+            components['observed'] = observed
+
+        super().__init__(generator, name, group, **components)
+
+
+class MinibatchLikelihood(Likelihood):
+    """
+        Likelihood to use if minibatch is used. Requires iid data.
+
+        :param generator: PyMC callable for distribution to use.
+        :param observed: Data to input as observed keyword in PyMC.
+        :param batch_size: Size of mini-batch (# of observations per evaluation)
+        :param name: Name of the corresponding variable to register in PyMC.
+        :param group: Group of which the component is defined for.
+        :param nan_param_mask: Masked distribution parameters to use for rows with `Nan`, must be defined for each keyword argument entered. Required if there are `Nan` in observed.
+        :param nan_data_mask: Masked observed value to use for rows with `Nan`. Required if there are `Nan` in observed.
+        """
+    def __init__(self, generator: Callable,
+                 observed: DataComponent,
+                 batch_size: int,
+                 name: str = 'likelihood',
+                 group: str = 'obs',
+                 nan_param_mask: Dict[str, Any] = None,
+                 nan_data_mask: Any = None,
+                 **kwargs: Any):
+        kwargs['total_size'] = len(observed.values)
+        super().__init__(generator, observed, name, group, nan_param_mask, nan_data_mask, **kwargs)
+
+        for k, v in self.subcomponents.items():
+            self.subcomponents[k] = v.to_minibatch(batch_size, group)
