1e9a9e3a5aa53c7ee026305ee2b559439ac252b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:49 +0000

    Add package pocketrockit/0.0.17/pocketrockit-0.0.17.tar.gz

---
+++ packages/pocketrockit/pocketrockit-0.0.17.tar.gz/pocketrockit-0.0.17/pocketrockit/examples/template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A Pocket Operator PO-20 Arcade"""
+
+from pocketrockit import Env, midiseq, player, track
+
+# fmt: off
+
+@track
+def track_name(env: Env):
+    """Put a little poem here"""
+
+    # @env object contains some global stuff like (currently: only) bpm
+    env.bpm = 60
+
+    key = "C4"
+
+    @player
+    def metronome():
+        yield from midiseq("x x x x", channel=128, note=37)
