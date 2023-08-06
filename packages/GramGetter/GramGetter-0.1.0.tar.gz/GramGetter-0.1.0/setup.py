eff7cd29d4bea8e43c33ca91301ef4a4f553beb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:48 +0000

    Add package GramGetter/0.1.0/GramGetter-0.1.0.tar.gz

---
+++ packages/GramGetter/GramGetter-0.1.0.tar.gz/GramGetter-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+requirements = ['requests>=2.4']
+setup(
+    name='GramGetter',
+    version='0.1.0',    # Update this for new versions
+    description='A crawler for scraping instagram content',
+    author='Mohamed Amine Said',
+    author_email='amine8said@gmail.com',
+    url='https://github.com/instaloader/instaloader',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
