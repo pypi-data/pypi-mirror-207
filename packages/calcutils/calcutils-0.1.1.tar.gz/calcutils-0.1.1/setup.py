2b01cd863ff076943e11d5d6f4763d31d5850e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:28 +0000

    Add package calcutils/0.1.1/calcutils-0.1.1.tar.gz

---
+++ packages/calcutils/calcutils-0.1.1.tar.gz/calcutils-0.1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='calcutils',
+      version='0.1.1',
+      description='a calcu moduel',
+      author='Lahani_Allen',
+      author_email='lahani_Allen@utc.com',
+      url='https://github.com/calcu_utils/calcu_utils',
+      include_package_data=True,
+      package_data={
+          # If any package contains *.txt files, include them:
+          '': ['*.p'],
+          'calcutils': ['calcutils/*.p'], }
+      )
