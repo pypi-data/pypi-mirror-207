8ca9b8db43db073843e7352661041bd4482c5729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:12 +0000

    Add package csrfvalidators/0.1.10/csrfvalidators-0.1.10.tar.gz

---
+++ packages/csrfvalidators/csrfvalidators-0.1.10.tar.gz/csrfvalidators-0.1.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csrfvalidators',
+    version='0.1.10',
+    description='validation',
+    author='nxbz44',
+    author_email='nxbz44@yopmail.com',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
