d2d6e8641216814b54e818de05b8772ef02cc965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package netmc/0.0.1/netmc-0.0.1-py3-none-any.whl

---
+++ packages/netmc/netmc-0.0.1-py3-none-any.whl/netmc/models.py
@@ -0,0 +1,22 @@
+import hypernetx as hnx
+import networkx as nx
+import pymc as pm
+import simplicial
+
+class PGraph:
+    def __init__(self, node_data, edge_data):
+        ...
+
+class PDiGraph:
+    def __init__(self, node_data, edge_data):
+        ...
+
+class PHypergraph:
+    def __init__(self, hnode_data, hedge_data):
+        ...
+
+class PSimplex:
+    ...
+
+class PMultiAryRelation:
+    ...
