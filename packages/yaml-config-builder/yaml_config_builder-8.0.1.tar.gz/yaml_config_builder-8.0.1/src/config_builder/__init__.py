97daa0f0d403c99aa7e45ded4294f38c1581e276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:41 +0000

    Add package yaml-config-builder/8.0.1/yaml_config_builder-8.0.1.tar.gz

---
+++ packages/yaml-config-builder/yaml_config_builder-8.0.1.tar.gz/yaml_config_builder-8.0.1/src/config_builder/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Open Logistics Foundation
+#
+# Licensed under the Open Logistics License 1.0.
+# For details on the licensing terms, see the LICENSE file.
+
+# -*- coding: utf-8 -*-
+from .base_config_class import BaseConfigClass
+from .config_builder import ConfigBuilder
+
+__all__ = ["BaseConfigClass", "ConfigBuilder"]
+__version__ = "8.0.1"
