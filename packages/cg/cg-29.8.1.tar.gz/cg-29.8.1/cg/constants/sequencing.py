cab9de90b8b01709b14b9a574c7baf9973ed4f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:33:00 +0000

    Add package cg/29.8.1/cg-29.8.1.tar.gz

---
+++ packages/cg/cg-29.8.1.tar.gz/cg-29.8.1/cg/constants/sequencing.py
@@ -0,0 +1,56 @@
+"""Constants related to all things sequencing"""
+from cgmodels.cg.constants import StrEnum
+
+
+class Sequencers(StrEnum):
+    """Sequencer instruments."""
+
+    ALL: str = "all"
+    HISEQX: str = "hiseqx"
+    HISEQGA: str = "hiseqga"
+    NOVASEQ: str = "novaseq"
+    OTHER: str = "other"
+
+
+sequencer_types = {
+    "D00134": Sequencers.HISEQGA,
+    "D00410": Sequencers.HISEQGA,
+    "D00415": Sequencers.HISEQGA,
+    "D00450": Sequencers.HISEQGA,
+    "D00456": Sequencers.HISEQGA,
+    "D00483": Sequencers.HISEQGA,
+    "M03284": Sequencers.HISEQGA,
+    "SN1025": Sequencers.HISEQGA,
+    "SN7001298": Sequencers.HISEQGA,
+    "SN7001301": Sequencers.HISEQGA,
+    "ST-E00198": Sequencers.HISEQX,
+    "ST-E00201": Sequencers.HISEQX,
+    "ST-E00214": Sequencers.HISEQX,
+    "ST-E00266": Sequencers.HISEQX,
+    "ST-E00269": Sequencers.HISEQX,
+    "A00187": Sequencers.NOVASEQ,
+    "A00621": Sequencers.NOVASEQ,
+    "A00689": Sequencers.NOVASEQ,
+    "A01901": Sequencers.NOVASEQ,
+}
+
+
+class SequencingMethod(StrEnum):
+    """Sequencing method types."""
+
+    TGS: str = "tgs"
+    WES: str = "wes"
+    WGS: str = "wgs"
+    WTS: str = "wts"
+
+
+class Variants(StrEnum):
+    """Type of variants."""
+
+    SNV: str = "snv"
+    SV: str = "sv"
+
+
+class RecordType(StrEnum):
+    Sample: str = "Sample"
+    Pool: str = "Pool"
---
+++ packages/cg/cg-29.8.1.tar.gz/cg-29.8.1/tests/cli/demultiplex/test_stats_command.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+from click.testing import CliRunner
+
+from cg.apps.cgstats.crud import find
+from cg.apps.cgstats.stats import StatsAPI
+from cg.cli.demultiplex.add import select_project_cmd
+from cg.models.cg_config import CGConfig
+from cg.models.demultiplex.demux_results import DemuxResults
+from cg.models.demultiplex.flow_cell import FlowCell
+
+
+def test_select_command(
+    cli_runner: CliRunner,
+    populated_stats_api: StatsAPI,
+    demultiplexed_runs: Path,
+    flow_cell: FlowCell,
+    demultiplex_context: CGConfig,
+):
+    demultiplex_context.cg_stats_api_ = populated_stats_api
+    # GIVEN a stats api with some information about a flowcell
+    flowcell_id: str = flow_cell.id
+    full_flow_cell_name: str = flow_cell.full_name
+    assert populated_stats_api.find_handler.get_flow_cell_by_name(flow_cell_name=flowcell_id)
+    demux_results = DemuxResults(
+        demux_dir=demultiplexed_runs / full_flow_cell_name,
+        flow_cell=flow_cell,
+        bcl_converter="bcl2fastq",
+    )
+
+    # GIVEN a project id
+    project_id: str = ""
+    for project in demux_results.projects:
+        project_id = project.split("_")[-1]
+    assert project_id
+
+    # WHEN exporting the sample information
+    result = cli_runner.invoke(
+        select_project_cmd, [flowcell_id, "--project", project_id], obj=demultiplex_context
+    )
+
+    # THEN assert that the command exits with success
+    assert result.exit_code == 0
+    # THEN assert that the flowcell id if in the printing
+    assert flowcell_id in result.output
