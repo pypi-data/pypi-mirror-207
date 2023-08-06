d0d20dc095b743302858ebd53884f71795b0e29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:29 +0000

    Add package finnsult/1.11/finnsult-1.11.tar.gz

---
+++ packages/finnsult/finnsult-1.11.tar.gz/finnsult-1.11/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "finnsult",
+    version = "1.11",
+    author = "Finn Everspaugh",
+    author_email = "finnventor@everspaugh.com",
+    description = "A program to insult people",
+    entry_points={
+        'console_scripts': [
+            'insult = insult:main',
+       ],
+    },
+    include_package_data=True,
+    install_requires=['pyyaml'],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://the-real-finnventor.github.io/insult/",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    package_dir = {"": "src"},
+    packages = ['insult'],
+)
