b2c9dc46b6ba41cfdb3626d96556e526c5ac8bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:29 +0000

    Add package enstools-compression/2023.5/enstools-compression-2023.5.tar.gz

---
+++ packages/enstools-compression/enstools-compression-2023.5.tar.gz/enstools-compression-2023.5/setup.py
@@ -0,0 +1,53 @@
+"""
+Setup file for enstools-compression
+"""
+from setuptools import setup, find_packages
+import os.path
+
+# Use the Readme file as long description.
+try:
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+
+def get_version():
+    from pathlib import Path
+    version_path = Path(__file__).parent / "VERSION"
+    with version_path.open() as version_file:
+        return version_file.read().strip()
+
+
+def find_enstools_packages():
+    """
+    Find the packages inside the enstools folder.
+    """
+
+    return [f'enstools.{p}' for p in (find_packages(f'{os.path.dirname(__file__)}/enstools'))]
+
+
+# perform the actual install operation
+setup(name="enstools-compression",
+      version=get_version(),
+      author="Oriol Tintó",
+      author_email="oriol.tinto@lmu.de",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url="https://github.com/wavestoweather/enstools-compression",
+
+      packages=find_enstools_packages(),
+
+      install_requires=[
+          "enstools>=2023.1",
+          "enstools-encoding>=2023.1",
+          "zfpy",
+          "hdf5plugin>=4.0.0",
+          "netCDF4",
+      ],
+      entry_points={
+          'console_scripts': [
+              'enstools-compression=enstools.compression.cli:main'
+          ],
+      },
+      )
