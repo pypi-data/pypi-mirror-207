fc9ca538334668e5af0998b8d56bac7a045601c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:41 +0000

    Add package kopia/0.1.4/kopia-0.1.4.tar.gz

---
+++ packages/kopia/kopia-0.1.4.tar.gz/kopia-0.1.4/kopia/mysqlc/app.py
@@ -0,0 +1,51 @@
+import typer
+
+import mysql.connector
+
+
+app = typer.Typer()
+
+
+@app.command()
+def dump(
+    database: str = typer.Argument(..., help="Database name"),
+    output_file: str = typer.Argument(..., help="Output file name"),
+    host: str = typer.Option("", "--host", "-h", help="Database host"),
+    user: str = typer.Option("", "--user", "-u", help="Database user"),
+    password: str = typer.Option("", "--password", "-p", help="Database password"),
+):
+    """Create a MySQL database dump."""
+    try:
+        # Connect to the database
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+        )
+
+        # Create a cursor object
+        cursor = connection.cursor()
+
+        # Execute the mysqldump command
+
+        cursor.execute(
+            "mysqldump"
+            f" --user {user}" if user else ""
+            f" --password {password}" if password else ""
+            f" --host {host}" if host else ""
+            f" {database} > {output_file}"
+        )
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+        # Print success message
+        typer.echo(f"MySQL database dump created successfully: {output_file}")
+    except mysql.connector.Error as e:
+        typer.echo(f"Error creating MySQL database dump: {e}")
+
+
+if __name__ == "__main__":
+    app()
---
+++ packages/kopia/kopia-0.1.4.tar.gz/kopia-0.1.4/kopia/sqlite/app.py
@@ -0,0 +1,39 @@
+import typer
+
+import sqlite3
+
+
+app = typer.Typer()
+
+
+@app.command()
+def dump(
+    database: str = typer.Argument(..., help="Database name"),
+    output_file: str = typer.Argument(..., help="Output file name"),
+):
+    """Create an SQLite database dump."""
+    try:
+        # Connect to the database
+        connection = sqlite3.connect(database)
+
+        # Create a cursor object
+        cursor = connection.cursor()
+
+        # Execute the SQLite dump command
+        with open(output_file, "w") as f:
+            for line in connection.iterdump():
+                f.write(f"{line}\n")
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+        # Print success message
+        typer.echo(f"SQLite database dump created successfully: {output_file}")
+
+    except sqlite3.Error as e:
+        typer.echo(f"Error creating SQLite database dump: {e}")
+
+
+if __name__ == "__main__":
+    app()
