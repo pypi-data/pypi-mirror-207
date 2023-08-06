76e0d5cf3f454b57e070ca6c0828443d731548f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:48 +0000

    Add package dynamiqs/0.1.0/dynamiqs-0.1.0.tar.gz

---
+++ packages/dynamiqs/dynamiqs-0.1.0.tar.gz/dynamiqs-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='dynamiqs',
+    version='0.1.0',
+    description='Coming soon!',
+    author='Pierre Guilmin',
+    author_email='pierreguilmin@gmail.com',
+    url='https://github.com/PierreGuilmin/dynamiqs',
+)
