0108d12b963fbf0f0bcbf3647f1fdbccc7fbb7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:05 +0000

    Add package satanua/0.0.2/satanua-0.0.2-py3-none-any.whl

---
+++ packages/satanua/satanua-0.0.2-py3-none-any.whl/satanua/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class DecoderModes:
+    AND_OR = "AND"
+    OR_AND = "OR"
+
+class LogicGateTypes:
+    AND = "Logic AND"
+    OR = "Logic OR"
+    NOT = "Logic NOT"
+    XOR = "Logic XOR"
+    NOR = "Logic NOR"
+    NAND = "Logic NAND"
