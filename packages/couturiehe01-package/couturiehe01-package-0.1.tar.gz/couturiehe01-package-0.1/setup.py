9424d5fa58bd09b4c9c26b4858344fc4af24f559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:44 +0000

    Add package couturiehe01-package/0.1/couturiehe01-package-0.1.tar.gz

---
+++ packages/couturiehe01-package/couturiehe01-package-0.1.tar.gz/couturiehe01-package-0.1/setup.py
@@ -0,0 +1,13 @@
+# hello.py
+from setuptools import setup, find_packages
+
+setup(
+    name="couturiehe01-package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    author_email="perso@hervecouturier.com",
+    description="A simple example Python package",
+    url="https://github.com/couturiehe01/my_package",
+)
