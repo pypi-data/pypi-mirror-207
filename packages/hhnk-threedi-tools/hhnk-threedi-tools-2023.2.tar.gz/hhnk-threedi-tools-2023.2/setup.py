a299a8593d0253145bb568a560b00b71b920e56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:24 +0000

    Add package hhnk-threedi-tools/2023.2/hhnk-threedi-tools-2023.2.tar.gz

---
+++ packages/hhnk-threedi-tools/hhnk-threedi-tools-2023.2.tar.gz/hhnk-threedi-tools-2023.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hhnk-threedi-tools",
+    version="2023.2",
+    description="HHNK watersystemen analysis tools",
+    url="https://github.com/threedi/hhnk-threedi-tools",
+    author="Wietse van Gerwen, Laure Ravier",
+    author_email="w.vangerwen@hhnk.nl",
+    maintainer="Wietse van Gerwen",
+    project_urls={
+        "Bug Tracker": "https://github.com/threedi/hhnk-threedi-tools/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # package_dir={'':'hhnk_threedi_tools'},
+    packages=find_packages(),
+    # packages=find_packages("", exclude=['tests']),
+    python_requires=">=3.7",
+    # install_requires=["hhnk-research-tools==2023.1", "xarray", "pytest"],
+    # setup_requires=['setuptools_scm'],
+    include_package_data=True,
+)
