1ab55083c1047eecda0d0aa71794f2493b5237c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:37 +0000

    Add package drf-exceptions-hog/0.4.0/drf_exceptions_hog-0.4.0-py3-none-any.whl

---
+++ packages/drf-exceptions-hog/drf_exceptions_hog-0.4.0-py3-none-any.whl/exceptions_hog/settings.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, Optional
+
+from django.conf import settings
+from rest_framework.settings import APISettings
+
+USER_SETTINGS: Optional[Any] = getattr(settings, "EXCEPTIONS_HOG", None)
+
+DEFAULTS: Dict = {
+    "EXCEPTION_REPORTING": "exceptions_hog.handler.exception_reporter",
+    "ENABLE_IN_DEBUG": False,
+    "NESTED_KEY_SEPARATOR": "__",
+    "SUPPORT_MULTIPLE_EXCEPTIONS": False,
+}
+
+# List of settings that may be in string import notation.
+# e.g. `exceptions_hog.exception_handler`
+IMPORT_STRINGS = ("EXCEPTION_REPORTING",)
+
+api_settings: APISettings = APISettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS)
