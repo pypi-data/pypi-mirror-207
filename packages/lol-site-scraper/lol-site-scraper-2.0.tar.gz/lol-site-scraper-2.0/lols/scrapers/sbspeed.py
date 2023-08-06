54ad84e2586c00c728c44e7f8562890f4e98d521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:31 +0000

    Add package lol-site-scraper/2.0/lol-site-scraper-2.0.tar.gz

---
+++ packages/lol-site-scraper/lol-site-scraper-2.0.tar.gz/lol-site-scraper-2.0/lols/scrapers/sbspeed.py
@@ -0,0 +1,6 @@
+# https://sbspeed.com/o2y969ooqiv9.html
+# https://sbspeed.com/na04mcapt1pn.html
+# https://sbspeed.com/xktqqkicemic.html
+# https://sbspeed.com/31lrx47v7bim.html
+# https://sbspeed.com/qhw49ecv68uc.html
+# https://sbspeed.com/681q6ice4nrq.html
