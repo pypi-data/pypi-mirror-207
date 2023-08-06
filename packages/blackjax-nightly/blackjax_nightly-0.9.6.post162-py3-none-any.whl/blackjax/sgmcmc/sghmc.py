8839cc62cec7b37ddf1ba35ce8e1464dd376d2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:44 +0000

    Add package blackjax-nightly/0.9.6.post162/blackjax_nightly-0.9.6.post162-py3-none-any.whl

---
+++ packages/blackjax-nightly/blackjax_nightly-0.9.6.post162-py3-none-any.whl/blackjax/sgmcmc/sghmc.py
@@ -0,0 +1,54 @@
+# Copyright 2020- The Blackjax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Public API for the Stochastic gradient Hamiltonian Monte Carlo kernel."""
+from typing import Callable
+
+import jax
+import jax.numpy as jnp
+
+import blackjax.sgmcmc.diffusions as diffusions
+from blackjax.types import PRNGKey, PyTree
+from blackjax.util import generate_gaussian_noise
+
+__all__ = ["build_kernel"]
+
+
+def build_kernel(alpha: float = 0.01, beta: float = 0) -> Callable:
+    """Stochastic gradient Hamiltonian Monte Carlo (SgHMC) algorithm."""
+    integrator = diffusions.sghmc(alpha, beta)
+
+    def kernel(
+        rng_key: PRNGKey,
+        position: PyTree,
+        grad_estimator: Callable,
+        minibatch: PyTree,
+        step_size: float,
+        num_integration_steps: int,
+        temperature: float = 1.0,
+    ) -> PyTree:
+        def body_fn(state, rng_key):
+            position, momentum = state
+            logdensity_grad = grad_estimator(position, minibatch)
+            position, momentum = integrator(
+                rng_key, position, momentum, logdensity_grad, step_size, temperature
+            )
+            return ((position, momentum), position)
+
+        momentum = generate_gaussian_noise(rng_key, position, 0, jnp.sqrt(step_size))
+        keys = jax.random.split(rng_key, num_integration_steps)
+        (position, momentum), _ = jax.lax.scan(body_fn, (position, momentum), keys)
+
+        return position
+
+    return kernel
