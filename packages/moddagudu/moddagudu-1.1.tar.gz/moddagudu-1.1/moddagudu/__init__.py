6cbd0c3ff596b1ae56135bf25bd8e674919b7277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:10 +0000

    Add package moddagudu/1.1/moddagudu-1.1.tar.gz

---
+++ packages/moddagudu/moddagudu-1.1.tar.gz/moddagudu-1.1/moddagudu/__init__.py
@@ -0,0 +1,1 @@
+from moddagudu import namaste
---
+++ packages/moddagudu/moddagudu-1.1.tar.gz/moddagudu-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='moddagudu',
+    version='1.1',
+    description='Naa First package idhi...mast undi ga?',
+    packages=['moddagudu'],
+)
