6a9edb71f6553c2a3e9f998c5c25ee8418663c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:56 +0000

    Add package goatl/0.6.5/goatl-0.6.5.tar.gz

---
+++ packages/goatl/goatl-0.6.5.tar.gz/goatl-0.6.5/src/goatl/__init__.py
@@ -0,0 +1,24 @@
+# type: ignore[attr-defined]
+"""The goat logger"""
+
+import sys
+from .core import log
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
+
+__all__ = ["log"]
+
+log.info
