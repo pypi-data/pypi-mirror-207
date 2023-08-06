f5dee2552146c60ba1b80b73690815f0b83b4adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:27 +0000

    Add package vijay-access-control/0.2/vijay-access_control-0.2.tar.gz

---
+++ packages/vijay-access-control/vijay-access_control-0.2.tar.gz/vijay-access_control-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vijay-access_control',
+    version='0.2',
+    description='s3 access control package',
+    packages=['access_control'],
+    install_requires=[],
+)
+
