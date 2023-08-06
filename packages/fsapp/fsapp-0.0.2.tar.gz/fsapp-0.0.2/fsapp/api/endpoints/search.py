e446873f05a13498cf1c46d192322a0d2927196b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:19 +0000

    Add package fsapp/0.0.2/fsapp-0.0.2.tar.gz

---
+++ packages/fsapp/fsapp-0.0.2.tar.gz/fsapp-0.0.2/fsapp/api/endpoints/search.py
@@ -0,0 +1,43 @@
+"""Марштруты для запроса данных и вспомогательных действий"""
+
+from fastapi import APIRouter, Depends, HTTPException
+from typing import Tuple, Union
+
+from .utils import make_response
+from .auth import oauth2_scheme
+
+from ...core.config import settings
+from ...api.schemas.requests import RequestBody
+from ...api.schemas.responses import ResponseBody
+from ...core.handlers import handlers
+
+router = APIRouter()
+
+
+@router.get("/")
+async def hello():
+    return {"message": f'Hello {settings.required.instance}'}
+
+
+# todo try\exept
+@router.post("/api/v1/search", response_model=ResponseBody)
+async def get_search_data(request: RequestBody, token=Depends(oauth2_scheme)):
+    print(handlers.instances)
+    if token != settings.token:
+        return HTTPException(status_code=503, )
+    # Получаем тип передаваемых данных
+    data_type = request.search.parameters[0].type
+    # Значения, переданные для поиска
+    values: list = request.search.parameters[0].value
+    # Смотрим, есть ли обработчик именно этого класса
+    if (search_class := handlers.get(data_type)) is not None:
+        try:
+            # Создание экземляра Вашего класса и передача ему параметров
+            searcher = handlers.get_instance(search_class)
+            # Передаем результат в функцию подготовки ответа и возвращаем ResponseBody
+            success, data = await searcher.execute_search(data_type, values)
+            return make_response(success, data)
+        except Exception as error:
+            return make_response(False, {"error": error.__str__()})
+    else:
+        return make_response(False, {"error": "Нет обработчиков для данной операции"})
