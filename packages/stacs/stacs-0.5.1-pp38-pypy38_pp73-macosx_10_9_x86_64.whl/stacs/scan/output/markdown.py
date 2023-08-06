67a715ec0e3586c7f6c68405d494cf5182cda7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:55 +0000

    Add package stacs/0.5.1/stacs-0.5.1-pp38-pypy38_pp73-macosx_10_9_x86_64.whl

---
+++ packages/stacs/stacs-0.5.1-pp38-pypy38_pp73-macosx_10_9_x86_64.whl/stacs/scan/output/markdown.py
@@ -0,0 +1,13 @@
+"""Defines a markdown output handler for STACS.
+
+SPDX-License-Identifier: BSD-3-Clause
+"""
+
+from typing import List
+
+from stacs.scan import model
+from stacs.scan.exceptions import NotImplementedException
+
+
+def render(findings: List[model.finding.Entry], pack: model.pack.Format) -> str:
+    raise NotImplementedException("Markdown output not yet implemented, sorry!")
