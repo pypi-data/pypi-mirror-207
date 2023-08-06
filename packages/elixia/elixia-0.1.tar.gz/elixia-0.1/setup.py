5f85de47f1ab6cb42973806593dcf8e11ad50f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:21 +0000

    Add package elixia/0.1/elixia-0.1.tar.gz

---
+++ packages/elixia/elixia-0.1.tar.gz/elixia-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elixia',
+    version='0.1',
+    description='A short description of your package',
+    packages=find_packages(),
+    install_requires=[
+        # Add other dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
