de2a2f91f257bfda5baf4734b92ce785b27b3a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:21 +0000

    Add package weao/1.0.0/weao-1.0.0-py3-none-any.whl

---
+++ packages/weao/weao-1.0.0-py3-none-any.whl/weao/constants.py
@@ -0,0 +1,38 @@
+"""     
+__      _____  __ _  ___  
+\ \ /\ / / _ \/ _` |/ _ \ 
+ \ V  V /  __/ (_| | (_) |
+  \_/\_/ \___|\__,_|\___/ 
+                          
+"""
+
+from typing import Dict, List, Final, Tuple, final
+
+from yarl import URL
+
+__all__: Tuple[str, ...] = ("API",)
+
+@final
+class API:
+    """
+    Class Representing Constants For API
+    """
+    BASE_URL: Final[URL] = URL("https://api.whatexploitsare.online")
+    ALTERNATIVE_BASE_URL: Final[URL] = URL("https://api.weao.dev")
+    HEADERS: Final[Dict[str, str]] = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.50",
+        "content-type": "application/json",
+    }
+    Exploits: Final[List[str]] = ["Synapse",
+                            "Script-Ware",
+                            "KRNL",
+                            "DX9WARE",
+                            "Electron",
+                            "WeAreDevs API",
+                            "Oxygen",
+                            "Fluxus",
+                            "Celestial",
+                            "Comet",
+                            "Elexe",
+    ]
+    LOG_LIMIT: Final[int] = 50
---
+++ packages/weao/weao-1.0.0-py3-none-any.whl/weao/errors.py
@@ -0,0 +1,57 @@
+"""     
+__      _____  __ _  ___  
+\ \ /\ / / _ \/ _` |/ _ \ 
+ \ V  V /  __/ (_| | (_) |
+  \_/\_/ \___|\__,_|\___/ 
+                          
+"""
+
+from typing import Tuple
+
+__all__: Tuple[str, ...] = (
+    "WEAOException",
+    "UnsupportedExploit",
+    "ExploitNotFound",
+    "StatusUnavailable",
+    "LogsUnavailable",
+)
+
+class WEAOException(Exception):
+    """Generic API Exception For WEAO
+
+    Attributes
+    -----------
+    message: :class:`str`
+        The messages of this request
+    """
+
+    def __init__(self, message: str) -> None:
+        self.message: str = message
+        super().__init__(message)
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} message={self.message}>"
+    
+class UnsupportedExploit(WEAOException):
+    """Raised When A Executor Is Unsupported"""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message=message)
+
+class ExploitNotFound(WEAOException):
+    """Raised When A Exploit Couldn't Be Found"""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message=message)
+
+class StatusUnavailable(WEAOException):
+    """Raised When Status Call Failed"""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message=message)
+
+class LogsUnavailable(WEAOException):
+    """Raised When Logs Call Failed"""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message=message)
