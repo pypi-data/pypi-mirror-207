66f7afe866e08ca1538b0ac84226a725db52c724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:56 +0000

    Add package tracer-spans-python/0.0.1/tracer_spans_python-0.0.1.tar.gz

---
+++ packages/tracer-spans-python/tracer_spans_python-0.0.1.tar.gz/tracer_spans_python-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'tracer_spans_python',
+    version = '0.0.1',
+    author = 'Otávio Mascarenhas',
+    author_email = 'otaviomascarenhaspessoal@gmail.com',
+    packages = ['tracers_spans'],
+    description = 'Projeto para facilitar a criação e envios de tracers e spans.',
+    license = 'MIT',
+    keywords = 'Tracers Spans tracers spans tracer span OpenTelemetry Zipkin',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
+    )
