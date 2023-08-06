fb3332e71452af1f64ea52a68fd9df9fee6061c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:21 +0000

    Add package robusta-cli/0.10.17a1/robusta_cli-0.10.17a1-py3-none-any.whl

---
+++ packages/robusta-cli/robusta_cli-0.10.17a1-py3-none-any.whl/robusta/core/exceptions.py
@@ -0,0 +1,8 @@
+class PrometheusNotFound(Exception):
+    """Exception, when Prometheus cannot be reached"""
+
+    pass
+class AlertsManagerNotFound(Exception):
+    """Exception, when Prometheus cannot be reached"""
+
+    pass
