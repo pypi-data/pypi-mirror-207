1c5cc3b35a56dd3facb1162c21cbb9a47173c348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:54 +0000

    Add package noisepy-seis/0.8.1/noisepy_seis-0.8.1-py3-none-any.whl

---
+++ packages/noisepy-seis/noisepy_seis-0.8.1-py3-none-any.whl/noisepy/seis/main.py
@@ -0,0 +1,226 @@
+import argparse
+import os
+import typing
+from enum import Enum
+from typing import Any, Callable, List
+
+import obspy
+
+from .asdfstore import ASDFCCStore, ASDFRawDataStore
+from .channelcatalog import CSVChannelCatalog, XMLStationChannelCatalog
+from .constants import DATE_FORMAT_HELP, STATION_FILE
+from .datatypes import Channel, ConfigParameters
+from .S0A_download_ASDF_MPI import download
+from .S1_fft_cc_MPI import cross_correlate
+from .S2_stacking import stack
+from .scedc_s3store import SCEDCS3DataStore
+from .utils import fs_join, get_filesystem
+
+# Utility running the different steps from the command line. Defines the arguments for each step
+
+default_start_date = "2016_07_01_0_0_0"  # start date of download
+default_end_date = "2016_07_02_0_0_0"  # end date of download
+default_data_path = "Documents/SCAL"
+
+
+class Step(Enum):
+    DOWNLOAD = 1
+    CROSS_CORRELATE = 2
+    STACK = 3
+    ALL = 4
+
+
+def valid_date(d: str) -> str:
+    _ = obspy.UTCDateTime(d)
+    return d
+
+
+def initialize_fft_params(raw_dir: str) -> ConfigParameters:
+    params = ConfigParameters()
+    dfile = fs_join(raw_dir, "download_info.txt")
+    if os.path.isfile(dfile):
+        down_info = eval(open(dfile).read())  # TODO: do proper json/yaml serialization
+        params.samp_freq = down_info["samp_freq"]
+        params.freqmin = down_info["freqmin"]
+        params.freqmax = down_info["freqmax"]
+        params.start_date = down_info["start_date"]
+        params.end_date = down_info["end_date"]
+        params.inc_hours = down_info["inc_hours"]
+        params.ncomp = down_info["ncomp"]
+    return params
+
+
+def get_channel_filter(sta_list: List[str]) -> Callable[[Channel], bool]:
+    if len(sta_list) == 1 and sta_list[0] == "*":
+        return lambda ch: True
+    else:
+        stations = set(sta_list)
+        return lambda ch: ch.station.name in stations
+
+
+def create_raw_store(raw_dir: str, sta_list: List[str], xml_path: str):
+    fs = get_filesystem(raw_dir)
+
+    def count(pat):
+        return len(fs.glob(fs_join(raw_dir, pat)))
+
+    # Use heuristics around which store to use by the files present
+    if count("*.h5") > 0:
+        return ASDFRawDataStore(raw_dir)
+    else:
+        assert count("*.ms") > 0 or count("*.sac") > 0, f"Can not find any .h5, .ms or .sac files in {raw_dir}"
+        if xml_path is not None:
+            catalog = XMLStationChannelCatalog(xml_path)
+        elif os.path.isfile(os.path.join(raw_dir, STATION_FILE)):
+            catalog = CSVChannelCatalog(raw_dir)
+        else:
+            raise ValueError(f"Either an --xml_path argument or a {STATION_FILE} must be provided")
+
+        return SCEDCS3DataStore(raw_dir, catalog, get_channel_filter(sta_list))
+
+
+def main(args: typing.Any):
+    raw_dir = args.raw_data_path
+
+    def get_raw_store():
+        stations = args.stations if "stations" in args else []
+        xml_path = args.xml_path if "xml_path" in args else None
+        return create_raw_store(raw_dir, stations, xml_path)
+
+    def run_cross_correlation():
+        ccf_dir = args.ccf_path
+        fft_params = initialize_fft_params(raw_dir)
+        fft_params.freq_norm = args.freq_norm
+        cc_store = ASDFCCStore(ccf_dir)
+        raw_store = get_raw_store()
+        cross_correlate(raw_store, fft_params, cc_store)
+
+    def run_download():
+        params = ConfigParameters()
+        params.start_date = args.start
+        params.end_date = args.end
+        params.inc_hours = args.inc_hours
+        download(args.raw_data_path, args.channels, args.stations, params)
+
+    if args.step == Step.DOWNLOAD:
+        run_download()
+    if args.step == Step.CROSS_CORRELATE:
+        run_cross_correlation()
+    if args.step == Step.STACK:
+        raw_store = get_raw_store()
+        stack(raw_store.get_station_list(), args.ccf_path, args.stack_path, args.method)
+    if args.step == Step.ALL:
+        run_download()
+        run_cross_correlation()
+        raw_store = get_raw_store()
+        stack(raw_store.get_station_list(), args.ccf_path, args.stack_path, args.method)
+
+
+def add_download_args(down_parser: argparse.ArgumentParser):
+    down_parser.add_argument(
+        "--start",
+        type=valid_date,
+        required=True,
+        help="Start date in the format: " + DATE_FORMAT_HELP,
+        default=default_start_date,
+    )
+    down_parser.add_argument(
+        "--end",
+        type=valid_date,
+        required=True,
+        help="End date in the format: " + DATE_FORMAT_HELP,
+        default=default_end_date,
+    )
+    down_parser.add_argument(
+        "--channels",
+        type=lambda s: s.split(","),
+        help="Comma separated list of channels",
+        default="BHE,BHN,BHZ",
+    )
+    down_parser.add_argument("--inc_hours", type=int, default=24, help="Time increment size (hrs)")
+
+
+def add_stations_arg(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--stations",
+        type=lambda s: s.split(","),
+        help="Comma separated list of stations or '*' for all",
+        default="*",
+    )
+
+
+def add_path(parser, prefix: str):
+    parser.add_argument(
+        f"--{prefix}_path",
+        type=str,
+        default=os.path.join(os.path.join(os.path.expanduser("~"), default_data_path), prefix.upper()),
+        help=f"Directory for {prefix} data",
+    )
+
+
+def add_paths(parser, types: List[str]):
+    for t in types:
+        add_path(parser, t)
+
+
+def add_cc_args(parser):
+    parser.add_argument("--freq_norm", choices=["rma", "no", "phase_only"], default="rma")
+    parser.add_argument("--xml_path", required=False, default=None)
+
+
+def add_stack_args(parser):
+    parser.add_argument(
+        "--method",
+        type=str,
+        required=True,
+        choices=[
+            "linear",
+            "pws",
+            "robust",
+            "nroot",
+            "selective",
+            "auto_covariance",
+            "all",
+        ],
+        help="Stacking method",
+    )
+
+
+def make_step_parser(subparsers: Any, step: Step, parser_config_funcs: List[Callable[[argparse.ArgumentParser], None]]):
+    parser = subparsers.add_parser(
+        step.name.lower(),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    for config_fn in parser_config_funcs:
+        config_fn(parser)
+
+
+def main_cli():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="step", required=True)
+    make_step_parser(
+        subparsers, Step.DOWNLOAD, [lambda p: add_paths(p, ["raw_data"]), add_download_args, add_stations_arg]
+    )
+    make_step_parser(
+        subparsers, Step.CROSS_CORRELATE, [lambda p: add_paths(p, ["raw_data", "ccf"]), add_cc_args, add_stations_arg]
+    )
+    make_step_parser(subparsers, Step.STACK, [lambda p: add_paths(p, ["raw_data", "stack", "ccf"]), add_stack_args])
+    make_step_parser(
+        subparsers,
+        Step.ALL,
+        [
+            lambda p: add_paths(p, ["raw_data", "ccf", "stack"]),
+            add_download_args,
+            add_cc_args,
+            add_stack_args,
+            add_stations_arg,
+        ],
+    )
+
+    args = parser.parse_args()
+    args.step = Step[args.step.upper()]
+    main(args)
+
+
+if __name__ == "__main__":
+    main_cli()
---
+++ packages/noisepy-seis/noisepy_seis-0.8.1-py3-none-any.whl/noisepy/seis/scedc_s3store.py
@@ -0,0 +1,121 @@
+import logging
+import os
+import re
+from datetime import datetime, timedelta, timezone
+from typing import Callable, List
+
+import obspy
+from datetimerange import DateTimeRange
+
+from noisepy.seis.channelcatalog import ChannelCatalog
+from noisepy.seis.stores import RawDataStore
+
+from .datatypes import Channel, ChannelData, ChannelType, Station
+from .utils import fs_join, get_filesystem
+
+logger = logging.getLogger(__name__)
+
+
+def channel_filter(stations: List[str], ch_prefix: str) -> Callable[[Channel], bool]:
+    """
+    Helper function for creating a channel filter to be used in the constructor of the store.
+    This filter uses a list of allowed station name along with a channel filter prefix.
+    """
+    sta_set = set(stations)
+
+    def filter(ch: Channel) -> bool:
+        return ch.station.name in sta_set and ch.type.name.lower().startswith(ch_prefix.lower())
+
+    return filter
+
+
+class SCEDCS3DataStore(RawDataStore):
+    """
+    A data store implementation to read from a directory of miniSEED (.ms) files from the SCEDC S3 bucket.
+    Every directory is a a day and each .ms file contains the data for a channel.
+    """
+
+    # TODO: Support reading directly from the S3 bucket
+
+    # for checking the filename has the form: CIGMR__LHN___2022002.ms
+    file_re = re.compile(r".*[0-9]{7}\.ms$", re.IGNORECASE)
+
+    def __init__(self, path: str, chan_catalog: ChannelCatalog, channel_filter: Callable[[Channel], bool] = None):
+        """
+        Parameters:
+            path: path to look for ms files. Can be a local file directory or an s3://... url path
+            chan_catalog: ChannelCatalog to retrieve inventory information for the channels
+            channel_filter: Function to decide whether a channel should be used or not,
+                            if None, all channels are used
+        """
+        super().__init__()
+        self.fs = get_filesystem(path)
+        self.path = path
+        self.channel_catalog = chan_catalog
+        if channel_filter is None:
+            channel_filter = lambda s: True  # noqa: E731
+
+        msfiles = [f for f in self.fs.glob(fs_join(path, "*.ms")) if self.file_re.match(f) is not None]
+        # store a dict of {timerange: list of channels}
+        self.channels = {}
+        timespans = []
+        for f in msfiles:
+            timespan = SCEDCS3DataStore._parse_timespan(f)
+            channel = SCEDCS3DataStore._parse_channel(os.path.basename(f))
+            if not channel_filter(channel):
+                continue
+            key = str(timespan)  # DataTimeFrame is not hashable
+            if key not in self.channels:
+                timespans.append(timespan)
+                self.channels[key] = [channel]
+            else:
+                self.channels[key].append(channel)
+        logger.info(
+            f"Init: {len(self.channels)} timespans and {sum(len(ch) for ch in  self.channels.values())} channels"
+        )
+
+    def get_channels(self, timespan: DateTimeRange) -> List[Channel]:
+        tmp_channels = self.channels.get(str(timespan), [])
+        return list(map(lambda c: self.channel_catalog.get_full_channel(timespan, c), tmp_channels))
+
+    def get_timespans(self) -> List[DateTimeRange]:
+        return list([DateTimeRange.from_range_text(d) for d in sorted(self.channels.keys())])
+
+    def read_data(self, timespan: DateTimeRange, chan: Channel) -> ChannelData:
+        # reconstruct the file name from the channel parameters
+        chan_str = (
+            f"{chan.station.network}{chan.station.name.ljust(5, '_')}{chan.type.name}"
+            f"{chan.station.location.ljust(3, '_')}"
+        )
+        filename = fs_join(self.path, f"{chan_str}{timespan.start_datetime.strftime('%Y%j')}.ms")
+        if not self.fs.exists(filename):
+            logger.warning(f"Could not find file {filename}")
+            return ChannelData.empty()
+
+        with self.fs.open(filename) as f:
+            stream = obspy.read(f)
+        return ChannelData(stream)
+
+    def get_inventory(self, timespan: DateTimeRange, station: Station) -> obspy.Inventory:
+        return self.channel_catalog.get_inventory(timespan, station)
+
+    def _parse_timespan(filename: str) -> DateTimeRange:
+        # The SCEDC S3 bucket stores files in the form: CIGMR__LHN___2022002.ms
+        year = int(filename[-10:-6])
+        day = int(filename[-5:-3])
+        jan1 = datetime(year, 1, 1, tzinfo=timezone.utc)
+        return DateTimeRange(jan1 + timedelta(days=day - 1), jan1 + timedelta(days=day))
+
+    def _parse_channel(filename: str) -> Channel:
+        # e.g.
+        # CIGMR__LHN___2022002
+        # CE13884HNZ10_2022002
+        network = filename[:2]
+        station = filename[2:7].rstrip("_")
+        channel = filename[7:10]
+        location = filename[10:12].strip("_")
+        return Channel(
+            ChannelType(channel, location),
+            # lat/lon/elev will be populated later
+            Station(network, station, -1, -1, -1, location),
+        )
