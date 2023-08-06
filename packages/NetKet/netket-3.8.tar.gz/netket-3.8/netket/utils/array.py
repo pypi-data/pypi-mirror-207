a47235ae84ef9d1694ab83b6e50975cab625e3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:27 +0000

    Add package NetKet/3.8/netket-3.8.tar.gz

---
+++ packages/NetKet/netket-3.8.tar.gz/netket-3.8/netket/utils/array.py
@@ -0,0 +1,74 @@
+# Copyright 2021 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+from netket.utils import module_version
+
+from .types import Array, DType, Shape
+from .struct import dataclass
+
+# TODO keep only  jax>=0.4 is required
+if module_version("jax") >= (0, 4, 0):
+    import jax
+
+    JaxArray = jax.Array
+else:
+    # pre jax 0.4
+    import jaxlib
+
+    JaxArray = jaxlib.xla_extension.DeviceArray
+
+
+@dataclass(cache_hash=True)
+class HashableArray:
+    """
+    This class wraps a numpy or jax array in order to make it hashable and
+    equality comparable (which is necessary since a well-defined hashable object
+    needs to satisfy :code:`obj1 == obj2` whenever :code:`hash(obj1) == hash(obj2)`.
+
+    The underlying array can also be accessed using :code:`numpy.asarray(self)`.
+    """
+
+    wrapped: Array
+    """The wrapped array. Note that this array is read-only."""
+
+    def __pre_init__(self, wrapped):
+        if isinstance(wrapped, HashableArray):
+            wrapped = wrapped.wrapped
+        else:
+            if isinstance(wrapped, JaxArray):
+                # __array__ only works if it's a numpy array.
+                wrapped = np.array(wrapped)
+            else:
+                wrapped = wrapped.copy()
+            if isinstance(wrapped, np.ndarray):
+                wrapped.flags.writeable = False
+
+        return (wrapped,), {}
+
+    def __hash__(self):
+        return hash(self.wrapped.tobytes())
+
+    def __eq__(self, other):
+        return np.all(self.wrapped == other.wrapped)
+
+    def __array__(self, dtype: DType = None):
+        if dtype is None:
+            dtype = self.wrapped.dtype
+        return self.wrapped.__array__(dtype)
+
+    @property
+    def shape(self) -> Shape:
+        return self.wrapped.shape
---
+++ packages/NetKet/netket-3.8.tar.gz/netket-3.8/netket/utils/types.py
@@ -0,0 +1,43 @@
+# Copyright 2021 The NetKet Authors - All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Sequence, Callable, Union
+
+import optax as _optax
+import jax as _jax
+import jaxlib as _jaxlib
+import numpy as _np
+
+PRNGKeyT = Any
+SeedT = Union[int, PRNGKeyT]
+
+Shape = Sequence[int]
+DType = Any  # this could be a real type?
+
+if not hasattr(_jax, "Array"):
+    # pre jax 0.4
+    Array = Union[_np.ndarray, _jaxlib.xla_extension.DeviceArray, _jax.core.Tracer]
+else:
+    # TODO keep only this after jax>=0.4 is required
+    Array = Union[_np.ndarray, _jax.Array]
+
+ArrayLike = Any  # Objects that are valid inputs to (np|jnp).asarray.
+
+NNInitFunc = Callable[[PRNGKeyT, Shape, DType], Array]
+
+PyTree = Any
+
+Scalar = Any
+
+ScalarOrSchedule = Union[Scalar, _optax.Schedule]
