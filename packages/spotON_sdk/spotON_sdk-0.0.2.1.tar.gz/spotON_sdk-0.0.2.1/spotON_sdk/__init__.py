ae0b5fa197eadbee054ab0e1a490f03834d2b0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:46 +0000

    Add package spotON_sdk/0.0.2.1/spotON_sdk-0.0.2.1.tar.gz

---
+++ packages/spotON_sdk/spotON_sdk-0.0.2.1.tar.gz/spotON_sdk-0.0.2.1/spotON_sdk/__init__.py
@@ -0,0 +1,15 @@
+"""
+The spotON_sdk package provides tools for interacting with the spotON API.
+
+This package includes modules for authenticating with the API, sending requests,
+and processing responses. It also includes utility functions for working with
+the data returned by the API.
+"""
+
+from entsoe import Area
+
+from .constants.markets import *
+from .logic.Config import *
+
+__version__ = "0.0.2.1"
+__all__ = [name for name in dir() if not name.startswith('_')]
---
+++ packages/spotON_sdk/spotON_sdk-0.0.2.1.tar.gz/spotON_sdk-0.0.2.1/tests/test.py
@@ -0,0 +1,12 @@
+import spotON_sdk
+
+
+
+
+print (spotON_sdk.Markets())
+print (spotON_sdk.Markets().get_Market_by_area_code("AT").country.emoji)
+
+for country in spotON_sdk.Markets().markets_List:
+    print (country.name)
+
+print (spotON_sdk.bidding_zones["SE_1"]["cities"])
