9cac8b5b70830e68bd9e694f431844fc0a3f2902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:16 +0000

    Add package simpleparallelize/0.1.2/simpleparallelize-0.1.2.tar.gz

---
+++ packages/simpleparallelize/simpleparallelize-0.1.2.tar.gz/simpleparallelize-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['simpleparallelize']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tqdm>=4.65.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'simpleparallelize',
+    'version': '0.1.2',
+    'description': 'A package that makes it easy to parallelize for loops',
+    'long_description': None,
+    'author': 'ericaleman',
+    'author_email': 'mail@ericaleman.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
