f954766d69338cee0cebfb68ab611fbb2a268603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package st-microservice/0.14.0a2/st_microservice-0.14.0a2-py3-none-any.whl

---
+++ packages/st-microservice/st_microservice-0.14.0a2-py3-none-any.whl/st_microservice/celery_app.py
@@ -0,0 +1,19 @@
+import celery
+from redis import from_url
+from st_microservice.general_utils import get_required_env
+
+
+BROKER_URI = get_required_env('BROKER_URI')
+redis = from_url(BROKER_URI, decode_responses=True)
+
+
+def get_send_message_function(task_self: celery.Task):
+    def send_message(message: str):
+        redis.publish(f'task-updates-{task_self}', message)
+
+    return send_message
+
+
+app = celery.Celery('tasks', broker=BROKER_URI, backend=BROKER_URI)
+app.conf.task_serializer = 'pickle'
+app.conf.accept_content = ['application/x-python-serialize']
