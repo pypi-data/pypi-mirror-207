4e614439fbcb11ed62a82df7f94995ae4ba2882a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:01 +0000

    Add package gayadsdb6969/1.0.0/gayadsdb6969-1.0.0.tar.gz

---
+++ packages/gayadsdb6969/gayadsdb6969-1.0.0.tar.gz/gayadsdb6969-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gayadsdb6969',
+    version='1.0.0',
+    description='sql db made simply easy with essential features needed for db users',
+    author='Pablo62050',
+    author_email='iesmks69@gmail.com',
+    packages=['gayadsdb'],
+    install_requires=[
+        'sqlite3'
+    ],
+)
