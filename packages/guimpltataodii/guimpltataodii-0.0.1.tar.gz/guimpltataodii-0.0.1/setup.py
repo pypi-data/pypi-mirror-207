7ff4fe40b5794f6400afd51d5e998764f1ef92e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:34 +0000

    Add package guimpltataodii/0.0.1/guimpltataodii-0.0.1.tar.gz

---
+++ packages/guimpltataodii/guimpltataodii-0.0.1.tar.gz/guimpltataodii-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "a test"
+LONG_DESCRIPTION = "a test but long"
+
+# Setting up
+setup(
+    name="guimpltataodii",
+    version=VERSION,
+    author="testGUIMPL",
+    author_email="testmail@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(where="./libname"),
+    install_requires=["matplotlib"],
+    keywords=["python", "video", "stream", "video stream", "camera stream", "sockets"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
