8513afcdaae50283926b7bb0f7bc574db52a31f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:22:11 +0000

    Add package hail/0.2.116/hail-0.2.116-py3-none-any.whl

---
+++ packages/hail/hail-0.2.116-py3-none-any.whl/hailtop/hail_logging.py
@@ -0,0 +1,40 @@
+import logging
+import sys
+from pythonjsonlogger import jsonlogger
+from aiohttp.abc import AbstractAccessLogger
+import datetime
+from time import timezone
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super().add_fields(log_record, record, message_dict)
+        # GCP Logging expects `severity` but jsonlogger uses `levelname`
+        log_record['severity'] = record.levelname
+        log_record['funcNameAndLine'] = f"{record.funcName}:{record.lineno}"
+        log_record['hail_log'] = 1
+
+
+def configure_logging():
+    fmt = CustomJsonFormatter('%(severity)s %(levelname)s %(asctime)s %(filename)s %(funcNameAndLine)s %(message)s')
+
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(fmt)
+
+    logging.basicConfig(handlers=[stream_handler], level=logging.INFO)
+
+
+class AccessLogger(AbstractAccessLogger):
+    def log(self, request, response, time):
+        tz = datetime.timezone(datetime.timedelta(seconds=-timezone))
+        now = datetime.datetime.now(tz)
+        start_time = now - datetime.timedelta(seconds=time)
+        start_time_str = start_time.strftime('[%d/%b/%Y:%H:%M:%S %z]')
+        self.logger.info(f'{request.scheme} {request.method} {request.path} '
+                         f'done in {time}s: {response.status}',
+                         extra={'remote_address': request.remote,
+                                'request_start_time': start_time_str,
+                                'request_duration': time,
+                                'response_status': response.status,
+                                'x_real_ip': request.headers.get("X-Real-IP")})
