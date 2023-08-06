7c29ebdf9075368cf65e4f5831d3b4aff07e66d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:34 +0000

    Add package minder-downloader/0.1.10/minder_downloader-0.1.10.tar.gz

---
+++ packages/minder-downloader/minder_downloader-0.1.10.tar.gz/minder_downloader-0.1.10/minder_downloader/upload.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+from .utils import BearerAuth, load_yaml
+import os 
+from .config import check_config
+
+
+
+def setup() -> tuple:
+    # Load credentials and server information from YAML file
+    check_config()
+    ROOT = os.environ.get('MINDER_DOWNLOADER_HOME', Path(__file__).parent)
+    INFO_PATH = f'{ROOT}{os.sep}info.yaml'
+    os.environ['MINDER_TOKEN'] = load_yaml(INFO_PATH)['token']
+    server = load_yaml(INFO_PATH)['server'] + '/export'
+    auth = BearerAuth(os.getenv('MINDER_TOKEN'))
+    return server,auth
+
+
+def upload_file(file_2_upload):
+    server,auth = setup()
+    file = open(f"{file_2_upload}", "rb")
+    binary_data = file.read()
+    requests.put(f'https://research.minder.care/api/reports/{file_2_upload}',
+                        data=binary_data,
+                        headers={'content-type': 'text/html'},
+                        auth=AUTH)
+    r1 = requests.get('https://research.minder.care/api/reports', auth=auth)
+    r1 = pd.Series(r1.json())
+    return f'https://research.minder.care/{r1[r1.str.contains(file_2_upload)].values[0]}'
