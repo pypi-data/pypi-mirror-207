0b0b9a29aff635e82d4d81962f4a44511ef375ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:16 +0000

    Add package deale-library/0.0.1/deale_library-0.0.1-py3-none-any.whl

---
+++ packages/deale-library/deale_library-0.0.1-py3-none-any.whl/deale-library/DynamoDB.py
@@ -0,0 +1,38 @@
+import boto3
+from boto3.dynamodb.conditions import Attr, Key
+
+
+class DynamoDBHandler:
+    """
+    A class to handle operations on AWS DynamoDB tables.
+    """
+    
+    def __init__(self):
+        """
+        Initialize a DynamoDB resource for a specific AWS region.
+
+        :param region_name: The name of the AWS region.
+        """
+        self.dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+
+    def scan(self, table_name, **kwargs):
+        """
+        Scan a DynamoDB table.
+
+        :param table_name: The name of the DynamoDB table.
+        :param **kwargs: Any keyword arguments accepted by the boto3 Table.scan() method.
+        :return: The response from the scan operation.
+        """
+        table = self.dynamodb.Table(table_name)
+        return table.scan(**kwargs)
+
+    def query(self, table_name, **kwargs):
+        """
+        Query a DynamoDB table.
+
+        :param table_name: The name of the DynamoDB table.
+        :param **kwargs: Any keyword arguments accepted by the boto3 Table.query() method.
+        :return: The response from the query operation.
+        """
+        table = self.dynamodb.Table(table_name)
+        return table.query(**kwargs)
