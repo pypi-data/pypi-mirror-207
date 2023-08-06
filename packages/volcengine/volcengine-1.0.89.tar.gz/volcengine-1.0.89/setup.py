8cb6f8654cda09a8a9824972fd69d6ad44a749fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:56 +0000

    Add package volcengine/1.0.89/volcengine-1.0.89.tar.gz

---
+++ packages/volcengine/volcengine-1.0.89.tar.gz/volcengine-1.0.89/setup.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+import sys
+
+from setuptools import setup, find_packages
+from volcengine import VERSION
+
+install_requires = ["requests", "retry", "pytz", "pycryptodome", "protobuf", "google", "six"]
+
+# 在Windows平台上不安装不兼容的第三方库
+if sys.platform in ("win32", "cygwin"):
+    install_requires.remove("lz4a")
+
+setup(
+    name="volcengine",
+    version=VERSION,
+    keywords=("pip", "volcengine", "volc-sdk-python"),
+    description="The Volcengine SDK for Python",
+    license="MIT Licence",
+
+    url="https://github.com/Volcengine/volc-sdk-python",
+    author="Volcengine SDK",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=install_requires
+)
