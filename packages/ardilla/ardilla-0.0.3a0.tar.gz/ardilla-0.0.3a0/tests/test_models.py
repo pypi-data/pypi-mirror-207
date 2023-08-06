64757280a99031c389e42b0ebbe5ad6da6a9cc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:13 +0000

    Add package ardilla/0.0.3a0/ardilla-0.0.3a0.tar.gz

---
+++ packages/ardilla/ardilla-0.0.3a0.tar.gz/ardilla-0.0.3a0/tests/test_models.py
@@ -0,0 +1,25 @@
+from ardilla.models import Model
+
+
+class User(Model):
+    id: int
+    name: str
+
+
+tablename = "user"
+schema = """
+CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+"""
+
+
+def test_default_tablename():
+    assert (
+        User.__tablename__ == "user"
+    ), f"wrong default tablename {User.__tablename__} != {tablename}"
+
+
+def test_defaul_schema():
+    assert User.__schema__.strip() == schema.strip()
