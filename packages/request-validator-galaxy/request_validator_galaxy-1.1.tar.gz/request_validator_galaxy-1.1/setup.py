fa9a0afe96c1b48076b8e0ec196ff598f70c0ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package request-validator-galaxy/1.1/request_validator_galaxy-1.1.tar.gz

---
+++ packages/request-validator-galaxy/request_validator_galaxy-1.1.tar.gz/request_validator_galaxy-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='request_validator_galaxy',
+    version='1.1',
+    license='',
+    author="Preetam Pawar",
+    author_email='preetam.pawar@galaxyweblinks.in',
+    packages=find_packages('pk_src'),
+    package_dir={'': 'pk_src'},
+    url='',
+    keywords='galaxy project',
+    install_requires=[
+          'flask',
+      ],
+
+)
