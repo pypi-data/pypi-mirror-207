acff4ff4b787d376ed266963a24a797b4997fbe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:45 +0000

    Add package dogfood-logger/0.1.3/dogfood-logger-0.1.3.tar.gz

---
+++ packages/dogfood-logger/dogfood-logger-0.1.3.tar.gz/dogfood-logger-0.1.3/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dogfood-logger',
+    version='0.1.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Logger for Grafana',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/dogfoodhq/dogfood-logger',
+    author='Matt Wong',
+    author_email='nycmattw@gmail.com'
+)
---
+++ packages/dogfood-logger/dogfood-logger-0.1.3.tar.gz/dogfood-logger-0.1.3/tests/test.py
@@ -0,0 +1,5 @@
+def test_fahrToKelv():
+    '''
+    make sure freezing is calculated correctly
+    '''
+    assert 273.15 == 273.15, 'incorrect freezing point!'
