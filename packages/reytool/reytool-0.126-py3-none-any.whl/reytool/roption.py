0e9e200ab425b95bc60c314ba69c58577d3b6d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:19 +0000

    Add package reytool/0.126/reytool-0.126-py3-none-any.whl

---
+++ packages/reytool/reytool-0.126-py3-none-any.whl/reytool/roption.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2023-02-18 19:27:04
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Method options.
+"""
+
+
+from typing import List, Literal
+
+
+# Default width of method rprint.
+print_width: int = 100
+
+# Default frame type of method rprint.
+print_default_frame_full: Literal["full", "half", "plain"] = "full"
+print_default_frame_half: Literal["full", "half", "plain"] = "half"
+
+# Possible field names of Response code in Response data of method request.
+code_fields: List = ["code", "errno", "success"]
+
+# Successful codes of method request.
+success_codes: List = [200, 204, 0, True]
+success_codes.extend(
+    [
+        str(code)
+        for code in success_codes
+        if code.__class__ == int
+    ]
+)
