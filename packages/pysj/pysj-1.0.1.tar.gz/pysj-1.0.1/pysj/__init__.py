2a46e3f9622be335d88a01ef100fd094094afe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:03 +0000

    Add package pysj/1.0.1/pysj-1.0.1.tar.gz

---
+++ packages/pysj/pysj-1.0.1.tar.gz/pysj-1.0.1/pysj/__init__.py
@@ -0,0 +1,8 @@
+""" Pysj makes Python development more comfortable, with utils, classes and helper
+functions I find myself reimplementing in several projects."""
+__version__ = "1.0.1"
+
+from .crypto import md5, sha1, sha256, uuid
+from .main import (ExtendedJSONDecoder, ExtendedJSONEncoder, Timer, chunk,
+                   first, flatten, isotime, months_in_interval, moving_average,
+                   n_wise, paginate, seconds, take, transpose, triplewise)
