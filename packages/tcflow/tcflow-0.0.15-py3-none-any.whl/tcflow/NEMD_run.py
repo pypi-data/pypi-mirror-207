00d80c63eaf5f235cede0a615a3c005bbd556d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:35 +0000

    Add package tcflow/0.0.15/tcflow-0.0.15-py3-none-any.whl

---
+++ packages/tcflow/tcflow-0.0.15-py3-none-any.whl/tcflow/NEMD_run.py
@@ -0,0 +1,111 @@
+from typing import List
+import sys,os,json
+from dflow import SlurmRemoteExecutor, Step, Workflow, argo_range,upload_artifact,download_artifact
+from dflow.python import OP, OPIO, Artifact, OPIOSign, PythonOPTemplate, Slices
+from pathlib import Path
+import dpdata
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import time
+from monty.serialization import loadfn
+
+import tcflow,matplotlib,sportran
+from tcflow.NEMD_MD_OPs import RunNEMD
+from tcflow.NEMD_reprocess_OPs import MakeSuperCells,analysis
+from tcflow import input_gen
+try:
+    import sportran as st
+except ImportError:
+    from sys import path
+    path.append('..')
+    import sportran as st
+
+from dflow.plugins import bohrium
+from dflow.plugins.bohrium import TiefblueClient
+from dflow import config, s3_config
+from dflow.plugins.dispatcher import DispatcherExecutor
+
+
+
+def NEMD_run_main():
+    # run ../scripts/start-slurm.sh first to start up a slurm cluster
+    machine_param = loadfn("machine.json")
+    email = machine_param.get("email",None)
+    password = machine_param.get("password",None)
+    program_id = machine_param.get("program_id",None)
+    lammps_image = machine_param.get("lammps_image",None)
+    tc_image = machine_param.get("tc_image",None)
+    cpu_scass_type = machine_param.get("cpu_scass_type",None)
+    gpu_scass_type = machine_param.get("gpu_scass_type",None)
+    upload_python_packages=[tcflow.__path__[0],matplotlib.__path__[0],sportran.__path__[0]]
+
+    param = loadfn("parameters.json")
+    input_gen.NVT_input(param)
+    NVT_input = upload_artifact("NVT.lammps")
+    data_input = upload_artifact(param["structure"])
+    if (param["force_field"]):force_field = upload_artifact(param["force_field"])
+    else:force_field = upload_artifact(param["structure"])
+    #gen = upload_artifact("input_gen.py")
+
+    gpu_dispatcher_executor = DispatcherExecutor(
+        machine_dict={
+            "batch_type": "Bohrium",
+            "context_type": "Bohrium",
+            "remote_profile": {
+                "email": email,
+                "password": password,
+                "program_id": program_id,
+                "input_data": {
+                    "job_type": "container",
+                    "platform": "ali",
+                    "scass_type": gpu_scass_type,
+                },
+            },
+        },
+    )
+    cpu_dispatcher_executor = DispatcherExecutor(
+        machine_dict={
+            "batch_type": "Bohrium",
+            "context_type": "Bohrium",
+            "remote_profile": {
+                "email": email,
+                "password": password,
+                "program_id": program_id,
+                "input_data": {
+                    "job_type": "container",
+                    "platform": "ali",
+                    "scass_type": cpu_scass_type,
+                },
+            },
+        },
+    )
+    wf = Workflow("nemd-tc")
+    SuperCell = Step("SuperCells",
+                PythonOPTemplate(MakeSuperCells,image=tc_image),
+                parameters={"param":param},
+                artifacts={"unit_cell":data_input},
+                executor=cpu_dispatcher_executor)
+    wf.add(SuperCell)
+    NEMD = Step("NEMD",
+                 PythonOPTemplate(RunNEMD,image=lammps_image,python_packages=upload_python_packages,
+                                  slices=Slices("{{item}}",
+                                                #sub_path=True,
+                                                input_parameter=["name"],
+                                                input_artifact=["data"],
+                                                output_artifact=["raw"],
+                                                )
+                                  ),
+                 parameters={"name":SuperCell.outputs.parameters["name"],"param":param,},
+                 artifacts={"data":SuperCell.outputs.artifacts["supercells"],"force_field":force_field},#"input_gen":gen,
+                 with_param=argo_range(len(param['supercell'])),
+                 key="nemd-{{item}}",
+                 executor=gpu_dispatcher_executor)
+    wf.add(NEMD)
+    thermal_conductivity = Step("nemd-extrapolation",
+                PythonOPTemplate(analysis,image=tc_image),
+                parameters={"param":param},
+                artifacts={"dat": NEMD.outputs.artifacts["raw"]},
+                executor=cpu_dispatcher_executor)
+    wf.add(thermal_conductivity)
+    wf.submit()
