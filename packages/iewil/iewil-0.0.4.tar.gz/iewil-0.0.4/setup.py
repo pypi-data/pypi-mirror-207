e10ef96f8828aa7e4e659ae3d57ba60d2588a765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:12 +0000

    Add package iewil/0.0.4/iewil-0.0.4.tar.gz

---
+++ packages/iewil/iewil-0.0.4.tar.gz/iewil-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'modul pribadi ygy'
+
+# Setting up
+setup(
+    name="iewil",
+    version=VERSION,
+    author="iewilmaestro",
+    author_email="<purna.iera@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['iewil'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
